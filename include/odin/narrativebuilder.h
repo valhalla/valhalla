fd7da2b576cd3a6efb36f6279fb4a976a41eb662
Duane Gearhart <duane@mapzen.com>
2014-12-04 10:20:34 -0500

    Initial odin checkin

---
+++ include/odin/narrativebuilder.h
@@ -0,0 +1,19 @@
+#ifndef VALHALLA_ODIN_NARRATIVEBUILDER_H_
+#define VALHALLA_ODIN_NARRATIVEBUILDER_H_
+
+#include "proto/trippath.h"
+
+namespace valhalla{
+namespace odin{
+
+class NarrativeBuilder {
+ public:
+  NarrativeBuilder(TripPath& tripPath);
+ protected:
+  TripPath& trip_path_;
+};
+
+}
+}
+
+#endif  // VALHALLA_ODIN_NARRATIVEBUILDER_H_
