3be79f05fa1a6b81dc7d626f4a4739ae9878146c
David Nesbitt <dave@mapzen.com>
2014-12-08 12:36:39 -0500

    Fix GraphId constructor

--- test/edgestatus.cc
+++ test/edgestatus.cc
@@ -20,38 +20,38 @@ void TestStatus() {
   EdgeStatus edgestatus;
 
   // Add some edges
-  edgestatus.Set(GraphId(1, 100100), kPermanent);
-  edgestatus.Set(GraphId(2, 100100), kPermanent);
-  edgestatus.Set(GraphId(3, 100100), kPermanent);
-  edgestatus.Set(GraphId(1, 55555), kTemporary);
-  edgestatus.Set(GraphId(2, 55555), kTemporary);
-  edgestatus.Set(GraphId(3, 55555), kTemporary);
-  edgestatus.Set(GraphId(1, 1), kPermanent);
-  edgestatus.Set(GraphId(2, 1), kPermanent);
-  edgestatus.Set(GraphId(3, 1), kPermanent);
+  edgestatus.Set(GraphId(555, 1, 100100), kPermanent);
+  edgestatus.Set(GraphId(555, 2, 100100), kPermanent);
+  edgestatus.Set(GraphId(555, 3, 100100), kPermanent);
+  edgestatus.Set(GraphId(555, 1, 55555), kTemporary);
+  edgestatus.Set(GraphId(555, 2, 55555), kTemporary);
+  edgestatus.Set(GraphId(555, 3, 55555), kTemporary);
+  edgestatus.Set(GraphId(555, 1, 1), kPermanent);
+  edgestatus.Set(GraphId(555, 2, 1), kPermanent);
+  edgestatus.Set(GraphId(555, 3, 1), kPermanent);
 
   // Test various get
-  TryGet(edgestatus, GraphId(1, 100100), kPermanent);
-  TryGet(edgestatus, GraphId(2, 100100), kPermanent);
-  TryGet(edgestatus, GraphId(3, 100100), kPermanent);
-  TryGet(edgestatus, GraphId(1, 55555), kTemporary);
-  TryGet(edgestatus, GraphId(2, 55555), kTemporary);
-  TryGet(edgestatus, GraphId(3, 55555), kTemporary);
-  TryGet(edgestatus, GraphId(1, 1), kPermanent);
-  TryGet(edgestatus, GraphId(2, 1), kPermanent);
-  TryGet(edgestatus, GraphId(3, 1), kPermanent);
+  TryGet(edgestatus, GraphId(555, 1, 100100), kPermanent);
+  TryGet(edgestatus, GraphId(555, 2, 100100), kPermanent);
+  TryGet(edgestatus, GraphId(555, 3, 100100), kPermanent);
+  TryGet(edgestatus, GraphId(555, 1, 55555), kTemporary);
+  TryGet(edgestatus, GraphId(555, 2, 55555), kTemporary);
+  TryGet(edgestatus, GraphId(555, 3, 55555), kTemporary);
+  TryGet(edgestatus, GraphId(555, 1, 1), kPermanent);
+  TryGet(edgestatus, GraphId(555, 2, 1), kPermanent);
+  TryGet(edgestatus, GraphId(555, 3, 1), kPermanent);
 
   // Clear and make sure all status are kUnreached
   edgestatus.Init();
-  TryGet(edgestatus, GraphId(1, 100100), kUnreached);
-  TryGet(edgestatus, GraphId(2, 100100), kUnreached);
-  TryGet(edgestatus, GraphId(3, 100100), kUnreached);
-  TryGet(edgestatus, GraphId(1, 55555), kUnreached);
-  TryGet(edgestatus, GraphId(2, 55555), kUnreached);
-  TryGet(edgestatus, GraphId(3, 55555), kUnreached);
-  TryGet(edgestatus, GraphId(1, 1), kUnreached);
-  TryGet(edgestatus, GraphId(2, 1), kUnreached);
-  TryGet(edgestatus, GraphId(3, 1), kUnreached);
+  TryGet(edgestatus, GraphId(555, 1, 100100), kUnreached);
+  TryGet(edgestatus, GraphId(555, 2, 100100), kUnreached);
+  TryGet(edgestatus, GraphId(555, 3, 100100), kUnreached);
+  TryGet(edgestatus, GraphId(555, 1, 55555), kUnreached);
+  TryGet(edgestatus, GraphId(555, 2, 55555), kUnreached);
+  TryGet(edgestatus, GraphId(555, 3, 55555), kUnreached);
+  TryGet(edgestatus, GraphId(555, 1, 1), kUnreached);
+  TryGet(edgestatus, GraphId(555, 2, 1), kUnreached);
+  TryGet(edgestatus, GraphId(555, 3, 1), kUnreached);
 }
 
 }
