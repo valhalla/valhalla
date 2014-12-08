c0e63a0491c3c049c357a44b7d7ded1d48864bb9
David Nesbitt <dave@mapzen.com>
2014-12-08 14:56:24 -0500

    Remove data.

--- include/mjolnir/directededgebuilder.h
+++ include/mjolnir/directededgebuilder.h
@@ -37,51 +37,6 @@ class DirectedEdgeBuilder : public baldr::DirectedEdge {
    * @param  speed  Speed in KPH.
   */
   void set_speed(const float speed);
-
- private:
-  // Length of the link in miles
-  float length_;
-
-  // End node
-  GraphId endnode_;
-
-  // Legal access to the directed link ((also include reverse direction access).
-  // TODO - come up with final set of values!
-  struct Access {
-    unsigned char car_          : 1;
-    unsigned char taxi_         : 1;
-    unsigned char truck_        : 1;
-    unsigned char pedestrian_   : 1;
-    unsigned char bicycle_      : 1;
-    unsigned char emergency_    : 1;
-    unsigned char horse_        : 1;  // ???
-    unsigned char spare_        : 1;
-  };
-  Access forwardaccess_;
-  Access reverseaccess_;
-
-  // Attributes. Can be used in edge costing methods to favor or avoid edges.
-  struct Attributes {
-    unsigned int ferry_          : 1;
-    unsigned int railferry_      : 1;  // ???
-    unsigned int toll_           : 1;
-    unsigned int private_        : 1;
-    unsigned int unpaved_        : 1;
-    unsigned int tunnel_         : 1;
-    unsigned int spare           : 30;
-  };
-  Attributes attributes_;
-
-  // TODO - some representation of road class / highway tag importance
-
-  // Speed in kilometers. Range 0-250 KPH. Applies to "normal vechicles".
-  // Save values above 250 as special cases (closures, construction,
-  // etc.)
-  // TODO - do we need or does OSM support "truck speed"
-  unsigned char speed_;
-
-  // TODO - byte alignment / sizing
-
 };
 
 #endif  // VALHALLA_MJOLNIR_DIRECTEDEDGEBUILDER_H_
