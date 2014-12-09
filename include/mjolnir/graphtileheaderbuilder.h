0a724a52d122e06073fa48d9d4c0de88435731a9
Duane Gearhart <duane@mapzen.com>
2014-12-09 09:17:26 -0500

    Updated args to be const

--- include/mjolnir/graphtileheaderbuilder.h
+++ include/mjolnir/graphtileheaderbuilder.h
@@ -13,16 +13,16 @@ class GraphTileHeaderBuilder : public baldr::GraphTileHeader {
   GraphTileHeaderBuilder();
 
   // Sets the number of nodes in this tile.
-  void set_nodecount(unsigned int nodecount);
+  void set_nodecount(const unsigned int nodecount);
 
   // Sets the number of directed edges in this tile.
-  void set_directededgecount(unsigned int directededgecount);
+  void set_directededgecount(const unsigned int directededgecount);
 
   // Sets the offset to the edge info.
-  void set_edgeinfo_offset(unsigned int edgeinfo_offset);
+  void set_edgeinfo_offset(const unsigned int edgeinfo_offset);
 
   // Sets the offset to the name list.
-  void set_namelist_offset(unsigned int namelist_offset);
+  void set_namelist_offset(const unsigned int namelist_offset);
 
 };
 
--- src/mjolnir/graphtileheaderbuilder.cc
+++ src/mjolnir/graphtileheaderbuilder.cc
@@ -9,23 +9,25 @@ GraphTileHeaderBuilder::GraphTileHeaderBuilder()
 }
 
 // Sets the number of nodes in this tile.
-void GraphTileHeaderBuilder::set_nodecount(unsigned int nodecount) {
+void GraphTileHeaderBuilder::set_nodecount(const unsigned int nodecount) {
   nodecount_ = nodecount;
 }
 
 // Sets the number of directed edges in this tile.
 void GraphTileHeaderBuilder::set_directededgecount(
-    unsigned int directededgecount) {
+    const unsigned int directededgecount) {
   directededgecount_ = directededgecount;
 }
 
 // Sets the offset to the edge info.
-void GraphTileHeaderBuilder::set_edgeinfo_offset(unsigned int edgeinfo_offset) {
+void GraphTileHeaderBuilder::set_edgeinfo_offset(
+    const unsigned int edgeinfo_offset) {
   edgeinfo_offset_ = edgeinfo_offset;
 }
 
 // Sets the offset to the name list.
-void GraphTileHeaderBuilder::set_namelist_offset(unsigned int namelist_offset) {
+void GraphTileHeaderBuilder::set_namelist_offset(
+    const unsigned int namelist_offset) {
   namelist_offset_ = namelist_offset;
 }
 
