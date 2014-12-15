a3402bf9343065fc141b4f0f4e8ae86b7096da41
David Nesbitt <dave@mapzen.com>
2014-12-15 08:21:10 -0500

    Outline of the A* algorithm. Still some missing pieces and cleanup needed.

--- include/thor/pathalgorithm.h
+++ include/thor/pathalgorithm.h
@@ -1,6 +1,7 @@
 #ifndef VALHALLA_THOR_PATHALGORITHM_H_
 #define VALHALLA_THOR_PATHALGORITHM_H_
 
+#include "baldr/graphreader.h"
 #include "thor/adjacencylist.h"
 #include "thor/astarheuristic.h"
 #include "thor/edgecost.h"
@@ -25,23 +26,45 @@ class PathAlgorithm {
    */
   virtual ~PathAlgorithm();
 
+  /**
+   * Form path.
+   * TODO - define inputs and outputs!
+   */
+  bool GetBestPath(baldr::GraphReader& graphreader, EdgeCost* edgecost);
+
  protected:
 
   // A* heuristic
   AStarHeuristic astarheuristic_;
 
   // Adjacency list
-  AdjacencyList<EdgeLabel>* adjacencylist_;
+  AdjacencyList* adjacencylist_;
 
   // Done set
   std::vector<EdgeLabel*> doneset_;
 
-  // Edge costing
-  EdgeCost* edgecost_;
-
   // Edge status
   EdgeStatus* edgestatus_;
 
+  /**
+   * Initialize
+   */
+  void Init(const PointLL& destll, EdgeCost* edgecost);
+
+  /**
+   * Add an edge at the origin to the adjacency list
+   */
+  void AddOriginEdge(const baldr::DirectedEdge& edge, EdgeCost* edgecost);
+
+  /**
+   * Clear the temporary information generated during path construction.
+   */
+  void Clear();
+
+  /**
+   * Test if the shortest path is found.
+   */
+  bool IsComplete();
 };
 
 }
