71726eed6640de8930f522068e7dad084f6b7e56
gknisely <greg@mapzen.com>
2014-12-17 15:55:59 -0500

    fixed merge issue.

--- src/mjolnir/pbfgraphbuilder/pbfgraphbuilder.h
+++ src/mjolnir/pbfgraphbuilder/pbfgraphbuilder.h
@@ -68,10 +68,68 @@ struct OSMWay {
   uint64_t osmwayid_;
   std::vector<uint64_t> nodelist_;
 
-  // TODO - add essential routing parameters
+  unsigned short road_class_;
+
+  bool auto_forward_;
+  bool bike_forward_;
+  bool auto_backward_;
+  bool bike_backward_;
+  bool pedestrian_;
+
+  bool oneway_;
+  bool roundabout_;
+  bool ferry_;
+  bool rail_;
+
+  std::string name_;
+  std::string name_en_;
+
+  unsigned short maxspeed_;
+  unsigned short minspeed_;
+
+  std::string ref_;
+  std::string int_ref_;
+
+  bool  surface_; //TODO:  Expand out?
+  unsigned short lanes_;
+  unsigned char  tunnel_;
+  unsigned char  toll_;
+  unsigned short bike_network_mask_;
+
+  std::string bike_national_ref_;
+  std::string  bike_regional_ref_;
+  std::string  bike_local_ref_;
 
   OSMWay(uint64_t id) {
     osmwayid_ = id;
+
+    road_class_ = 0;
+
+    auto_forward_ = false;
+    bike_forward_ = false;
+    auto_backward_ = false;
+    bike_backward_ = false;
+    pedestrian_ = false;
+
+    oneway_ = false;
+    roundabout_ = false;
+    ferry_ = false;
+    rail_ = false;
+    name_ = "";
+    name_en_ = "";
+    maxspeed_ = 0;
+    minspeed_ = 0;
+    ref_ = "";
+    int_ref_ = "";
+    surface_ = false;
+    lanes_ = 0;
+    tunnel_  = false;
+    toll_  = false;
+
+    bike_network_mask_ = 0;
+    bike_national_ref_ = "";
+    bike_regional_ref_ = "";
+    bike_local_ref_ = "";
   }
 };
 
