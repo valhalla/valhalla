4fd7116efb69faef54a0ca2bab1e8dfebaf07d5f
gknisely <greg@mapzen.com>
2014-12-17 15:41:14 -0500

    clean up.

--- src/mjolnir/pbfgraphbuilder/luatagtransform.cc
+++ src/mjolnir/pbfgraphbuilder/luatagtransform.cc
@@ -87,31 +87,31 @@ std::string LuaTagTransform::GetLuaNodeScript() const {
 namespace{
 void stackdump_g(lua_State* l)
 {
-    int i;
-    int top = lua_gettop(l);
+  int i;
+  int top = lua_gettop(l);
 
-    printf("total in stack %d\n",top);
+  printf("total in stack %d\n",top);
 
-    for (i = 1; i <= top; i++)
-    {  /* repeat for each level */
-        int t = lua_type(l, i);
-        switch (t) {
-            case LUA_TSTRING:  /* strings */
-                printf("string: '%s'\n", lua_tostring(l, i));
-                break;
-            case LUA_TBOOLEAN:  /* booleans */
-                printf("boolean %s\n",lua_toboolean(l, i) ? "true" : "false");
-                break;
-            case LUA_TNUMBER:  /* numbers */
-                printf("number: %g\n", lua_tonumber(l, i));
-                break;
-            default:  /* other values */
-                printf("%s\n", lua_typename(l, t));
-                break;
-        }
-        printf("  ");  /* put a separator */
+  for (i = 1; i <= top; i++)
+  {  /* repeat for each level */
+    int t = lua_type(l, i);
+    switch (t) {
+      case LUA_TSTRING:  /* strings */
+        printf("string: '%s'\n", lua_tostring(l, i));
+        break;
+      case LUA_TBOOLEAN:  /* booleans */
+        printf("boolean %s\n",lua_toboolean(l, i) ? "true" : "false");
+        break;
+      case LUA_TNUMBER:  /* numbers */
+        printf("number: %g\n", lua_tonumber(l, i));
+        break;
+      default:  /* other values */
+        printf("%s\n", lua_typename(l, t));
+        break;
     }
-    printf("\n");  /* end the listing */
+    printf("  ");  /* put a separator */
+  }
+  printf("\n");  /* end the listing */
 }
 }
 
