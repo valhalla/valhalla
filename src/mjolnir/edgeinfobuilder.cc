49395cdb1f94205d9e972ed59155f149fefced35
Duane Gearhart <duane@mapzen.com>
2015-01-04 23:22:34 -0500

    Debug updates

--- src/mjolnir/edgeinfobuilder.cc
+++ src/mjolnir/edgeinfobuilder.cc
@@ -62,14 +62,16 @@ std::size_t EdgeInfoBuilder::SizeOf() const {
 
 void EdgeInfoBuilder::SerializeToOstream(std::ostream& out) const {
   // TODO - rm later
-  /*std::cout << "------------------------------------------------------"
+  std::cout << "------------------------------------------------------"
             << std::endl;
   std::cout << __FILE__ << ":" << __LINE__ << std::endl;
+  std::cout << "EdgeInfoBuilder::SizeOf=" << SizeOf() << std::endl;
   std::cout << "nodea=" << nodea_.value() << "  nodea_.tileid="
             << nodea_.tileid() << "  nodea_.level=" << nodea_.level()
             << "  nodea_.id=" << nodea_.id() << std::endl;
-  std::cout << "nodeb=" << nodeb_.value() << "  nodeb_.tileid=" << nodeb_.tileid() << "  nodeb_.level="
-            << nodeb_.level() << "  nodeb_.id=" << nodeb_.id() << std::endl;
+  std::cout << "nodeb=" << nodeb_.value() << "  nodeb_.tileid="
+            << nodeb_.tileid() << "  nodeb_.level=" << nodeb_.level()
+            << "  nodeb_.id=" << nodeb_.id() << std::endl;
   std::cout << "item_=" << item_.value << std::endl;
   std::cout << "street_name_offset_list_offset="
             << street_name_offset_list_offset() << "  name_count="
@@ -83,7 +85,7 @@ void EdgeInfoBuilder::SerializeToOstream(std::ostream& out) const {
   }
   std::cout << "exit_sign_count=" << exit_signs_.size() << std::endl;
   std::cout << "======================================================="
-            << std::endl;*/
+            << std::endl;
 
   out.write(reinterpret_cast<const char*>(&nodea_), sizeof(GraphId));
   out.write(reinterpret_cast<const char*>(&nodeb_), sizeof(GraphId));
