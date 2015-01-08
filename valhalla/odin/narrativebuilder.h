e5d9fe9ca63a312ea82d439380e6ac42c33dde2f
Duane Gearhart <duane@mapzen.com>
2015-01-08 13:43:52 -0500

    Added simple narrative logic for end to end test

--- valhalla/odin/narrativebuilder.h
+++ valhalla/odin/narrativebuilder.h
@@ -1,6 +1,8 @@
 #ifndef VALHALLA_ODIN_NARRATIVEBUILDER_H_
 #define VALHALLA_ODIN_NARRATIVEBUILDER_H_
 
+#include <vector>
+
 #include <valhalla/proto/trippath.pb.h>
 
 namespace valhalla{
@@ -16,6 +18,9 @@ class NarrativeBuilder {
   TripPath& trip_path_;
 
   // TODO - temps for initial end to end test
+  std::vector<std::vector<std::string>> maneuver_names_;
+  std::vector<float> maneuver_distance_;
+
 
 };
 
