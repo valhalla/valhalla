9a6fc96c48335d2810b84f27a4935634170cee46
Duane Gearhart <duane@mapzen.com>
2015-01-08 11:43:41 -0500

    Updates because leg was removed from pb Also, passing TripPath along TODO - set path location

--- src/thor/trippathbuilder.cc
+++ src/thor/trippathbuilder.cc
@@ -20,17 +20,13 @@ TripPathBuilder::~TripPathBuilder() {
 }
 
 // For now just find the length of the path!
-// TODO - alter the return to be trip path structure.
 // TODO - probably need the location information passed in - to
 // add to the TripPath
-TripPath TripPathBuilder::Build(GraphReader& graphreader,
-                             const std::vector<GraphId>& pathedges) {
+void TripPathBuilder::Build(
+    GraphReader& graphreader, const std::vector<GraphId>& pathedges,
+    TripPath& trip_path) {
   // TripPath is a protocol buffer that contains information about the
   // trip
-  TripPath trip_path;
-
-  // TODO - support multiple legs
-  TripPath_Leg* trip_leg = trip_path.add_leg();
 
   std::vector<std::string> names;
   for (const auto& edge : pathedges) {
@@ -38,7 +34,7 @@ TripPath TripPathBuilder::Build(GraphReader& graphreader,
     const DirectedEdge* directededge = graphtile->directededge(edge);
 
     // Add a node. TODO - where do we get its attributes?
-    TripPath_Node* trip_node = trip_leg->add_node();
+    TripPath_Node* trip_node = trip_path.add_node();
 
     // Add edge to the trip node and set its attributes
     TripPath_Edge* trip_edge = trip_node->add_edge();
@@ -51,28 +47,38 @@ TripPath TripPathBuilder::Build(GraphReader& graphreader,
     trip_edge->set_length(directededge->length());
 
     trip_edge->set_speed(directededge->speed());
-    
-    if (directededge->forward()) { //Edge is in the forward direction.
+
+    if (directededge->forward()) {  //Edge is in the forward direction.
 
       if (directededge->forwardaccess() && directededge->reverseaccess())
-        trip_edge->set_driveability(TripPath_Driveability::TripPath_Driveability_kBoth);
+        trip_edge->set_driveability(
+            TripPath_Driveability::TripPath_Driveability_kBoth);
       else if (directededge->forwardaccess() && !directededge->reverseaccess())
-        trip_edge->set_driveability(TripPath_Driveability::TripPath_Driveability_kForward);
+        trip_edge->set_driveability(
+            TripPath_Driveability::TripPath_Driveability_kForward);
       else if (!directededge->forwardaccess() && directededge->reverseaccess())
-        trip_edge->set_driveability(TripPath_Driveability::TripPath_Driveability_kBackward);
-      else trip_edge->set_driveability(TripPath_Driveability::TripPath_Driveability_kNone);
+        trip_edge->set_driveability(
+            TripPath_Driveability::TripPath_Driveability_kBackward);
+      else
+        trip_edge->set_driveability(
+            TripPath_Driveability::TripPath_Driveability_kNone);
 
       //TODO - Add shape.
 
-    } else { //Edge is in the reverse direction.  We must flip everything.
+    } else {  //Edge is in the reverse direction.  We must flip everything.
 
       if (directededge->forwardaccess() && directededge->reverseaccess())
-        trip_edge->set_driveability(TripPath_Driveability::TripPath_Driveability_kBoth);
+        trip_edge->set_driveability(
+            TripPath_Driveability::TripPath_Driveability_kBoth);
       else if (!directededge->forwardaccess() && directededge->reverseaccess())
-        trip_edge->set_driveability(TripPath_Driveability::TripPath_Driveability_kForward);
+        trip_edge->set_driveability(
+            TripPath_Driveability::TripPath_Driveability_kForward);
       else if (directededge->forwardaccess() && !directededge->reverseaccess())
-        trip_edge->set_driveability(TripPath_Driveability::TripPath_Driveability_kBackward);
-      else trip_edge->set_driveability(TripPath_Driveability::TripPath_Driveability_kNone);
+        trip_edge->set_driveability(
+            TripPath_Driveability::TripPath_Driveability_kBackward);
+      else
+        trip_edge->set_driveability(
+            TripPath_Driveability::TripPath_Driveability_kNone);
 
       //TODO - Add shape in the reverse direction.
 
@@ -82,24 +88,22 @@ TripPath TripPathBuilder::Build(GraphReader& graphreader,
 
     trip_edge->set_toll(directededge->toll());
 
-    
     // TODO - add other connected edges to the node...
 
-/**
-    // TODO - rm later
-    std::cout << "-------------------------------------------------------"
-              << std::endl;
-    names = graphtile->GetNames(directededge->edgedataoffset(), names);
-    for (auto& name : names) {
-      std::cout << "   name=" << name << std::endl;
-    }
-    const std::shared_ptr<EdgeInfo> edgeinfo = graphtile->edgeinfo(
-        directededge->edgedataoffset());
-    // TODO - rm later
-    //edgeinfo->ToOstream();
-*/
+    /**
+     // TODO - rm later
+     std::cout << "-------------------------------------------------------"
+     << std::endl;
+     names = graphtile->GetNames(directededge->edgedataoffset(), names);
+     for (auto& name : names) {
+     std::cout << "   name=" << name << std::endl;
+     }
+     const std::shared_ptr<EdgeInfo> edgeinfo = graphtile->edgeinfo(
+     directededge->edgedataoffset());
+     // TODO - rm later
+     //edgeinfo->ToOstream();
+     */
   }
-  return trip_path;
 }
 
 }
