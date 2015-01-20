9abfb547d121fd6da1b015ac409b74c53cb32536
Kevin Kreiser <kevinkreiser@gmail.com>
2015-01-20 13:19:43 -0500

    a couple of updates for efficiencies sake

--- valhalla/midgard/logging.h
+++ valhalla/midgard/logging.h
@@ -12,35 +12,10 @@
 
 namespace valhalla {
 namespace midgard {
+
 namespace logging {
 
-//a factory that can create Loggers (that derive from 'Logger') via function pointers
-//this way you could make your own Logger that sends log messages to who knows where
-class Logger;
-using LoggerCreator = Logger *(*)(const std::string &);
-class LoggerFactory : 
-    public std::unordered_map<std::string, LoggerCreator> {
- public:
-  Logger* produce(const std::string& config) const {
-    auto found = find(config.substr(0, config.find_first_of(',')));
-    if(found != end()) {
-      return found->second(config);
-    }
-    throw std::runtime_error("Couldn't produce Logger from: " + config);
-  }
-};
-
-static LoggerFactory& GetFactory() {
-  static LoggerFactory factory_singleton{};
-  return factory_singleton;
-}
-
-//register your custom Loggers here
-static bool RegisterLogger(const std::string& name, LoggerCreator function_ptr) {
-  auto success = GetFactory().emplace(name, function_ptr);
-  return success.second;
-}
-
+//turn a string into a T
 template <class T>
 T atoT(const std::string& input)
 {
@@ -49,7 +24,7 @@ T atoT(const std::string& input)
   stream >> std::ws >> output >> std::ws;
   if(!stream.eof())
     throw std::runtime_error("Couldn't convert string to integral type");
-  return output; 
+  return output;
 }
 
 //returns formated to: 'year/mo/dy hr:mn:sc.xxxxxx'
@@ -57,9 +32,9 @@ std::string TimeStamp() {
   //get the time
   std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
   std::time_t tt = std::chrono::system_clock::to_time_t(tp);
-  std::tm gmt = {0}; gmtime_r(&tt, &gmt);
+  std::tm gmt{}; gmtime_r(&tt, &gmt);
   using sec_t = std::chrono::duration<double>;
-  std::chrono::duration<double> fractional_seconds = 
+  std::chrono::duration<double> fractional_seconds =
     (tp - std::chrono::system_clock::from_time_t(tt)) + std::chrono::seconds(gmt.tm_sec);
   //format the string
   std::string buffer("year/mo/dy hr:mn:sc.xxxxxx");
@@ -68,55 +43,73 @@ std::string TimeStamp() {
   return buffer;
 }
 
-//the log levels we support
-enum class LogLevel : char { TRACE, DEBUG, INFO, WARN, ERROR };
-struct EnumHasher { template <typename T> std::size_t operator()(T t) const { return static_cast<std::size_t>(t); } };
-
-//returns standard message format: 'timestamp [LOG LEVEL]: message'
-std::string StandardMessage(const std::string& message, const LogLevel level) {
-  std::string output;
-  output.reserve(message.length() + 64);
-  output.append(TimeStamp());
-  switch(level) {
-    case LogLevel::ERROR:
-      output.append(" [ERROR]: ");
-      break;
-    case LogLevel::WARN:
-      output.append(" [WARN]: ");
-      break;
-    case LogLevel::INFO:
-      output.append(" [INFO]: ");
-      break;
-    case LogLevel::DEBUG:
-      output.append(" [DEBUG]: ");
-      break;
-    case LogLevel::TRACE:
-      output.append(" [TRACE]: ");
-      break;
+//a factory that can create loggers (that derive from 'logger') via function pointers
+//this way you could make your own logger that sends log messages to who knows where
+class Logger;
+using LoggingConfig = std::unordered_map<std::string, std::string>;
+using LoggerCreator = Logger *(*)(const LoggingConfig&);
+class LoggerFactory :
+    public std::unordered_map<std::string, LoggerCreator> {
+ public:
+  Logger* Produce(const LoggingConfig& config) const {
+    //grab the type
+    auto type = config.find("type");
+    if(type == config.end())
+      throw std::runtime_error("Logging factory configuration requires a type of logger");
+    //grab the logger
+    auto found = find(type->second);
+    if(found != end())
+      return found->second(config);
+    //couldn't get a logger
+    throw std::runtime_error("Couldn't produce logger for type: " + type->second);
   }
-  output.append(message);
-  output.push_back('\n');
-  return output;
+};
+
+//statically get a factory
+static LoggerFactory& GetFactory() {
+  static LoggerFactory factory_singleton{};
+  return factory_singleton;
 }
 
-//Logger base class, not pure virtual so you can use it has a null Logger if you want
+//register your custom loggers here
+static bool RegisterLogger(const std::string& name, LoggerCreator function_ptr) {
+  auto success = GetFactory().emplace(name, function_ptr);
+  return success.second;
+}
+
+//the Log levels we support
+enum class LogLevel : char { TRACE, DEBUG, INFO, WARN, ERROR };
+struct EnumHasher { template <typename T> std::size_t operator()(T t) const { return static_cast<std::size_t>(t); } };
+const std::unordered_map<LogLevel, std::string, EnumHasher> uncolored
+  {
+    {LogLevel::ERROR, " [ERROR] "}, {LogLevel::WARN, " [WARN] "}, {LogLevel::INFO, " [INFO] "},
+    {LogLevel::DEBUG, " [DEBUG] "}, {LogLevel::TRACE, " [TRACE] "}
+  };
+const std::unordered_map<LogLevel, std::string, EnumHasher> colored
+  {
+    {LogLevel::ERROR, " \x1b[31;1m[ERROR]\x1b[0m "}, {LogLevel::WARN, " \x1b[33;1m[WARN]\x1b[0m "},
+    {LogLevel::INFO, " \x1b[32;1m[INFO]\x1b[0m "}, {LogLevel::DEBUG, " \x1b[34;1m[DEBUG]\x1b[0m "},
+    {LogLevel::TRACE, " \x1b[37;1m[TRACE]\x1b[0m "}
+  };
+
+//logger base class, not pure virtual so you can use as a null logger if you want
 class Logger {
  public:
   Logger() = delete;
-  Logger(const std::string& config) {};
+  Logger(const LoggingConfig& config) {};
   virtual ~Logger() {};
   virtual void Log(const std::string&, const LogLevel) {};
  protected:
   std::mutex lock;
 };
 static bool logger_registered = 
-  RegisterLogger("", [](const std::string& config){Logger* l = new Logger(config); return l;});
+  RegisterLogger("", [](const LoggingConfig& config){Logger* l = new Logger(config); return l;});
 
-//Logger that writes to standard out
+//logger that writes to standard out
 class StdOutLogger : public Logger {
  public:
   StdOutLogger() = delete;
-  StdOutLogger(const std::string& config):Logger(config),levels(GetLogDirectives(config)) {}
+  StdOutLogger(const LoggingConfig& config) : Logger(config),levels(config.find("color") != config.end() ? colored : uncolored) {}
   virtual void Log(const std::string& message, const LogLevel level) {
     std::string output;
     output.reserve(message.length() + 64);
@@ -127,65 +120,56 @@ class StdOutLogger : public Logger {
     //cout is thread safe, to avoid multiple threads interleaving on one line
     //though, we make sure to only call the << operator once on std::cout
     //otherwise the << operators from different threads could interleave
+    //obviously we dont care if flushes interleave
     std::cout << output;
+    std::cout.flush();
   }
  protected:
-  static std::unordered_map<LogLevel, std::string, EnumHasher> GetLogDirectives(const std::string& config) {
-    //if it not colored don't use color
-    auto pos = config.find_first_of(',');
-    if(pos == std::string::npos || pos == config.length() - 1 || config.substr(pos + 1) != "color")
-      return {{LogLevel::ERROR, " [ERROR] "}, {LogLevel::WARN, " [WARN] "}, {LogLevel::INFO, " [INFO] "}, 
-        {LogLevel::DEBUG, " [DEBUG] "}, {LogLevel::TRACE, " [TRACE] "} };
-    //use color
-    return {{LogLevel::ERROR, " \x1b[31;1m[ERROR]\x1b[0m "}, {LogLevel::WARN, " \x1b[33;1m[WARN]\x1b[0m "}, 
-      {LogLevel::INFO, " \x1b[32;1m[INFO]\x1b[0m "}, {LogLevel::DEBUG, " \x1b[34;1m[DEBUG]\x1b[0m "}, 
-      {LogLevel::TRACE, " \x1b[37;1m[TRACE]\x1b[0m "} };
-  }
   const std::unordered_map<LogLevel, std::string, EnumHasher> levels;
 };
 static bool std_out_logger_registered = 
-  RegisterLogger("std_out", [](const std::string& config){Logger* l = new StdOutLogger(config); return l;});
+  RegisterLogger("std_out", [](const LoggingConfig& config){Logger* l = new StdOutLogger(config); return l;});
 
-//TODO: add reopen interval
 //TODO: add log rolling
-//Logger that writes to file
+//logger that writes to file
 class FileLogger : public Logger {
  public:
   FileLogger() = delete;
-  FileLogger(const std::string& config):Logger(config) {
+  FileLogger(const LoggingConfig& config):Logger(config) {
     //grab the file name
-    auto pos = config.find_first_of(',');
-    if(pos == std::string::npos || pos == config.length() - 1)
-      throw std::runtime_error("No output file provided to file Logger");
-    file_name = config.substr(++pos);
+    auto name = config.find("file_name");
+    if(name == config.end())
+      throw std::runtime_error("No output file provided to file logger");
+    file_name = name->second;
     
     //if we specify an interval
     reopen_interval = std::chrono::seconds(300);
-    pos = file_name.find_first_of(',', pos);
-    if(pos != std::string::npos && pos != config.length() - 1)
+    auto interval = config.find("reopen_interval");
+    if(interval != config.end())
     {
-      auto interval = file_name.substr(pos + 1);
-      file_name.resize(pos);
       try {        
-        reopen_interval = std::chrono::seconds(atoT<int>(interval));
+        reopen_interval = std::chrono::seconds(atoT<int>(interval->second));
       }
       catch(...) {
-        throw std::runtime_error(interval + " is not a valid reopen interval");
+        throw std::runtime_error(interval->second + " is not a valid reopen interval");
       }
     }
     
     //crack the file open
     ReOpen();
   }
-  virtual ~FileLogger() {
-    try{ file.close(); }catch(...){}
-  }
   virtual void Log(const std::string& message, const LogLevel level) {
-    std::string output(StandardMessage(message, level));
+    std::string output;
+    output.reserve(message.length() + 64);
+    output.append(TimeStamp());
+    output.append(uncolored.find(level)->second);
+    output.append(message);
+    output.push_back('\n');
     lock.lock();
     file << output;
+    file.flush();
     lock.unlock();
-    ReOpen();    
+    ReOpen();
   }
  protected:
   void ReOpen() {
@@ -213,14 +197,16 @@ class FileLogger : public Logger {
   std::chrono::system_clock::time_point last_reopen;
 };
 static bool file_logger_registered = 
-  RegisterLogger("file", [](const std::string& config){Logger* l = new FileLogger(config); return l;});
+  RegisterLogger("file", [](const LoggingConfig& config){Logger* l = new FileLogger(config); return l;});
 
-static Logger& GetLogger(const std::string& config = "std_out,color") {
-  static std::unique_ptr<Logger> singleton(GetFactory().produce(config));
+//statically get a logger using the factory
+static Logger& GetLogger(const LoggingConfig& config = { {"type", "std_out"}, {"color", ""} }) {
+  static std::unique_ptr<Logger> singleton(GetFactory().Produce(config));
   return *singleton;
 }
 
-static void Configure(const std::string& config) {
+//statically configure logging
+static void Configure(const LoggingConfig& config) {
   GetLogger(config);
 }
 
@@ -237,48 +223,6 @@ static void Configure(const std::string& config) {
 #define LOG_ERROR(x) ::valhalla::midgard::logging::GetLogger().Log(x, ::valhalla::midgard::logging::LogLevel::ERROR);
 
 }
-}
-}
 
-#include <thread>
-#include <future>
-#include <vector>
-#include <functional>
-
-size_t work() {
-  std::ostringstream s; s << "hi my name is: " << std::this_thread::get_id();
-  
-  for(size_t i  = 0; i < 2; ++i) {
-    //std::async is pretty uninteresting unless you make things yield
-    LOG_ERROR(s.str()); std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    LOG_WARN(s.str()); std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    LOG_INFO(s.str()); std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    LOG_DEBUG(s.str()); std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    LOG_TRACE(s.str()); std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  }
-  return 10;
 }
-
-int main(void) {
-  //configure logging, if you dont it defaults to standard out logging with colors
-  //valhalla::midgard::logging::Configure("file,test.log,1");
-  
-  //start up some threads
-  std::vector<std::future<size_t> > results;
-  for(size_t i = 0; i < 4; ++i) {
-    results.emplace_back(std::async(std::launch::async, work));
-  }
-  
-  //dont really care about the results but we can pretend
-  bool exit_code = 0;
-  for(auto& result : results) {
-    try {
-      size_t count = result.get();
-    }
-    catch(std::exception& e) {
-      std::cout << e.what();
-      exit_code++;
-    }
-  }
-  return exit_code;
 }
