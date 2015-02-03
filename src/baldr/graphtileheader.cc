df6a884475f502ad2c16d145e8449d61175857e2
David Nesbitt <dave@mapzen.com>
2015-02-03 10:47:44 -0500

    Update to use fixed size types.

--- src/baldr/graphtileheader.cc
+++ src/baldr/graphtileheader.cc
@@ -1,53 +1,81 @@
+#include <string.h>
 #include "baldr/graphtileheader.h"
 #include "baldr/nodeinfo.h"
 #include "baldr/directededge.h"
 #include "baldr/edgeinfo.h"
 #include "config.h"
 
-namespace valhalla{
-namespace baldr{
+namespace valhalla {
+namespace baldr {
 
+// Default constructor. This is written with GraphTileHeaderBuilder and
+// read within GraphTile. Set the version and internal version here so a
+// "default" GraphTileHeader can be used to compare to what is read from
+// a tile.
 GraphTileHeader::GraphTileHeader()
-    : nodecount_(0),
+    : date_created_{},
+      nodecount_(0),
       directededgecount_(0),
       edgeinfo_offset_(0),
       textlist_offset_(0),
-      internal_version_(0) {
-
-  internal_version_ = NodeInfo::internal_version() + DirectedEdge::internal_version() + GraphId::internal_version();
-
-  date_created_ = std::time(nullptr);
-
-  version_ = PACKAGE_VERSION;
-
+      exitlist_offset_(0),
+      admin_offset_(0),
+      merlist_offset_(0) {
+  internal_version_ = NodeInfo::internal_version() +
+                      DirectedEdge::internal_version() +
+                      GraphId::internal_version();
+  strncpy(version_, PACKAGE_VERSION, kMaxVersionSize);
+  version_[kMaxVersionSize-1] = 0;
 }
 
+// Get the internal version
 int64_t GraphTileHeader::internal_version() const {
   return internal_version_;
 }
 
-std::time_t GraphTileHeader::date_created() const {
+// Get the date created
+uint64_t GraphTileHeader::date_created() const {
   return date_created_;
 }
 
+// Get the version string.
 std::string GraphTileHeader::version() const {
   return version_;
 }
 
-size_t GraphTileHeader::nodecount() const {
+// Get the count of nodes in the tile.
+uint32_t GraphTileHeader::nodecount() const {
   return nodecount_;
 }
 
-size_t GraphTileHeader::directededgecount() const {
+// Get the count of directed edges in the tile.
+uint32_t GraphTileHeader::directededgecount() const {
   return directededgecount_;
 }
 
-size_t GraphTileHeader::edgeinfo_offset() const {
+// Get the offset in bytes to the start of the edge information.
+uint32_t GraphTileHeader::edgeinfo_offset() const {
   return edgeinfo_offset_;
 }
 
-size_t GraphTileHeader::textlist_offset() const {
+// Get the offset in bytes to the start of the text / names list.
+uint32_t GraphTileHeader::textlist_offset() const {
   return textlist_offset_;
+}
+
+// Gets the offset in bytes to the exit list.
+uint32_t GraphTileHeader::exitlist_offset() const {
+  return exitlist_offset_;
+}
+
+// Get the offset in bytes to the administrative information. (TODO)
+uint32_t GraphTileHeader::admin_offset() const {
+  return admin_offset_;
+}
+
+// Get the offset in bytes to the Multi-Edge Restriction list. (TODO)
+uint32_t GraphTileHeader::merlist_offset() const {
+  return merlist_offset_;
 }
 
 }
