b1d42f4df889af9c7c498eda7d9b53d19005aa60
Kevin Kreiser <kevinkreiser@gmail.com>
2015-02-09 15:59:48 -0500

    avoid division in decoding, probably doesnt matter much

--- src/midgard/util.cc
+++ src/midgard/util.cc
@@ -12,6 +12,7 @@
 
 namespace {
 constexpr double POLYLINE_PRECISION = 1E6;
+constexpr double INV_POLYLINE_PRECISION = 1.0 / POLYLINE_PRECISION;
 }
 
 namespace valhalla {
@@ -119,8 +120,8 @@ container_t decode(const std::string& encoded) {
     int lat = deserialize(last_lat);
     int lon = deserialize(last_lon);
     //shift the decimal point 5 places to the left
-    output.emplace_back(static_cast<typename container_t::value_type::first_type>(static_cast<double>(lon) / POLYLINE_PRECISION),
-                        static_cast<typename container_t::value_type::second_type>(static_cast<double>(lat) / POLYLINE_PRECISION));
+    output.emplace_back(static_cast<typename container_t::value_type::first_type>(static_cast<double>(lon) * INV_POLYLINE_PRECISION),
+                        static_cast<typename container_t::value_type::second_type>(static_cast<double>(lat) * INV_POLYLINE_PRECISION));
     //remember the last one we encountered
     last_lon = lon;
     last_lat = lat;
