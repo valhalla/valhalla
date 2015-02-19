9b80a5de63f8f600393638dafbd2ca9719933551
Duane Gearhart <duane@mapzen.com>
2015-02-18 22:48:38 -0500

    Added HasSameBaseName capability and tests

--- src/odin/streetname.cc
+++ src/odin/streetname.cc
@@ -2,8 +2,16 @@
 
 #include "odin/streetname.h"
 
+#include <boost/algorithm/string/predicate.hpp>
+
 namespace valhalla {
 namespace odin {
+
+// TODO - locale
+const std::vector<std::string> StreetName::pre_dirs_ { "North ", "East ",
+    "South ", "West ", "Northeast ", "Southeast ", "Southwest ", "Northwest " };
+const std::vector<std::string> StreetName::post_dirs_ { " North", " East",
+    " South", " West", " Northeast", " Southeast", " Southwest", " Northwest" };
 
 StreetName::StreetName(const std::string& value)
     : value_(value) {
@@ -15,6 +23,42 @@ const std::string& StreetName::value() const {
 
 bool StreetName::operator ==(const StreetName& rhs) const {
   return (value_ == rhs.value_);
+}
+
+bool StreetName::StartsWith(const std::string& prefix) const {
+  return boost::algorithm::starts_with(value_, prefix);
+}
+
+bool StreetName::EndsWith(const std::string& suffix) const {
+  return boost::algorithm::ends_with(value_, suffix);
+}
+
+std::string StreetName::GetPreDir() const {
+  for (const auto& pre_dir : StreetName::pre_dirs_) {
+    if (StartsWith(pre_dir))
+      return pre_dir;
+  }
+  return "";
+}
+
+std::string StreetName::GetPostDir() const {
+  for (const auto& post_dir : StreetName::post_dirs_) {
+    if (EndsWith(post_dir))
+      return post_dir;
+  }
+  return "";
+}
+
+std::string StreetName::GetBaseName() const {
+  std::string pre_dir = GetPreDir();
+  std::string post_dir = GetPostDir();
+
+  return value_.substr(pre_dir.size(),
+                       (value_.size() - pre_dir.size() - post_dir.size()));
+}
+
+bool StreetName::HasSameBaseName(const StreetName& rhs) const {
+  return (GetBaseName() == rhs.GetBaseName());
 }
 
 }
--- valhalla/odin/streetname.h
+++ valhalla/odin/streetname.h
@@ -2,9 +2,10 @@
 #define VALHALLA_ODIN_STREETNAME_H_
 
 #include <string>
+#include <vector>
 
-namespace valhalla{
-namespace odin{
+namespace valhalla {
+namespace odin {
 
 class StreetName {
  public:
@@ -14,11 +15,25 @@ class StreetName {
 
   bool operator ==(const StreetName& rhs) const;
 
+  bool StartsWith(const std::string& prefix) const;
+
+  bool EndsWith(const std::string& suffix) const;
+
+  std::string GetPreDir() const;
+
+  std::string GetPostDir() const;
+
+  std::string GetBaseName() const;
+
+  bool HasSameBaseName(const StreetName& rhs) const;
+
   // TODO - add more functionality later and comments
 
  protected:
   std::string value_;
 
+  static const std::vector<std::string> pre_dirs_;
+  static const std::vector<std::string> post_dirs_;
 };
 
 }
