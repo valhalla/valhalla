19ebefdafe2d486d03323b7d60682aa9b6ebdbb1
David Nesbitt <dave@mapzen.com>
2015-02-24 10:16:35 -0500

    Add set methods for new data members. Update comments and method order to match baldr/directededge.h

--- src/mjolnir/directededgebuilder.cc
+++ src/mjolnir/directededgebuilder.cc
@@ -63,7 +63,39 @@ void DirectedEdgeBuilder::set_endnode(const GraphId& endnode) {
 
 // Get the offset to the common edge data.
 void DirectedEdgeBuilder::set_edgeinfo_offset(const uint32_t offset) {
-  dataoffsets_.edgeinfo_offset = offset;
+  if (offset > kMaxEdgeInfoOffset) {
+    // Consider this a catastrophic error
+    LOG_ERROR("Exceeded maximum edgeinfo offset: " + std::to_string(offset));
+    throw std::runtime_error("DirectedEdgeBuilder: exceeded maximum edgeinfo offset");
+  } else {
+    dataoffsets_.edgeinfo_offset = offset;
+  }
+}
+
+// Set the access conditions flag.
+void DirectedEdgeBuilder::set_access_conditions(const bool access) {
+  dataoffsets_.access_conditions = access;
+}
+
+// Set the flag indicating this directed edge starts a simple, timed turn
+// restriction (from one edge to another).
+void DirectedEdgeBuilder::start_ttr(const bool ttr) {
+  dataoffsets_.start_ttr = ttr;
+}
+
+// Set the flag indicating this directed edge starts a multi-edge turn
+// restriction. These are restrictions from one edge to another via one or
+// more edges. Can include times.
+void DirectedEdgeBuilder::start_mer(const bool mer) {
+  dataoffsets_.start_mer = mer;
+}
+
+// Set the flag indicating this directed edge ends a multi-edge turn
+// restriction. These are restrictions from one edge to another via one
+// or more edges. This is the end edge of such a restriction.
+// Can include times.
+void DirectedEdgeBuilder::end_mer(const bool mer) {
+  dataoffsets_.end_mer = mer;
 }
 
 // Sets the exit flag.
@@ -81,123 +113,86 @@ void DirectedEdgeBuilder::set_length(const uint32_t length) {
   }
 }
 
-// Sets the number of lanes
-void DirectedEdgeBuilder::set_lanecount(const uint32_t lanecount) {
-  // TOTO - make sure we don't exceed some max value
-  attributes_.lanecount = lanecount;
-}
-
-// Set all forward access modes to true (used for transition edges)
-void DirectedEdgeBuilder::set_all_forward_access() {
-  forwardaccess_.v = kAllAccess;
-}
-
-// Sets the car access of the edge in each direction.
-void DirectedEdgeBuilder::set_caraccess(const bool forward, const bool car) {
-  if (forward) {
-    forwardaccess_.fields.car = car;
+// Sets the elevation factor (0-16) for the edge. TODO.
+void DirectedEdgeBuilder::set_elevation(const uint32_t factor) {
+  if (factor > kMaxElevationFactor) {
+    LOG_WARN("Exceeding max. elevation factor: " + std::to_string(factor));
+    geoattributes_.elevation = 0;
   } else {
-    reverseaccess_.fields.car = car;
+    geoattributes_.elevation = factor;
   }
 }
 
-// Sets the taxi access of the edge in each direction.
-void DirectedEdgeBuilder::set_taxiaccess(const bool forward, const bool taxi) {
-  if (forward) {
-    forwardaccess_.fields.taxi = taxi;
+// Sets the curvature factor (0-16) for the edge. TODO.
+void DirectedEdgeBuilder::set_curvature(const uint32_t factor) {
+  if (factor > kMaxCurvatureFactor) {
+    LOG_WARN("Exceeding max. curvature factor: " + std::to_string(factor));
+    geoattributes_.curvature = 0;
   } else {
-    reverseaccess_.fields.taxi = taxi;
+    geoattributes_.curvature = factor;
   }
 }
 
-// Sets the truck access of the edge in each direction.
-void DirectedEdgeBuilder::set_truckaccess(const bool forward, const bool truck) {
-  if (forward) {
-    forwardaccess_.fields.truck = truck;
-  } else {
-    reverseaccess_.fields.truck = truck;
-  }
+// Set the flag indicating driving is on the right hand side of the road
+// along this edge?
+void DirectedEdgeBuilder::set_drive_on_right(const bool rsd) {
+  attributes_.drive_on_right = rsd;
 }
 
-// Sets the pedestrian access of the edge in each direction.
-void DirectedEdgeBuilder::set_pedestrianaccess(const bool forward, const bool pedestrian) {
-  if (forward) {
-    forwardaccess_.fields.pedestrian = pedestrian;
-  } else {
-    reverseaccess_.fields.pedestrian = pedestrian;
-  }
-}
-
-// Sets the bicycle access of the edge in each direction.
-void DirectedEdgeBuilder::set_bicycleaccess(const bool forward, const bool bicycle) {
-  if (forward) {
-    forwardaccess_.fields.bicycle = bicycle;
-  } else {
-    reverseaccess_.fields.bicycle = bicycle;
-  }
-}
-
-// Sets the emergency access of the edge in each direction.
-void DirectedEdgeBuilder::set_emergencyaccess(const bool forward, const bool emergency) {
-  if (forward) {
-    forwardaccess_.fields.emergency = emergency;
-  } else {
-    reverseaccess_.fields.emergency = emergency;
-  }
-}
-
-// Sets the horse access of the edge in each direction.
-void DirectedEdgeBuilder::set_horseaccess(const bool forward, const bool horse) {
-  if (forward) {
-    forwardaccess_.fields.horse = horse;
-  } else {
-    reverseaccess_.fields.horse = horse;
-  }
-}
-
-//Sets the ferry flag.
+// Sets the flag indicating this edge is a ferry (or part of a ferry).
 void DirectedEdgeBuilder::set_ferry(const bool ferry) {
   attributes_.ferry = ferry;
 }
 
-//Sets the rail ferry flag.
+// Sets the flag indicating this edge is a rail ferry (or part of a
+// rail ferry). Example is the EuroTunnel (Channel Tunnel).
 void DirectedEdgeBuilder::set_railferry(const bool railferry) {
   attributes_.railferry = railferry;
 }
 
-//Sets the toll flag.
+// Sets the flag indicating this edge has a toll or is it part of a toll road.
 void DirectedEdgeBuilder::set_toll(const bool toll) {
   attributes_.toll = toll;
 }
 
-//Sets the destination only (private) flag.
+// Sets the flag indicating this edge has seasonal access
+void DirectedEdgeBuilder::set_seasonal(const bool seasonal) {
+  attributes_.seasonal = seasonal;
+}
+
+// Sets the destination only (private) flag. This indicates the edge should
+// allow access only to locations that are destinations and not allow
+// "through" traffic
 void DirectedEdgeBuilder::set_dest_only(const bool destonly) {
   attributes_.dest_only = destonly;
 }
 
-//Sets the tunnel flag.
+// Sets the flag indicating this edge has is a tunnel of part of a tunnel.
 void DirectedEdgeBuilder::set_tunnel(const bool tunnel) {
   attributes_.tunnel = tunnel;
 }
 
-// Sets the bridge flag.
+// Sets the flag indicating this edge has is a bridge of part of a bridge.
 void DirectedEdgeBuilder::set_bridge(const bool bridge) {
   attributes_.bridge = bridge;
 }
 
-// Sets the roundabout flag.
+// Sets the flag indicating the  edge is part of a roundabout.
 void DirectedEdgeBuilder::set_roundabout(const bool roundabout) {
   attributes_.roundabout = roundabout;
 }
 
-// Sets the surface.
-void DirectedEdgeBuilder::set_surface(const Surface surface) {
-  classification_.surface = static_cast<uint8_t>(surface);
+// Sets the flag indicating the edge is unreachable by driving. This can
+// happen if a driveable edge is surrounded by pedestrian only edges (e.g.
+// in a city center) or is not properly connected to other edges.
+void DirectedEdgeBuilder::set_unreachable(const bool unreachable) {
+  attributes_.unreachable = unreachable;
 }
 
-// Sets the cycle lane.
-void DirectedEdgeBuilder::set_cyclelane(const CycleLane cyclelane) {
-  attributes_.cycle_lane = static_cast<uint8_t>(cyclelane);
+// Sets the flag indicating a traffic signal is present at the end of
+// this edge.
+void DirectedEdgeBuilder::set_traffic_signal(const bool signal) {
+  attributes_.traffic_signal = signal;
 }
 
 // Set the flag for whether this edge represents a transition up one level
@@ -239,14 +234,21 @@ void DirectedEdgeBuilder::set_opp_index(const uint32_t opp_index) {
   attributes_.opp_index = opp_index;
 }
 
-//Sets the bike network mask
-void DirectedEdgeBuilder::set_bikenetwork(const uint32_t bikenetwork) {
-  attributes_.bikenetwork = bikenetwork;
+// Sets the type of cycle lane (if any) present on this edge.
+void DirectedEdgeBuilder::set_cyclelane(const CycleLane cyclelane) {
+  attributes_.cycle_lane = static_cast<uint8_t>(cyclelane);
 }
 
-// Sets the intersection internal flag.
-void DirectedEdgeBuilder::set_internal(const bool internal) {
-  classification_.internal = internal;
+// Sets the bike network mask indicating which (if any) bicycle networks are
+// along this edge. See baldr/directededge.h for definitions.
+void DirectedEdgeBuilder::set_bikenetwork(const uint32_t bikenetwork) {
+  if (bikenetwork > kMaxBicycleNetwork) {
+    LOG_ERROR("Bicycle Network mask exceeds maximum: " +
+              std::to_string(bikenetwork));
+    attributes_.bikenetwork = 0;
+  } else {
+    attributes_.bikenetwork = bikenetwork;
+  }
 }
 
 // Set the index of the directed edge on the local level of the graph
@@ -258,6 +260,17 @@ void DirectedEdgeBuilder::set_localedgeidx(const uint32_t idx) {
     attributes_.localedgeidx = kMaxEdgesPerNode;
   } else {
     attributes_.localedgeidx = idx;
+  }
+}
+
+// Sets the number of lanes
+void DirectedEdgeBuilder::set_lanecount(const uint32_t lanecount) {
+  // TOTO - make sure we don't exceed some max value
+  if (lanecount > kMaxLaneCount) {
+    LOG_ERROR("Exceeding maximum lane count: " + std::to_string(lanecount));
+    attributes_.lanecount = kMaxLaneCount;
+  } else {
+    attributes_.lanecount = lanecount;
   }
 }
 
@@ -274,27 +287,126 @@ void DirectedEdgeBuilder::set_restrictions(const uint32_t mask) {
   }
 }
 
-// Sets the road classification.
-void DirectedEdgeBuilder::set_classification(const RoadClass roadclass) {
-  classification_.classification = static_cast<uint8_t>(roadclass);
-}
-
-// Sets the link flag.
-void DirectedEdgeBuilder::set_link(const uint8_t link) {
-  classification_.link = link;
-}
-
-// Sets the use.
+// Sets the specialized use type of this edge.
 void DirectedEdgeBuilder::set_use(const Use use) {
   attributes_.use = static_cast<uint8_t>(use);
+}
+
+// Set all forward access modes to true (used for transition edges)
+void DirectedEdgeBuilder::set_all_forward_access() {
+  forwardaccess_.v = kAllAccess;
+}
+
+// Sets the car access of the edge in the specified direction.
+void DirectedEdgeBuilder::set_caraccess(const bool forward, const bool car) {
+  if (forward) {
+    forwardaccess_.fields.car = car;
+  } else {
+    reverseaccess_.fields.car = car;
+  }
+}
+
+// Sets the taxi access of the edge in the specified direction.
+void DirectedEdgeBuilder::set_taxiaccess(const bool forward, const bool taxi) {
+  if (forward) {
+    forwardaccess_.fields.taxi = taxi;
+  } else {
+    reverseaccess_.fields.taxi = taxi;
+  }
+}
+
+// Sets the truck access of the edge in the specified direction.
+void DirectedEdgeBuilder::set_truckaccess(const bool forward,
+                                          const bool truck) {
+  if (forward) {
+    forwardaccess_.fields.truck = truck;
+  } else {
+    reverseaccess_.fields.truck = truck;
+  }
+}
+
+// Sets the pedestrian access of the edge in the specified direction.
+void DirectedEdgeBuilder::set_pedestrianaccess(const bool forward,
+                                               const bool pedestrian) {
+  if (forward) {
+    forwardaccess_.fields.pedestrian = pedestrian;
+  } else {
+    reverseaccess_.fields.pedestrian = pedestrian;
+  }
+}
+
+// Sets the bicycle access of the edge in the specified direction.
+void DirectedEdgeBuilder::set_bicycleaccess(const bool forward,
+                                            const bool bicycle) {
+  if (forward) {
+    forwardaccess_.fields.bicycle = bicycle;
+  } else {
+    reverseaccess_.fields.bicycle = bicycle;
+  }
+}
+
+// Sets the emergency access of the edge in the specified direction.
+void DirectedEdgeBuilder::set_emergencyaccess(const bool forward,
+                                              const bool emergency) {
+  if (forward) {
+    forwardaccess_.fields.emergency = emergency;
+  } else {
+    reverseaccess_.fields.emergency = emergency;
+  }
+}
+
+// Sets the horse access of the edge in the specified direction.
+void DirectedEdgeBuilder::set_horseaccess(const bool forward,
+                                          const bool horse) {
+  if (forward) {
+    forwardaccess_.fields.horse = horse;
+  } else {
+    reverseaccess_.fields.horse = horse;
+  }
+}
+
+// Sets the high occupancy vehicle (HOV) access of the edge in the specified
+// direction.
+void DirectedEdgeBuilder::set_hovaccess(const bool forward, const bool hov) {
+  if (forward) {
+    forwardaccess_.fields.hov = hov;
+  } else {
+    reverseaccess_.fields.hov = hov;
+  }
 }
 
 // Sets the speed in KPH.
 void DirectedEdgeBuilder::set_speed(const float speed) {
   // TODO - protect against exceeding max speed
-  speed_ = static_cast<unsigned char>(speed + 0.5f);
+  if (speed > kMaxSpeed) {
+    LOG_ERROR("Exceeding maximum speed: " + std::to_string(speed));
+    speed_ = static_cast<unsigned char>(kMaxSpeed);
+  } else {
+    speed_ = static_cast<unsigned char>(speed + 0.5f);
+  }
 }
 
+// Sets the classification (importance) of this edge.
+void DirectedEdgeBuilder::set_classification(const RoadClass roadclass) {
+  classification_.classification = static_cast<uint8_t>(roadclass);
+}
+
+// Sets the surface type (see baldr/graphconstants.h). This is a general
+// indication of smoothness.
+void DirectedEdgeBuilder::set_surface(const Surface surface) {
+  classification_.surface = static_cast<uint8_t>(surface);
+}
+
+// Sets the link flag indicating the edge is part of a link or connection
+// (ramp or turn channel).
+void DirectedEdgeBuilder::set_link(const uint8_t link) {
+  classification_.link = link;
+}
+
+// Sets the intersection internal flag.
+void DirectedEdgeBuilder::set_internal(const bool internal) {
+  classification_.internal = internal;
+}
 
 }
 }
