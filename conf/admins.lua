9c61eaf90254cd6a64aed5495f9122086bf94423
gknisely <greg@mapzen.com>
2015-03-05 07:59:05 -0500

    added missing states and islands.

--- conf/admins.lua
+++ conf/admins.lua
@@ -42,8 +42,35 @@ function rels_proc (kv, nokeys)
      (kv["admin_level"] == "2" or kv["admin_level"] == "4") or kv["admin_level"] == "6") then
 
      --save only states/prov for USA, MX, and CA.
-     if (kv["name"] == "United States of America" or 
-         kv["name"] == "Estados Unidos Mexicanos" or kv["name"] == "Canada") then
+     if (kv["name"] == "United States of America" or kv["name"] == "Canada") then
+       return 1, kv
+     end
+
+     if (kv["admin_level"] == "4" and 
+        (kv["name"] ~= "Alabama" and kv["name"] ~= "Alaska" and kv["name"] ~= "Alberta" and
+         kv["name"] ~= "Arizona" and kv["name"] ~= "Arkansas" and kv["name"] ~= "British Columbia" and
+         kv["name"] ~= "California" and kv["name"] ~= "Colorado" and kv["name"] ~= "Connecticut" and
+         kv["name"] ~= "Delaware" and kv["name"] ~= "District of Columbia" and kv["name"] ~= "Florida" and 
+         kv["name"] ~= "Georgia" and kv["name"] ~= "Idaho" and kv["name"] ~= "Illinois" and 
+         kv["name"] ~= "Indiana" and kv["name"] ~= "Iowa" and kv["name"] ~= "Kansas" and 
+         kv["name"] ~= "Kentucky" and kv["name"] ~= "Louisiana" and kv["name"] ~= "Maine" and 
+         kv["name"] ~= "Manitoba" and kv["name"] ~= "Maryland" and kv["name"] ~= "Massachusetts" and 
+         kv["name"] ~= "Michigan" and kv["name"] ~= "Minnesota" and kv["name"] ~= "Mississippi" and 
+         kv["name"] ~= "Missouri" and kv["name"] ~= "Montana" and kv["name"] ~= "Nebraska" and
+         kv["name"] ~= "Nevada" and kv["name"] ~= "New Brunswick" and kv["name"] ~= "Newfoundland and Labrador" and
+         kv["name"] ~= "New Hampshire" and kv["name"] ~= "New Jersey" and kv["name"] ~= "New Mexico" and
+         kv["name"] ~= "New York" and kv["name"] ~= "North Carolina" and kv["name"] ~= "North Dakota" and
+         kv["name"] ~= "Northwest Territories" and kv["name"] ~= "Nova Scotia" and kv["name"] ~= "Nunavut" and
+         kv["name"] ~= "Ohio" and kv["name"] ~= "Oklahoma" and kv["name"] ~= "Ontario" and
+         kv["name"] ~= "Oregon" and kv["name"] ~= "Pennsylvania" and kv["name"] ~= "Prince Edward Island" and 
+         kv["name"] ~= "Québec" and kv["name"] ~= "Rhode Island" and kv["name"] ~= "Saskatchewan" and 
+         kv["name"] ~= "South Carolina" and kv["name"] ~= "South Dakota" and kv["name"] ~= "Tennessee" and
+         kv["name"] ~= "Texas" and kv["name"] ~= "Utah" and kv["name"] ~= "Vermont" and 
+         kv["name"] ~= "Virginia" and kv["name"] ~= "Washington" and kv["name"] ~= "West Virginia" and
+         kv["name"] ~= "Wisconsin" and kv["name"] ~= "Wyoming" and kv["name"] ~= "Yukon" and
+         kv["name"] ~= "Hawaii" and kv["name"] ~= "American Samoa" and 
+         kv["name"] ~= "United States Minor Outlying Islands" and 
+         kv["name"] ~= "Puerto Rico" and kv["name"] ~= "United States Virgin Islands")) then
        return 1, kv
      end
 
