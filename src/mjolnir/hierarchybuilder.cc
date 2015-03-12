a24f766eaa9e26cac038335101f5a84b3497f7ce
David Nesbitt <dave@mapzen.com>
2015-03-12 13:23:31 -0400

    Update to set is_shortcut flag. Always add shortcuts (so we get proper opposing edges) but only "mask up to the max so only that # supersedes an edge.

--- src/mjolnir/hierarchybuilder.cc
+++ src/mjolnir/hierarchybuilder.cc
@@ -367,20 +367,14 @@ if (nodea.level() == 0) {
      % nodea % baseni->latlng().lat() % baseni->latlng().lng() % nodeb).str());
 }
 **/
-      // Add shortcut directed edge (if we have not reached max per node)
-      if (shortcut < kMaxShortcutsFromNode) {
-        // Add to the shortcut map (associates the base edge index to the
-        // shortcut index). Remove superseded mask that may have been copied
-        // from base level directed edge
-        shortcuts[i] = shortcut+1;
-        newedge.set_shortcut(shortcut+1);
-        newedge.set_superseded(0);
-        shortcut++;
-        directededges.emplace_back(std::move(newedge));
-      } else {
-        LOG_INFO("Skip adding shortcut at " + std::to_string(baseni->latlng().lat()) +
-                 "," + std::to_string(baseni->latlng().lng()));
-      }
+      // Add shortcut edge. Add to the shortcut map (associates the base edge
+      // index to the shortcut index).Remove superseded mask that may have
+      // been copied from base level directed edge
+      shortcuts[i] = shortcut+1;
+      newedge.set_shortcut(shortcut+1);
+      newedge.set_superseded(0);
+      directededges.emplace_back(std::move(newedge));
+      shortcut++;
     }
   }
   info.shortcutcount_ += directededges.size();
