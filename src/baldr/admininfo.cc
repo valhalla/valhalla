c44945ad75add64c4ce6abc031b2b2bdc2614bea
gknisely <greg@mapzen.com>
2015-03-13 18:44:54 -0400

    added admininfo.

---
+++ src/baldr/admininfo.cc
@@ -0,0 +1,52 @@
+#include "baldr/admininfo.h"
+
+namespace valhalla {
+namespace baldr {
+
+AdminInfo::AdminInfo(char* ptr, const char* names_list, const size_t names_list_length)
+  : names_list_(names_list), names_list_length_(names_list_length) {
+
+  item_ = reinterpret_cast<PackedItem*>(ptr);
+  ptr += sizeof(PackedItem);
+
+  // Set name_offset_list_ pointer
+  name_offset_list_ = reinterpret_cast<uint32_t*>(ptr);
+  ptr += (name_count() * sizeof(uint32_t));
+}
+
+AdminInfo::~AdminInfo() {
+  //nothing to delete these are all shallow pointers for the moment held
+  //by another object
+}
+
+const uint32_t AdminInfo::name_count() const {
+  return item_->fields.name_count;
+}
+
+const uint32_t AdminInfo::admin_level() const {
+  return item_->fields.admin_level;
+}
+
+const uint32_t AdminInfo::GetNameOffset(uint8_t index) const {
+  if(index < item_->fields.name_count)
+    return name_offset_list_[index];
+  else
+    throw std::runtime_error("NameOffset index was out of bounds");
+}
+
+const std::vector<std::string> AdminInfo::GetNames() const {
+  // Get each name
+  std::vector<std::string> names;
+  for (uint32_t i = 0; i < name_count(); i++) {
+    uint32_t offset = GetNameOffset(i);
+    if (offset < names_list_length_) {
+      names.push_back(names_list_ + offset);
+    } else {
+      throw std::runtime_error("GetNames: offset exceeds size of text list");
+    }
+  }
+  return names;
+}
+
+}
+}
---
+++ valhalla/baldr/admininfo.h
@@ -0,0 +1,90 @@
+#ifndef VALHALLA_BALDR_ADMININFO_H_
+#define VALHALLA_BALDR_ADMININFO_H_
+
+#include <vector>
+#include <string>
+#include <ostream>
+#include <iostream>
+
+#include <valhalla/midgard/pointll.h>
+#include <valhalla/midgard/util.h>
+#include <valhalla/baldr/graphid.h>
+
+using namespace valhalla::midgard;
+
+namespace valhalla {
+namespace baldr {
+
+constexpr size_t kMaxNames = 255;
+
+/**
+ * Admin information
+ */
+class AdminInfo {
+ public:
+  AdminInfo() = delete;
+  AdminInfo(const AdminInfo& other) = delete;
+
+  /**
+   * Constructor
+   *
+   * @param pointer to a bit of memory that has the info for this admin
+   */
+  AdminInfo(char* ptr, const char* names_list, const size_t names_list_length);
+
+  /**
+   * Destructor
+   *
+   */
+  virtual ~AdminInfo();
+
+  // Returns the name count
+  const uint32_t name_count() const;
+
+  // Returns the admin level
+  const uint32_t admin_level() const;
+
+  // Returns the name index at the specified index.
+  const uint32_t GetNameOffset(uint8_t index) const;
+
+  /**
+   * Convenience method to get the names for an admin
+   * @return   Returns a list (vector) of names.
+   */
+  const std::vector<std::string> GetNames() const;
+
+  // Operator EqualTo.
+  bool operator ==(const AdminInfo& rhs) const;
+
+  // Packed items: counts for names and level
+  union PackedItem {
+    struct Fields {
+
+      uint32_t admin_level         :2;
+      uint32_t name_count          :8;
+      uint32_t spare               :22;
+    } fields;
+    uint32_t value;
+  };
+
+
+ protected:
+
+  // Where we keep the statistics about how large the vectors below are
+  PackedItem* item_;
+
+  // List of name indexes
+  uint32_t* name_offset_list_;
+
+  // The list of names within the tile
+  const char* names_list_;
+
+  // The size of the names list
+  const size_t names_list_length_;
+
+};
+
+}
+}
+
+#endif  // VALHALLA_BALDR_ADMININFO_H_
