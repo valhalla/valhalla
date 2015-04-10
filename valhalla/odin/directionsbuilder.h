306ae84bb1eb9d3b191dac468d453cc063721bb7
Duane Gearhart <duane@mapzen.com>
2015-04-10 13:41:14 -0400

    Added locations to the TripDirections PBF Added assignments in the DirectionsBuilder

--- valhalla/odin/directionsbuilder.h
+++ valhalla/odin/directionsbuilder.h
@@ -7,8 +7,8 @@
 #include <valhalla/proto/tripdirections.pb.h>
 #include <valhalla/odin/maneuver.h>
 
-namespace valhalla{
-namespace odin{
+namespace valhalla {
+namespace odin {
 
 class DirectionsBuilder {
  public:
@@ -17,7 +17,8 @@ class DirectionsBuilder {
   TripDirections Build(TripPath& trip_path);
 
  protected:
-  TripDirections PopulateTripDirections(std::list<Maneuver>& maneuvers);
+  TripDirections PopulateTripDirections(TripPath& trip_path,
+                                        std::list<Maneuver>& maneuvers);
 
 };
 
