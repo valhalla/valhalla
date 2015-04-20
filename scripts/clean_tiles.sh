ce64803bba38f2249403c949650d08131016858f
gknisely <greg@mapzen.com>
2015-04-20 16:17:45 -0400

    rsync is now mv and updated number to save to be var.

--- scripts/clean_tiles.sh
+++ scripts/clean_tiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+number_to_save=$2
 tile_dirs=`ls -d $1.* | sort -r`
 let count=0
 for tile_dir in ${tile_dirs} 
 do
-   if [ $count -ge 2 ]; then
+   if [ $count -ge $number_to_save ]; then
       rm -rf $tile_dir
    fi
    let count=count+1
