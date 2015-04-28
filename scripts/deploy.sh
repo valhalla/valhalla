9127eb43ca5b0cecf4af65508f136e26cb4c4032
gknisely <greg@mapzen.com>
2015-04-28 11:36:08 -0400

    deployment scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,55 @@
+base_dir=$1
+config=$2
+src_dir=$3
+extracts_dir=$4
+log=$5
+
+# make sure only one is running at any time...
+LOCK_FILE="${base_dir}/locks/mjolnir.lock"
+mkdir -p "${base_dir}/locks"
+
+(set -C; : > ${LOCK_FILE}) 2> /dev/null
+
+if [ $? != "0" ]; then
+   echo "Lock File exists - exiting"
+   exit 1
+fi
+
+extracts=${extracts_dir}/*.pbf
+# update each pbf
+for file in $(extracts); do
+  ${src_dir}/mjolnir/scripts/minutely_update.sh update ${extracts_dir} ${file} || exit $?
+done
+
+mjolnir_tile_dir=`cat ${config} | jq '.mjolnir.hierarchy.tile_dir' | sed 's/^"\(.*\)"$/\1/'` || exit $?
+tile_dir=$(echo ${mjolnir_tile_dir} | sed 's/mjolnir_tiles/tiles/g') || exit $?
+
+# clean mjolnir tiles
+rm -rf ${mjolnir_tile_dir}/* || exit $?
+
+# create admins
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib pbfadminbuilder -c ${config} ${extracts} || exit $?
+
+# move the admin log
+mv ${log} ${log}.admins || exit $?
+
+# cut tiles from the data
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib pbfgraphbuilder -c ${config} ${extracts} || exit $?
+
+# clean tile dir 
+rm -rf ${tile_dir}/* || exit $?
+
+# move the newly created tiles to the tile dir
+mv ${mjolnir_tile_dir}/* ${tile_dir}/ || exit $?
+
+# clean up
+rm -rf ${base_dir}/*.bin || exit $?
+
+# remove cron jobs.
+crontab -r
+
+# add new cron job for updates.
+(crontab -l 2>/dev/null; echo "*/5 * * * * ${src_dir}/mjolnir/scripts/update_tiles.sh ${base_dir} ${config} ${src_dir} ${extracts_dir}") | crontab -
+
+rm ${LOCK_FILE} || exit $?
+
---
+++ scripts/update_tiles.sh
@@ -0,0 +1,50 @@
+base_dir=$1
+config=$2
+src_dir=$3
+extracts_dir=$4
+
+# make sure only one is running at any time...
+LOCK_FILE="${base_dir}/locks/mjolnir.lock"
+mkdir -p "${base_dir}/locks"
+
+(set -C; : > ${LOCK_FILE}) 2> /dev/null
+
+#wait until lock is removed 
+if [ $? != "0" ]; then
+   exit 0
+fi
+
+extracts=${extracts_dir}/*.pbf
+# update each pbf
+for file in $(extracts); do
+  ${src_dir}/mjolnir/scripts/minutely_update.sh update ${extracts_dir} ${file} || exit $?
+done
+
+mjolnir_tile_dir=`cat ${config} | jq '.mjolnir.hierarchy.tile_dir' | sed 's/^"\(.*\)"$/\1/'` || exit $?
+tile_dir=$(echo ${mjolnir_tile_dir} | sed 's/mjolnir_tiles/tiles/g') || exit $?
+
+# clean mjolnir tiles
+rm -rf ${mjolnir_tile_dir}/* || exit $?
+
+# cut tiles from the data
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib pbfgraphbuilder -c ${config} ${extracts} || exit $?
+
+# backup tiles
+${src_dir}/mjolnir/scripts/backup_tiles.sh ${tile_dir} || exit $?
+
+# clean tile dir 
+rm -rf ${tile_dir}/* || exit $?
+
+# move the newly created tiles to the tile dir
+mv ${mjolnir_tile_dir}/* ${tile_dir}/ || exit $?
+
+# clean backup tiles
+${src_dir}/mjolnir/scripts/clean_tiles.sh ${tile_dir} 2 || exit $?
+
+# clean up
+rm -rf ${base_dir}/*.bin || exit $?
+
+rm ${LOCK_FILE} || exit $?
+
+
+
