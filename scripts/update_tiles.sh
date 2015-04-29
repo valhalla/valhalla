49b510236859834fde2483050f219ef77259ef61
gknisely <greg@mapzen.com>
2015-04-29 13:59:54 -0400

    cleaning of mjolnir tile dir not needed.

--- scripts/update_tiles.sh
+++ scripts/update_tiles.sh
@@ -25,9 +25,6 @@ extracts=`find ${extracts_dir} -type f -name "*.pbf"`
 mjolnir_tile_dir=`cat ${config} | jq '.mjolnir.hierarchy.tile_dir' | sed 's/^"\(.*\)"$/\1/'` || exit $?
 tile_dir=$(echo ${mjolnir_tile_dir} | sed 's/mjolnir_tiles/tiles/g') || exit $?
 
-# clean mjolnir tiles
-rm -rf ${mjolnir_tile_dir}/* || exit $?
-
 export PATH=$PATH:/usr/local/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
