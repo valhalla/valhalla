19d52aa63efc4c431a31194a353993bd0f4f7996
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-07 18:18:33 -0400

    add curl dep for server

--- scripts/install_server_deps.sh
+++ scripts/install_server_deps.sh
@@ -8,10 +8,13 @@ sudo make install
 popd
 rm -rf libzmq
 
+# need curl for url de/encode
+sudo apt-get install -y libcurl4-openssl-dev
+
 # grab experimental zmq-based server API:
 git clone https://github.com/kevinkreiser/prime_server.git
 pushd prime_server
-./autogen.sh && ./configure --without-libsodium && make check
+./autogen.sh && ./configure && make check
 sudo make install
 popd
 rm -rf prime_server
