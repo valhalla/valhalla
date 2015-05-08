bdc84d0940a8158179befaf4af97cf6f2ab261f6
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-08 10:52:26 -0400

    need submodules

--- scripts/install_server_deps.sh
+++ scripts/install_server_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # grab the latest zmq library:
-git clone https://github.com/zeromq/libzmq.git
+git clone --depth=1 --recurse-submodules --single-branch --branch=master https://github.com/zeromq/libzmq.git
 pushd libzmq
 ./autogen.sh && ./configure --without-libsodium --without-documentation && make check -j2
 sudo make install
@@ -9,7 +9,7 @@ popd
 rm -rf libzmq
 
 # grab experimental zmq-based server API:
-git clone https://github.com/kevinkreiser/prime_server.git
+git clone --depth=1 --recurse-submodules --single-branch --branch=master https://github.com/kevinkreiser/prime_server.git
 pushd prime_server
 ./autogen.sh && ./configure && make check -j2
 sudo make install
