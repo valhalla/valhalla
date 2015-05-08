23209bafaa321ca3a7aefd8c1c427643b9758dfa
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-08 08:36:08 -0400

    try installing github deps without side for all but the make install bit

--- scripts/install_server_deps.sh
+++ scripts/install_server_deps.sh
@@ -8,9 +8,6 @@ sudo make install
 popd
 rm -rf libzmq
 
-# need curl for url de/encode
-sudo apt-get install -y libcurl4-openssl-dev
-
 # grab experimental zmq-based server API:
 git clone https://github.com/kevinkreiser/prime_server.git
 pushd prime_server
