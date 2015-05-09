43da052421da9abf2096283842bacdb29660b71e
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-09 15:12:45 -0400

    make circle happy

--- scripts/install_server_deps.sh
+++ scripts/install_server_deps.sh
@@ -4,7 +4,7 @@ set -e
 # grab the latest zmq library:
 git clone --depth=1 --recurse-submodules --single-branch --branch=master https://github.com/zeromq/libzmq.git
 pushd libzmq
-./autogen.sh && ./configure --without-libsodium --without-documentation && make check
+./autogen.sh && ./configure --without-libsodium --without-documentation && make -j4
 sudo make install
 popd
 rm -rf libzmq
@@ -12,7 +12,7 @@ rm -rf libzmq
 # grab experimental zmq-based server API:
 git clone --depth=1 --recurse-submodules --single-branch --branch=master https://github.com/kevinkreiser/prime_server.git
 pushd prime_server
-./autogen.sh && ./configure && make check -j2
+./autogen.sh && ./configure && make test -j4
 sudo make install
 popd
 rm -rf prime_server
