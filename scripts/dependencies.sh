2a12f69281a20336f9d227ae38aabd68501783f0
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-12 14:36:01 -0400

    dont need an update

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,9 +2,7 @@
 set -e
 
 export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":" | sed -e "s/:$//g"`
-#sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo rm -f -f /etc/apt/sources.list.d/neo4j.list
-sudo apt-get update
 sudo apt-get install -y autoconf automake libtool make gcc-4.8 g++-4.8 libboost1.54-dev libboost-system1.54-dev libboost-thread1.54-dev lcov
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 
@@ -17,8 +15,10 @@ wait
 
 #build sync
 for dep in midgard; do
+	pushd $dep
 	./autogen.sh
 	./configure CPPFLAGS=-DBOOST_SPIRIT_THREADSAFE
 	make -j4
 	sudo make install
+	popd
 done
