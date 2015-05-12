18a80172838f591948459901a059ff8c4068baaa
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-12 14:50:15 -0400

    dont need to do this

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,7 +2,6 @@
 set -e
 
 export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":" | sed -e "s/:$//g"`
-sudo rm -f -f /etc/apt/sources.list.d/neo4j.list
 sudo apt-get install -y autoconf automake libtool make gcc-4.8 g++-4.8 libboost1.54-dev libboost-system1.54-dev libboost-thread1.54-dev lcov
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 
