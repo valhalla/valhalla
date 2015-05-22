53065192dc71f899162436a54563b361fff4f609
gknisely <greg@mapzen.com>
2015-05-22 16:55:04 -0400

    added const

--- src/baldr/datetime.cc
+++ src/baldr/datetime.cc
@@ -17,11 +17,12 @@ boost::gregorian::date pivot_date_ = boost::gregorian::from_undelimited_string(k
 
 //Get the number of days that have elapsed from the pivot date for the inputed date.
 //Date in the format of 20150516 or 2015-05-06T08:00
-uint32_t days_from_pivot_date(std::string date_time) {
+uint32_t days_from_pivot_date(const std::string& date_time) {
   boost::gregorian::date e_date;
   if (date_time.find("T") != std::string::npos) {
-    date_time.erase(boost::remove_if(date_time, boost::is_any_of("-,:")), date_time.end());
-    e_date = boost::gregorian::date_from_iso_string(date_time);
+    std::string dt = date_time;
+    dt.erase(boost::remove_if(dt, boost::is_any_of("-,:")), dt.end());
+    e_date = boost::gregorian::date_from_iso_string(dt);
   }
   else
     e_date = boost::gregorian::from_undelimited_string(date_time);
@@ -34,11 +35,12 @@ uint32_t days_from_pivot_date(std::string date_time) {
 
 //Get the dow mask
 //Date in the format of 20150516 or 2015-05-06T08:00
-uint32_t day_of_week_mask(std::string date_time) {
+uint32_t day_of_week_mask(const std::string& date_time) {
   boost::gregorian::date date;
   if (date_time.find("T") != std::string::npos) {
-    date_time.erase(boost::remove_if(date_time, boost::is_any_of("-,:")), date_time.end());
-    date = boost::gregorian::date_from_iso_string(date_time);
+    std::string dt = date_time;
+    dt.erase(boost::remove_if(dt, boost::is_any_of("-,:")), dt.end());
+    date = boost::gregorian::date_from_iso_string(dt);
   }
   else
     date = boost::gregorian::from_undelimited_string(date_time);
@@ -75,17 +77,19 @@ uint32_t day_of_week_mask(std::string date_time) {
 }
 
 //Get the number of seconds midnight that have elapsed.
-uint32_t seconds_from_midnight(std::string date_time) {
+uint32_t seconds_from_midnight(const std::string& date_time) {
   //date_time is in the format of HH:MM:SS or HH:MM or YYYY-MM-DDTHH:MM
   //hours can be greater than 24.
   //please see GTFS spec:
   //https://developers.google.com/transit/gtfs/reference#stop_times_fields
 
+  boost::posix_time::time_duration td;
   std::size_t found = date_time.find("T"); // YYYY-MM-DDTHH:MM
   if (found != std::string::npos)
-    date_time = date_time.substr(found+1);
+    td = boost::posix_time::duration_from_string(date_time.substr(found+1));
+  else
+    td = boost::posix_time::duration_from_string(date_time);
 
-  boost::posix_time::time_duration td(boost::posix_time::duration_from_string(date_time));
   return static_cast<uint32_t>(td.total_seconds());
 }
 
