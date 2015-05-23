d32f785bf991a2268985a75303af2ed322a996d6
gknisely <greg@mapzen.com>
2015-05-23 08:47:59 -0400

    fixing circle.

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,7 +2,7 @@
 set -e
 
 export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":" | sed -e "s/:$//g"`
-sudo apt-get install -y autoconf automake libtool make gcc-4.8 g++-4.8 libboost1.54-dev libboost-system1.54-dev libboost-filesystem1.54-dev libboost-thread1.54-dev libboost-date-time1.54-dev lcov
+sudo apt-get install -y autoconf automake libtool make gcc-4.8 g++-4.8 libboost1.54-dev libboost-system1.54-dev libboost-filesystem1.54-dev libboost-thread1.54-dev libboost-serialization1.54-dev libboost-date-time1.54-dev lcov
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 
