6e94d8c6fb7db81d24bb3ed59845f3f45c1cbf44
gknisely <greg@mapzen.com>
2015-05-23 07:57:53 -0400

    added get_duration and tests.

--- valhalla/baldr/datetime.h
+++ valhalla/baldr/datetime.h
@@ -11,14 +11,14 @@ namespace DateTime {
   /**
    * Get the number of days elapsed from the pivot date until
    * inputed date.
-   * @param   date in the format of 20150516 or 2015-05-06T08:00
+   * @param   date_time in the format of 20150516 or 2015-05-06T08:00
    * @return  Returns the number of days.
    */
   uint32_t days_from_pivot_date(const std::string& date_time);
 
   /**
    * Get the dow mask.
-   * @param   date in the format of 20150516 or 2015-05-06T08:00
+   * @param   date_time in the format of 20150516 or 2015-05-06T08:00
    * @return  Returns the dow mask.
    */
   uint32_t day_of_week_mask(const std::string& date_time);
@@ -26,10 +26,18 @@ namespace DateTime {
   /**
    * Get the number of seconds elapsed from midnight.
    * Hours can be greater than 24.
-   * @param   time in the format of 01:34:15 or 2015-05-06T08:00
+   * @param   date_time in the format of 01:34:15 or 2015-05-06T08:00
    * @return  Returns the seconds from midnight.
    */
   uint32_t seconds_from_midnight(const std::string& date_time);
+
+  /**
+   * Add x seconds to a date_time and return a ISO date_time string.
+   * @param   date_time in the format of 01:34:15 or 2015-05-06T08:00
+   * @param   seconds to add to the date.
+   * @return  Returns ISO formatted string
+   */
+  std::string get_duration(const std::string& date_time, uint32_t seconds);
 
 }
 }
