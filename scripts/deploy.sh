a5eaf1ed53ccab1d5379464ed48fb4217508e575
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-27 12:29:13 -0400

    whatever they set we'll just make it say yes cause thats what i tmeans

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ if [ $WITH_UPDATES ]; then
   crontab -r
 
   # add new cron job for updates.
-  (crontab -l 2>/dev/null; echo "*/5 * * * * cd ${base_dir}; WITH_UPDATES=${WITH_UPDATES} ${src_dir}/mjolnir/scripts/update_tiles.sh ${base_dir} ${config} ${src_dir} ${extracts_dir} >> ${log_dir}/update_cron.log 2>&1") | crontab -
+  (crontab -l 2>/dev/null; echo "*/5 * * * * cd ${base_dir}; WITH_UPDATES=YES ${src_dir}/mjolnir/scripts/update_tiles.sh ${base_dir} ${config} ${src_dir} ${extracts_dir} >> ${log_dir}/update_cron.log 2>&1") | crontab -
 
   # add new cron job for log clean up.
   (crontab -l 2>/dev/null; echo "0 0 * * 0 rm ${log_dir}/*.log") | crontab -
