11147b04f0dfb9d903e5a48775d0f2d5da6260da
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-27 12:13:58 -0400

    simplify. only one deploy which either has updates or not via env var since rest was same. update tiles can be used one off or in the cron repeatedly

--- scripts/update_tiles.sh
+++ scripts/update_tiles.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 base_dir=$1
@@ -10,58 +9,62 @@ extracts_dir=$4
 # make sure only one is running at any time...
 LOCK_FILE="${base_dir}/locks/mjolnir.lock"
 mkdir -p "${base_dir}/locks"
-
 (set -C; : > ${LOCK_FILE}) 2> /dev/null
 
-#wait until lock is removed 
+# try again later when lock is removed 
 if [ $? != "0" ]; then
    echo "Lock file exists"
    exit 0
 fi
 
-extracts=`find ${extracts_dir} -type f -name "*.pbf"`
-files=`find ${extracts_dir} -type f -name "*.pbf" -printf '%f '`
-
-# update each pbf
-for file in ${files}; do
-  ${src_dir}/mjolnir/scripts/minutely_update.sh update ${extracts_dir} ${file}
-done
-
-mjolnir_tile_dir=`cat ${config} | jq -r '.mjolnir.hierarchy.tile_dir'`
-tile_dir=$(echo ${mjolnir_tile_dir} | sed 's/mjolnir_tiles/tiles/g')
-
 export PATH=$PATH:/usr/local/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
+# only if they want updates will we update the pbfs
+if [ $WITH_UPDATES ]; then
+  files=`find ${extracts_dir} -type f -name "*.pbf" -printf '%f '`
+  for file in ${files}; do
+    ${src_dir}/mjolnir/scripts/minutely_update.sh update ${extracts_dir} ${file}
+  done
+fi
+
+# get all the lua programs for data import
+cp -rp ${src_dir}/mjolnir/conf/*.lua $(dirname ${config})
+
+# make the dir where this will go
+tile_dir=$(jq -r '.mjolnir.hierarchy.tile_dir' ${config})
+cur_tile_dir=$(dirname ${tile_dir})/tiles_$(date +%Y_%m_%d-%H_%M_%S)/
+
+# if we dont have admins we must create them
+extracts=$(find ${extracts_dir} -type f -name "*.pbf")
+admin_file=$(jq -r '.mjolnir.admin.admin_dir' ${config})/$(jq -r '.mjolnir.admin.db_name' ${config})
+if [ ! -e $admin_file ]; then
+  pbfadminbuilder -c ${config} ${extracts}
+fi
+
 # cut tiles from the data
-pbfgraphbuilder -c ${config} ${extracts}
-
-# backup tiles
-date_time=`date +%Y_%m_%d_%H%M%S`
-mv ${tile_dir} ${tile_dir}.$date_time
-mkdir -p ${tile_dir}
-
-# clean tile dir 
-rm -rf ${tile_dir}/*
-
-# move the newly created tiles to the tile dir
-mv ${mjolnir_tile_dir}/* ${tile_dir}/
-
-# cp admin db
-db_name=`cat ${config} | jq -r '.mjolnir.admin.db_name'`
-cp -rp ${tile_dir}/${db_name} ${mjolnir_tile_dir}/${db_name}
+pbfgraphbuilder -c ${config} $(find ${extracts_dir} -type f -name "*.pbf")
+rm -rf *.bin
 
 # generate connectivity map geojson, tile dir is as good a place as any
 # we can ship the whole tile dir to s3 anyway, admin connectivity and all
-pushd ${tile_dir}/
+pushd ${tile_dir}
 connectivitymap -c ${config}
 popd
 
-# clean backup tiles
-${src_dir}/mjolnir/scripts/clean_tiles.sh ${tile_dir} 2
+# backup files and tile dirs, keep the admin stuff though
+mkdir -p ${cur_tile_dir}
+mv ${tile_dir}/* ${cur_tile_dir}
+cp -rp ${cur_tile_dir}/$(basename admin_file) ${tile_dir}
 
-# clean up
-rm -rf ${base_dir}/*.bin
+# trim backed up tile directories to a certain number
+count=0
+for dir in $(ls -d tiles_.* | sort -r); do
+   if [ $count -ge 3 ]; then
+      rm -rf $dir
+   fi
+   let count=count+1
+done
 
 rm ${LOCK_FILE}
 
