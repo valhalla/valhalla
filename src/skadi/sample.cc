4aa7748882b54756b2f695b26909337edc66b7c3
Kevin Kreiser <kevinkreiser@gmail.com>
2015-06-30 14:42:16 -0400

    singleton for the gdal setup and tear down to hide it from interface

--- src/skadi/sample.cc
+++ src/skadi/sample.cc
@@ -4,18 +4,29 @@
 #include <stdexcept>
 #include <gdal.h>
 
+namespace {
+
+  struct sample_driver_t{
+    sample_driver_t() {
+      GDALAllRegister();
+    }
+    ~sample_driver_t() {
+      GDALDestroyDriverManager();
+    }
+  };
+
+  //singleton so we can hide this from the interface
+  void initialize() {
+    static sample_driver_t driver;
+  }
+
+}
+
 namespace valhalla {
 namespace skadi {
 
-  sample_driver_t::sample_driver_t() {
-    GDALAllRegister();
-  }
-
-  sample_driver_t::~sample_driver_t() {
-    GDALDestroyDriverManager();
-  }
-
-  sample::sample(const sample_driver_t& driver, const std::string& data_source) {
+  sample::sample(const std::string& data_source) {
+    initialize();
     GDALDatasetH src_ptr = GDALOpenEx(data_source.c_str(), GDAL_OF_RASTER, nullptr, nullptr, nullptr);
     if(src_ptr == nullptr)
       throw std::runtime_error("Couldn't open " + data_source);
