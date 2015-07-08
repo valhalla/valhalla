12fe0f7af3d048e004fb80c8980d3dcc6ba0f9ce
Kevin Kreiser <kevinkreiser@gmail.com>
2015-07-08 11:23:14 -0400

    load read sample locations from file

--- src/skadi/skadi_benchmark.cc
+++ src/skadi/skadi_benchmark.cc
@@ -1,5 +1,8 @@
-#include <cstdlib>
-#include <iostream>
+#include <fstream>
+#include <list>
+#include <utility>
+
+#include <valhalla/midgard/logging.h>
 
 #include "skadi/sample.h"
 
@@ -7,10 +10,24 @@ int main(int argc, char** argv) {
 
   if(argc < 2)
     throw std::runtime_error("No data source specified");
+  if(argc < 3)
+    throw std::runtime_error("No coordinate postings provided");
 
+  LOG_INFO("Loading data source");
   valhalla::skadi::sample sample(argv[1]);
-  std::cout << sample.get(std::make_pair(-76.579355, 40.451491)) << std::endl;
-  std::cout << sample.get(std::make_pair(-76.473434, 40.597690)) << std::endl;
+
+  LOG_INFO("Loading coordinate postings");
+  std::list<std::pair<double, double> > postings{{}};
+  std::ifstream file(argv[2]);
+  while(file >> postings.back().first >> postings.back().second)
+    postings.emplace_back();
+  postings.pop_back();
+
+  LOG_INFO("Sampling at postings");
+  for(const auto& p : postings)
+    sample.get(p);
+
+  LOG_INFO("Finished");
 
   return EXIT_SUCCESS;
 }
