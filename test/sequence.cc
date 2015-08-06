8c490ee2692149feb1369f14e18de132836e8f36
Kevin Kreiser <kevinkreiser@gmail.com>
2015-08-06 17:21:28 -0400

    really add the test

---
+++ test/sequence.cc
@@ -0,0 +1,61 @@
+#include "test.h"
+#include "valhalla/midgard/sequence.h"
+
+using namespace valhalla::midgard;
+
+struct osm_node {
+  uint64_t id;
+  float lng;
+  float lat;
+  uint32_t attributes;
+};
+
+std::string write_nodes(const uint64_t count) {
+  std::string file_name = "nodes.nd";
+  sequence<osm_node> sequence(file_name, true, 512);
+  for(uint64_t i = count - 1; i < count; --i)
+    sequence.push_back({i, 0.f, 0.f, 0});
+  return file_name;
+}
+
+void sort_nodes(const std::string& file_name) {
+  sequence<osm_node> sequence(file_name, false, 512);
+  sequence.sort([](const osm_node& a, const osm_node& b){return a.id < b.id;});
+}
+
+void read_nodes(const std::string& file_name, const uint64_t count) {
+  sequence<osm_node> sequence(file_name, false, 512);
+  auto less_than = [](const osm_node& a, const osm_node& b){return a.id < b.id;};
+  for(uint64_t i = 0; i < count; ++i) {
+    //grab an element
+    auto element = sequence[i];
+    osm_node node = *element;
+    if(node.id != i)
+      throw std::runtime_error("Found wrong node at: " + std::to_string(i));
+    //change it
+    node.lat = 7; node.lng = 7;
+    //write it back
+    element = node;
+    //find the same element with binary search (far slower)
+    osm_node target{i};
+    if(!sequence.find(target, less_than))
+      throw std::runtime_error("Didn't find node " + std::to_string(i));
+  }
+}
+
+void test_read_write() {
+  size_t count = 1024;
+  auto file_name = write_nodes(count);
+  sort_nodes(file_name);
+  read_nodes(file_name, count);
+}
+
+
+int main() {
+  test::suite suite("logging");
+
+  //check file logging
+  suite.test(TEST_CASE(test_read_write));
+
+  return suite.tear_down();
+}
