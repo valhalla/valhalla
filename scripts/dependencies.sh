dda2177a62e1bd4d13703fbf751ea1c3eaec3f97
Kevin Kreiser <kevinkreiser@gmail.com>
2015-08-11 11:50:03 -0400

    get gcc/++-4.9 via ppa but only update that one because the rest takes long and is prone to fail

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,8 +2,8 @@
 set -e
 
 export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":" | sed -e "s/:$//g"`
-#sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-#sudo apt-get update
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/ubuntu-toolchain-r-test-$(lsb_release -c -s).list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 sudo apt-get install -y autoconf automake libtool make gcc-4.9 g++-4.9 libboost1.54-dev libboost-program-options1.54-dev libboost-filesystem1.54-dev libboost-system1.54-dev libboost-thread1.54-dev lcov libcurl4-openssl-dev geotiff-bin libgeotiff-dev libgeotiff2
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 90
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
