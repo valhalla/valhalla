72a21847635f2226726adc95d0fec7a283d6bae3
Kevin Kreiser <kevinkreiser@gmail.com>
2015-08-20 10:24:11 -0400

    estimate average sustained grade of a series of heights

--- src/skadi/util.cc
+++ src/skadi/util.cc
@@ -3,7 +3,7 @@
 namespace {
 
   template <class T>
-  T clamp(T val, T low, T high) {
+  T clamp(T val, const T low, const T high) {
     return std::min<T>(std::max<T>(val, low), high);
   }
 
@@ -11,30 +11,33 @@ namespace {
 
 namespace valhalla {
   namespace skadi {
-    std::pair<double, double> discretized_deltas(const std::vector<double>& heights, const double interval_distance, const double maximum_grade){
-      //nothing to do here
-      std::pair<double, double> deltas{0, 0};
-      if(heights.size() < 2)
-        return deltas;
 
-      //measure amount of downward and upward change over the distance independently
-      double down = 0, up = 0;
-      for(auto h = heights.cbegin() + 1; h != heights.cend(); ++h) {
-        auto diff = static_cast<float>(*h - *std::prev(h));
-        if(diff > 0) {
-          deltas.second += diff;
-          up += interval_distance;
-        }
-        else {
-          deltas.first -= diff;
-          down += interval_distance;
-        }
-      }
-
-      //clamp from 0 to max and get ratio of max
-      deltas.first = clamp(deltas.first / down, 0.0, maximum_grade) / maximum_grade;
-      deltas.second = clamp(deltas.second / up, 0.0, maximum_grade) / maximum_grade;
-      return deltas;
+    double energy_weighting(double& grade) {
+      //dont consider anything steeper than -10% or +15%
+      grade = clamp(grade, -10.0, 15.0);
+      //ascent gets weighted more than descent
+      return 1.0 + grade / (grade > 0 ? 7.0 : 17.0);
     }
+
+    double weighted_grade(const std::vector<double>& heights, const double interval_distance, const std::function<double (double&)>& grade_weighting){
+      //grab the scaled grade for each section
+      double total_grade = 0;
+      double total_weight = 0;
+      //multiply grades by 100 to move from 0-1 into 0-100 for grade percentage
+      auto scale = 100.0 / interval_distance;
+      for(auto h = heights.cbegin() + 1; h != heights.cend(); ++h) {
+        //get the grade for this section
+        auto grade = (*h - *std::prev(h)) * scale;
+        //find the weight and possibly change the grade
+        auto weight = grade_weighting(grade);
+        //keep this part of weighted grade
+        total_grade += grade * weight;
+        //keep this part of weight
+        total_weight += weight;
+      }
+      //get the average weighted grade by homogenizing total weight
+      return total_grade * (1.0 / total_weight);
+    }
+
   }
 }
