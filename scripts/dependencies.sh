489f81f59b42b477af6d17ab0bfd77f668504641
Kevin Kreiser <kevinkreiser@gmail.com>
2015-08-21 09:09:38 -0400

    skadi depends on service

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -21,7 +21,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 $DIR/install_service_deps.sh &
 
 #build sync
-for dep in midgard baldr skadi sif mjolnir; do
+for dep in midgard baldr sif; do
 	pushd deps/$dep
 	./autogen.sh
 	./configure CPPFLAGS=-DBOOST_SPIRIT_THREADSAFE
@@ -30,3 +30,14 @@ for dep in midgard baldr skadi sif mjolnir; do
 	popd
 done
 wait
+
+#build sync
+for dep in skadi mjolnir; do
+        pushd deps/$dep
+        ./autogen.sh
+        ./configure CPPFLAGS=-DBOOST_SPIRIT_THREADSAFE
+        make -j4
+        sudo make install
+        popd
+done
+
