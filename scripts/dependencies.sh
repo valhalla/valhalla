64f42d383192ae998081247e23de66c9f7e697af
Kevin Kreiser <kevinkreiser@gmail.com>
2015-08-31 11:45:40 -0400

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -6,5 +6,7 @@ sudo apt-get update
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/ubuntu-toolchain-r-test-$(lsb_release -c -s).list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 sudo apt-get install -y autoconf automake libtool make gcc-4.9 g++-4.9 lcov
+update-alternatives --remove-all gcc
+update-alternatives --remove-all g++
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 90
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
