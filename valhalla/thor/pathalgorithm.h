5a5bc8f9c3c81e043e68b0e24296a35ede6e6dad
David Nesbitt <dave@mapzen.com>
2015-09-28 08:54:41 -0400

    Change bucket count to what it was prior to this PR.

--- valhalla/thor/pathalgorithm.h
+++ valhalla/thor/pathalgorithm.h
@@ -21,7 +21,7 @@
 namespace valhalla {
 namespace thor {
 
-constexpr uint32_t kBucketCount = 100000;
+constexpr uint32_t kBucketCount = 20000;
 
 // If the destination is at a node we want the incoming edge Ids
 // with distance = 1.0 (the full edge). This returns and updated
