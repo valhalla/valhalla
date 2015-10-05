4de0607279c144faf91f33230edfcd1f50f1acd6
Duane Gearhart <duane@mapzen.com>
2015-10-05 16:45:50 -0400

    added roundabout exit count to trip directions

--- src/odin/directionsbuilder.cc
+++ src/odin/directionsbuilder.cc
@@ -244,6 +244,11 @@ TripDirections DirectionsBuilder::PopulateTripDirections(
 
     }
 
+    // Roundabout exit count
+    if (maneuver.roundabout_exit_count() > 0) {
+      trip_maneuver->set_roundabout_exit_count(maneuver.roundabout_exit_count());
+    }
+
   }
 
   // Populate summary
