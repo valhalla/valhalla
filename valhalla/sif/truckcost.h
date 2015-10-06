85ddf869e0912f9fd337c9553f6ec87df3d99063
David Nesbitt <dave@mapzen.com>
2015-10-06 10:03:11 -0400

    Add costing for truck routing. Currently only considers access (no special access restrictions like height, weight, etc.).

---
+++ valhalla/sif/truckcost.h
@@ -0,0 +1,18 @@
+#ifndef VALHALLA_SIF_TRUCKCOST_H_
+#define VALHALLA_SIF_TRUCKCOST_H_
+
+#include <valhalla/sif/dynamiccost.h>
+
+namespace valhalla {
+namespace sif {
+
+/**
+ * Create a truckcost
+ * @param  config  Property tree with configuration / options.
+ */
+cost_ptr_t CreateTruckCost(const boost::property_tree::ptree& config);
+
+}
+}
+
+#endif  // VALHALLA_SIF_TRUCKCOST_H_
