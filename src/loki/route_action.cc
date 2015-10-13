41a4bf6c82b6df043993124f794d863eb59a69c9
Kevin Kreiser <kevinkreiser@gmail.com>
2015-10-13 13:07:27 -0400

    spacing

--- src/loki/route_action.cc
+++ src/loki/route_action.cc
@@ -103,9 +103,8 @@ namespace valhalla {
 
         //check if distance between latlngs exceed max distance limit for each mode of travel
         auto path_distance = std::prev(location)->latlng_.Distance(location->latlng_);
-        max_distance-=path_distance;
-
-        if (max_distance < 0)
+        max_distance -= path_distance;
+        if(max_distance < 0)
           throw std::runtime_error("Path distance exceeds the max distance limit.");
 
         LOG_INFO("location_distance::" + std::to_string(path_distance));
