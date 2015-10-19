f67c3527917340f326360dd39495371132dbb181
kdiluca <kristen@mapzen.com>
2015-10-19 15:58:27 -0400

    fixed to pull from service_limits matrix sections of config for max area and max locations

--- src/loki/matrix_action.cc
+++ src/loki/matrix_action.cc
@@ -51,16 +51,28 @@ namespace valhalla {
   namespace loki {
 
     worker_t::result_t loki_worker_t::matrix(const ACTION_TYPE& action, boost::property_tree::ptree& request) {
+       auto matrix_type = "";
+       switch (action) {
+        case ONE_TO_MANY:
+          matrix_type = "one_to_many";
+          break;
+        case MANY_TO_ONE:
+          matrix_type = "many_to_one";
+          break;
+        case MANY_TO_MANY:
+          matrix_type = "many_to_many";
+          break;
+      }
       auto costing = request.get<std::string>("costing");
       if(costing.empty())
         throw std::runtime_error("No edge/node costing provided");
 
       //see if any locations pairs are unreachable or too far apart
       auto lowest_level = reader.GetTileHierarchy().levels().rbegin();
-      auto max_distance = config.get<float>("service_limits." + costing + ".max_distance");
-      auto max_route_locations = config.get<size_t>("service_limits." + costing + ".max_locations");
+      auto max_area = config.get<float>("service_limits." + std::string(matrix_type) + ".max_area");
+      auto max_locations = config.get<size_t>("service_limits." + std::string(matrix_type) + ".max_locations");
       //check that location size does not exceed max.
-      if (locations.size() > max_route_locations)
+      if (locations.size() > max_locations)
         throw std::runtime_error("Number of locations exceeds the max location limit.");
 
       LOG_INFO("Location size::" + std::to_string(locations.size()));
@@ -74,29 +86,17 @@ namespace valhalla {
 
         //check if distance between latlngs exceed max distance limit for each mode of travel
         auto path_distance = std::sqrt(midgard::DistanceApproximator::DistanceSquared(std::prev(location)->latlng_, location->latlng_));
-        if (path_distance > max_distance)
-          throw std::runtime_error("Path distance exceeds the max distance limit.");
+        if (path_distance > max_area)
+          throw std::runtime_error("Path distance exceeds the max area limit.");
 
         LOG_INFO("Location distance::" + std::to_string(path_distance));
       }
-
-      switch (action) {
-        case ONE_TO_MANY:
-          request.put("matrix_type", "one_to_many");
-          break;
-        case MANY_TO_ONE:
-          request.put("matrix_type", "many_to_one");
-          break;
-        case MANY_TO_MANY:
-          request.put("matrix_type", "many_to_many");
-          break;
-      }
-
       //correlate the various locations to the underlying graph
       for(size_t i = 0; i < locations.size(); ++i) {
         auto correlated = loki::Search(locations[i], reader, costing_filter);
         request.put_child("correlated_" + std::to_string(i), correlated.ToPtree(i));
       }
+      request.put("matrix_type", std::string(matrix_type));
 
       std::stringstream stream;
       boost::property_tree::write_info(stream, request);
