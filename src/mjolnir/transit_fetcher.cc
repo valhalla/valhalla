f5716d66078dfe7ad2e3d649cb1338c24b7b8291
Kevin Kreiser <kevinkreiser@gmail.com>
2015-10-26 13:57:29 -0400

    first cut of writing some pbf transit tiles

--- src/mjolnir/transit_fetcher.cc
+++ src/mjolnir/transit_fetcher.cc
@@ -1,23 +1,30 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <string>
 #include <memory>
 #include <unordered_set>
 #include <thread>
 #include <future>
+#include <random>
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/format.hpp>
+#include <boost/filesystem/operations.hpp>
 #include <curl/curl.h>
 
 #include <valhalla/midgard/logging.h>
 #include <valhalla/baldr/graphid.h>
 #include <valhalla/baldr/tilehierarchy.h>
+#include <valhalla/baldr/graphtile.h>
+
+#include "proto/transit.pb.h"
 
 using namespace boost::property_tree;
 using namespace valhalla::midgard;
 using namespace valhalla::baldr;
+using namespace valhalla::mjolnir;
 
 struct logged_error_t: public std::runtime_error {
   logged_error_t(const std::string& msg):std::runtime_error(msg) {
@@ -37,12 +44,15 @@ struct curler_t {
   //for now we only need to handle json
   //with templates we could return a string or whatever
   ptree operator()(const std::string& url) {
-    result.clear();
+    result.str("");
     assert_curl(curl_easy_setopt(connection.get(), CURLOPT_URL, url.c_str()), "Failed to set URL ");
     assert_curl(curl_easy_perform(connection.get()), "Failed to fetch url");
     ptree pt;
     try { read_json(result, pt); } catch(...) { throw logged_error_t(result.str()); }
     return pt;
+  }
+  std::string last() const {
+    return result.str();
   }
 protected:
   void assert_curl(CURLcode code, const std::string& msg){
@@ -97,33 +107,167 @@ std::unordered_set<GraphId> which_tiles(const ptree& pt) {
 using fetch_itr_t = std::unordered_set<GraphId>::const_iterator;
 void fetch_tiles(const ptree& pt, fetch_itr_t start, fetch_itr_t end, std::promise<std::list<GraphId> >& promise) {
   TileHierarchy hierarchy(pt.get_child("mjolnir.hierarchy"));
+  const auto& tiles = hierarchy.levels().rbegin()->second.tiles;
   std::list<GraphId> dangling;
   curler_t curler;
   auto now = time(nullptr);
-  auto utc = gmtime(&now);
+  auto* utc = gmtime(&now); utc->tm_year += 1900; ++utc->tm_mon; //TODO: use timezone code?
+  std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+  std::uniform_int_distribution<size_t> distribution(static_cast<size_t>(100), static_cast<size_t>(300));
 
   //for each tile
   for(; start != end; ++start) {
-    //while there is more to do in this tile
-    bool tile_done = false;
-    while(!tile_done) {
-      //make stops call
-      ptree response;
+    auto bbox = tiles.TileBounds(start->fields.tileid);
+    ptree response;
+    Transit tile;
+
+    //pull out all the STOPS
+    std::unordered_map<std::string, uint64_t> stops;
+    auto extra_params = (boost::format("&service_from_date=%1%-%2%-%3%&api_key=%4%")
+      % utc->tm_year % utc->tm_mon % utc->tm_mday % pt.get<std::string>("api_key")).str();
+    boost::optional<std::string> request = (boost::format(pt.get<std::string>("base_url") +
+      "/api/v1/stops?per_page=1000&bbox=%1%,%2%,%3%,%4%")
+      % bbox.minx() % bbox.miny() % bbox.maxx() % bbox.maxy()).str();
+    while(request) {
+      //grab some stuff
       try {
-        response = curler(pt.get<std::string>("base_url") + "/api/v1/stops?per_page=1000&service_from_date=%1%-%2%-%3%&bbox=%4%,%5%,%6%,%7%");
+        LOG_INFO(*request + extra_params);
+        response = curler(*request + extra_params);
+      }//if it doesnt come back, take a rest and try again
+      catch(const std::exception& e) {
+        LOG_WARN(e.what());
+        std::this_thread::sleep_for(std::chrono::milliseconds(distribution(generator)));
+        continue;
       }
-      catch(const logged_error_t& e) {
 
+      //copy stops in, keeping map of stopid to graphid
+      try {
+        for(const auto& stop_pt : response.get_child("stops")) {
+          const auto& ll_pt = stop_pt.second.get_child("geometry.coordinates");
+          auto lon = ll_pt.front().second.get_value<float>();
+          auto lat = ll_pt.back().second.get_value<float>();
+          if(!bbox.Contains({lon, lat}))
+            continue;
+          auto* stop = tile.add_stops();
+          stop->set_lon(lon);
+          stop->set_lat(lat);
+          stop->set_onestop_id(stop_pt.second.get<std::string>("onestop_id"));
+          stop->set_graphid(*start + stops.size());
+          stops.emplace(stop->onestop_id(), stop->graphid());
+          //TODO: copy rest of attributes
+        }
+        //please sir may i have some more?
+        request = response.get_optional<std::string>("meta.next");
+      }//if it doesnt come back, take a rest and try again
+      catch(const std::exception& e) {
+        LOG_WARN(curler.last());
+        std::this_thread::sleep_for(std::chrono::milliseconds(distribution(generator)));
+        continue;
       }
-      //TODO: copy stops in, keeping map of stopid to graphid
-
-      //TODO: make stop pairs call using service_from_date=2015-10-21
-      //TODO: copy pairs in, noting if any dont have stops
-
-      //TODO: make routes call
-      //TODO: copy routes in
+      //break; //TODO: testing, remove
     }
-    //TODO: write pbf to file
+
+    //pull out all ROUTES
+    request = (boost::format(pt.get<std::string>("base_url") +
+      "/api/v1/routes?per_page=1000&bbox=%1%,%2%,%3%,%4%")
+      % bbox.minx() % bbox.miny() % bbox.maxx() % bbox.maxy()).str();
+    std::unordered_map<std::string, size_t> routes;
+    while(request) {
+      //grab some stuff
+      try {
+        LOG_INFO(*request + extra_params);
+        response = curler(*request + extra_params);
+      }//if it doesnt come back, take a rest and try again
+      catch(const std::exception& e) {
+        LOG_WARN(e.what());
+        std::this_thread::sleep_for(std::chrono::milliseconds(distribution(generator)));
+        continue;
+      }
+
+      //copy routes in, keeping track of routeid to route index
+      try {
+        for(const auto& route_pt : response.get_child("routes")) {
+          auto* route = tile.add_routes();
+          route->set_onestop_id(route_pt.second.get<std::string>("onestop_id"));
+          routes.emplace(route->onestop_id(), routes.size());
+          //TODO: copy rest of attributes
+        }
+
+        //please sir may i have some more?
+        request = response.get_optional<std::string>("meta.next");
+      }//if it doesnt come back, take a rest and try again
+      catch(const std::exception& e) {
+        LOG_WARN(curler.last());
+        std::this_thread::sleep_for(std::chrono::milliseconds(distribution(generator)));
+        continue;
+      }
+      //break; //TODO: testing, remove
+    }
+
+    //pull out all SCHEDULE_STOP_PAIRS
+    request = (boost::format(pt.get<std::string>("base_url") +
+      "/api/v1/schedule_stop_pairs?per_page=1000&bbox=%1%,%2%,%3%,%4%")
+      % bbox.minx() % bbox.miny() % bbox.maxx() % bbox.maxy()).str();
+    while(request) {
+      //grab some stuff
+      try {
+        LOG_INFO(*request + extra_params);
+        response = curler(*request + extra_params);
+      }//if it doesnt come back, take a rest and try again
+      catch(const std::exception& e) {
+        LOG_WARN(e.what());
+        std::this_thread::sleep_for(std::chrono::milliseconds(distribution(generator)));
+        continue;
+      }
+
+      //copy pairs in, noting if any dont have stops
+      try {
+        for(const auto& pair_pt : response.get_child("schedule_stop_pairs")) {
+          auto* pair = tile.add_stop_pairs();
+          //origin
+          pair->set_origin_onestop_id(pair_pt.second.get<std::string>("origin_onestop_id"));
+          auto origin = stops.find(pair->origin_onestop_id());
+          if(origin != stops.cend())
+            pair->set_origin_graphid(origin->second);
+          else if(dangling.size() && dangling.back() != *start)
+            dangling.push_back(*start);
+          //destination
+          pair->set_destination_onestop_id(pair_pt.second.get<std::string>("destination_onestop_id"));
+          auto destination = stops.find(pair->destination_onestop_id());
+          if(destination != stops.cend())
+            pair->set_destination_graphid(destination->second);
+          else if(dangling.size() && dangling.back() != *start)
+            dangling.push_back(*start);
+          //route
+          auto route = routes.find(pair_pt.second.get<std::string>("route_onestop_id"));
+          if(route == routes.cend()) {
+            LOG_ERROR("No route for pair: " + pair->origin_onestop_id() + " --> " + pair->destination_onestop_id());
+            tile.mutable_stop_pairs()->RemoveLast();
+            continue;
+          }
+          pair->set_route_index(route->second);
+          //TODO: copy rest of attributes
+        }
+
+        //please sir may i have some more?
+        request = response.get_optional<std::string>("meta.next");
+      }//if it doesnt come back, take a rest and try again
+      catch(const std::exception& e) {
+        LOG_WARN(curler.last());
+        std::this_thread::sleep_for(std::chrono::milliseconds(distribution(generator)));
+        continue;
+      }
+      //break; //TODO: testing, remove
+    }
+
+    //write pbf to file
+    auto file_name = GraphTile::FileSuffix(*start, hierarchy);
+    file_name = file_name.substr(0, file_name.size() - 3) + "pbf";
+    boost::filesystem::path transit_tile = pt.get<std::string>("mjolnir.transit_dir") + '/' + file_name;
+    if (!boost::filesystem::exists(transit_tile.parent_path()))
+      boost::filesystem::create_directories(transit_tile.parent_path());
+    std::fstream stream(transit_tile.string(), std::ios::out | std::ios::trunc | std::ios::binary);
+    tile.SerializeToOstream(&stream);
   }
 
   //give back the work for later
@@ -210,6 +354,10 @@ void stitch(const ptree& pt, const std::list<GraphId>& tiles) {
     );
   }
 
+  //wait for them to finish
+  for (auto& thread : threads)
+    thread->join();
+
   LOG_INFO("Finished");
 }
 
@@ -224,7 +372,7 @@ int main(int argc, char** argv) {
   ptree pt;
   boost::property_tree::read_json(std::string(argv[1]), pt);
   pt.add("base_url", std::string(argv[2]));
-  pt.add("key", std::string(argv[3]));
+  pt.add("api_key", std::string(argv[3]));
 
   //yes we want to curl
   curl_global_init(CURL_GLOBAL_DEFAULT);
