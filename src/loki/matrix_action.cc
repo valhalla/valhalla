2f8246be5b6c6a487f8eca0e77f401f67290994b
kdiluca <kristen@mapzen.com>
2015-10-28 13:52:25 -0400

    simplified max distance logic; throw generic runtime error idential to exceeding route max distance

--- src/loki/matrix_action.cc
+++ src/loki/matrix_action.cc
@@ -66,19 +66,18 @@ namespace valhalla {
        case ONE_TO_MANY:
          matrix_type = "one_to_many";
          max_distance = config.get<float>("service_limits." + std::string(matrix_type) + ".max_distance");
-         check_max_distance(0,0,0,locations.size(),max_distance);
+         check_max_distance(0,0,locations.size(),max_distance);
          break;
        case MANY_TO_ONE:
          matrix_type = "many_to_one";
          max_distance = config.get<float>("service_limits." + std::string(matrix_type) + ".max_distance");
-         for(size_t i = 0; i < locations.size(); ++i)
-           check_max_distance(i,locations.size() - 1, i, i + 1,max_distance);
+         check_max_distance(locations.size()-1,0,locations.size()-1,max_distance);
          break;
        case MANY_TO_MANY:
          matrix_type = "many_to_many";
          max_distance = config.get<float>("service_limits." + std::string(matrix_type) + ".max_distance");
-         for(size_t i = 0; i < locations.size(); ++i)
-            check_max_distance(i, 0, locations.size() * i, locations.size() * (i + 1),max_distance);
+         for(size_t i = 0; i < locations.size()-1; ++i)
+            check_max_distance(i,(i+1),locations.size(),max_distance);
          break;
      }
 
@@ -102,15 +101,14 @@ namespace valhalla {
       return result;
     }
 
-    void loki_worker_t::check_max_distance(const size_t origin, const size_t destination, const size_t start, const size_t end, float max_distance) {
+    void loki_worker_t::check_max_distance(const size_t origin, const size_t start, const size_t end, float max_distance) {
       //one to many should be distance between:a,b a,c ; many to one: a,c b,c ; many to many should be all pairs
       for(size_t i = start; i < end; ++i) {
         //check if distance between latlngs exceed max distance limit the chosen matrix type
-        auto path_distance = locations[origin].latlng_.Distance(locations[destination + (i - start)].latlng_);
-        max_distance-=path_distance;
+        auto path_distance = locations[origin].latlng_.Distance(locations[i].latlng_);
 
-        if (max_distance < 0)
-          throw std::runtime_error("The path distance of " + std::to_string(path_distance) + " exceeds the max distance limit by " + std::to_string(max_distance) + ".");
+        if (path_distance > max_distance)
+          throw std::runtime_error("Path distance exceeds the max distance limit.");
       }
     }
   }
