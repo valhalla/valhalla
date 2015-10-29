bc98c758290819b374c5b27f855cc16678beb111
gknisely <greg@mapzen.com>
2015-10-29 08:46:30 -0400

    removed attributes that are no longer needed.

--- src/baldr/transitstop.cc
+++ src/baldr/transitstop.cc
@@ -6,14 +6,10 @@ namespace baldr {
 
 // Constructor with arguments
 TransitStop::TransitStop(const uint32_t stopid, const uint32_t one_stop_offset,
-            const uint32_t name_offset, const uint32_t desc_offset,
-            const uint32_t parent_stopid, const uint32_t fare_zoneid)
+                         const uint32_t name_offset)
     : stopid_(stopid),
       one_stop_offset_(one_stop_offset),
-      name_offset_(name_offset),
-      desc_offset_(desc_offset),
-      parent_stopid_(parent_stopid),
-      fare_zoneid_(fare_zoneid) {
+      name_offset_(name_offset) {
 }
 
 // Get the internal stop Id.
@@ -29,21 +25,6 @@ uint32_t TransitStop::one_stop_offset() const {
 // Get the text/name offset for the stop name.
 uint32_t TransitStop::name_offset() const {
   return name_offset_;
-}
-
-// Get the text/name offset for the stop description.
-uint32_t TransitStop::desc_offset() const {
-  return desc_offset_;
-}
-
-// Get the internal stop Id of the parent stop.
-uint32_t TransitStop::parent_stopid() const {
-  return parent_stopid_;
-}
-
-// Get the fare zone Id.
-uint32_t TransitStop::fare_zoneid() const {
-  return fare_zoneid_;
 }
 
 // operator < - for sorting. Sort by stop Id.
--- valhalla/baldr/transitstop.h
+++ valhalla/baldr/transitstop.h
@@ -15,8 +15,7 @@ class TransitStop {
  public:
   // Constructor with arguments
   TransitStop(const uint32_t stopid, const uint32_t one_stop_offset,
-              const uint32_t name_offset, const uint32_t desc_offset,
-              const uint32_t parent_stopid, const uint32_t fare_zoneid);
+              const uint32_t name_offset);
 
   /**
    * Get the internal stop Id.
@@ -37,25 +36,6 @@ class TransitStop {
   uint32_t name_offset() const;
 
   /**
-   * Get the text/name offset for the stop description.
-   * @return  Returns the description offset in the text/name list.
-   */
-  uint32_t desc_offset() const;
-
-  /**
-   * Get the internal stop Id of the parent stop.
-   * @return  Returns the internal Id of the parent stop, 0 if there is
-   *          no parent.
-   */
-  uint32_t parent_stopid() const;
-
-  /**
-   * Get the fare zone Id.
-   * @return  Returns the fare zone Id (0 if none).
-   */
-  uint32_t fare_zoneid() const;
-
-  /**
    * operator < - for sorting. Sort by stop Id.
    * @param  other  Other transit stop to compare to.
    * @return  Returns true if stop Id < other stop Id.
@@ -72,14 +52,6 @@ class TransitStop {
   // Stop name offset in the text/name list.
   uint32_t name_offset_;
 
-  // Stop description offset in the text/name list.
-  uint32_t desc_offset_;
-
-  // Stop Id of the parent station.
-  uint32_t parent_stopid_;
-
-  // Zone Id for fares
-  uint32_t fare_zoneid_;
 };
 
 }
