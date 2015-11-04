9603aedbab8c3f4a2f7a3355d273638a8090ab01
David Nesbitt <dave@mapzen.com>
2015-11-04 16:37:46 -0500

    Add set methods to GraphTileHeader.

--- src/baldr/graphtileheader.cc
+++ src/baldr/graphtileheader.cc
@@ -11,16 +11,12 @@ using namespace valhalla::baldr;
 namespace valhalla {
 namespace baldr {
 
-// Default constructor. This is written with GraphTileHeaderBuilder and
-// read within GraphTile. Set the version and internal version here so a
-// "default" GraphTileHeader can be used to compare to what is read from
-// a tile.
+// Default constructor.
 GraphTileHeader::GraphTileHeader() {
   memset(this, 0, sizeof(GraphTileHeader));
   strncpy(version_, PACKAGE_VERSION, kMaxVersionSize);
   version_[kMaxVersionSize-1] = 0;
   date_created_ = DateTime::seconds_since_epoch();
-
 }
 
 // Get the GraphId (tileid and level) of this tile.
@@ -28,9 +24,19 @@ const GraphId& GraphTileHeader::graphid() const {
   return graphid_;
 }
 
+// Set the graph Id of this tile.
+void GraphTileHeader::set_graphid(const baldr::GraphId& graphid) {
+  graphid_ = graphid;
+}
+
 // Get the date created
 uint64_t GraphTileHeader::date_created() const {
   return date_created_;
+}
+
+// Set the date created
+void GraphTileHeader::set_date_created(const uint64_t date) {
+  date_created_ = date;
 }
 
 // Get the version string.
@@ -38,9 +44,20 @@ std::string GraphTileHeader::version() const {
   return version_;
 }
 
+// Set the version string.
+void GraphTileHeader::set_version(const std::string& version) {
+  strncpy(version_, version.c_str(), kMaxVersionSize);
+  version_[kMaxVersionSize-1] = 0;
+}
+
 // Get the relative road density within this tile.
 uint32_t GraphTileHeader::density() const {
   return static_cast<uint32_t>(density_);
+}
+
+// Set the relative road density within this tile.
+void GraphTileHeader::set_density(const uint32_t density) {
+  density_ = density;
 }
 
 // Get the relative quality of name assignment for this tile.
@@ -48,9 +65,19 @@ uint32_t GraphTileHeader::name_quality() const {
   return static_cast<uint32_t>(name_quality_);
 }
 
+// Set the relative quality of name assignment for this tile.
+void GraphTileHeader::set_name_quality(const uint32_t name_quality) {
+  name_quality_ = name_quality;
+}
+
 // Get the relative quality of speed assignment for this tile.
 uint32_t GraphTileHeader::speed_quality() const {
   return static_cast<uint32_t>(speed_quality_);
+}
+
+// Set the relative quality of speed assignment for this tile.
+void GraphTileHeader::set_speed_quality(const uint32_t speed_quality) {
+  speed_quality_ = speed_quality;
 }
 
 // Get the relative quality of exit signs for this tile.
@@ -58,9 +85,19 @@ uint32_t GraphTileHeader::exit_quality() const {
   return static_cast<uint32_t>(exit_quality_);
 }
 
+// Set the relative quality of exit signs for this tile.
+void GraphTileHeader::set_exit_quality(const uint32_t exit_quality) {
+  exit_quality_ = exit_quality;
+}
+
 // Get the count of nodes in the tile.
 uint32_t GraphTileHeader::nodecount() const {
   return nodecount_;
+}
+
+// Sets the number of nodes in this tile.
+void GraphTileHeader::set_nodecount(const uint32_t count) {
+  nodecount_ = count;
 }
 
 // Get the count of directed edges in the tile.
@@ -68,9 +105,19 @@ uint32_t GraphTileHeader::directededgecount() const {
   return directededgecount_;
 }
 
+// Sets the number of directed edges in this tile.
+void GraphTileHeader::set_directededgecount(const uint32_t count) {
+  directededgecount_ = count;
+}
+
 // Gets the number of signs in the tile.
 uint32_t GraphTileHeader::signcount() const {
   return signcount_;
+}
+
+// Sets the sign count.
+void GraphTileHeader::set_signcount(const uint32_t count) {
+  signcount_ = count;
 }
 
 // Gets the number of transit departures in this tile.
@@ -78,9 +125,19 @@ uint32_t GraphTileHeader::departurecount() const {
   return departurecount_;
 }
 
+// Sets the number of transit departures in this tile.
+void GraphTileHeader::set_departurecount(const uint32_t departures) {
+  departurecount_ = departures;
+}
+
 // Gets the number of transit stops in this tile.
 uint32_t GraphTileHeader::stopcount() const {
   return stopcount_;
+}
+
+// Sets the number of transit stops in this tile.
+void GraphTileHeader::set_stopcount(const uint32_t stops) {
+  stopcount_ = stops;
 }
 
 // Gets the number of transit routes in this tile.
@@ -88,9 +145,19 @@ uint32_t GraphTileHeader::routecount() const {
   return routecount_;
 }
 
+// Sets the number of transit routes in this tile.
+void GraphTileHeader::set_routecount(const uint32_t routes) {
+  routecount_ = routes;
+}
+
 // Gets the number of transit transfers in this tile.
 uint32_t GraphTileHeader::transfercount() const {
   return transfercount_;
+}
+
+// Sets the number of transit transfers in this tile.
+void GraphTileHeader::set_transfercount(const uint32_t transfers) {
+  transfercount_ = transfers;
 }
 
 // Gets the number of access restrictions in this tile.
@@ -98,9 +165,19 @@ uint32_t GraphTileHeader::access_restriction_count() const {
   return access_restriction_count_;
 }
 
+// Sets the number of restrictions in this tile.
+void GraphTileHeader::set_access_restriction_count(const uint32_t n) {
+  access_restriction_count_ = n;
+}
+
 // Gets the number of admins in the tile.
 uint32_t GraphTileHeader::admincount() const {
   return admincount_;
+}
+
+// Sets the admin count.
+void GraphTileHeader::set_admincount(const uint32_t count) {
+  admincount_ = count;
 }
 
 // Get the offset in bytes to the start of the edge information.
@@ -108,14 +185,34 @@ uint32_t GraphTileHeader::edgeinfo_offset() const {
   return edgeinfo_offset_;
 }
 
+// Sets the offset to the edge info.
+void GraphTileHeader::set_edgeinfo_offset(const uint32_t offset) {
+  edgeinfo_offset_ = offset;
+}
+
 // Get the offset in bytes to the start of the text / names list.
 uint32_t GraphTileHeader::textlist_offset() const {
   return textlist_offset_;
 }
 
+// Sets the offset to the text list.
+void GraphTileHeader::set_textlist_offset(const uint32_t offset) {
+  textlist_offset_ = offset;
+}
+
 // Get the offset in bytes to the complex restriction list.
 uint32_t GraphTileHeader::complex_restriction_offset() const {
   return complex_restriction_offset_;
+}
+
+// Sets the offset to the list of complex restrictions.
+void GraphTileHeader::set_complex_restriction_offset(const uint32_t offset) {
+  complex_restriction_offset_ = offset;
+}
+
+// Sets the edge bin offsets
+void GraphTileHeader::set_edge_cell_offsets(const uint32_t (&offsets)[kCellCount]) {
+  memcpy(cell_offsets_, offsets, sizeof(cell_offsets_));
 }
 
 // Get the offset to the given cell in the 5x5 grid.
--- valhalla/baldr/graphtileheader.h
+++ valhalla/baldr/graphtileheader.h
@@ -35,6 +35,12 @@ class GraphTileHeader {
   const baldr::GraphId& graphid() const;
 
   /**
+   * Set the graph Id of this tile.
+   * @param  graphid  GraphId (tileid and level) of this tile.
+   */
+  void set_graphid(const baldr::GraphId& graphid);
+
+  /**
    * Gets the date when this tile was created. Returns a Unix timestamp
    * (seconds since 1/1/1970)
    * @return  Returns the date this tile was created.
@@ -42,10 +48,22 @@ class GraphTileHeader {
   uint64_t date_created() const;
 
   /**
-   * Gets the version of this tile
+   * Set the date created.
+   * @param  date  Unix timestamp. Time in seconds since 1/1/1970.
+   */
+  void set_date_created(const uint64_t date);
+
+  /**
+   * Gets the version of this tile.
    * @return  Returns the version of this tile.
    */
   std::string version() const;
+
+  /**
+   * Set the version string.
+   * @param  version Version string.
+   */
+  void set_version(const std::string& version);
 
   /**
    * Get the relative road density within this tile.
@@ -54,10 +72,22 @@ class GraphTileHeader {
   uint32_t density() const;
 
   /**
+   * Set the relative road density within this tile.
+   * @param  density  Relative road density within this tile (0-15).
+   */
+  void set_density(const uint32_t density);
+
+  /**
    * Get the relative quality of name assignment for this tile.
    * @return  Returns relative name quality for this tile (0-15).
    */
   uint32_t name_quality() const;
+
+  /**
+   * Set the relative quality of name assignment for this tile.
+   * @param   name_quality  Relative name quality for this tile (0-15).
+   */
+  void set_name_quality(const uint32_t name_quality);
 
   /**
    * Get the relative quality of speed assignment for this tile.
@@ -66,10 +96,22 @@ class GraphTileHeader {
   uint32_t speed_quality() const;
 
   /**
+   * Set the relative quality of speed assignment for this tile.
+   * @param  speed_quality   Relative speed quality for this tile (0-15).
+   */
+  void set_speed_quality(const uint32_t speed_quality);
+
+  /**
    * Get the relative quality of exit signs for this tile.
    * @return  Returns relative exit sign quality for this tile (0-15).
    */
   uint32_t exit_quality() const;
+
+  /**
+   * Set the relative quality of exit signs for this tile.
+   * @param  exit_quality   Relative exit sign quality for this tile (0-15).
+   */
+  void set_exit_quality(const uint32_t exit_quality);
 
   /**
    * Gets the number of nodes in this tile.
@@ -78,10 +120,22 @@ class GraphTileHeader {
   uint32_t nodecount() const;
 
   /**
+   * Sets the number of nodes in this tile.
+   * @param  count  Number of nodes within the tile.
+   */
+  void set_nodecount(const uint32_t count);
+
+  /**
    * Gets the number of directed edges in this tile.
    * @return  Returns the number of directed edges.
    */
   uint32_t directededgecount() const;
+
+  /**
+   * Sets the number of directed edges in this tile.
+   * @param  count  Number of directed edges within the tile.
+   */
+  void set_directededgecount(const uint32_t count);
 
   /**
    * Gets the number of signs in this tile.
@@ -90,10 +144,22 @@ class GraphTileHeader {
   uint32_t signcount() const;
 
   /**
+   * Sets the number of signs within this tile.
+   * @param count Number of signs within the tile.
+   */
+  void set_signcount(const uint32_t count);
+
+  /**
    * Gets the number of transit departures in this tile.
    * @return  Returns the number of transit departures.
    */
   uint32_t departurecount() const;
+
+  /**
+   * Sets the number of transit departures in this tile.
+   * @param departures  The number of transit departures.
+   */
+  void set_departurecount(const uint32_t departures);
 
   /**
    * Gets the number of transit stops in this tile.
@@ -102,10 +168,22 @@ class GraphTileHeader {
   uint32_t stopcount() const;
 
   /**
+   * Sets the number of transit stops in this tile.
+   * @param  stops  The number of transit stops.
+   */
+  void set_stopcount(const uint32_t stops);
+
+  /**
    * Gets the number of transit routes in this tile.
    * @return  Returns the number of transit routes.
    */
   uint32_t routecount() const;
+
+  /**
+   * Sets the number of transit routes in this tile.
+   * @param  routes  The number of transit routes.
+   */
+  void set_routecount(const uint32_t routes);
 
   /**
    * Gets the number of transit transfers in this tile.
@@ -114,10 +192,22 @@ class GraphTileHeader {
   uint32_t transfercount() const;
 
   /**
+   * Sets the number of transit transfers in this tile.
+   * @param  transfers   The number of transit transfers.
+   */
+  void set_transfercount(const uint32_t transfers);
+
+  /**
    * Gets the number of access restrictions in this tile.
    * @return  Returns the number of restrictions.
    */
   uint32_t access_restriction_count() const;
+
+  /**
+   * Sets the number of access restrictions in this tile.
+   * @param  restrictions   The number of access restrictions.
+   */
+  void set_access_restriction_count(const uint32_t restrictions);
 
   /**
    * Gets the number of admin records in this tile.
@@ -126,16 +216,34 @@ class GraphTileHeader {
   uint32_t admincount() const;
 
   /**
+   * Sets the number of admin records within this tile.
+   * @param count Number of admin records within the tile.
+   */
+  void set_admincount(const uint32_t count);
+
+  /**
    * Gets the offset to the edge info.
    * @return  Returns the number of bytes to offset to the edge information.
    */
   uint32_t edgeinfo_offset() const;
 
   /**
+   * Sets the offset to the edge info.
+   * @param offset Offset in bytes to the start of the edge information.
+   */
+  void set_edgeinfo_offset(const uint32_t offset);
+
+  /**
    * Gets the offset to the text list.
    * @return  Returns the number of bytes to offset to the text list.
    */
   uint32_t textlist_offset() const;
+
+  /**
+   * Sets the offset to the text list.
+   * @param offset Offset in bytes to the start of the text list.
+   */
+  void set_textlist_offset(const uint32_t offset);
 
   /**
    * Get the offset to the administrative information.
@@ -152,6 +260,13 @@ class GraphTileHeader {
   uint32_t complex_restriction_offset() const;
 
   /**
+   * Sets the offset to the list of complex restrictions.
+   * @param offset Offset in bytes to the start of the complex restriction
+   *               list.
+   */
+  void set_complex_restriction_offset(const uint32_t offset);
+
+  /**
    * Get the offset to the given cell in the 5x5 grid, the cells contain
    * graphids for all the edges that intersect the cell
    * @param  column of the grid
@@ -159,6 +274,12 @@ class GraphTileHeader {
    * @return the begin and end offset in the list of edge ids
    */
   std::pair<uint32_t, uint32_t> cell_offset(size_t column, size_t row) const;
+
+  /**
+   * Sets the edge cell offsets
+   * @param the offsets
+   */
+  void set_edge_cell_offsets(const uint32_t (&offsets)[baldr::kCellCount]);
 
  protected:
   // GraphId (tileid and level) of this tile
