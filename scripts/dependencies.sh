8fbef0391dba69b5ebb44730b65714e167f415a7
Duane Gearhart <duane@mapzen.com>
2015-12-16 11:02:11 -0500

    add yaml parser dependency

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -5,7 +5,7 @@ export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":
 sudo apt-get update
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/ubuntu-toolchain-r-test-$(lsb_release -c -s).list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-sudo apt-get install -y autoconf automake libtool make gcc-4.9 g++-4.9 libboost1.54-dev libboost-program-options1.54-dev libboost-filesystem1.54-dev libboost-system1.54-dev libboost-thread1.54-dev lcov protobuf-compiler libprotobuf-dev lua5.2 liblua5.2-dev libsqlite3-dev libspatialite-dev libgeos-dev libcurl4-openssl-dev
+sudo apt-get install -y autoconf automake libtool make cmake gcc-4.9 g++-4.9 libboost1.54-dev libboost-program-options1.54-dev libboost-filesystem1.54-dev libboost-system1.54-dev libboost-thread1.54-dev lcov protobuf-compiler libprotobuf-dev lua5.2 liblua5.2-dev libsqlite3-dev libspatialite-dev libgeos-dev libcurl4-openssl-dev
 update-alternatives --remove-all gcc || true
 update-alternatives --remove-all g++ || true
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 90
@@ -22,6 +22,7 @@ wait
 #install the service deps in the background
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 $DIR/install_service_deps.sh &
+$DIR/install_yml_cpp.sh &
 
 #build sync
 for dep in midgard baldr skadi sif mjolnir; do
