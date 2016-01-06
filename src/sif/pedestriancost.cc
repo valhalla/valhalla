cc9f854ee1a55ca1207adc363b536f0613b28b2e
David Nesbitt <dave@mapzen.com>
2016-01-06 09:47:11 -0500

    Add 20 second penalty exiting a transit connection edge to prevent entering transit stops as a shortcut (without taking a transit line)

--- src/sif/pedestriancost.cc
+++ src/sif/pedestriancost.cc
@@ -303,7 +303,9 @@ Cost PedestrianCost::TransitionCost(const baldr::DirectedEdge* edge,
     return { step_penalty_, 0.0f };
   }
 
-  // No cost from transit connection
+  // Nominal cost from transit connection
+  // TODO - validate if this is needed to prevent going into transit
+  // stops (without boarding a transit line) as a shortcut
   if (pred.use() == Use::kTransitConnection) {
     return { 20.0f, 0.0f };
   }
