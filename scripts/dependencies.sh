6eb8ab57d434d606ad893d4535dac705df1d2fc9
Kevin Kreiser <kevinkreiser@gmail.com>
2016-01-12 10:03:23 -0500

    move thor tools to tools repo, remove conf

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -5,7 +5,7 @@ export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":
 sudo apt-get update
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/ubuntu-toolchain-r-test-$(lsb_release -c -s).list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-sudo apt-get install -y autoconf automake libtool make cmake gcc-4.9 g++-4.9 libboost1.54-dev libboost-program-options1.54-dev libboost-filesystem1.54-dev libboost-system1.54-dev libboost-thread1.54-dev lcov protobuf-compiler libprotobuf-dev lua5.2 liblua5.2-dev libsqlite3-dev libspatialite-dev libgeos-dev libcurl4-openssl-dev
+sudo apt-get install -y autoconf automake libtool make cmake gcc-4.9 g++-4.9 libboost1.54-dev libboost-program-options1.54-dev libboost-filesystem1.54-dev libboost-system1.54-dev libboost-thread1.54-dev lcov protobuf-compiler libprotobuf-dev libcurl4-openssl-dev
 update-alternatives --remove-all gcc || true
 update-alternatives --remove-all g++ || true
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 90
@@ -13,7 +13,7 @@ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
 
 #clone async
 mkdir -p deps
-for dep in midgard baldr skadi sif mjolnir loki odin; do
+for dep in midgard baldr sif odin; do
 	rm -rf $dep
 	git clone --depth=1 --recurse-submodules --single-branch --branch=master https://github.com/valhalla/$dep.git deps/$dep &
 done
@@ -25,7 +25,7 @@ $DIR/install_service_deps.sh &
 $DIR/install_yml_cpp.sh &
 
 #build sync
-for dep in midgard baldr skadi sif mjolnir; do
+for dep in midgard baldr sif; do
 	pushd deps/$dep
 	./autogen.sh
 	./configure CPPFLAGS=-DBOOST_SPIRIT_THREADSAFE
@@ -36,7 +36,7 @@ done
 wait
 
 #build sync
-for dep in loki odin; do
+for dep in odin; do
         pushd deps/$dep
         ./autogen.sh
         ./configure CPPFLAGS=-DBOOST_SPIRIT_THREADSAFE
