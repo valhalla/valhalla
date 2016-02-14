e2142187d371b86c8aee5980b8361a839bac2973
Tao Peng <ptpttt@gmail.com>
2016-02-14 15:43:17 +0100

    Run ldconfig after installation to update libarary cache

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,3 +6,4 @@ export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":
 ./configure --enable-coverage CPPFLAGS=-DBOOST_SPIRIT_THREADSAFE
 make CPPFLAGS=-DNDEBUG CXXFLAGS="-std=c++11 -O3" test -j4
 sudo make install
+sudo ldconfig
