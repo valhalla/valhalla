d807fa24d07054a53f2f6c277cba63517635da35
Tao Peng <ptpttt@gmail.com>
2016-02-25 23:44:04 +0100

    Update config from valhalla/conf

--- conf/generate_mm.sh
+++ conf/generate_mm.sh
@@ -16,7 +16,7 @@ fi
 
 # 2. Install jq at https://stedolan.github.io/jq/
 
-# 3. Add mjolnir.hierarchy and costing_options from Valhalla
+# 3. Add `mjolnir` and `costing_options` from Valhalla
 #    configuration to mm.partial.json to generate mm.json:
-jq -s '.[1].mjolnir.hierarchy = .[0].mjolnir.hierarchy | .[1].costing_options = .[0].costing_options | .[1]' \
+jq -s '.[1].mjolnir = .[0].mjolnir | .[1].costing_options = .[0].costing_options | .[1]' \
    conf/valhalla.json mm.partial.json > mm.json
