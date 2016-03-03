02198aec405316e3b5a1ca060422ad4dc04cf671
kdiluca <kristen@mapzen.com>
2016-03-03 14:33:51 -0500

    Revert "updated for DNT and locking prime_server at 0.3.2"
    This reverts commit b03db62251928c4a8800b496c38458322b5395f6.

--- scripts/install_service_deps.sh
+++ scripts/install_service_deps.sh
@@ -14,9 +14,8 @@ popd
 
 # grab experimental zmq-based server API:
 rm -rf prime_server
-wget -O prime_server0.3.2.tar.gz https://github.com/kevinkreiser/prime_server/archive/0.3.2.tar.gz
-tar pxvf prime_server0.3.2.tar.gz
-pushd prime_server-0.3.2
+git clone --depth=1 --recurse-submodules --single-branch --branch=master https://github.com/kevinkreiser/prime_server.git
+pushd prime_server
 ./autogen.sh
 ./configure
 make -j4
