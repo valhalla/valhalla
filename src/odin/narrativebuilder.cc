eafa779725743b8f02321b147af31427e97a4cab
Duane Gearhart <duane@mapzen.com>
2016-03-30 10:13:57 -0400

    Added post_transit_connection_destination phrases to en-US

--- src/odin/narrativebuilder.cc
+++ src/odin/narrativebuilder.cc
@@ -3094,9 +3094,9 @@ std::string NarrativeBuilder::FormVerbalTransitTransferInstruction(
 
 std::string NarrativeBuilder::FormPostTransitConnectionDestinationInstruction(
     Maneuver& maneuver) {
-  // 0 "Head <FormCardinalDirection>."
-  // 1 "Head <FormCardinalDirection> on <STREET_NAMES>."
-  // 2 "Head <FormCardinalDirection> on <BEGIN_STREET_NAMES>. Continue on <STREET_NAMES>."
+  // "0": "Head <CARDINAL_DIRECTION>.",
+  // "1": "Head <CARDINAL_DIRECTION> on <STREET_NAMES>.",
+  // "2": "Head <CARDINAL_DIRECTION> on <BEGIN_STREET_NAMES>. Continue on <STREET_NAMES>."
 
   // Assign the street names and the begin street names
   std::string street_names = FormOldStreetNames(maneuver, maneuver.street_names(),
@@ -3142,9 +3142,9 @@ std::string NarrativeBuilder::FormPostTransitConnectionDestinationInstruction(
 
 std::string NarrativeBuilder::FormVerbalPostTransitConnectionDestinationInstruction(
     Maneuver& maneuver, uint32_t element_max_count, std::string delim) {
-  // 0 "Head <FormCardinalDirection>."
-  // 1 "Head <FormCardinalDirection> on <STREET_NAMES>."
-  // 2 "Head <FormCardinalDirection> on <BEGIN_STREET_NAMES>. Continue on <STREET_NAMES>."
+  // "0": "Head <CARDINAL_DIRECTION>.",
+  // "1": "Head <CARDINAL_DIRECTION> on <STREET_NAMES>.",
+  // "2": "Head <CARDINAL_DIRECTION> on <BEGIN_STREET_NAMES>."
 
   // Assign the street names and the begin street names
   std::string street_names = FormOldStreetNames(maneuver, maneuver.street_names(),
