e4d249fa2d979831c0ffaf106f931c44c4123a33
Kevin Kreiser <kevinkreiser@gmail.com>
2016-04-14 10:33:09 -0400

    dont need this anymore its in midgard

--- version.sh
+++ version.sh
@@ -2,18 +2,16 @@
 
 #grabs the version string from the header file for use autotools
 #which then throws it into a pc file so that pkg-config can use it
+DIR=$(pkg-config --cflags libvalhalla_midgard | tr ' ' '\n' | grep -F -- "-I" | sed -e "s/^-I//g")
 
-DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-DIR=$(echo ${DIR} | awk -F'/' '{print $(NF)}')
-
-if [ ! -f valhalla/$DIR/version.h ]; then
+if [ ! -f ${DIR}/valhalla/version.h ]; then
 	echo "Could not find version information"
 	exit 1
 fi
 
-major=$(grep -m1 -F VERSION_MAJOR valhalla/$DIR/version.h | sed -e "s/.*MAJOR \([0-9]\+\)/\1/g")
-minor=$(grep -m1 -F VERSION_MINOR valhalla/$DIR/version.h | sed -e "s/.*MINOR \([0-9]\+\)/\1/g")
-patch=$(grep -m1 -F VERSION_PATCH valhalla/$DIR/version.h | sed -e "s/.*PATCH \([0-9]\+\)/\1/g")
+major=$(grep -m1 -F VERSION_MAJOR ${DIR}/valhalla/version.h | sed -e "s/.*MAJOR \([0-9]\+\)/\1/g")
+minor=$(grep -m1 -F VERSION_MINOR ${DIR}/valhalla/version.h | sed -e "s/.*MINOR \([0-9]\+\)/\1/g")
+patch=$(grep -m1 -F VERSION_PATCH ${DIR}/valhalla/version.h | sed -e "s/.*PATCH \([0-9]\+\)/\1/g")
 
 if [ -z $major ] || [ -z $minor ] || [ -z $patch ]; then
 	echo "Malformed version information"
