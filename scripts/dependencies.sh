09ad4a33fdd6252addcf0a1df0041b778352444f
Kevin Kreiser <kevinkreiser@gmail.com>
2016-04-14 11:04:14 -0400

    too much on that regex

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -33,8 +33,8 @@ for dep in midgard baldr; do
 	pushd $dep
 	./autogen.sh
 	./configure CPPFLAGS=-DBOOST_SPIRIT_THREADSAFE
-	make pkg-config -j4
-	sudo make pkg-config install
+	make -j4
+	sudo make install
 	popd
 done
 wait
@@ -44,8 +44,8 @@ for dep in odin; do
         pushd $dep
         ./autogen.sh
         ./configure CPPFLAGS=-DBOOST_SPIRIT_THREADSAFE
-        make pkg-config -j4
-        sudo make pkg-config install
+        make -j4
+        sudo make install
         popd
 done
 
