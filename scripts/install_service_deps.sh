971ad7d233fb62f7a12b1e29bb22057caf2870a0
Kevin Kreiser <kevinkreiser@gmail.com>
2016-04-15 11:25:58 -0400

    refer to the tag by name

--- scripts/install_service_deps.sh
+++ scripts/install_service_deps.sh
@@ -13,9 +13,8 @@ popd
 
 # grab experimental zmq-based server API:
 rm -rf prime_server
-git clone --depth=1 --recurse-submodules --single-branch --branch=master https://github.com/kevinkreiser/prime_server.git
+git clone --depth=1 --single-branch --branch 0.3.2 --recursive https://github.com/kevinkreiser/prime_server.git
 pushd prime_server
-git checkout 9564abc58f13740cfefa73d98bf86138833a7777
 ./autogen.sh
 ./configure
 make -j4
