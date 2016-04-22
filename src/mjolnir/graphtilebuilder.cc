7675283f813b3eafa8fac7aefe7413cede48ecfd
gknisely <greg@mapzen.com>
2016-04-22 10:46:59 -0400

    exclude transitconnections.

--- src/mjolnir/graphtilebuilder.cc
+++ src/mjolnir/graphtilebuilder.cc
@@ -678,7 +678,7 @@ std::array<std::vector<GraphId>, kBinCount> GraphTileBuilder::BinEdges(const Til
   const auto* start_edge = tile->directededge(0);
   for(const DirectedEdge* edge = start_edge; edge < start_edge + tile->header()->directededgecount(); ++edge) {
     //dont bin these
-    if(edge->is_shortcut() || edge->trans_up() || edge->trans_down())
+    if(edge->is_shortcut() || edge->trans_up() || edge->trans_down() || edge->use() == Use::kTransitConnection)
       continue;
 
     //already binned this
