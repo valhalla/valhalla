635b9d3ea083a66a87a76d8fa48039d07cf4dc69
Kevin Kreiser <kevinkreiser@gmail.com>
2016-04-28 14:44:30 -0400

    yeah no regex in gcc 4.8

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,4 +2,10 @@
 set -e
 
 export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":" | sed -e "s/:$//g"`
-sudo apt-get install -y autoconf automake pkg-config libtool make pkg-config gcc g++ lcov
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/ubuntu-toolchain-r-test-$(lsb_release -c -s).list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+sudo apt-get install -y autoconf automake pkg-config libtool make pkg-config gcc-4.9 g++-4.9 lcov
+update-alternatives --remove-all gcc || true
+update-alternatives --remove-all g++ || true
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 90
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
