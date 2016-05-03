7157fa8fc217476cf2ab12eba001c4aacf52c86c
Kevin Kreiser <kevinkreiser@gmail.com>
2016-05-03 16:06:23 -0400

    didnt delete enough

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -5,10 +5,6 @@ export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":
 sudo add-apt-repository -y ppa:kevinkreiser/prime-server
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/kevinkreiser-prime-server-$(lsb_release -c -s).list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 sudo apt-get install -y autoconf automake pkg-config libtool make pkg-config cmake pkg-config gcc g++ lcov libboost1.54-all-dev protobuf-compiler libprotobuf-dev libprime-server-dev
-update-alternatives --remove-all gcc || true
-update-alternatives --remove-all g++ || true
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 90
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
 
 #clone async
 mkdir -p deps
