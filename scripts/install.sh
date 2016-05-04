3a258541a5bc0ab1acbfdc2646060298033baeb3
Tao Peng <ptpttt@gmail.com>
2016-05-04 12:13:30 +0200

    Pass $nproc to specify the number of make jobs

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,6 @@ set -e
 export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":" | sed -e "s/:$//g"`
 ./autogen.sh
 ./configure --enable-coverage CPPFLAGS=-DBOOST_SPIRIT_THREADSAFE
-make CPPFLAGS=-DNDEBUG CXXFLAGS="-std=c++11 -O3" test -j4
+make CPPFLAGS=-DNDEBUG CXXFLAGS="-std=c++11 -O3" test -j$(nproc)
 sudo make install
 sudo ldconfig
