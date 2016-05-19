7e35e8351c9ad719c3b13705830ed49af8ddcbca
Kevin Kreiser <kevinkreiser@gmail.com>
2016-05-19 15:51:24 -0400

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -16,7 +16,6 @@ fi
 #clone async
 mkdir -p deps
 for dep in midgard baldr; do
-	rm -rf $dep
 	git clone --depth=1 --recurse --single-branch https://github.com/valhalla/$dep.git deps/$dep && \
 	pushd deps/$dep && \
 	git fetch --depth=1 origin 'refs/tags/*:refs/tags/*' && \
