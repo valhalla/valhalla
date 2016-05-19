2e519e0172a12db50abb9b92af10ae26e2dc5eae
Kevin Kreiser <kevinkreiser@gmail.com>
2016-05-19 16:02:43 -0400

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -8,19 +8,15 @@ sudo add-apt-repository -y ppa:kevinkreiser/prime-server
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/kevinkreiser-prime-server-$(lsb_release -c -s).list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 sudo apt-get install -y autoconf automake pkg-config libtool make pkg-config gcc g++ lcov libboost1.54-all-dev protobuf-compiler libprotobuf-dev libprime-server-dev
 
-if [ -n "$1" ] && [ -d "$1" ]; then
-        pushd "$1"
-else
-        pushd .
-fi
-
-#clone
+#clone async
+mkdir -p deps
 for dep in midgard baldr odin; do
 	git clone --depth=1 --recurse --single-branch https://github.com/valhalla/$dep.git deps/$dep && \
 	pushd deps/$dep && \
 	git fetch --depth=1 origin 'refs/tags/*:refs/tags/*' && \
 	popd &
 done
+wait
 
 #build sync
 for dep in midgard baldr odin; do
@@ -31,5 +27,3 @@ for dep in midgard baldr odin; do
 	sudo make install
 	popd
 done
-
-popd
