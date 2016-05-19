8536d6ebd675df1d75d3d6aaf6398b4708e21477
Kevin Kreiser <kevinkreiser@gmail.com>
2016-05-19 16:24:26 -0400

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -4,11 +4,22 @@ set -e
 export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":" | sed -e "s/:$//g"`
 sudo apt-get install -y autoconf automake pkg-config libtool make pkg-config gcc g++ lcov libboost1.54-all-dev protobuf-compiler libprotobuf-dev lua5.2 liblua5.2-dev libsqlite3-dev libspatialite-dev libgeos-dev libgeos++-dev libcurl4-openssl-dev
 
-#skadi has everything
+#clone async
 mkdir -p deps
-rm -rf deps/skadi
-git clone --depth=1 --recurse-submodules --single-branch --branch=master https://github.com/valhalla/skadi.git deps/skadi
-pushd deps/skadi
-scripts/dependencies.sh
-scripts/install.sh
-popd
+for dep in midgard baldr skadi; do
+	git clone --depth=1 --recurse --single-branch https://github.com/valhalla/$dep.git deps/$dep && \
+	pushd deps/$dep && \
+	git fetch --depth=1 origin 'refs/tags/*:refs/tags/*' && \
+	popd &
+done
+wait
+
+#build sync
+for dep in midgard baldr skadi; do
+	pushd deps/$dep
+	./autogen.sh
+	./configure CPPFLAGS="-DBOOST_SPIRIT_THREADSAFE -DBOOST_NO_CXX11_SCOPED_ENUMS"
+	make -j4
+	sudo make install
+	popd
+done
