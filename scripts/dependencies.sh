6469d49996ac4c4e1a49ff29a7acca7509a08f13
Kevin Kreiser <kevinkreiser@gmail.com>
2016-05-20 09:00:23 -0400

    no need to fetch dependency tags

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -9,10 +9,7 @@ sudo apt-get install -y autoconf automake pkg-config libtool make pkg-config gcc
 #clone async
 mkdir -p deps
 for dep in midgard baldr sif; do
-	git clone --depth=1 --recurse --single-branch https://github.com/valhalla/$dep.git deps/$dep && \
-	pushd deps/$dep && \
-	git fetch --depth=1 origin 'refs/tags/*:refs/tags/*' && \
-	popd &
+	git clone --depth=1 --recurse --single-branch https://github.com/valhalla/$dep.git deps/$dep &
 done
 wait
 
