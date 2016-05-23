75fda40fc7096d7cc57d6f2f34b8b675d7527e55
Kevin Kreiser <kevinkreiser@gmail.com>
2016-05-23 11:46:05 -0400

    move headers

---
+++ valhalla/meili/geometry_helpers.h
@@ -0,0 +1,259 @@
+// -*- mode: c++ -*-
+#ifndef MMP_GEOMETRY_HELPERS_H_
+#define MMP_GEOMETRY_HELPERS_H_
+
+#include <algorithm>
+#include <cassert>
+
+#include <valhalla/midgard/aabb2.h>
+#include <valhalla/midgard/constants.h>
+#include <valhalla/midgard/distanceapproximator.h>
+#include <valhalla/midgard/pointll.h>
+
+
+namespace {
+
+inline float
+normalize(float num, float den)
+{
+  assert (num >= 0.f && den >= 0.f);
+  return den > 0.f? std::min(std::max(num / den, 0.f), 1.f) : 0.f;
+}
+
+}
+
+
+namespace mmp {
+namespace helpers {
+
+using namespace valhalla;
+
+
+template <typename coord_t>
+inline coord_t
+LocateAlong(const coord_t& start, const coord_t& end, float offset)
+{
+  return {start.x() + (end.x() - start.x()) * offset,
+        start.y() + (end.y() - start.y()) * offset};
+}
+
+
+template <typename iterator_t>
+float LineStringLength(const iterator_t& begin,
+                       const iterator_t& end)
+{
+  if (begin == end) {
+    return 0.f;
+  }
+
+  float length = 0.f;
+  for (auto vertex = std::next(begin); vertex != end; vertex++) {
+    length += std::prev(vertex)->Distance(*vertex);
+  }
+
+  return length;
+}
+
+
+template <typename iterator_t>
+std::vector<typename iterator_t::value_type>
+ClipLineString(const iterator_t& begin, const iterator_t& end,
+               float source, float target)
+{
+  if (target < source || target < 0.f || 1.f < source || begin == end) {
+    return {};
+  }
+
+  source = std::min(std::max(source, 0.f), 1.f);
+  target = std::min(std::max(target, 0.f), 1.f);
+
+  std::vector<typename iterator_t::value_type> clip;
+  bool open = false;
+
+  float total_length = LineStringLength(begin, end),
+  prev_vertex_length = 0.f,
+       source_length = total_length * source,
+       target_length = total_length * target;
+
+  iterator_t prev_vertex = begin;
+  for (auto vertex = std::next(begin); vertex != end; vertex++) {
+    auto segment_length = prev_vertex->Distance(*vertex),
+          vertex_length = prev_vertex_length + segment_length;
+
+    if (!open && source_length < vertex_length) {
+      auto offset = normalize(source_length - prev_vertex_length, segment_length);
+      assert(0.f <= offset && offset <= 1.f);
+      clip.push_back(LocateAlong(*prev_vertex, *vertex, offset));
+      open = true;
+    }
+
+    if (open && target_length < vertex_length) {
+      auto offset = normalize(target_length - prev_vertex_length, segment_length);
+      assert(0.f <= offset && offset <= 1.f);
+      clip.push_back(LocateAlong(*prev_vertex, *vertex, offset));
+      open = false;
+      break;
+    }
+
+    if (open) {
+      clip.push_back(*vertex);
+    }
+
+    prev_vertex = vertex;
+    prev_vertex_length = vertex_length;
+  }
+
+  if (clip.empty()) {
+    assert(1.f == source && 1.f == target);
+    clip.push_back(*prev_vertex);
+    clip.push_back(*prev_vertex);
+  }
+
+  assert(clip.size() != 1);
+  return clip;
+}
+
+
+inline uint8_t
+get_turn_degree180(uint16_t left, uint16_t right)
+{
+  const auto turn = std::abs(left - right);
+  assert(0 <= turn && turn < 360);
+  return turn > 180? 360 - turn : turn;
+}
+
+
+inline float
+TranslateLatitudeInMeters(float lat, float meters)
+{
+  float offset = meters / midgard::kMetersPerDegreeLat;
+  return lat + offset;
+}
+
+
+inline float
+TranslateLatitudeInMeters(const midgard::PointLL& lnglat, float meters)
+{ return TranslateLatitudeInMeters(lnglat.lat(), meters); }
+
+
+inline float
+TranslateLongitudeInMeters(const midgard::PointLL& lnglat, float meters)
+{
+  float offset = meters / midgard::DistanceApproximator::MetersPerLngDegree(lnglat.lat());
+  return lnglat.lng() + offset;
+}
+
+
+inline midgard::AABB2<midgard::PointLL>
+ExpandMeters(const midgard::AABB2<midgard::PointLL>& bbox, float meters)
+{
+  if (meters < 0.f) {
+    throw std::runtime_error("Expect non-negative meters");
+  }
+
+  midgard::PointLL minpt(TranslateLongitudeInMeters(bbox.minpt(), -meters),
+                         TranslateLatitudeInMeters(bbox.minpt(), -meters));
+  midgard::PointLL maxpt(TranslateLongitudeInMeters(bbox.maxpt(), meters),
+                         TranslateLatitudeInMeters(bbox.maxpt(), meters));
+  return {minpt, maxpt};
+}
+
+
+inline midgard::AABB2<midgard::PointLL>
+ExpandMeters(const midgard::PointLL& pt, float meters)
+{
+  if (meters < 0.f) {
+    throw std::runtime_error("Expect non-negative meters");
+  }
+
+  midgard::PointLL minpt(TranslateLongitudeInMeters(pt, -meters),
+                         TranslateLatitudeInMeters(pt, -meters));
+  midgard::PointLL maxpt(TranslateLongitudeInMeters(pt, meters),
+                         TranslateLatitudeInMeters(pt, meters));
+  return {minpt, maxpt};
+}
+
+
+// snapped point, sqaured distance, segment index, offset
+template <typename coord_t>
+std::tuple<coord_t, float, typename std::vector<coord_t>::size_type, float>
+Project(const coord_t& p,
+        const typename std::vector<coord_t>& shape,
+        const midgard::DistanceApproximator& approximator,
+        float snap_distance = 0.f)
+{
+  if (shape.empty()) {
+    throw std::invalid_argument("Got empty shape");
+  }
+
+  coord_t closest_point(shape.front());
+  float closest_distance = approximator.DistanceSquared(closest_point);
+  decltype(shape.size()) closest_segment = 0;
+  float closest_partial_length = 0.f;
+  float total_length = 0.f;
+
+  //for each segment
+  for(decltype(shape.size()) i = 0; i < shape.size() - 1; ++i) {
+    //project a onto b where b is the origin vector representing this segment
+    //and a is the origin vector to the point we are projecting, (a.b/b.b)*b
+    const auto& u = shape[i];
+    const auto& v = shape[i + 1];
+    auto bx = v.first - u.first;
+    auto by = v.second - u.second;
+    auto sq = bx*bx + by*by;
+    const auto scale = sq > 0? (((p.first - u.first)*bx + (p.second - u.second)*by) / sq) : 0.f;
+    //projects along the ray before u
+    if (scale <= 0.f) {
+      bx = u.first;
+      by = u.second;
+    }//projects along the ray after v
+    else if (scale >= 1.f) {
+      bx = v.first;
+      by = v.second;
+    }//projects along the ray between u and v
+    else {
+      bx = bx*scale + u.first;
+      by = by*scale + u.second;
+    }
+    //check if this point is better
+    coord_t point(bx, by);
+    const auto distance = approximator.DistanceSquared(point);
+    if (distance < closest_distance) {
+      closest_point = std::move(point);
+      closest_distance = distance;
+      closest_segment = i;
+      closest_partial_length = total_length;
+    }
+
+    //total edge length
+    total_length += u.Distance(v);
+  }
+
+  // Offset is a float between 0 and 1 representing the location of
+  // the closest point on LineString to the given Point, as a fraction
+  // of total 2d line length.
+  closest_partial_length += shape[closest_segment].Distance(closest_point);
+  float offset = total_length > 0.f? static_cast<float>(closest_partial_length / total_length) : 0.f;
+  offset = std::max(0.f, std::min(offset, 1.f));
+
+  // Snapp to vertexes if it's close
+  if (total_length * offset <= snap_distance) {
+    closest_point = shape.front();
+    closest_distance = approximator.DistanceSquared(closest_point);
+    closest_segment = 0;
+    offset = 0.f;
+  } else if (total_length * (1.f - offset) <= snap_distance) {
+    closest_point = shape.back();
+    closest_distance = approximator.DistanceSquared(closest_point);
+    closest_segment = shape.size() - 1;
+    offset = 1.f;
+  }
+
+  return std::make_tuple(std::move(closest_point), closest_distance, closest_segment, offset);
+}
+
+}
+}
+
+
+#endif // MMP_GEOMETRY_HELPERS_H_
---
+++ valhalla/meili/viterbi_search.h
@@ -0,0 +1,736 @@
+// -*- mode: c++ -*-
+#ifndef MMP_VITERBI_SEARCH_H_
+#define MMP_VITERBI_SEARCH_H_
+
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <cassert>
+
+#include <mmp/priority_queue.h>
+
+
+namespace mmp {
+
+using Time = uint32_t;
+using StateId = uint32_t;
+
+constexpr Time kInvalidTime = std::numeric_limits<Time>::max();
+constexpr StateId kInvalidStateId = std::numeric_limits<StateId>::max();
+
+
+template <typename T>
+struct LabelTemplate: public LabelInterface<StateId>
+{
+  LabelTemplate(double c, const T* a, const T* p)
+      : costsofar(c), state(a), predecessor(p) {}
+
+  StateId id() const override
+  { return state->id(); }
+
+  double sortcost() const override
+  { return costsofar; }
+
+  // Accumulated cost since time = 0
+  double costsofar;
+
+  const T* state;
+
+  const T* predecessor;
+};
+
+
+template <typename T>
+class IViterbiSearch;
+
+
+template <typename T>
+class StateIterator:
+    public std::iterator<std::forward_iterator_tag, T>
+{
+ public:
+  StateIterator(IViterbiSearch<T>* vs, StateId id, Time time)
+      : vs_(vs), id_(id), time_(time) {}
+
+  StateIterator(IViterbiSearch<T>* vs): vs_(vs)
+  { set_end(); }
+
+  // Postfix increment
+  StateIterator operator++(int)
+  {
+    if (!is_end()) {
+      auto copy = *this;
+      goback();
+      return copy;
+    }
+    return *this;
+  }
+
+  // Prefix increment
+  StateIterator<T> operator++()
+  {
+    if (!is_end()) {
+      goback();
+    }
+    return *this;
+  }
+
+  bool operator==(const StateIterator<T>& other) const
+  { return id_ == other.id_ && time_ == other.time_ && vs_ == other.vs_; }
+
+  bool operator!=(const StateIterator<T>& other) const
+  { return !(*this == other); }
+
+  // Derefrencnce
+  const T& operator*() const
+  { return vs_->state(id_); }
+
+  // Pointer dereference
+  const T* operator->() const
+  { return &(vs_->state(id_)); }
+
+  // Invalid iterator can't be dereferenced. Invalid iterator can be
+  // used to indicate that no state has been found at a time
+
+  // TODO give it a meaningful name
+  bool IsValid() const
+  { return id_ != kInvalidStateId; }
+
+ private:
+  IViterbiSearch<T>* vs_;
+
+  StateId id_;
+
+  Time time_;
+
+  bool is_end() const
+  { return id_ == kInvalidStateId && time_ == kInvalidTime; }
+
+  void set_end()
+  {
+    id_ = kInvalidStateId;
+    time_ = kInvalidTime;
+  }
+
+  void goback()
+  {
+    if (time_ > 0) {
+      id_ = vs_->predecessor(id_);
+      time_ --;
+      if (id_ == kInvalidStateId) {
+        id_ = vs_->SearchWinner(time_);
+      }
+      // If id_ is valid then assert(vs_->state(id_).time() == time_)
+      assert(id_ == kInvalidStateId || vs_->state(id_).time() == time_);
+    } else {
+      set_end();
+    }
+  }
+};
+
+
+template <typename T>
+class IViterbiSearch
+{
+  friend class StateIterator<T>;
+
+ public:
+  using state_iterator = StateIterator<T>;
+
+  IViterbiSearch(): path_end_(state_iterator(this)) {};
+
+  virtual ~IViterbiSearch() {};
+
+  virtual StateId SearchWinner(Time time) = 0;
+
+  state_iterator SearchPath(Time time)
+  { return state_iterator(this, SearchWinner(time), time); }
+
+  state_iterator PathEnd() const
+  { return path_end_; }
+
+  virtual StateId predecessor(StateId id) const = 0;
+
+  // Get the state reference given its ID
+  virtual const T& state(StateId id) const = 0;
+
+  virtual double AccumulatedCost(const StateId id) const = 0;
+
+  virtual double AccumulatedCost(const T& state) const
+  { return AccumulatedCost(state.id()); }
+
+ protected:
+  // Calculate transition cost from left state to right state
+  virtual float TransitionCost(const T& left, const T& right) const = 0;
+
+  // Calculate emission cost of a state
+  virtual float EmissionCost(const T& state) const = 0;
+
+  // Calculate the a state's costsofar based on its predecessor's
+  // costsofar, transition cost from predecessor to this state,
+  // and emission cost of this state
+  virtual double CostSofar(double prev_costsofar,
+                           float transition_cost,
+                           float emission_cost) const = 0;
+
+ private:
+  const state_iterator path_end_;
+};
+
+
+template <typename T, bool Maximize>
+class NaiveViterbiSearch: public IViterbiSearch<T>
+{
+ public:
+  // An invalid costsofar indicates that a state is unreachable
+  static constexpr double
+  kInvalidCost = Maximize? -std::numeric_limits<double>::infinity()
+      : std::numeric_limits<double>::infinity();
+
+  ~NaiveViterbiSearch();
+
+  void Clear();
+
+  StateId SearchWinner(Time time) override;
+
+  StateId predecessor(StateId id) const override;
+
+  const T& state(StateId id) const override;
+
+  double AccumulatedCost(const StateId id) const override;
+
+  double AccumulatedCost(const T& state) const override;
+
+ protected:
+  std::vector<std::vector<const T*>> states_;
+
+  std::vector<const T*> state_;
+
+  std::vector<const T*> winner_;
+
+  virtual float TransitionCost(const T& left, const T& right) const override = 0;
+
+  virtual float EmissionCost(const T& state) const override = 0;
+
+  virtual double CostSofar(double prev_costsofar, float transition_cost, float emission_cost) const override = 0;
+
+ private:
+  using label_type = LabelTemplate<T>;
+
+  std::vector<std::vector<label_type>> history_;
+
+  void UpdateLabels(std::vector<label_type>& labels,
+                    const std::vector<label_type>& prev_labels) const;
+
+  std::vector<label_type> InitLabels(const std::vector<const T*>& column,
+                                     bool use_emission_cost) const;
+
+  const T* FindWinner(const std::vector<label_type>& labels) const;
+
+  const label_type& label(const T& state) const;
+};
+
+
+template <typename T, bool Maximize>
+inline NaiveViterbiSearch<T, Maximize>::~NaiveViterbiSearch()
+{ Clear(); }
+
+
+template <typename T, bool Maximize>
+void NaiveViterbiSearch<T, Maximize>::Clear()
+{
+  history_.clear();
+  states_.clear();
+  winner_.clear();
+  for (auto state : state_) {
+    delete state;
+  }
+  state_.clear();
+}
+
+
+template <typename T, bool Maximize>
+inline double
+NaiveViterbiSearch<T, Maximize>::AccumulatedCost(const StateId id) const
+{ return id != kInvalidStateId? AccumulatedCost(state(id)) : kInvalidCost; }
+
+
+template <typename T, bool Maximize>
+inline double
+NaiveViterbiSearch<T, Maximize>::AccumulatedCost(const T& state) const
+{ return label(state).costsofar; }
+
+
+template <typename T, bool Maximize>
+StateId NaiveViterbiSearch<T, Maximize>::SearchWinner(Time target)
+{
+  if (states_.size() <= target) {
+    return kInvalidStateId;
+  }
+
+  // Use the cache
+  if (target < winner_.size()) {
+    return winner_[target]? winner_[target]->id() : kInvalidStateId;
+  }
+
+  for (Time time = winner_.size(); time <= target; ++time) {
+    const auto& column = states_[time];
+    std::vector<label_type> labels;
+
+    // Update labels
+    if (time == 0) {
+      labels = InitLabels(column, true);
+    } else {
+      labels = InitLabels(column, false);
+      UpdateLabels(labels, history_.back());
+    }
+    assert(labels.size() == column.size());
+
+    auto winner = FindWinner(labels);
+    if (!winner && time > 0) {
+      // If it's not reachable by prevous column, we find the winner
+      // with the best emission cost only
+      labels = InitLabels(column, true);
+      winner = FindWinner(labels);
+    }
+    winner_.push_back(winner);
+    history_.push_back(labels);
+  }
+
+  return winner_[target]? winner_[target]->id() : kInvalidStateId;
+}
+
+
+template <typename T, bool Maximize>
+inline StateId
+NaiveViterbiSearch<T, Maximize>::predecessor(StateId id) const
+{
+  if (id != kInvalidStateId) {
+    const auto predecessor = label(state(id)).predecessor;
+    return predecessor? predecessor->id() : kInvalidStateId;
+  }
+  return kInvalidStateId;
+}
+
+
+template <typename T, bool Maximize>
+inline const T&
+NaiveViterbiSearch<T, Maximize>::state(StateId id) const
+{ return *state_[id]; }
+
+
+template <typename T, bool Maximize>
+void NaiveViterbiSearch<T, Maximize>::UpdateLabels(
+    std::vector<label_type>& labels,
+    const std::vector<label_type>& prev_labels) const
+{
+  for (const auto& prev_label : prev_labels) {
+    auto prev_state = prev_label.state;
+
+    auto prev_costsofar = prev_label.costsofar;
+    if (kInvalidCost == prev_costsofar) {
+      continue;
+    }
+
+    for (auto& label : labels) {
+      auto state = label.state;
+
+      auto emission_cost = EmissionCost(*state);
+      if (kInvalidCost == emission_cost) {
+        continue;
+      };
+
+      auto transition_cost = TransitionCost(*prev_state, *state);
+      if (kInvalidCost == transition_cost) {
+        continue;
+      }
+
+      auto costsofar = CostSofar(prev_costsofar, transition_cost, emission_cost);
+      if (kInvalidCost == costsofar) {
+        continue;
+      }
+
+      if (Maximize) {
+        label = std::max(label_type(costsofar, state, prev_state), label);
+      } else {
+        label = std::min(label_type(costsofar, state, prev_state), label);
+      }
+    }
+  }
+}
+
+
+template <typename T, bool Maximize>
+std::vector<typename NaiveViterbiSearch<T, Maximize>::label_type>
+NaiveViterbiSearch<T, Maximize>::InitLabels(
+    const std::vector<const T*>& column,
+    bool use_emission_cost) const
+{
+  std::vector<label_type> labels;
+  for (const auto state : column) {
+    auto initial_cost = use_emission_cost? EmissionCost(*state) : kInvalidCost;
+    labels.emplace_back(initial_cost, state, nullptr);
+  }
+  return labels;
+}
+
+
+template <typename U, bool Maximize>
+U FindExtremeElement(U begin, U end, double invalid_cost)
+{
+  U extreme_itr = end;
+
+  for (U itr = begin; itr != end; itr++) {
+    // Filter out invalid costs
+    if (invalid_cost == itr->costsofar) {
+      continue;
+    }
+
+    // Find the first extreme element
+    if (Maximize) {
+      if (extreme_itr == end || extreme_itr->costsofar < itr->costsofar) {
+        extreme_itr = itr;
+      }
+    } else {
+      if (extreme_itr == end || itr->costsofar < extreme_itr->costsofar) {
+        extreme_itr = itr;
+      }
+    }
+  }
+
+  return extreme_itr;
+}
+
+
+template <typename T, bool Maximize>
+const T*
+NaiveViterbiSearch<T, Maximize>::FindWinner(const std::vector<label_type>& labels) const
+{
+  if (labels.empty()) {
+    return nullptr;
+  }
+  auto itr = FindExtremeElement<decltype(labels.cbegin()), Maximize>(labels.cbegin(), labels.cend(), kInvalidCost);
+  if (itr == labels.cend()) {
+    return nullptr;
+  }
+  assert(kInvalidCost != itr->costsofar);
+  return itr->state;
+}
+
+
+// Linear search a state's label
+template <typename T, bool Maximize>
+const typename NaiveViterbiSearch<T, Maximize>::label_type&
+NaiveViterbiSearch<T, Maximize>::label(const T& state) const
+{
+  auto time = state.time();
+
+  for (auto& label : history_[time]) {
+    if (label.state->id() == state.id()) {
+      return label;
+    }
+  }
+
+  assert(false);
+  throw std::runtime_error("impossible that label not found; if it happened, check SearchWinner");
+}
+
+
+template <typename T>
+class ViterbiSearch: public IViterbiSearch<T>
+{
+ public:
+  ViterbiSearch(): earliest_time_(0) {}
+
+  ~ViterbiSearch();
+
+  void Clear();
+
+  StateId SearchWinner(Time time) override;
+
+  const T& state(StateId id) const override;
+
+  StateId predecessor(StateId id) const override;
+
+  virtual bool IsInvalidCost(double cost) const;
+
+  using IViterbiSearch<T>::AccumulatedCost;
+
+  virtual double AccumulatedCost(const StateId id) const override;
+
+ protected:
+  std::vector<const T*> state_;
+
+  std::vector<const T*> winner_;
+
+  std::vector<std::vector<const T*>> unreached_states_;
+
+  virtual float TransitionCost(const T& left, const T& right) const override = 0;
+
+  virtual float EmissionCost(const T& state) const override = 0;
+
+  virtual double CostSofar(double prev_costsofar, float transition_cost, float emission_cost) const override = 0;
+
+ private:
+  struct Label: public LabelTemplate<T> {
+    Label()
+        : LabelTemplate<T>(-1.f, nullptr, nullptr) {
+    }
+    Label(double c, const T* a, const T* p)
+        : LabelTemplate<T>(c, a, p) {
+    }
+  };
+
+  SPQueue<Label> queue_;
+
+  std::unordered_map<StateId, Label> scanned_labels_;
+
+  // Initialize labels from a column and push them into priority queue
+  void InitQueue(const std::vector<const T*>& column);
+
+  void AddSuccessorsToQueue(const T* state);
+
+  Time IterativeSearch(Time target, bool request_new_start);
+
+  Time earliest_time_;
+};
+
+
+template <typename T>
+inline ViterbiSearch<T>::~ViterbiSearch()
+{ Clear(); }
+
+
+template <typename T>
+StateId ViterbiSearch<T>::SearchWinner(Time time)
+{
+  // Use the cache
+  if (time < winner_.size()) {
+    return winner_[time]? winner_[time]->id() : kInvalidStateId;
+  }
+
+  if (unreached_states_.empty()) {
+    return kInvalidStateId;
+  }
+
+  Time target = std::min(time, static_cast<Time>(unreached_states_.size()) - 1);
+  Time searched_time = IterativeSearch(target, false);
+  while (searched_time < target) {
+    searched_time = IterativeSearch(target, true);
+  }
+
+  if (time < winner_.size() && winner_[time]) {
+    return winner_[time]->id();
+  }
+  return kInvalidStateId;
+}
+
+
+template <typename T>
+inline StateId
+ViterbiSearch<T>::predecessor(StateId id) const
+{
+  auto it = scanned_labels_.find(id);
+  if (it != scanned_labels_.end()) {
+    return (it->second).predecessor? (it->second).predecessor->id() : kInvalidStateId;
+  } else {
+    return kInvalidStateId;
+  }
+}
+
+
+template <typename T>
+inline bool
+ViterbiSearch<T>::IsInvalidCost(double cost) const
+{ return cost < 0.f; }
+
+
+template <typename T>
+double ViterbiSearch<T>::AccumulatedCost(const StateId id) const
+{
+  const auto itr = scanned_labels_.find(id);
+  if (itr == scanned_labels_.end()) {
+    return -1.f;
+  } else {
+    return itr->second.costsofar;
+  }
+}
+
+
+template <typename T>
+inline const T& ViterbiSearch<T>::state(StateId id) const
+{ return *state_[id]; }
+
+
+template <typename T>
+void ViterbiSearch<T>::Clear()
+{
+  earliest_time_ = 0;
+  queue_.clear();
+  scanned_labels_.clear();
+  unreached_states_.clear();
+  winner_.clear();
+  for (auto state : state_) {
+    delete state;
+  }
+  state_.clear();
+}
+
+
+template <typename T>
+void ViterbiSearch<T>::InitQueue(const std::vector<const T*>& column)
+{
+  queue_.clear();
+  for (const auto state : column) {
+    auto emission_cost = EmissionCost(*state);
+    if (IsInvalidCost(emission_cost)) {
+      continue;
+    }
+    queue_.push(Label(emission_cost, state, nullptr));
+  }
+}
+
+
+template <typename T>
+void ViterbiSearch<T>::AddSuccessorsToQueue(const T* state)
+{
+  assert(state->time() + 1 < unreached_states_.size());
+  if (unreached_states_.size() <= state->time() + 1) {
+    return;
+  }
+
+  auto label_itr = scanned_labels_.find(state->id());
+  assert(label_itr != scanned_labels_.end());
+  if (label_itr == scanned_labels_.end()) {
+    return;
+  }
+  assert(label_itr->second.state == state);  // TODO remove
+  auto costsofar = label_itr->second.costsofar;
+  assert(!IsInvalidCost(costsofar));
+
+  auto next_column = unreached_states_[state->time() + 1];
+  for (const auto& next_state : next_column) {
+    auto emission_cost = EmissionCost(*next_state);
+    if (IsInvalidCost(emission_cost)) {
+      continue;
+    }
+
+    auto transition_cost = TransitionCost(*state, *next_state);
+    if (IsInvalidCost(transition_cost)) {
+      continue;
+    }
+
+    auto next_costsofar = CostSofar(costsofar, transition_cost,  emission_cost);
+    if (IsInvalidCost(next_costsofar)) {
+      continue;
+    }
+
+    queue_.push(Label(next_costsofar, next_state, state));
+  }
+}
+
+
+// Remove a state from its column
+template <typename T, typename U>
+bool remove_state(const T& state, U& column)
+{
+  StateId id = state.id();
+  auto itr = std::find_if(column.begin(), column.end(),
+                          [id] (const T* state) {
+                            return id == state->id();
+                          });
+  if (itr == column.end()) {
+    return false;
+  }
+  column.erase(itr);
+  return true;
+}
+
+
+template <typename T>
+Time ViterbiSearch<T>::IterativeSearch(Time target, bool request_new_start)
+{
+  assert(!unreached_states_.empty() && target < unreached_states_.size());
+  if (unreached_states_.empty()) {
+    throw std::runtime_error("empty states");
+  }
+
+  if (target < winner_.size()) {
+    return target;
+  }
+
+  // So here we have: assert(winner_.size() <= target && target < unreached_states_.size());
+
+  Time source;
+
+  // Initialize queue
+  if (!request_new_start && !winner_.empty() && winner_.back()) {
+    source = winner_.size() - 1;
+    AddSuccessorsToQueue(winner_[source]);
+  } else {
+    source = winner_.size();
+    InitQueue(unreached_states_[source]);
+  }
+
+  // Start with the source time, which will be searched anyhow
+  auto searched_time = source;
+
+  while (!queue_.empty()) {
+    // Pop up the state with the lowest cost
+    auto label = queue_.top();
+    queue_.pop();
+    auto state = label.state;
+    auto time = state->time();
+
+    // Skip labels that are earlier than the earliest time, since
+    // they are impossible to be optimal
+    if (time < earliest_time_) {
+      continue;
+    }
+
+    // Mark it as scanned and remember its cost and predecessor
+    assert(scanned_labels_.find(state->id())==scanned_labels_.end());
+    scanned_labels_[state->id()] = label;
+
+    // Remove it from its column
+    bool removed = remove_state(*state, unreached_states_[time]);
+    assert(removed);
+
+    // Earlier labels can't reach current time with better cost, so we
+    // mark time + 1 as the earliest time to skip all earlier labels
+    if (unreached_states_[time].empty()) {
+      earliest_time_ = time + 1;
+    }
+
+    // If it's the first state that arrives at this column, mark it as
+    // the winner at this time
+    if (winner_.size() <= time) {
+      assert(time == winner_.size());
+      winner_.push_back(state);
+    }
+
+    searched_time = std::max(time, searched_time);
+
+    if (target <= time) {
+      break;
+    }
+
+    AddSuccessorsToQueue(state);
+  }
+
+  // Guarantee that either winner (if found) or nullptr is saved at
+  // searched time
+  while (winner_.size() <= searched_time) {
+    winner_.push_back(nullptr);
+  }
+
+  assert(searched_time < winner_.size());
+
+  return searched_time;
+}
+
+
+}
+
+
+#endif // MMP_VITERBI_SEARCH_H_
