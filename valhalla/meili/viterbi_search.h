valhalla/valhalla

commit 1c7e9baae42935d66c232c376ab80daa2da37af9

Throw out-of-bounds if possible in ViterbiSearch
when the given state id is out of bounds, throw the error
instead of returning invalid id or cost

this is designed to be consistent with two implementations

valhalla/meili/viterbi_search.h
@@ -155,7 +155,7 @@ class IViterbiSearch
   // Get the state reference given its ID
   virtual const T& state(StateId id) const = 0;
 
-  virtual double AccumulatedCost(const StateId id) const = 0;
+  virtual double AccumulatedCost(StateId id) const = 0;
 
   virtual double AccumulatedCost(const T& state) const
   { return AccumulatedCost(state.id()); }
@@ -198,7 +198,7 @@ class NaiveViterbiSearch: public IViterbiSearch<T>
 
   const T& state(StateId id) const override;
 
-  double AccumulatedCost(const StateId id) const override;
+  double AccumulatedCost(StateId id) const override;
 
   double AccumulatedCost(const T& state) const override;
 
@@ -252,7 +252,7 @@ void NaiveViterbiSearch<T, Maximize>::Clear()
 
 template <typename T, bool Maximize>
 inline double
-NaiveViterbiSearch<T, Maximize>::AccumulatedCost(const StateId id) const
+NaiveViterbiSearch<T, Maximize>::AccumulatedCost(StateId id) const
 { return id != kInvalidStateId? AccumulatedCost(state(id)) : kInvalidCost; }
 
 
@@ -433,7 +433,7 @@ class ViterbiSearch: public IViterbiSearch<T>
 
   using IViterbiSearch<T>::AccumulatedCost;
 
-  virtual double AccumulatedCost(const StateId id) const override;
+  virtual double AccumulatedCost(StateId id) const override;
 
  protected:
   std::vector<const T*> state_;
@@ -514,7 +514,11 @@ template <typename T>
 inline StateId
 ViterbiSearch<T>::predecessor(StateId id) const
 {
-  const auto it = scanned_labels_.find(id);
+  // Let it throw out-of-bound here. This is by design to be
+  // consistent with NaiveViterbiSearch::predecessor
+  const auto& state = state_[id];
+
+  const auto it = scanned_labels_.find(state->id());
   if (it != scanned_labels_.end()) {
     return (it->second).predecessor? (it->second).predecessor->id() : kInvalidStateId;
   } else {
@@ -530,9 +534,13 @@ ViterbiSearch<T>::IsInvalidCost(double cost) const
 
 
 template <typename T>
-double ViterbiSearch<T>::AccumulatedCost(const StateId id) const
+double ViterbiSearch<T>::AccumulatedCost(StateId id) const
 {
-  const auto it = scanned_labels_.find(id);
+  // Let it throw out-of-bound here. This is by design to be
+  // consistent with NaiveViterbiSearch::AccumulatedCost
+  const auto& state = state_[id];
+
+  const auto it = scanned_labels_.find(state->id());
   if (it == scanned_labels_.end()) {
     return -1.f;
   } else {

commit f860dd86f994bc525069f13a4e7b096f411cc430

Fix segment fault caused by commit 1c7e9ba
since then the predecessor method would report segment fault if you
request the predecessor of an invalid id

valhalla/meili/viterbi_search.h
@@ -90,7 +90,7 @@ class StateIterator: public std::iterator<std::forward_iterator_tag, T>
   {
     // Hold the invariant in the beginning (see below about the
     // invariant)
-    if (!(time != kInvalidTime || id_ == kInvalidStateId)) {
+    if (!(time_ != kInvalidTime || id_ == kInvalidStateId)) {
       throw std::runtime_error("invalid pair of id and time");
     }
   }
@@ -137,9 +137,9 @@ class StateIterator: public std::iterator<std::forward_iterator_tag, T>
  private:
   IViterbiSearch<T>* vs_;
 
-  // Invariant: (time == kInvalidTime && id_ == kInvalidStateId) || time != kInvalidTime
-  // same as: !(time == kInvalidTime && id_ != kInvalidStateId)
-  // same as: time != kInvalidTime || id_ == kInvalidStateId
+  // Invariant: (time == kInvalidTime && id == kInvalidStateId) || time != kInvalidTime
+  // same as: !(time == kInvalidTime && id != kInvalidStateId)
+  // same as: time != kInvalidTime || id == kInvalidStateId
   StateId id_;
 
   Time time_;
@@ -155,8 +155,15 @@ class StateIterator: public std::iterator<std::forward_iterator_tag, T>
 
     if (0 < time_) {
       time_ --;
-      id_ = vs_->predecessor(id_);
-      if (id_ == kInvalidStateId) {
+      if (id_ != kInvalidStateId) {
+        id_ = vs_->predecessor(id_);
+        // Search at previous time directly if the predecessor not
+        // found
+        if (id_ == kInvalidStateId) {
+          id_ = vs_->SearchWinner(time_);
+        }
+      } else {
+        // No way go back, then search at previous time directly
         id_ = vs_->SearchWinner(time_);
       }
       if (!(id_ == kInvalidStateId || vs_->state(id_).time() == time_)) {