0423402684b7ae99311ffa6a6e77865ee018b3f0
gknisely <greg@mapzen.com>
2016-06-14 17:09:13 -0400

    no .so needed.

--- src/mjolnir/admin.cc
+++ src/mjolnir/admin.cc
@@ -27,7 +27,7 @@ sqlite3 * GetDBHandle(const std::string database) {
 
     // loading SpatiaLite as an extension
     sqlite3_enable_load_extension(db_handle, 1);
-    sql = "SELECT load_extension('libspatialite.so')";
+    sql = "SELECT load_extension('libspatialite')";
     ret = sqlite3_exec(db_handle, sql.c_str(), nullptr, nullptr, &err_msg);
     if (ret != SQLITE_OK) {
       LOG_ERROR("load_extension() error: " + std::string(err_msg));
