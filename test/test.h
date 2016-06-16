9f97367daada0e8bab0c387f8d9afd801b11a5b3
Kevin Kreiser <kevinkreiser@gmail.com>
2016-06-16 15:06:48 -0400

    update test harness to include a few extra things from @ptpt

--- test/test.h
+++ test/test.h
@@ -1,3 +1,5 @@
+// -*- mode: c++ -*-
+
 #ifndef TEST_HPP
 #define TEST_HPP
 
@@ -8,23 +10,45 @@
 
 namespace test{
 
-  //function pointer to run as a test
-  using test_function = void (*)(void);
+//function pointer to run as a test
+using test_function = void (*)(void);
 
-  struct suite {
-    public:
-      //initializes the test suite
-      explicit suite(const std::string& test_suite_name);
-      //run the test
-      void test(const std::string& test_name, test_function function);
-      //returns EXIT_FAILURE if any tests failed otherwise returns EXIT_SUCCESS
-      int tear_down();
-    private:
-      //number of failed tests
-      size_t failed;
-  };
+struct suite {
+ public:
+  //initializes the test suite
+  explicit suite(const std::string& test_suite_name);
+  //run the test
+  void test(const std::string& test_name, test_function function);
+  //returns EXIT_FAILURE if any tests failed otherwise returns EXIT_SUCCESS
+  int tear_down();
+ private:
+  //number of failed tests
+  size_t failed;
+};
 
+template <typename value_t>
+void assert_bool(value_t value, const std::string& message)
+{
+  if (!value) {
+    throw std::runtime_error(message);
+  }
+}
 
+template <typename exception_t, typename function_t>
+void assert_throw(function_t func, const std::string& message)
+{
+  bool thrown = false;
+
+  try {
+    func();
+  } catch (const exception_t& ex) {
+    thrown = true;
+  }
+
+  if (!thrown) {
+    throw std::runtime_error(message);
+  }
+}
 
 }
 
