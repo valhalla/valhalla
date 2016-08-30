f8401bcbe4bfe589f199efe478c322b9a7056f87
kdiluca <kristen@mapzen.com>
2016-08-30 14:56:28 -0400

    jsonify non 200 responses; refactored to add things to header

--- valhalla/tyr/service.h
+++ valhalla/tyr/service.h
@@ -3,11 +3,31 @@
 
 #include <boost/property_tree/ptree.hpp>
 
+#include <prime_server/prime_server.hpp>
+#include <prime_server/http_protocol.hpp>
+
+#include <valhalla/baldr/json.h>
+
+
 namespace valhalla {
   namespace tyr {
 
     void run_service(const boost::property_tree::ptree& config);
 
+    class tyr_worker_t {
+     public:
+      tyr_worker_t(const boost::property_tree::ptree& config);
+      virtual ~tyr_worker_t();
+      prime_server::worker_t::result_t work(const std::list<zmq::message_t>& job, void* request_info);
+      void cleanup();
+
+     protected:
+
+      prime_server::worker_t::result_t jsonify_error(uint64_t code, const std::string& status, const std::string& error, http_request_t::info_t& request_info) const;
+
+      boost::property_tree::ptree config;
+      float long_request;
+    };
   }
 }
 
