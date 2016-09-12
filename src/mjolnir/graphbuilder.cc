78fa76142de4777f048c731790c17f3c13950830
David Nesbitt <dave@mapzen.com>
2016-09-12 09:20:41 -0400

    Do not compute grade for very short edges. DO not compute grade for ferries.

--- src/mjolnir/graphbuilder.cc
+++ src/mjolnir/graphbuilder.cc
@@ -38,6 +38,9 @@ namespace {
 //how many meters to resample shape to when checking elevations
 constexpr double POSTING_INTERVAL = 60;
 
+// Do not compute grade for intervals less than 10 meters.
+constexpr double kMinimumInterval = 10.0f;
+
 /**
  * we need the nodes to be sorted by graphid and then by osmid to make a set of tiles
  * we also need to then update the egdes that pointed to them
@@ -597,47 +600,46 @@ void BuildTileSet(const std::string& ways_file, const std::string& way_nodes_fil
             //length
             auto length = valhalla::midgard::length(shape);
 
-            //grade estimation and max slopes
-            uint32_t forward_grade = 6, reverse_grade = 6;
-            float forward_max_up_slope = 0.0f, forward_max_down_slope = 0.0f;
-            float reverse_max_up_slope = 0.0f, reverse_max_down_slope = 0.0f;
-            if(sample && !w.tunnel()) {
-              //evenly sample the shape
-              std::list<PointLL> resampled;
-              //if it is really short or a bridge just do both ends
-              auto interval = POSTING_INTERVAL;
-              if(length < POSTING_INTERVAL * 3 || w.bridge()) {
-                resampled = {shape.front(), shape.back()};
-                interval = length;
-              }
-              else {
-                resampled = valhalla::midgard::resample_spherical_polyline(shape, interval);
-              }
-              //get the heights at each point
-              auto heights = sample->get_all(resampled);
+            // Grade estimation and max slopes
+            std::tuple<double,double,double> forward_grades(0.0, 0.0, 0.0);
+            std::tuple<double,double,double> reverse_grades(0.0, 0.0, 0.0);
+            if(sample && !w.tunnel() && !w.ferry()) {
+              // Skip very short edges
+              if (length > kMinimumInterval) {
+                // Evenly sample the shape. If it is really short or a bridge
+                // just do both ends
+                auto interval = POSTING_INTERVAL;
+                std::list<PointLL> resampled;
+                if(length < POSTING_INTERVAL * 3 || w.bridge()) {
+                  resampled = {shape.front(), shape.back()};
+                  interval = length;
+                }
+                else {
+                  resampled = valhalla::midgard::resample_spherical_polyline(shape, interval);
+                }
 
-              //compute grades in both directions, valid range is between -10 and +15, which we then
-              //map to a value between 0 and 15 (to use for indices into a factor array)
-              auto grades = valhalla::skadi::weighted_grade(heights, interval);
-              forward_grade = static_cast<uint32_t>(std::get<0>(grades)  * .6 + 6.5);
-              forward_max_up_slope = std::get<1>(grades);
-              forward_max_down_slope = std::get<2>(grades);
-              std::reverse(heights.begin(), heights.end());
-              grades = valhalla::skadi::weighted_grade(heights, interval);
-              reverse_grade = static_cast<uint32_t>(std::get<0>(grades) * .6 + 6.5);
-              reverse_max_up_slope = std::get<1>(grades);
-              reverse_max_down_slope = std::get<2>(grades);
+                // Get the heights at each sampled point. Compute "weighted"
+                // grades as well as max grades in both directions. Valid range
+                // for weighted grades is between -10 and +15 which is then
+                // mapped to a value between 0 to 15 for use in costing.
+                auto heights = sample->get_all(resampled);
+                forward_grades = valhalla::skadi::weighted_grade(heights, interval);
+                std::reverse(heights.begin(), heights.end());
+                reverse_grades = valhalla::skadi::weighted_grade(heights, interval);
+              }
             }
 
             //TODO: curvature
             uint32_t curvature = 0;
 
             //add it in
+            uint32_t forward_grade = static_cast<uint32_t>(std::get<0>(forward_grades)  * .6 + 6.5);
+            uint32_t reverse_grade = static_cast<uint32_t>(std::get<0>(reverse_grades) * .6 + 6.5);
             auto inserted = geo_attribute_cache.insert({edge_info_offset,
               std::make_tuple(static_cast<uint32_t>(length + .5), forward_grade,
                               reverse_grade, curvature,
-                              forward_max_up_slope, forward_max_down_slope,
-                              reverse_max_up_slope, reverse_max_down_slope)});
+                              std::get<1>(forward_grades), std::get<2>(forward_grades),
+                              std::get<1>(reverse_grades), std::get<2>(reverse_grades))});
             found = inserted.first;
           }//now we have the edge info offset
           else {
