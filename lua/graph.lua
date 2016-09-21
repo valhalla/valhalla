535550243b46ba0cf1194b170d086c2e99ffbffe
gknisely <greg@mapzen.com>
2016-09-21 16:47:38 -0400

    added logic to process wheelchair tag.  if no tag, then use foot tag.

--- lua/graph.lua
+++ lua/graph.lua
@@ -432,6 +432,24 @@ foot_node = {
 ["footway"] = 2
 }
 
+wheelchair_node = {
+["no"] = 0,
+["yes"] = 256,
+["designated"] = 256,
+["limited"] = 256,
+["official"] = 256,
+["destination"] = 256,
+["public"] = 256,
+["permissive"] = 256,
+["only"] = 256,
+["private"] = 256,
+["impassable"] = 0,
+["partial"] = 0,
+["bad"] = 0,
+["half"] = 0,
+["assisted"] = 256
+}
+
 bus_node = {
 ["no"] = 0,
 ["yes"] = 64,
@@ -955,6 +973,8 @@ function filter_tags_generic(kv)
         use = 20
      elseif kv["pedestrian"] == "false" and kv["auto_forward"] == "false" and kv["auto_backward"] == "false" and (kv["bike_forward"] == "true" or kv["bike_backward"] == "true") then
         use = 20
+     elseif (kv["highway"] == "footway" and kv["footway"] == "sidewalk") or (kv["sidewalk"] == "true") then
+        use = 24
      elseif kv["highway"] == "footway" then
         use = 25
      elseif kv["highway"] == "steps" then
@@ -1130,7 +1150,8 @@ function nodes_proc (kv, nokeys)
     access = "false"
   end 
 
-  local foot_tag = foot_node[kv["foot"]] 
+  local foot_tag = foot_node[kv["foot"]]
+  local wheelchair_tag = wheelchair_node[kv["wheelchair"]]
   local bike_tag = bicycle_node[kv["bicycle"]]
   local truck_tag = truck_node[kv["hgv"]]
   local auto_tag = motor_vehicle_node[kv["motorcar"]]
@@ -1145,6 +1166,11 @@ function nodes_proc (kv, nokeys)
   --if bus was not set and car is
   if bus_tag == nil and auto_tag == 1 then
     bus_tag = 64
+  end
+
+  --if wheelchair was not set and foot is
+  if wheelchair_tag == nil and foot_tag == 1 then
+    wheelchair_tag = 256
   end
 
   --if truck was not set and car is
@@ -1173,6 +1199,7 @@ function nodes_proc (kv, nokeys)
   local truck = truck_tag or 8 
   local bus = bus_tag or 64
   local foot = foot_tag or 2
+  local wheelchair = wheelchair_tag or 256
   local bike = bike_tag or 4
   local emergency = emergency_tag or 16
 
@@ -1182,6 +1209,7 @@ function nodes_proc (kv, nokeys)
     truck = truck_tag or 0
     bus = bus_tag or 0
     foot = foot_tag or 0
+    wheelchair = wheelchair_tag or 0
     bike = bike_tag or 0
     emergency = emergency_tag or 0
   end 
@@ -1205,6 +1233,7 @@ function nodes_proc (kv, nokeys)
       truck = truck_tag or 0
       bus = bus_tag or 0
       foot = foot_tag or 2
+      wheelchair = wheelchair_tag or 256
       bike = bike_tag or 4
       emergency = emergency_tag or 0
     end
@@ -1220,6 +1249,7 @@ function nodes_proc (kv, nokeys)
          truck = truck_tag or 8
          bus = bus_tag or 64
          foot = foot_tag or 2
+         wheelchair = wheelchair_tag or 256
          bike = bike_tag or 4
          emergency = emergency_tag or 16
     end
@@ -1278,7 +1308,7 @@ function nodes_proc (kv, nokeys)
   end
  
   --store a mask denoting access
-  kv["access_mask"] = bit32.bor(auto, emergency, truck, bike, foot, bus)
+  kv["access_mask"] = bit32.bor(auto, emergency, truck, bike, foot, wheelchair, bus)
 
   return 0, kv
 end
