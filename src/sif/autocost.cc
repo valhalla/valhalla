491650bcdb57bb95de937b000283b36a000deb5f
kdiluca <kristen@mapzen.com>
2016-09-21 16:43:48 -0400

    updated defaults to match what our valhalla config defaulted to

--- src/sif/autocost.cc
+++ src/sif/autocost.cc
@@ -16,7 +16,7 @@ namespace sif {
 namespace {
 constexpr float kDefaultManeuverPenalty         = 5.0f;   // Seconds
 constexpr float kDefaultDestinationOnlyPenalty  = 600.0f; // Seconds
-constexpr float kDefaultAlleyPenalty            = 30.0f;  // Seconds
+constexpr float kDefaultAlleyPenalty            = 5.0f;   // Seconds
 constexpr float kDefaultGateCost                = 30.0f;  // Seconds
 constexpr float kDefaultGatePenalty             = 300.0f; // Seconds
 constexpr float kDefaultTollBoothCost           = 15.0f;  // Seconds
