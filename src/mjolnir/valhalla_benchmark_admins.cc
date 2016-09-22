2fd3e34bbbdd18e727e178fb961609f650ffee3e
Guillaume Pinot <texitoi@texitoi.eu>
2016-09-22 15:40:28 +0200

    make it working on debian jessie

--- src/mjolnir/valhalla_benchmark_admins.cc
+++ src/mjolnir/valhalla_benchmark_admins.cc
@@ -164,7 +164,7 @@ std::cout << "In Benchmark" << std::endl;
 
     // loading SpatiaLite as an extension
     sqlite3_enable_load_extension(db_handle, 1);
-#if SQLITE_VERSION_NUMBER > 3008002
+#if SQLITE_VERSION_NUMBER > 3008007
     sql = "SELECT load_extension('mod_spatialite')";
 #else
     sql = "SELECT load_extension('libspatialite')";
