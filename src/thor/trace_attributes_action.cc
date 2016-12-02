70ab190345e0b796914077c01440298c967c3e3c
kdiluca <kristen@mapzen.com>
2016-12-02 11:42:57 -0500

    changed back to using TripPath

--- src/thor/trace_attributes_action.cc
+++ src/thor/trace_attributes_action.cc
@@ -8,7 +8,7 @@ using namespace prime_server;
 #include <valhalla/midgard/constants.h>
 #include <valhalla/baldr/json.h>
 #include <valhalla/baldr/errorcode_util.h>
-#include <valhalla/odin/enhancedtrippath.h>
+#include <valhalla/proto/trippath.pb.h>
 
 
 #include "thor/service.h"
@@ -27,23 +27,23 @@ namespace {
   const headers_t::value_type JSON_MIME { "Content-type", "application/json;charset=utf-8" };
   const headers_t::value_type JS_MIME { "Content-type", "application/javascript;charset=utf-8" };
 
-  json::MapPtr serialize(odin::EnhancedTripPath* trip_path, const boost::optional<std::string>& id, double scale) {
+  json::MapPtr serialize(odin::TripPath trip_path, const boost::optional<std::string>& id, double scale) {
     json::ArrayPtr edges = json::array({});
-    for (int i = 1; i <= trip_path->node().size(); i++) {
-      auto edge = trip_path->GetPrevEdge(i,0);
-      if (edge) {
+    for (int i = 1; i < trip_path.node().size(); i++) {
+      if (trip_path.node(i-1).has_edge()) {
+        auto edge = trip_path.node(i - 1).edge();
         auto names = json::array({});
-        for (const auto& name : edge->name())
+        for (const auto& name : edge.name())
           names->push_back(name);
 
         edges->emplace_back(json::map({
-          {"max_downward_grade", static_cast<int64_t>(edge->max_downward_grade())},
-          {"max_upward_grade", static_cast<int64_t>(edge->max_upward_grade())},
-          {"weighted_grade", json::fp_t{edge->weighted_grade(), 3}},
-          {"length", json::fp_t{edge->length() * scale, 3}},
-          {"speed", json::fp_t{edge->speed() * scale, 3}},
-          {"way_id", static_cast<uint64_t>(edge->way_id())},
-          {"id", static_cast<uint64_t>(edge->id())},
+          {"max_downward_grade", static_cast<int64_t>(edge.max_downward_grade())},
+          {"max_upward_grade", static_cast<int64_t>(edge.max_upward_grade())},
+          {"weighted_grade", json::fp_t{edge.weighted_grade(), 3}},
+          {"length", json::fp_t{edge.length() * scale, 3}},
+          {"speed", json::fp_t{edge.speed() * scale, 3}},
+          {"way_id", static_cast<uint64_t>(edge.way_id())},
+          {"id", static_cast<uint64_t>(edge.id())},
           {"names", names}
         }));
       }
@@ -102,7 +102,6 @@ worker_t::result_t thor_worker_t::trace_attributes(
       }
     } else throw valhalla_exception_t{400, 443};
   }
-  odin::EnhancedTripPath* etrip_path = static_cast<odin::EnhancedTripPath*>(&trip_path);
   auto id = request.get_optional<std::string>("id");
   //length and speed default to km
   double scale = 1;
@@ -111,8 +110,7 @@ worker_t::result_t thor_worker_t::trace_attributes(
     scale = kMilePerKm;
 
   //serialize output to Thor
-  json::MapPtr json;
-  json = serialize(etrip_path, id, scale);
+  json::MapPtr json = serialize(trip_path, id, scale);
 
   //jsonp callback if need be
   std::ostringstream stream;
