9e65ec48a6463976c0925995ea541b6ab8bbef37
Matt Amos <zerebubuth@gmail.com>
2017-01-13 16:25:39 +0000

    Added a function to expose AABB based tile lookup from TileHierarchy.

--- src/baldr/tilehierarchy.cc
+++ src/baldr/tilehierarchy.cc
@@ -47,5 +47,24 @@ uint8_t TileHierarchy::get_level(const RoadClass roadclass) const {
   }
 }
 
+std::vector<GraphId> TileHierarchy::GetGraphIds(
+  const midgard::AABB2<midgard::PointLL> &bbox,
+  uint8_t level) const {
+
+  std::vector<GraphId> ids;
+
+  auto itr = levels_.find(level);
+  if (itr != levels_.end()) {
+    auto tile_ids = itr->second.tiles.TileList(bbox);
+    ids.reserve(tile_ids.size());
+
+    for (auto tile_id : tile_ids) {
+      ids.emplace_back(tile_id, level, 0);
+    }
+  }
+
+  return ids;
+}
+
 }
 }
