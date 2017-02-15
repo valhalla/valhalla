c7b92b56314bc98a9fae10b751a8fb54dbb6642d
Kevin Kreiser <kevinkreiser@gmail.com>
2017-02-15 15:43:48 -0500

    make matcher json not be all strings

--- valhalla/meili/traffic_segment_matcher.h
+++ valhalla/meili/traffic_segment_matcher.h
@@ -5,11 +5,12 @@
 #include <sstream>
 #include <boost/property_tree/ptree.hpp>
 
-#include <valhalla/baldr/graphreader.h>
-#include <valhalla/baldr/graphid.h>
-#include <valhalla/meili/map_matcher.h>
-#include <valhalla/meili/map_matcher_factory.h>
-#include <valhalla/meili/match_route.h>
+#include "baldr/graphreader.h"
+#include "baldr/graphid.h"
+#include "baldr/json.h"
+#include "meili/map_matcher.h"
+#include "meili/map_matcher_factory.h"
+#include "meili/match_route.h"
 
 namespace valhalla {
 namespace meili {
@@ -19,13 +20,6 @@ struct EdgeOnTrace {
   valhalla::baldr::GraphId edge_id;
   float dist;
   float secs;
-
-  EdgeOnTrace(const valhalla::baldr::GraphId& id, const float d,
-              const float s)
-      : edge_id(id),
-        dist(d),
-        secs(s) {
-  }
 };
 
 // Unique edges along the GPS trace
@@ -35,15 +29,6 @@ struct UniqueEdgeOnTrace {
   float end_pct;
   float secs1;
   float secs2;
-
-  UniqueEdgeOnTrace(const valhalla::baldr::GraphId& id, const float p1,
-              const float p2, const float t1, const float t2)
-      : edge_id(id),
-        start_pct(p1),
-        end_pct(p2),
-        secs1(t1),
-        secs2(t2) {
-  }
 };
 
 
@@ -52,31 +37,9 @@ struct MatchedTrafficSegments {
   bool partial_start;                   // Begins along the segment
   bool partial_end;                     // Ends along the segment
   valhalla::baldr::GraphId segment_id;  // Traffic segment unique Id.
-  float begin_time;                     // Begin time along this segment.
+  float start_time;                     // Begin time along this segment.
   float end_time;                       // End time along this segment.
   uint32_t length;                      // Length in meters along this segment
-
-  MatchedTrafficSegments(const bool start, const bool end,
-                         const valhalla::baldr::GraphId& id,
-                         const float bt, const float et, const float l)
-      : partial_start(start),
-        partial_end(end),
-        segment_id(id),
-        begin_time(bt),
-        end_time(et),
-        length(l) {
-  }
-
-  boost::property_tree::ptree ToPtree() const {
-    boost::property_tree::ptree segment;
-    segment.put<bool>("partial_start", partial_start);
-    segment.put<bool>("partial_end", partial_end);
-    segment.put("segment_id", segment_id.value);
-    segment.put<float>("begin_time", begin_time);
-    segment.put<float>("end_time", end_time);
-    segment.put("length", length);
-    return segment;
-  }
 };
 
 /**
