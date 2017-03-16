0ebc39432e48798df29aae11fac8808abb0e5e06
gknisely <greg@mapzen.com>
2017-03-16 14:04:45 -0400

    reverted back.

--- src/sif/transitcost.cc
+++ src/sif/transitcost.cc
@@ -568,18 +568,7 @@ Cost TransitCost::EdgeCost(const baldr::DirectedEdge* edge,
                            const baldr::TransitDeparture* departure,
                            const uint32_t curr_time) const {
   // Separate wait time from time on transit
-  // adjust the time if it is after midnight.
-  uint32_t current = curr_time;
-  while (current >= kSecondsPerDay)
-    current -= kSecondsPerDay;
-
-  float wait_time = departure->departure_time() - current;
-
-  // handle case where departure time is after midnight say 60 and
-  // current time is before midnight say 86340 which causes a negative
-  // wait time.
-  if (wait_time < 0.0f)
-    wait_time += kSecondsPerDay;
+  float wait_time = departure->departure_time() - curr_time;
 
   // Cost is modulated by mode-based weight factor
   float weight = 1.0f;
