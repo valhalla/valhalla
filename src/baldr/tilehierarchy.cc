bbab4ab2174f3664e26d06503e64475a87113651
dnesbitt <dnesbitt61@gmail.com>
2017-04-05 09:44:26 -0400

    Make TileHierarchy a set of static methods and the hierarchy levels static data. Remove the tile_dir from TileHiearchy so the class simply deals with the hierarchy of graph tiles and the tile sizing.

--- src/baldr/tilehierarchy.cc
+++ src/baldr/tilehierarchy.cc
@@ -6,27 +6,21 @@ using namespace valhalla::midgard;
 namespace valhalla {
 namespace baldr {
 
-TileHierarchy::TileHierarchy(const std::string& tile_dir):tile_dir_(tile_dir) {
-  levels_ = {
-    {2, TileLevel{2, stringToRoadClass.find("ServiceOther")->second, "local", Tiles<PointLL>{{{-180, -90}, {180, 90}}, .25, kBinsDim}}},
-    {1, TileLevel{1, stringToRoadClass.find("Tertiary")->second, "arterial", Tiles<PointLL>{{{-180, -90}, {180, 90}}, 1, kBinsDim}}},
-    {0, TileLevel{0, stringToRoadClass.find("Primary")->second, "highway", Tiles<PointLL>{{{-180, -90}, {180, 90}}, 4, kBinsDim}}}
-  };
-}
+// Static tile levels
+std::map<uint8_t, TileLevel> TileHierarchy::levels_  = {
+  {2, TileLevel{2, stringToRoadClass.find("ServiceOther")->second, "local",
+    midgard::Tiles<midgard::PointLL>{{{-180, -90}, {180, 90}}, .25, static_cast<unsigned short>(kBinsDim)}}},
+  {1, TileLevel{1, stringToRoadClass.find("Tertiary")->second, "arterial",
+    midgard::Tiles<midgard::PointLL>{{{-180, -90}, {180, 90}}, 1, static_cast<unsigned short>(kBinsDim)}}},
+  {0, TileLevel{0, stringToRoadClass.find("Primary")->second, "highway",
+    midgard::Tiles<midgard::PointLL>{{{-180, -90}, {180, 90}}, 4, static_cast<unsigned short>(kBinsDim)}}}
+};
 
-TileHierarchy::TileHierarchy(){}
-
-const std::map<unsigned char, TileHierarchy::TileLevel>& TileHierarchy::levels() const {
-  return levels_;
-}
-
-const std::string& TileHierarchy::tile_dir() const {
-  return tile_dir_;
-}
-
-GraphId TileHierarchy::GetGraphId(const midgard::PointLL& pointll, const unsigned char level) const {
+// Returns the GraphId of the requested tile based on a lat,lng and a level.
+// If the level is not supported an invalid id will be returned.
+GraphId TileHierarchy::GetGraphId(const midgard::PointLL& pointll, const uint8_t level) {
   GraphId id;
-  const auto& tl = levels_.find(level);
+  const auto& tl = levels().find(level);
   if(tl != levels_.end()) {
     auto tile_id = tl->second.tiles.TileId(pointll);
     if (tile_id >= 0) {
@@ -37,24 +31,24 @@ GraphId TileHierarchy::GetGraphId(const midgard::PointLL& pointll, const unsigne
 }
 
 // Gets the hierarchy level given the road class.
-uint8_t TileHierarchy::get_level(const RoadClass roadclass) const {
-  if (roadclass <= levels_.find(0)->second.importance) {
+uint8_t TileHierarchy::get_level(const RoadClass roadclass) {
+  if (roadclass <= levels().find(0)->second.importance) {
     return 0;
-  } else if (roadclass <= levels_.find(1)->second.importance) {
+  } else if (roadclass <= levels().find(1)->second.importance) {
     return 1;
   } else {
     return 2;
   }
 }
 
+// Returns all the GraphIds of the tiles which intersect the given bounding
+// box at that level.
 std::vector<GraphId> TileHierarchy::GetGraphIds(
-  const midgard::AABB2<midgard::PointLL> &bbox,
-  uint8_t level) const {
-
+        const midgard::AABB2<midgard::PointLL> &bbox,
+        const uint8_t level) {
   std::vector<GraphId> ids;
-
-  auto itr = levels_.find(level);
-  if (itr != levels_.end()) {
+  auto itr = levels().find(level);
+  if (itr != levels().end()) {
     auto tile_ids = itr->second.tiles.TileList(bbox);
     ids.reserve(tile_ids.size());
 
@@ -62,21 +56,19 @@ std::vector<GraphId> TileHierarchy::GetGraphIds(
       ids.emplace_back(tile_id, level, 0);
     }
   }
-
   return ids;
 }
 
+// Returns all the GraphIds of the tiles which intersect the given bounding
+// box at any level.
 std::vector<GraphId> TileHierarchy::GetGraphIds(
-  const midgard::AABB2<midgard::PointLL> &bbox) const {
-
+        const midgard::AABB2<midgard::PointLL> &bbox) {
   std::vector<GraphId> ids;
-
-  for (const auto &entry : levels_) {
+  for (const auto &entry : levels()) {
     auto level_ids = GetGraphIds(bbox, entry.first);
     ids.reserve(ids.size() + level_ids.size());
     ids.insert(ids.end(), level_ids.begin(), level_ids.end());
   }
-
   return ids;
 }
 
