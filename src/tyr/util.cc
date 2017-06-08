2c27c36a39f3f3574720fefa7359c51119a403fe
Alex Wilton <student.alexwilton@gmail.com>
2017-06-08 09:37:10 -0400

    Parsed the language value from the trip object in json and pushed it to the route trip proto object.

--- src/tyr/util.cc
+++ src/tyr/util.cc
@@ -1,3 +1,4 @@
+#include <string>
 #include "valhalla/tyr/util.h"
 
 #include "proto/route.pb.h"
@@ -6,6 +7,8 @@
 namespace valhalla {
 namespace tyr {
 
+
+
 void jsonRouteToProtoRoute (const std::string& json_route, Route& proto_route) {
   rapidjson::Document d;
   d.Parse (json_route.c_str());
@@ -13,8 +16,7 @@ void jsonRouteToProtoRoute (const std::string& json_route, Route& proto_route) {
   auto json_trip = GetOptionalFromRapidJson<rapidjson::Value::Object> (d, "/trip");
   if (!json_trip) {
     std::cout << "Empty" << std::endl;
-  } else {
-    std::cout << "Not Empty" << std::endl;
+    return;
   }
 
   if (proto_route.has_trip())
@@ -22,7 +24,12 @@ void jsonRouteToProtoRoute (const std::string& json_route, Route& proto_route) {
 
   Route::Trip* route_trip = proto_route.mutable_trip();
 
+  auto language = json_trip->FindMember("language");//GetOptionalFromRapidJson<std::string> (d, "/trip/language");
+  if (language != json_trip->end()) {
+    route_trip->set_language(language->value.GetString());
+  }
 
+  std::cout << "route language: " << *route_trip->mutable_language() << std::endl;
 
 }
 
