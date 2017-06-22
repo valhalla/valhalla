b20760577082414ca69755338b09ba3d26e9bfc6
dnesbitt <dnesbitt61@gmail.com>
2017-06-22 10:25:52 -0400

    Update comments

--- src/meili/traffic_segment_matcher.cc
+++ src/meili/traffic_segment_matcher.cc
@@ -379,10 +379,6 @@ std::vector<traffic_segment_t> TrafficSegmentMatcher::form_segments(const std::l
         end_time = prev->epoch_time + (right->epoch_time - prev->epoch_time) * ratio;
       }
 
-      // start_time and end_time can both be -1 (if the merged segment does not
-      // start a segment and does not end a segment - for example by taking
-      // local roads, so do not break if this occurs...
-
       //figure out the total length of the segment
       int length = start_length != -1 && end_length != -1 ? (end_length - start_length) +.5f : -1;
 
@@ -427,8 +423,7 @@ std::vector<traffic_segment_t> TrafficSegmentMatcher::form_segments(const std::l
       //print(traffic_segments.back());
 
       // Break out of the loop once we are past where we have interpolations
-      // (e.g. for a long edge with many segments). TODO - will this break
-      // too early if there are discontinuities in the trace?
+      // (e.g. for a long edge with many segments).
       if (idx > 0 && right->epoch_time == -1) {
         break;
       }
