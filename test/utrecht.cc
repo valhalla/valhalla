84ff90410d389d508ef3cedcf4e835dfe2ba2f28
Kevin Kreiser <kevinkreiser@gmail.com>
2017-07-17 11:22:56 -0400

    reorganize utrecht test as a model for other tests

--- test/utrecht.cc
+++ test/utrecht.cc
@@ -19,6 +19,10 @@ using namespace valhalla::baldr;
 namespace {
 
 const std::string config_file = "test/test_config_ut";
+std::string ways_file = "test_ways_utrecht.bin";
+std::string way_nodes_file = "test_way_nodes_utrecht.bin";
+std::string access_file = "test_access_utrecht.bin";
+std::string restriction_file = "test_complex_restrictions_utrecht.bin";
 
 const auto node_predicate = [](const OSMWayNode& a, const OSMWayNode& b) {
   return a.node.osmid < b.node.osmid;
@@ -42,18 +46,16 @@ OSMWay GetWay(uint64_t way_id, sequence<OSMWay>& ways) {
   return *found;
 }
 
-
-void Bike(const std::string& config_file) {
+void Parse() {
   boost::property_tree::ptree conf;
-  boost::property_tree::json_parser::read_json(config_file, conf);
-
-  std::string ways_file = "test_ways_utrecht.bin";
-  std::string way_nodes_file = "test_way_nodes_utrecht.bin";
-  std::string access_file = "test_access_utrecht.bin";
-  std::string restriction_file = "test_complex_restrictions_utrecht.bin";
-
+  conf.add_child("mjolnir", boost::property_tree::ptree{}).put("tile_dir", "test/data/parser_tiles");
   auto osmdata = PBFGraphParser::Parse(conf.get_child("mjolnir"), {"test/data/utrecht_netherlands.osm.pbf"}, ways_file,
                                        way_nodes_file, access_file, restriction_file);
+}
+
+void TestBike() {
+  boost::property_tree::ptree conf;
+  conf.add_child("mjolnir", boost::property_tree::ptree{}).put("tile_dir", "test/data/parser_tiles");
   sequence<OSMWay> ways(ways_file, false);
   ways.sort(way_predicate);
 
@@ -122,23 +124,11 @@ void Bike(const std::string& config_file) {
       way.auto_backward() != false || way.bike_backward() != true || way.bus_backward() != false) {
     throw std::runtime_error("Access is not correct for way 7007629.");
   }
-
-  boost::filesystem::remove(ways_file);
-  boost::filesystem::remove(way_nodes_file);
-  boost::filesystem::remove(access_file);
 }
 
-void Bus(const std::string& config_file) {
+void TestBus() {
   boost::property_tree::ptree conf;
-  boost::property_tree::json_parser::read_json(config_file, conf);
-
-  std::string ways_file = "test_ways_utrecht.bin";
-  std::string way_nodes_file = "test_way_nodes_utrecht.bin";
-  std::string access_file = "test_access_utrecht.bin";
-  std::string restriction_file = "test_complex_restrictions_utrecht.bin";
-
-  auto osmdata = PBFGraphParser::Parse(conf.get_child("mjolnir"), {"test/data/utrecht_netherlands.osm.pbf"},
-                                       ways_file, way_nodes_file, access_file, restriction_file);
+  conf.add_child("mjolnir", boost::property_tree::ptree{}).put("tile_dir", "test/data/parser_tiles");
   sequence<OSMWay> ways(ways_file, false);
   ways.sort(way_predicate);
 
@@ -147,36 +137,12 @@ void Bus(const std::string& config_file) {
       way.auto_backward() != false || way.bike_backward() != true || way.bus_backward() != true) {
     throw std::runtime_error("Access is not correct for way 33648196.");
   }
+}
 
+void TearDown() {
   boost::filesystem::remove(ways_file);
   boost::filesystem::remove(way_nodes_file);
   boost::filesystem::remove(access_file);
-}
-
-void TestBike() {
-  //write the tiles with it
-  Bike(config_file);
-}
-
-void TestBus() {
-  //write the tiles with it
-  Bus(config_file);
-}
-
-void DoConfig() {
-  std::ofstream file;
-  try {
-    file.open(config_file, std::ios_base::trunc);
-    file << "{ \
-      \"mjolnir\": { \
-      \"tile_dir\": \"test/data/parser_tiles\" \
-      } \
-    }";
-  }
-  catch(...) {
-
-  }
-  file.close();
 }
 
 }
@@ -185,9 +151,10 @@ int main() {
 
   test::suite suite("utrecht");
 
-  suite.test(TEST_CASE(DoConfig));
+  suite.test(TEST_CASE(Parse));
   suite.test(TEST_CASE(TestBike));
   suite.test(TEST_CASE(TestBus));
+  suite.test(TEST_CASE(TearDown));
 
   return suite.tear_down();
 }
