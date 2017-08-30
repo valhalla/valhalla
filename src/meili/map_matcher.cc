9a32fe8571237e07c198ad1aa3547c2a23ecfcc4
Kevin Kreiser <kevinkreiser@gmail.com>
2017-08-30 13:26:28 -0400

    be paranoid about having partial time data in a map match

--- src/meili/map_matcher.cc
+++ src/meili/map_matcher.cc
@@ -27,7 +27,8 @@ GreatCircleDistance(const Measurement& left,
 inline float
 ClockDistance(const Measurement& left,
               const Measurement& right)
-{ return right.epoch_time() - left.epoch_time(); }
+{ return right.epoch_time() > 0 && left.epoch_time() >= 0 ?
+    right.epoch_time() - left.epoch_time() : 0; }
 
 
 struct Interpolation {
