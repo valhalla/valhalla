459fc23ad2ab313cde7bdf0df84df12d10419ea5
Kevin Kreiser <kevinkreiser@gmail.com>
2018-01-18 23:00:43 -0500

    less sudo

--- .circleci/make_test.sh
+++ .circleci/make_test.sh
@@ -2,9 +2,9 @@
 set -e
 
 # get some dependencies
-sudo add-apt-repository -y ppa:valhalla-core/valhalla
-sudo apt-get update
-sudo apt-get install -y autoconf automake make libtool pkg-config g++ gcc jq lcov protobuf-compiler vim-common libboost1.54-all-dev libboost-all-dev libcurl4-openssl-dev libgeos-dev libgeos++-dev liblua5.2-dev libprime-server0.6.3-dev libprotobuf-dev libspatialite-dev libsqlite3-dev libsqlite3-mod-spatial python-all-dev zlib1g-dev liblz4-dev lua5.2 prime-server0.6.3-bin ccache
+add-apt-repository -y ppa:valhalla-core/valhalla
+apt-get update
+apt-get install -y autoconf automake make libtool pkg-config g++ gcc jq lcov protobuf-compiler vim-common libboost1.54-all-dev libboost-all-dev libcurl4-openssl-dev libgeos-dev libgeos++-dev liblua5.2-dev libprime-server0.6.3-dev libprotobuf-dev libspatialite-dev libsqlite3-dev libsqlite3-mod-spatial python-all-dev zlib1g-dev liblz4-dev lua5.2 prime-server0.6.3-bin ccache
 
 # test some easier stuff
 for f in *.json; do
