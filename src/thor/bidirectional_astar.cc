e3ea35368ca9b47576ae916c2189b6e551018fcd
Kevin Kreiser <kevinkreiser@gmail.com>
2018-03-12 10:22:05 -0400

    dont need mutable

--- src/thor/bidirectional_astar.cc
+++ src/thor/bidirectional_astar.cc
@@ -619,7 +619,7 @@ void BidirectionalAStar::SetOrigin(GraphReader& graphreader,
 
   // Set the origin timezone
   if (nodeinfo != nullptr && origin.has_date_time() &&
-      *origin.mutable_date_time() == "current") {
+      origin.date_time() == "current") {
     origin.date_time() == DateTime::iso_date_time(
     		DateTime::get_tz_db().from_index(nodeinfo->timezone()));
   }
