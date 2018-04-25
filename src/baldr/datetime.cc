5a5ff6e77f4016a201ecd17377c2928618685d14
Greg Knisely <greg.knisely@mapbox.com>
2018-04-25 13:46:07 -0400

    added set methods.  added range/validity checks to set methods where fields can overflow.  made the daterange protected.

--- src/baldr/datetime.cc
+++ src/baldr/datetime.cc
@@ -1140,8 +1140,8 @@ std::vector<uint64_t> get_time_range(const std::string& str) {
           stream.ignore();
           stream >> min;
 
-          timedomain.daterange.begin_hrs = hour;
-          timedomain.daterange.begin_mins = min;
+          timedomain.set_begin_hrs(hour);
+          timedomain.set_begin_mins(min);
 
           //process the hour off
           found = on_off.at(1).find(":");
@@ -1156,10 +1156,10 @@ std::vector<uint64_t> get_time_range(const std::string& str) {
           stream.ignore();
           stream >> min;
 
-          timedomain.daterange.end_hrs = hour;
-          timedomain.daterange.end_mins = min;
+          timedomain.set_end_hrs(hour);
+          timedomain.set_end_mins(min);
 
-          time_domains.push_back(timedomain.value);
+          time_domains.push_back(timedomain.td_value());
 
         }
       }
@@ -1226,71 +1226,70 @@ std::vector<uint64_t> get_time_range(const std::string& str) {
 
         //Feb#16-Oct#15
         if (months_dow.size() == 4 && is_date && is_range) {
-          timedomain.daterange.begin_month = static_cast<uint8_t>(get_month(months_dow.at(0)));
-          timedomain.daterange.begin_day_dow = std::stoi(months_dow.at(1));
+          timedomain.set_type(kYMD);
+          timedomain.set_begin_month(static_cast<uint8_t>(get_month(months_dow.at(0))));
+          timedomain.set_begin_day_dow(std::stoi(months_dow.at(1)));
 
-          timedomain.daterange.end_month = static_cast<uint8_t>(get_month(months_dow.at(2)));
-          timedomain.daterange.end_day_dow = std::stoi(months_dow.at(3));
+          timedomain.set_end_month(static_cast<uint8_t>(get_month(months_dow.at(2))));
+          timedomain.set_end_day_dow(std::stoi(months_dow.at(3)));
 
-          timedomain.daterange.type = kYMD;
           break;
         } //May 16-31
         else if (months_dow.size() == 3 && is_date && is_range) {
-          timedomain.daterange.begin_month = static_cast<uint8_t>(get_month(months_dow.at(0)));
-          timedomain.daterange.begin_day_dow = std::stoi(months_dow.at(1));
+          timedomain.set_type(kYMD);
+          timedomain.set_begin_month(static_cast<uint8_t>(get_month(months_dow.at(0))));
+          timedomain.set_begin_day_dow(std::stoi(months_dow.at(1)));
 
-          timedomain.daterange.end_month = timedomain.daterange.begin_month;
-          timedomain.daterange.end_day_dow = std::stoi(months_dow.at(2));
-
-          timedomain.daterange.type = kYMD;
+          timedomain.set_end_month(timedomain.begin_month());
+          timedomain.set_end_day_dow(std::stoi(months_dow.at(2)));
           break;
         }
         //Apr-Sep or May 15
         else if (months_dow.size() == 2) {
 
-          timedomain.daterange.begin_month = static_cast<uint8_t>(get_month(months_dow.at(0)));
+          timedomain.set_begin_month(static_cast<uint8_t>(get_month(months_dow.at(0))));
           baldr::MONTH month = get_month(months_dow.at(1));
 
           if (month != MONTH::kNone) {
-            timedomain.daterange.end_month = static_cast<uint8_t>(month);
-            timedomain.daterange.type = kYMD;
+            timedomain.set_type(kYMD);
+            timedomain.set_end_month(static_cast<uint8_t>(month));
           } else if (is_date) { // May 15
-            timedomain.daterange.type = kYMD;
-            timedomain.daterange.begin_day_dow = std::stoi(months_dow.at(1));
-            timedomain.daterange.end_month = timedomain.daterange.begin_month;
-            timedomain.daterange.end_day_dow = timedomain.daterange.begin_day_dow;
+            timedomain.set_type(kYMD);
+            timedomain.set_begin_day_dow(std::stoi(months_dow.at(1)));
+            timedomain.set_end_month(timedomain.begin_month());
+            timedomain.set_end_day_dow(timedomain.begin_day_dow());
           } else {
             return time_domains;
           }
 
           break;
         } else if (months_dow.size() == 1) {//May
-          timedomain.daterange.type = kYMD;
-          timedomain.daterange.begin_month = static_cast<uint8_t>(get_month(months_dow.at(0)));
-          timedomain.daterange.end_month = static_cast<uint8_t>(get_month(months_dow.at(0)));
+          timedomain.set_type(kYMD);
+          timedomain.set_begin_month(static_cast<uint8_t>(get_month(months_dow.at(0))));
+          timedomain.set_end_month(static_cast<uint8_t>(get_month(months_dow.at(0))));
           break;
         } else if (is_nth_week) {//Oct Su[-1]-Mar Su[4] Su 09:00-16:00
           if (get_month(md) != MONTH::kNone) {
 
-            timedomain.daterange.type = kNthDow;
-            if (timedomain.daterange.begin_month == 0) {
+            timedomain.set_type(kNthDow);
+            if (timedomain.begin_month() == 0) {
 
               //assume the restriction is the entire week.
-              timedomain.daterange.dow = kAllDaysOfWeek;
+              timedomain.set_dow(kAllDaysOfWeek);
 
-              timedomain.daterange.begin_month = static_cast<uint8_t>(get_month(md));
+              timedomain.set_begin_month(static_cast<uint8_t>(get_month(md)));
               //assume no range.  Dec Su[-1] Su-Sa 15:00-17:00 starts on the last week
               //in Dec and ends in the last week in Dec
               if (!is_range)
-                timedomain.daterange.end_month = timedomain.daterange.begin_month;
+                timedomain.set_end_month(timedomain.begin_month());
             } else {
-              timedomain.daterange.end_month = static_cast<uint8_t>(get_month(md));
+              timedomain.set_end_month(static_cast<uint8_t>(get_month(md)));
 
               if (is_range && is_date &&
                   md != months_dow.at(months_dow.size()-1)) { //Dec Su[-1]-Mar 3 Sat
 
                 if (months_dow.at(months_dow.size()-1).find('[') == std::string::npos) {
-                  timedomain.daterange.end_day_dow = std::stoi(months_dow.at(months_dow.size()-1));
+                  timedomain.set_end_day_dow(std::stoi(months_dow.at(months_dow.size()-1)));
                   break;
                 } else ends_nth_week = true;
               }
@@ -1298,23 +1297,23 @@ std::vector<uint64_t> get_time_range(const std::string& str) {
 
           } else if (get_dow(md) != DOW::kNone) {
 
-            if (timedomain.daterange.begin_day_dow == 0)
-              timedomain.daterange.begin_day_dow = static_cast<uint8_t>(get_dow(md));
-            else timedomain.daterange.end_day_dow = static_cast<uint8_t>(get_dow(md));
+            if (timedomain.begin_day_dow() == 0)
+              timedomain.set_begin_day_dow(static_cast<uint8_t>(get_dow(md)));
+            else timedomain.set_end_day_dow(static_cast<uint8_t>(get_dow(md)));
 
           } else if (md.find('[') != std::string::npos && md.find(']') != std::string::npos) {
             md.erase(boost::remove_if(md, boost::is_any_of("[]")), md.end());
 
-            if (timedomain.daterange.begin_week == 0 && !ends_nth_week) {
-              timedomain.daterange.begin_week = std::stoi(md);
+            if (timedomain.begin_week() == 0 && !ends_nth_week) {
+              timedomain.set_begin_week(std::stoi(md));
               //assume no range.  Dec Su[-1] Su-Sa 15:00-17:00 starts on the last week
               //in Dec and ends in the last week in Dec
               if (!is_range)
-                timedomain.daterange.end_week = timedomain.daterange.begin_week;
-            } else timedomain.daterange.end_week = std::stoi(md);
-          } else if (is_date && is_range && timedomain.daterange.begin_month != 0 &&
-              timedomain.daterange.end_month == 0) { //Mar 3-Dec Su[-1] Sat
-              timedomain.daterange.begin_day_dow = std::stoi(md);
+                timedomain.set_end_week(timedomain.begin_week());
+            } else timedomain.set_end_week(std::stoi(md));
+          } else if (is_date && is_range && timedomain.begin_month() != 0 &&
+              timedomain.end_month() == 0) { //Mar 3-Dec Su[-1] Sat
+              timedomain.set_begin_day_dow(std::stoi(md));
           }
         }
       }
@@ -1324,28 +1323,28 @@ std::vector<uint64_t> get_time_range(const std::string& str) {
       //Mo,We,Th,Fr
       if (!is_range) {
         //wipe out assumption that this restriction is for the entire week.
-        if (timedomain.daterange.type == kNthDow)
-          timedomain.daterange.dow = 0;
+        if (timedomain.type() == kNthDow)
+          timedomain.set_dow(0);
 
         for (auto& md : months_dow){
-          timedomain.daterange.dow += get_dow_mask(md);
+          timedomain.set_dow(timedomain.dow() + get_dow_mask(md));
         }
 
         if (months_dow_times.size() == 2) {
           std::string week = months_dow_times.at(1);
           //Su[1] every 1st Sunday of every month.
           if (week.find('[') != std::string::npos && week.find(']') != std::string::npos) {
-            timedomain.daterange.type = kNthDow;
+            timedomain.set_type(kNthDow);
             week.erase(boost::remove_if(week, boost::is_any_of("[]")), week.end());
-            timedomain.daterange.begin_week = std::stoi(week);
+            timedomain.set_begin_week(std::stoi(week));
             break;
           }
         }
       //Mo-Fr
       } else if (months_dow.size() == 2) {
         //wipe out assumption that this restriction is for the entire week.
-        if (timedomain.daterange.type == kNthDow)
-          timedomain.daterange.dow = 0;
+        if (timedomain.type() == kNthDow)
+          timedomain.set_dow(0);
 
         uint8_t b_index = static_cast<uint8_t>(get_dow(months_dow.at(0)));
         uint8_t e_index = static_cast<uint8_t>(get_dow(months_dow.at(1)));
@@ -1353,14 +1352,14 @@ std::vector<uint64_t> get_time_range(const std::string& str) {
         if (b_index > e_index) { //Th - Tu
 
           while (b_index <= static_cast<uint8_t>(DOW::kSaturday)) {
-            timedomain.daterange.dow += (1 << (b_index-1));
+            timedomain.set_dow(timedomain.dow() + (1 << (b_index-1)));
             b_index++;
           }
           b_index = static_cast<uint8_t>(DOW::kSunday);
         }
 
         while (b_index <= e_index) {
-          timedomain.daterange.dow += (1 << (b_index-1));
+          timedomain.set_dow(timedomain.dow() + (1 << (b_index-1)));
           b_index++;
         }
 
@@ -1396,8 +1395,8 @@ std::vector<uint64_t> get_time_range(const std::string& str) {
           stream.ignore();
           stream >> min;
 
-          timedomain.daterange.begin_hrs = hour;
-          timedomain.daterange.begin_mins = min;
+          timedomain.set_begin_hrs(hour);
+          timedomain.set_begin_mins(min);
 
           //process the hour off
           found = on_off.at(1).find(":");
@@ -1412,18 +1411,18 @@ std::vector<uint64_t> get_time_range(const std::string& str) {
           stream.ignore();
           stream >> min;
 
-          timedomain.daterange.end_hrs = hour;
-          timedomain.daterange.end_mins = min;
+          timedomain.set_end_hrs(hour);
+          timedomain.set_end_mins(min);
 
-          time_domains.push_back(timedomain.value);
+          time_domains.push_back(timedomain.td_value());
         }
       }
     }
   }
 
   //no time.
-  if (time_domains.size() == 0 && timedomain.value)
-    time_domains.push_back(timedomain.value);
+  if (time_domains.size() == 0 && timedomain.td_value())
+    time_domains.push_back(timedomain.td_value());
 
   return time_domains;
 }
--- valhalla/baldr/timedomain.h
+++ valhalla/baldr/timedomain.h
@@ -2,14 +2,21 @@
 #define VALHALLA_BALDR_TIMEDOMAIN_H_
 
 #include <valhalla/baldr/graphconstants.h>
+#include "midgard/logging.h"
 #include <vector>
 
 namespace valhalla {
 namespace baldr {
 
-constexpr uint32_t kYMD    = 0;
-constexpr uint32_t kNthDow = 1;
-
+constexpr uint32_t kYMD     = 0;
+constexpr uint32_t kNthDow  = 1;
+constexpr uint32_t kMaxDateRangeWeek = 5;
+constexpr uint32_t kMaxDateRangeDowMask = 127;
+constexpr uint32_t kMaxDateRangeHrs = 23;
+constexpr uint32_t kMaxDateRangeMins = 59;
+constexpr uint32_t kMaxDateRangeMonth = 12;
+constexpr uint32_t kMaxDateRangeDay = 31;
+constexpr uint32_t kMaxDateRangeDow = 7;
 union TimeDomain {
  public:
   /**
@@ -28,10 +35,174 @@ union TimeDomain {
   }
 
   /**
+    * Set the type.
+    * @param  v  the type.
+    */
+   void set_type(const bool type) {
+     daterange.type = type;
+   }
+
+   /**
+    * Set the days of week this time domain is active.
+    * @param dow  the dow.  This is a mask (e.g., Mo-Fr = 62)
+    */
+   void set_dow(const uint8_t dow) {
+     if (dow > kMaxDateRangeDowMask) {
+       LOG_WARN("Exceeding max dow value. Skipping");
+     } else {
+       daterange.dow = dow;
+     }
+   }
+
+   /**
+    * Set the hour when this time domain starts
+    * @param begin_hrs  the begin_hrs.
+    */
+   void set_begin_hrs(const uint8_t begin_hrs) {
+     if (begin_hrs == 24) {
+       daterange.begin_hrs = 0;
+     } else if (begin_hrs > kMaxDateRangeHrs) {
+       LOG_WARN("Exceeding max begin hrs value. Skipping");
+     } else {
+       daterange.begin_hrs = begin_hrs;
+     }
+   }
+
+   /**
+    * Set the min when this time domain starts
+    * @param begin_mins  the begin_mins.
+    */
+   void set_begin_mins(const uint8_t begin_mins) {
+     if (begin_mins == 60) {
+       daterange.begin_mins = 0;
+     } else if (begin_mins > kMaxDateRangeMins) {
+       LOG_WARN("Exceeding max begin mins value. Skipping");
+     } else {
+       daterange.begin_mins = begin_mins;
+     }
+   }
+
+   /**
+    * Set the month when this time domain starts
+    * @param begin_month  the begin_month.
+    */
+   void set_begin_month(const uint8_t begin_month) {
+     if (begin_month > kMaxDateRangeMonth) {
+       LOG_WARN("Exceeding max begin month value. Skipping");
+     } else {
+       daterange.begin_month = begin_month;
+     }
+   }
+
+   /**
+    * Set the day when this time domain starts
+    * @param beging_day_dow   the begin_day_dow.
+    *                         begin_day_dow = begin day or dow enum (i.e. 1st Sunday in some month)
+    */
+   void set_begin_day_dow(const uint8_t begin_day_dow) {
+     if (daterange.type == kYMD && begin_day_dow > kMaxDateRangeDay) {
+       LOG_WARN("Exceeding max begin day value. Skipping");
+     } else if (daterange.type == kNthDow && begin_day_dow > kMaxDateRangeDow) {
+       LOG_WARN("Exceeding max begin dow value. Skipping");
+     } else {
+       daterange.begin_day_dow = begin_day_dow;
+     }
+   }
+
+   /**
+    * Set the week when this time domain starts
+    *@param begin_week    the begin_week.
+    *                     which week(1-5) does this start. (i.e. 1st week in Oct)
+    */
+   void set_begin_week(const uint8_t begin_week) {
+     if (begin_week > kMaxDateRangeWeek) {
+       LOG_WARN("Exceeding max begin week value. Skipping");
+     } else {
+       daterange.begin_week = begin_week;
+     }
+   }
+
+   /**
+    * Set the hour when this time domain ends
+    * @param end_hrs  the end_hrs.
+    */
+   void set_end_hrs(const uint8_t end_hrs) {
+     if (end_hrs == 24) {
+       daterange.end_hrs = 0;
+     } else if (end_hrs > kMaxDateRangeHrs) {
+       LOG_WARN("Exceeding max end hrs value. Skipping");
+     } else {
+       daterange.end_hrs = end_hrs;
+     }
+   }
+
+   /**
+    * Set the min when this time domain ends
+    * @return end_mins  the end_mins.
+    */
+   void set_end_mins(const uint8_t end_mins) {
+     if (end_mins == 60) {
+       daterange.end_mins = 0;
+     } else if (end_mins > kMaxDateRangeMins) {
+       LOG_WARN("Exceeding max end mins value. Skipping");
+     } else {
+       daterange.end_mins = end_mins;
+     }
+   }
+
+   /**
+    * Set the month when this time domain ends
+    * @param end_month  the end_month.
+    */
+   void set_end_month(const uint8_t end_month) {
+     if (end_month > kMaxDateRangeMonth) {
+       LOG_WARN("Exceeding max end month value. Skipping");
+     } else {
+       daterange.end_month = end_month;
+     }
+   }
+
+   /**
+    * Set the day when this time domain ends
+    * @param end_day_dow    the end_day_dow.
+    *                       end_day_dow = end day or dow enum (i.e. 1st Sunday in some month)
+    */
+   void set_end_day_dow(const uint8_t end_day_dow) {
+     if (daterange.type == kYMD && end_day_dow > kMaxDateRangeDay) {
+       LOG_WARN("Exceeding max end day value. Skipping");
+     } else if (daterange.type == kNthDow && end_day_dow > kMaxDateRangeDow) {
+       LOG_WARN("Exceeding max end dow value. Skipping");
+     } else {
+       daterange.end_day_dow = end_day_dow;
+     }
+   }
+
+   /**
+    * Set the week when this time domain ends
+    * @param end_week   the end_week.
+    *                   which week(1-5) does this end. (i.e. 1st week in Oct)
+    */
+   void set_end_week(const uint8_t end_week) {
+     if (end_week > kMaxDateRangeWeek) {
+       LOG_WARN("Exceeding max end week value. Skipping");
+     } else {
+       daterange.end_week = end_week;
+     }
+   }
+
+   /**
+    * Gets the value.
+    * @return   Returns the value.
+    */
+   uint64_t td_value() const {
+     return value;
+   }
+
+  /**
    * Gets the type.
    * @return   Returns the type.
    */
-  uint32_t type() const {
+   uint8_t type() const {
     return daterange.type;
   }
 
@@ -39,7 +210,7 @@ union TimeDomain {
    * Gets the days of week this time domain is active.
    * @return   Returns the dow.  This is a mask (e.g., Mo-Fr = 62)
    */
-  uint32_t dow() const {
+  uint8_t dow() const {
     return daterange.dow;
   }
 
@@ -47,7 +218,7 @@ union TimeDomain {
    * Gets the hour when this time domain starts
    * @return   Returns the begin_hrs.
    */
-  uint32_t begin_hrs() const {
+  uint8_t begin_hrs() const {
     return daterange.begin_hrs;
   }
 
@@ -55,7 +226,7 @@ union TimeDomain {
    * Gets the min when this time domain starts
    * @return   Returns the begin_mins.
    */
-  uint32_t begin_mins() const {
+  uint8_t begin_mins() const {
     return daterange.begin_mins;
   }
 
@@ -63,7 +234,7 @@ union TimeDomain {
    * Gets the month when this time domain starts
    * @return   Returns the begin_month.
    */
-  uint32_t begin_month() const {
+  uint8_t begin_month() const {
     return daterange.begin_month;
   }
 
@@ -72,16 +243,7 @@ union TimeDomain {
    * @return   Returns the begin_day_dow.
    *           begin_day_dow = begin day or dow enum (i.e. 1st Sunday in some month)
    */
-  uint32_t begin_day() const {
-    return daterange.begin_day_dow;
-  }
-
-  /**
-   * Gets the dow when this time domain starts
-   * @return   Returns the begin_day_dow.
-   *           begin_day_dow = begin day or dow enum (i.e. 1st Sunday in some month)
-   */
-  uint32_t begin_dow() const {
+  uint8_t begin_day_dow() const {
     return daterange.begin_day_dow;
   }
 
@@ -90,7 +252,7 @@ union TimeDomain {
    * @return   Returns the begin_week.
    *           which week(1-5) does this start. (i.e. 1st week in Oct)
    */
-  uint32_t begin_week() const {
+  uint8_t begin_week() const {
     return daterange.begin_week;
   }
 
@@ -98,7 +260,7 @@ union TimeDomain {
    * Gets the hour when this time domain ends
    * @return   Returns the end_hrs.
    */
-  uint32_t end_hrs() const {
+  uint8_t end_hrs() const {
     return daterange.end_hrs;
   }
 
@@ -106,7 +268,7 @@ union TimeDomain {
    * Gets the min when this time domain ends
    * @return   Returns the end_mins.
    */
-  uint32_t end_mins() const {
+  uint8_t end_mins() const {
     return daterange.end_mins;
   }
 
@@ -114,7 +276,7 @@ union TimeDomain {
    * Gets the month when this time domain ends
    * @return   Returns the end_month.
    */
-  uint32_t end_month() const {
+  uint8_t end_month() const {
     return daterange.end_month;
   }
 
@@ -123,16 +285,7 @@ union TimeDomain {
    * @return   Returns the end_day_dow.
    *           end_day_dow = end day or dow enum (i.e. 1st Sunday in some month)
    */
-  uint32_t end_day() const {
-    return daterange.end_day_dow;
-  }
-
-  /**
-   * Gets the dow when this time domain ends
-   * @return   Returns the end_day_dow.
-   *           end_day_dow = end day or dow enum (i.e. 1st Sunday in some month)
-   */
-  uint32_t end_dow() const {
+  uint8_t end_day_dow() const {
     return daterange.end_day_dow;
   }
 
@@ -141,7 +294,7 @@ union TimeDomain {
    * @return   Returns the end_week.
    *           which week(1-5) does this end. (i.e. 1st week in Oct)
    */
-  uint32_t end_week() const {
+  uint8_t end_week() const {
     return daterange.end_week;
   }
 
@@ -160,6 +313,7 @@ union TimeDomain {
     return value;
   }
 
+ protected:
   struct DateRange {
     uint64_t type             :  1;
     uint64_t dow              :  7; // day of week for this restriction
