760a5d35e43bdc47e9d8f723696e636d504d4816
Daniel Patterson <danpat@danpat.net>
2018-05-16 00:40:11 +0000

    Enable clang-tidy on builds.

--- .circleci/make_test.sh
+++ .circleci/make_test.sh
@@ -5,7 +5,7 @@ set -e
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:valhalla-core/valhalla
 apt-get update
-apt-get install -y autoconf automake make libtool pkg-config g++ gcc jq lcov locales coreutils protobuf-compiler vim-common libboost-all-dev libcurl4-openssl-dev libgeos-dev libgeos++-dev liblua5.2-dev libprime-server0.6.3-dev libprotobuf-dev libspatialite-dev libsqlite3-dev libsqlite3-mod-spatialite python-all-dev zlib1g-dev liblz4-dev lua5.2 prime-server0.6.3-bin ccache
+apt-get install -y autoconf automake make libtool pkg-config g++ gcc jq lcov locales coreutils protobuf-compiler vim-common libboost-all-dev libcurl4-openssl-dev libgeos-dev libgeos++-dev liblua5.2-dev libprime-server0.6.3-dev libprotobuf-dev libspatialite-dev libsqlite3-dev libsqlite3-mod-spatialite python-all-dev zlib1g-dev liblz4-dev lua5.2 prime-server0.6.3-bin ccache bear clang-tidy-5.0 clang-5.0
 
 # test some easier stuff
 for f in *.json; do
@@ -25,5 +25,5 @@ mkdir -p ${HOME}/.ccache && echo "max_size = 4.0G" > ${HOME}/.ccache/ccache.conf
 ./configure CC="ccache gcc" CXX="ccache g++" #--enable-coverage
 ccache -z
 ccache -s
-make test -j1 || true
+bear make test -j1 || true
 ccache -s
