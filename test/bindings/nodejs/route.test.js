1595ff151617229be2825ae6dcfbf1414909fed0
allierowan <allie.rowan@mapbox.com>
2018-07-31 12:17:32 -0400

    refactor so that we return when we throw a napi error

--- test/bindings/nodejs/route.test.js
+++ test/bindings/nodejs/route.test.js
@@ -3,6 +3,13 @@ var config = require('./fixtures/basic_config');
 var Valhalla = require('../../../')(JSON.stringify(config));
 var valhalla = new Valhalla(JSON.stringify(config));
 
+test('route: request sends clear error if called with undefined', function(assert) {
+  assert.throws(() => { valhalla.route() }, /Failed to get argument string length/, 'Throws error when called with no arg');
+  assert.throws(() => { valhalla.route(null) }, /Failed to get argument string length/, 'Throws error when called with null');
+  assert.throws(() => { valhalla.route(undefined) }, /Failed to get argument string length/, 'Throws error when called with undefined');
+  assert.end();
+});
+
 test('route: can get a route in Hershey', function(assert) {
   var hersheyRequest = '{"locations":[{"lat":40.546115,"lon":-76.385076,"type":"break"}, {"lat":40.544232,"lon":-76.385752,"type":"break"}],"costing":"auto"}';
   var route = JSON.parse(valhalla.route(hersheyRequest));
