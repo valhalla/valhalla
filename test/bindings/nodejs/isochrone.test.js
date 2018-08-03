696098e49b3c98b67d8098cf50e9f916a8836e24
Allie Rowan <allie.rowan@mapbox.com>
2018-08-03 10:30:39 -0400

    Port to node-addon-api and make async (#1457)
    * port the node bindings to use node-addon-api and make use of it's async
    functionality. THrow better errors
    
    * package.json

--- test/bindings/nodejs/isochrone.test.js
+++ test/bindings/nodejs/isochrone.test.js
@@ -5,23 +5,31 @@ var valhalla = new Valhalla(JSON.stringify(config));
 
 test('isochrone: can return info for locations', function(assert) {
   var isochroneRequest = '{"locations":[{"lat":40.546115,"lon":-76.385076}],"costing":"pedestrian","contours":[{"time":15,"color":"ff0000"}]}';
-  var isochrone = JSON.parse(valhalla.isochrone(isochroneRequest));
-  assert.ok(isochrone);
-  assert.equal(isochrone.features.length, 1, 'returns one feature');
-  assert.equal(isochrone.features[0].geometry.type, 'LineString', 'feature returned is a linestring');
-  assert.ok(isochrone.features[0].geometry.coordinates, 'feature has geometry with coordinates');
-  assert.equal(isochrone.features[0].properties.color, '#ff0000', 'color returned in properties matches request');
-  assert.end();
+  valhalla.isochrone(isochroneRequest, (err, result) => {
+    var isochrone = JSON.parse(result);
+    assert.ok(isochrone);
+    assert.equal(isochrone.features.length, 1, 'returns one feature');
+    assert.equal(isochrone.features[0].geometry.type, 'LineString', 'feature returned is a linestring');
+    assert.ok(isochrone.features[0].geometry.coordinates, 'feature has geometry with coordinates');
+    assert.equal(isochrone.features[0].properties.color, '#ff0000', 'color returned in properties matches request');
+    assert.end();
+  });
 });
 
 test('isochrone: throws error if can\'t match input to map', function(assert) {
   var badRequest = '{"locations":[{"lat":50,"lon":-76.385076}],"costing":"pedestrian","contours":[{"time":15,"color":"ff0000"}]}';
-  assert.throws(() => { valhalla.isochrone(badRequest), /Error: No suitable edges near location/, 'Throws an error when cant match input to map'});
-  assert.end();
+  valhalla.isochrone(badRequest, (err, result) => {
+    assert.notOk(result, 'Should not pass back result');
+    assert.equal(err.message, '{"error_code":171,"http_code":400,"message":"No suitable edges near location"}', 'Throws an error when cant match input to map with correct codes');
+    assert.end();
+  });
 });
 
 test('isochrone: returns an error if request format is wrong', function(assert) {
   var badRequest = '{"locations":[40.546115,-76.385076], [40.544232,"lon":-76.385752],"costing":"auto"}';
-  assert.throws(() => { valhalla.isochrone(badRequest) }, /std::exception/, 'Throws an error when request format is wrong');
-  assert.end();
+  valhalla.isochrone(badRequest, (err, result) => {
+    assert.notOk(result, 'Should not pass back result');
+    assert.equal(err.message, '{"error_code":100,"http_code":400,"message":"Failed to parse json request"}', 'Throws an error when request format is wrong');
+    assert.end();
+  });
 });
--- test/bindings/nodejs/optimized_route.test.js
+++ test/bindings/nodejs/optimized_route.test.js
@@ -5,23 +5,31 @@ var valhalla = new Valhalla(JSON.stringify(config));
 
 test('optimizedRoute: can return info for locations', function(assert) {
   var optimizedRouteRequest = '{"locations":[{"lat":40.546115,"lon":-76.385076},{"lat":40.544232,"lon":-76.385752},{"lat":40.543152,"lon":-76.385862},{"lat":40.543952,"lon":-76.386162}],"costing":"auto","directions_options":{"units":"miles"}}';
-  var optimizedRoute = JSON.parse(valhalla.optimizedRoute(optimizedRouteRequest));
-  assert.ok(optimizedRoute);
-  assert.equal(optimizedRoute.trip.status_message, 'Found route between points', 'has successful status message');
-  assert.equal(optimizedRoute.trip.legs.length, 1, 'returns one leg');
-  assert.equal(optimizedRoute.trip.legs[0].maneuvers.length, 6, 'returns 6 maneuvers');
-  assert.equal(optimizedRoute.trip.locations.length, 4, 'returns back the 4 input locations');
-  assert.end();
+  valhalla.optimizedRoute(optimizedRouteRequest, (err, result) => {
+    var optimizedRoute = JSON.parse(result);
+    assert.ok(optimizedRoute);
+    assert.equal(optimizedRoute.trip.status_message, 'Found route between points', 'has successful status message');
+    assert.equal(optimizedRoute.trip.legs.length, 1, 'returns one leg');
+    assert.equal(optimizedRoute.trip.legs[0].maneuvers.length, 6, 'returns 6 maneuvers');
+    assert.equal(optimizedRoute.trip.locations.length, 4, 'returns back the 4 input locations');
+    assert.end();
+  });
 });
 
 test('optimizedRoute: throws error if can\'t match input to map', function(assert) {
   var badRequest = '{"locations":[{"lat":50,"lon":-76.385076}],"costing":"pedestrian","contours":[{"time":15,"color":"ff0000"}]}';
-  assert.throws(() => { valhalla.optimizedRoute(badRequest), /Error: No suitable edges near location/, 'Throws an error when cant match input to map'});
-  assert.end();
+  valhalla.optimizedRoute(badRequest, (err, result) => {
+    assert.notOk(result, 'Should not pass back result');
+    assert.equal(err.message, '{"error_code":120,"http_code":400,"message":"Insufficient number of locations provided"}', 'Throws an error when can\'t match input to map with correct codes');
+    assert.end();;
+  });
 });
 
 test('optimizedRoute: returns an error if request format is wrong', function(assert) {
   var badRequest = '{"locations":[40.546115,-76.385076], [40.544232,"lon":-76.385752],"costing":"auto"}';
-  assert.throws(() => { valhalla.optimizedRoute(badRequest) }, /std::exception/, 'Throws an error when request format is wrong');
-  assert.end();
+  valhalla.optimizedRoute(badRequest, (err, result) => {
+    assert.notOk(result, 'Should not pass back result');
+    assert.equal(err.message, '{"error_code":100,"http_code":400,"message":"Failed to parse json request"}', 'Throws an error when request format is wrong');
+    assert.end()
+  });
 });
