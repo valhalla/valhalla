c4584386fb3a8424848d235b3e35dab32230223c
kdiluca <kristen.diluca@mapbox.com>
2018-08-09 09:42:03 -0400

    added a description comment at beginning of script

--- run_route_scripts/generate_tests/create_test_request_routes.py
+++ run_route_scripts/generate_tests/create_test_request_routes.py
@@ -5,6 +5,14 @@ import random
 import json
 import re
 
+
+### This script can take either a *_locations.txt file generated with randomness
+### (created from create_random_points_within_radius) or a manually generated location set.
+### The create_routes function with create routes from each location in the list to
+### every other location in the list.  You can specify the number of input locations.
+### Example: 100 input locations create approx. 10k routes.
+
+
 lls = []
 
 def read_locations():
@@ -16,7 +24,6 @@ def read_locations():
   return lls
 
 def create_routes(lls): 	
-#need to loop to next line for 2nd latlng
   for c in range(0, len(lls)):
     for i in range(1, len(lls)):
       from_ll = re.split(',',str(lls[c]))
@@ -27,7 +34,6 @@ def create_routes(lls):
         print json.dumps({'costing': sys.argv[2], 'locations': [{'lat': from_ll[0], 'lon': from_ll[1]}, {'lat': to_ll[0], 'lon': to_ll[1]}]}, sort_keys=True, separators=(',',':'))
       
 
-# use those 100 locations to generate 10K routes
 lls = read_locations()
 create_routes(lls)
 
