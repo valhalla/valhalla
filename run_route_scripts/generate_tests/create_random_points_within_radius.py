16082509a43ad95f35cebad055981cbb06f89940
kdiluca <kristen.diluca@mapbox.com>
2018-08-09 10:09:47 -0400

    added a description comment at beginning of script

--- run_route_scripts/generate_tests/create_random_points_within_radius.py
+++ run_route_scripts/generate_tests/create_random_points_within_radius.py
@@ -4,15 +4,21 @@ import math
 import random
 import json
 
+### Enter a location in an urban area and then generate a set random locations nearby. 
+### Continue to use a similar process to generate a smaller number near surrounding urban areas
+### until you get the number of locations you need. Next, these locations are used to generate approx. 10K routes
+
+
 RADIUS = int(sys.argv[4])
 RADIUS_IN_DEGREES = RADIUS / 111300.0
 inputloc = (float(sys.argv[1]), float(sys.argv[2]))
 randlocs = []
 
 def get_random_locs(inputloc_x, inputloc_y):
-  for i in range(0,int(sys.argv[3])):  #Choose number of latlon to be generated
-    a = float(random.uniform(0.0,1.0))
-    b = float(random.uniform(0.0,1.0))
+  for i in range(0,int(sys.argv[3])):  # Choose number of lat/lngs to be generated
+    # creates a more uniform range around the center point 
+    a = float(random.uniform(-1.0, 1.0))
+    b = float(random.uniform(-1.0, 1.0))
     r = RADIUS_IN_DEGREES * math.sqrt(a)
     c = 2 * math.pi * b
     x = r * math.cos(c) 
@@ -22,8 +28,7 @@ def get_random_locs(inputloc_x, inputloc_y):
     randlocs.append([round(xLat, 5), round(yLong, 5)]);
   return randlocs
 
-# enter a location in a city and then generate a set of 20 or so random locations nearby. Then a similar process 
-# to generate a smaller number near Anahiem, etc. until you get 100 locations. But then use those 100 locations to generate 10K routes
+
 get_random_locs(inputloc[0], inputloc[1])
 print (randlocs)
 
