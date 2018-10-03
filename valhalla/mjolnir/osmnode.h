18576c69b640aa070f837bb1cddf38e582bdd48d
dnesbitt <david.nesbitt@mapbox.com>
2018-10-03 10:44:48 -0400

    Deprecate trafficalgorithm and valhalla_build_speeds. These are a legacy proof of concept that no longer applies as we are adding new traffic ingestion and pathfinding algorithms. Also move some simple access methods from osmnode.cc and osmadmin.cc into .h files.

--- valhalla/mjolnir/osmnode.h
+++ valhalla/mjolnir/osmnode.h
@@ -46,125 +46,158 @@ struct OSMNode {
    * Sets the lat,lng.
    * @param  ll  Lat,lng of the node.
    */
-  void set_latlng(const std::pair<float, float>& ll);
+  void set_latlng(const std::pair<float, float>& ll) {
+    lng = ll.first;
+    lat = ll.second;
+  }
 
   /**
    * Gets the lat,lng.
    * @return   Returns the lat,lng of the node.
    */
-  std::pair<float, float> latlng() const;
+  std::pair<float, float> latlng() const{
+    return std::make_pair(lng, lat);
+  }
 
   /**
    * Set access mask.
    */
-  void set_access_mask(const uint32_t access_mask);
+  void set_access_mask(const uint32_t access_mask)  {
+    attributes_.access_mask = access_mask;
+  }
 
   /**
    * Get the access mask.
    */
-  uint32_t access_mask() const;
-
-  /**
-   * Set payment mask.
-   */
-  // void set_payment_mask(const uint32_t payment_mask);
-
-  /**
-   * Get the payment mask.
-   */
-  // uint32_t payment_mask() const;
+  uint32_t access_mask() const {
+    return attributes_.access_mask;
+  }
 
   /**
    * Sets the type.
    * @param  type
    */
-  void set_type(const baldr::NodeType type);
+  void set_type(const baldr::NodeType type) {
+    attributes_.type = static_cast<uint8_t>(type);
+  }
 
   /**
    * Get the type.
    * @return  Returns the type of node.
    */
-  baldr::NodeType type() const;
+  baldr::NodeType type() const {
+    return static_cast<baldr::NodeType>(attributes_.type);
+  }
 
   /**
    * Set the exit to flag
    */
-  void set_exit_to(const bool exit_to);
+  void set_exit_to(const bool exit_to) {
+    attributes_.exit_to = exit_to;
+  }
 
   /**
    * Get the exit to flag
    */
-  bool exit_to() const;
+  bool exit_to() const {
+    return attributes_.exit_to;
+  }
 
   /**
    * Set the ref flag
    */
-  void set_ref(const bool ref);
+  void set_ref(const bool ref) {
+    attributes_.ref = ref;
+  }
 
   /**
    * Get the ref flag
    */
-  bool ref() const;
+  bool ref() const {
+    return attributes_.ref;
+  }
 
   /**
    * Set the name flag (for some exit information)
    */
-  void set_name(const bool name);
+  void set_name(const bool name) {
+    attributes_.name = name;
+  }
 
   /**
    * Get the name flag
    */
-  bool name() const;
+  bool name() const {
+    return attributes_.name;
+  }
 
   /**
    * Set the intersection flag.
    * @param  intersection  Is this node part of an intersection? True if
    *                       this node is an end node of more than 1 way.
    */
-  void set_intersection(const bool intersection);
+  void set_intersection(const bool intersection) {
+    attributes_.intersection = intersection;
+  }
 
   /**
    * Get the intersection flag
    * @return  Returns true if the node is part of an intersection (end
    *          node of more than 1 way), false if not.
    */
-  bool intersection() const;
+  bool intersection() const {
+    return attributes_.intersection;
+  }
 
   /**
    * Set traffic_signal flag.
    */
-  void set_traffic_signal(const bool traffic_signal);
+  void set_traffic_signal(const bool traffic_signal) {
+    attributes_.traffic_signal = traffic_signal;
+  }
 
   /**
    * Get the traffic_signal flag.
    */
-  bool traffic_signal() const;
+  bool traffic_signal() const {
+    return attributes_.traffic_signal;
+  }
 
   /**
    * Set forward_signal flag.
    */
-  void set_forward_signal(const bool forward_signal);
+  void set_forward_signal(const bool forward_signal) {
+    attributes_.forward_signal = forward_signal;
+  }
 
   /**
    * Get the forward_signal flag.
    */
-  bool forward_signal() const;
+  bool forward_signal() const {
+    return attributes_.forward_signal;
+  }
 
   /**
    * Set backward_signal flag.
    */
-  void set_backward_signal(const bool backward_signal);
+  void set_backward_signal(const bool backward_signal)  {
+    attributes_.backward_signal = backward_signal;
+  }
 
   /**
    * Get the backward_signal flag.
    */
-  bool backward_signal() const;
+  bool backward_signal() const {
+    return attributes_.backward_signal;
+  }
 
   /**
    * Get the attributes.
    * @return  Returns the attributes.
    */
-  const NodeAttributes& attributes() const;
+//  const NodeAttributes& attributes() const {
+//    return attributes_;
+//  }
 };
 
 } // namespace mjolnir
