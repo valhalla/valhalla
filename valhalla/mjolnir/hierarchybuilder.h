a96380acc0bd1e45630bff67f899077ee5cb9635
dnesbitt <david.nesbitt@mapbox.com>
2019-02-01 15:33:25 -0500

    Add names for the temporary bin files to the functions (they are passed in externally so they can be cleaned up/deleted when done).

--- valhalla/mjolnir/hierarchybuilder.h
+++ valhalla/mjolnir/hierarchybuilder.h
@@ -8,15 +8,20 @@ namespace valhalla {
 namespace mjolnir {
 
 /**
- * Class used to construct temporary data used to build the initial graph.
+ * Class used to divide the road network graph into hierarchy levels.
  */
 class HierarchyBuilder {
 public:
   /**
    * Build the set of hierarchies based on the TileHierarchy configuration
    * and the current local hierarchy.
+   * @param  pt              Configuration property tree
+   * @param  new_to_old_bin  Filename of the new to old node association file
+   * @param  old_to_new_bin  Filename for the old to new node association file.
    */
-  static void Build(const boost::property_tree::ptree& pt);
+  static void Build(const boost::property_tree::ptree& pt,
+                    const std::string& new_to_old_bin,
+                    const std::string& old_to_new_bin);
 };
 
 } // namespace mjolnir
