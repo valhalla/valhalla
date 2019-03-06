aeafc74e43cf1b9662e9fab4fbae48480edbd3a5
Duane Gearhart <duane.gearhart@mapbox.com>
2019-03-06 15:53:41 -0500

    updated IsMergeManeuverType to use the maneuver bgin turn degree and make sure there is name consistencey for curr edge and intersecting edge

--- src/odin/maneuversbuilder.cc
+++ src/odin/maneuversbuilder.cc
@@ -1238,7 +1238,7 @@ void ManeuversBuilder::SetManeuverType(Maneuver& maneuver, bool none_type_allowe
     }
   }
   // Process merge
-  else if (IsMergeManeuverType(prev_edge, curr_edge)) {
+  else if (IsMergeManeuverType(maneuver, prev_edge, curr_edge)) {
     maneuver.set_type(TripDirections_Maneuver_Type_kMerge);
     LOG_TRACE("ManeuverType=MERGE");
   }
@@ -1658,15 +1658,16 @@ bool ManeuversBuilder::IncludeUnnamedPrevEdge(int node_index,
   return false;
 }
 
-bool ManeuversBuilder::IsMergeManeuverType(EnhancedTripPath_Edge* prev_edge,
+bool ManeuversBuilder::IsMergeManeuverType(Maneuver& maneuver,
+                                           EnhancedTripPath_Edge* prev_edge,
                                            EnhancedTripPath_Edge* curr_edge) const {
-
+  auto* node = trip_path_->GetEnhancedNode(maneuver.begin_node_index());
   if (prev_edge && prev_edge->IsRampUse() && !curr_edge->IsRampUse() &&
       (curr_edge->IsHighway() ||
        (((curr_edge->road_class() == TripPath_RoadClass_kTrunk) ||
          (curr_edge->road_class() == TripPath_RoadClass_kPrimary)) &&
-        curr_edge->IsOneway() &&
-        curr_edge->IsForward(GetTurnDegree(prev_edge->end_heading(), curr_edge->begin_heading()))))) {
+        curr_edge->IsOneway() && curr_edge->IsForward(maneuver.turn_degree()) &&
+        node->HasIntersectingEdgeCurrNameConsistency()))) {
     return true;
   }
 
