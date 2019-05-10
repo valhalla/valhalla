4e7667dabb3594b741faad4b537a062bb149d6a7
David Nesbitt <dnesbitt61@gmail.com>
2019-05-10 07:35:11 -0400

    Do not use surface type in bicycle edge filter (#1800)
    * Do not use surface type in bicycle edge filter (rely on EdgeCost to penalize
    poor surfaces based on the avoid_bad_surfaces option)
    
    * Add bike surface fix.

--- src/sif/bicyclecost.cc
+++ src/sif/bicyclecost.cc
@@ -388,10 +388,9 @@ protected:
    */
   virtual const EdgeFilter GetEdgeFilter() const {
     // Throw back a lambda that checks the access for this type of costing
-    Surface s = worst_allowed_surface_;
-    return [s](const baldr::DirectedEdge* edge) {
+    return [](const baldr::DirectedEdge* edge) {
       if (edge->is_shortcut() || !(edge->forwardaccess() & kBicycleAccess) ||
-          edge->use() == Use::kSteps || edge->surface() > s) {
+          edge->use() == Use::kSteps) {
         return 0.0f;
       } else {
         // TODO - use classification/use to alter the factor
