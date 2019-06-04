a06d4fda9839533088886b9fb4154a1ea177aa57
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2019-06-04 13:08:41 -0400

    Distance is in meters

--- src/thor/triplegbuilder.cc
+++ src/thor/triplegbuilder.cc
@@ -177,7 +177,7 @@ void SetShapeAttributes(const AttributesController& controller,
     // Set the shape attributes
     for (++shape_begin; shape_begin < shape_end; ++shape_begin) {
       double distance = shape_begin->Distance(*(shape_begin - 1));
-      double distance_pct = distance / edge_length;
+      double distance_pct = (distance / 1000) / edge_length;
       double time = edge_time * distance_pct;
       // Set shape attributes time per shape point if requested
       if (controller.attributes.at(kShapeAttributesTime)) {
