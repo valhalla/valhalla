7550da898a3a5a11570fa32ba465b6d03d9c7be0
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2020-02-21 15:57:21 -0500

    Update tests

--- test/node_search.cc
+++ test/node_search.cc
@@ -261,8 +261,8 @@ void graph_builder::write_tiles(uint8_t level) const {
       bool add;
       // make more complex edge geom so that there are 3 segments, affine
       // combination doesnt properly handle arcs but who cares
-      edge_info_offset = tile.AddEdgeInfo(edge_index, e.first, e.second, 123, 456, 0, 55, shape,
-                                          {std::to_string(edge_index)}, 0, add);
+      edge_info_offset = tile.AddEdgeInfo(edge_index, e.first, e.second, 123, 456, 0, 55, false,
+                                          shape, {std::to_string(edge_index)}, 0, add);
     }
     edge_builder.set_edgeinfo_offset(edge_info_offset);
 
