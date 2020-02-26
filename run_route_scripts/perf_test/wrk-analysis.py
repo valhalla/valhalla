9ae728a3b0595ec2c052d5b889b4901f6e866531
Buro Mookerji <buro.mookerji@mapbox.com>
2020-02-25 23:00:27 -0800

    Add other latency quantiles and plots

--- run_route_scripts/perf_test/wrk-analysis.py
+++ run_route_scripts/perf_test/wrk-analysis.py
@@ -11,9 +11,12 @@ import pandas as pd
 
 
 def analyze_benchmark(measurements, metadata):
+    # Parse
     measurements_df = pd.read_csv(measurements)
     metadata_df = pd.read_csv(metadata)
     combined = measurements_df.join(metadata_df)
+
+    # Plot throughput
     combined['throughput_qps'] \
       = combined['summary.requests']/(combined['summary.duration(usec)']/10**6)
     # Plot separate performance envelopes for different benchmark tests.
@@ -24,13 +27,45 @@ def analyze_benchmark(measurements, metadata):
     for test_name, df in combined.groupby('test_name'):
         qps_mean = df.groupby('concurrency').mean()['throughput_qps']
         qps_stddev = df.groupby('concurrency').std()['throughput_qps']
-        ax = qps_mean.plot(yerr=qps_stddev, label='test_name')
+        ax = qps_mean.plot(yerr=qps_stddev, label=test_name)
     plt.legend()
     ax.set_title('Query Throughput Under Load')
     ax.set_xlabel('Number of Concurrent Clients')
     ax.set_ylabel('Throughput (Queries/Second)')
-    ax.get_figure().savefig(measurements + '.png')
+    ax.get_figure().savefig(measurements + '-throughput.png')
     combined.to_csv(measurements + '-combined.csv')
+
+    # Plot mean latency
+    # NOTE: Pandas boxplots apparently don't support labels :(
+    if 'latency.mean(usec)' in combined:
+        combined['latency.mean(sec)'] = combined['latency.mean(usec)'] / 10**6
+        plt.figure()
+        for test_name, df in combined.groupby('test_name'):
+            ax = combined.boxplot(
+                by='concurrency',
+                column='latency.mean(sec)',
+                grid=False,
+            )
+        ax.set_title('Latency (mean) Under Load')
+        ax.set_xlabel('Number of Concurrent Clients')
+        ax.set_ylabel('Latency (Second)')
+        ax.get_figure().savefig(measurements + '-latency-mean.png')
+
+    # Plot p99 latency
+    if 'latency:percentile(99.0)(usec)' in combined:
+        combined['latency:percentile(99.0)(sec)'] \
+          = combined['latency:percentile(99.0)(usec)'] / 10**6
+        plt.figure()
+        for test_name, df in combined.groupby('test_name'):
+            ax = combined.boxplot(
+                by='concurrency',
+                column='latency:percentile(99.0)(sec)',
+                grid=False,
+            )
+        ax.set_title('Latency (p99) Under Load')
+        ax.set_xlabel('Number of Concurrent Clients')
+        ax.set_ylabel('Latency (Second)')
+        ax.get_figure().savefig(measurements + '-latency-p99.png')
 
 
 @click.group()
