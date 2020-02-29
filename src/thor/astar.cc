4559af9b237be61d1c0b7fd1747660edaaf5311d
Anders Bennehag <anders.bennehag@mapbox.com>
2020-02-29 13:02:25 -0500

    Check for complex restriction where bidirectional expansion meets (#2242)
    Adds test with complex restriction where bidirectional meet
    
    The edge that connects the two directions of expansion in Bidirectional
    is dropped if it is part of a complex restriction. The correct behaviour
    is instead to walk the edges back and forth to see if any complex
    restriction really is active.
    
    References https://github.com/valhalla/valhalla/issues/2228
    
    * leftover from merge
    
    * Adds end-edge from restriction
    
    * IsBridgingEdgeRestricted grabs restrictions from opposite direction. All tests pass
    
    * Simplifies IsBridgingEdgeRestricted by only running in forward direction
    
    * Revert changes to valhalla_build_timezones
    
    * Expands astar tileset and restriction and fixes missing opp_local_idx for edges
    
    * Removes unneeded CheckPatchPathForRestrictions
    
    * nit: Hinting for GetGraphTile
    
    * Single melborne from greg, noroute on melborne test
    
    * Adds second test-case, of a X-crossing
    
    * Updates readme
    
    * Nit cleanup comment
    
    * bump ci
    
    * test/summary: Verifies shape of path and losens `EXPECT_NEAR`
    
    due to x86 32bit precision
    
    * add admin db for netherlands.
    
    * updated based on drive on right
    
    * Enables commented test and fixes complex restriction in reverse direction
    
    * fix summary test
    
    * fix one test
    
    * update durations for admin enhanced utrecht
    
    * fix comment, include start and end in the restriction bit on the edge label, fix up a* fake tile setup
    
    * Fixes faulty fwd_pred
    
    Co-authored-by: Greg Knisely <greg.knisely@mapbox.com>
    Co-authored-by: Kevin Kreiser <kevinkreiser@gmail.com>

--- src/thor/astar.cc
+++ src/thor/astar.cc
@@ -534,19 +534,5 @@ std::vector<PathInfo> AStarPathAlgorithm::FormPath(const uint32_t dest) {
   return path;
 }
 
-bool CheckPatchPathForRestrictions(
-    const std::vector<valhalla::baldr::GraphId>& patch_path,
-    const std::vector<std::vector<valhalla::baldr::GraphId>>& list_of_restrictions) {
-  for (auto& restriction_ids : list_of_restrictions) {
-    if (std::search(patch_path.cbegin(), patch_path.cend(), restriction_ids.cbegin(),
-                    restriction_ids.cend()) != patch_path.cend()) {
-      // We found a restriction that matches parts of patch_path
-      return true;
-    }
-  }
-  // None of the restrictions matched
-  return false;
-};
-
 } // namespace thor
 } // namespace valhalla
