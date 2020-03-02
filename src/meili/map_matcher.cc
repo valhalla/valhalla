3fce7bbee4f247aa944d2344a771a8c35a6574e5
Yuzhe Yan <yuzhe.yan@mapbox.com>
2020-03-02 13:09:09 -0800

    flip the checking sequence to let the unit test coverage pass and also, save the tile update

--- src/meili/map_matcher.cc
+++ src/meili/map_matcher.cc
@@ -265,6 +265,27 @@ MatchResult FindMatchResult(const MapMatcher& mapmatcher,
     // when the instersection match fails, we do a more labor intensive search at transitions
     // for both prev_edge's ending node and next_edge's starting node and see if we could do a
     // node snap match
+
+    if (next_opp_de && edge.id.level() != next_opp_de->endnode().level()) {
+      baldr::GraphId end_node = next_opp_de->endnode();
+      for (const auto& trans : tile->GetNodeTransitions(end_node)) {
+        // we only care about if the nodes are in the same level
+        if (trans.endnode().level() != candidate_node.level()) {
+          continue;
+        }
+
+        // bail when nodes are the same level node is not a sibling of the target node
+        end_node = trans.endnode();
+        tile = graph_reader.GetGraphTile(end_node);
+        if (tile == nullptr || end_node != candidate_node) {
+          break;
+        }
+
+        return {edge.projected, std::sqrt(edge.distance), next_edge, 0.f, measurement.epoch_time(),
+                stateid};
+      }
+    }
+
     if (prev_de && edge.id.level() != prev_de->endnode().level()) {
       baldr::GraphId end_node = prev_de->endnode();
       tile = graph_reader.GetGraphTile(end_node);
@@ -274,6 +295,7 @@ MatchResult FindMatchResult(const MapMatcher& mapmatcher,
           continue;
         }
 
+        // bail when nodes are the same level node is not a sibling of the target node
         end_node = trans.endnode();
         tile = graph_reader.GetGraphTile(end_node);
         if (tile == nullptr || end_node != candidate_node) {
@@ -281,26 +303,6 @@ MatchResult FindMatchResult(const MapMatcher& mapmatcher,
         }
 
         return {edge.projected, std::sqrt(edge.distance), prev_edge, 1.f, measurement.epoch_time(),
-                stateid};
-      }
-    }
-
-    if (next_opp_de && edge.id.level() != next_opp_de->endnode().level()) {
-      baldr::GraphId end_node = next_opp_de->endnode();
-      tile = graph_reader.GetGraphTile(end_node);
-      for (const auto& trans : tile->GetNodeTransitions(end_node)) {
-        // we only care about if the nodes are in the same level
-        if (trans.endnode().level() != candidate_node.level()) {
-          continue;
-        }
-
-        end_node = trans.endnode();
-        tile = graph_reader.GetGraphTile(end_node);
-        if (tile == nullptr || end_node != candidate_node) {
-          break;
-        }
-
-        return {edge.projected, std::sqrt(edge.distance), next_edge, 0.f, measurement.epoch_time(),
                 stateid};
       }
     }
