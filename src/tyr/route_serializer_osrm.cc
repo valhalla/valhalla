8d987603d44fc0003672c3411b303f6e5b228364
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2020-03-17 17:50:55 -0400

    Don't label all ramps as motorway in osrm response

--- src/tyr/route_serializer_osrm.cc
+++ src/tyr/route_serializer_osrm.cc
@@ -500,8 +500,8 @@ json::ArrayPtr intersections(const valhalla::DirectionsLeg::Maneuver& maneuver,
       if (maneuver.portions_toll() || curr_edge->toll()) {
         classes.push_back("toll");
       }
-      // TODO We might want to have more specific logic for ramp
-      if ((curr_edge->road_class() == TripLeg_RoadClass_kMotorway) || curr_edge->IsRampUse()) {
+      // TODO We might want to label motorway for motorway_link
+      if (curr_edge->road_class() == TripLeg_RoadClass_kMotorway) {
         classes.push_back("motorway");
       }
       if (curr_edge->use() == TripLeg::Use::TripLeg_Use_kFerryUse) {
