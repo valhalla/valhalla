d857d7532553569c78dcce1711f458f29685564e
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2020-03-19 11:51:18 -0400

    Add a turn

--- test/gurka/test_maxspeed.cc
+++ test/gurka/test_maxspeed.cc
@@ -6,6 +6,8 @@ using namespace valhalla;
 TEST(Standalone, Maxspeed) {
   const std::string ascii_map = R"(
       A----B----C----D----E----F
+                               |
+                               G
   )";
 
   const gurka::ways ways = {
@@ -14,10 +16,11 @@ TEST(Standalone, Maxspeed) {
       {"CD", {{"highway", "motorway"}, {"maxspeed", ""}}},
       {"DE", {{"highway", "motorway"}}},
       {"EF", {{"highway", "motorway"}, {"maxspeed", "none"}}},
+      {"FG", {{"highway", "motorway"}, {"maxspeed", "40"}}},
   };
   const auto layout = gurka::detail::map_to_coordinates(ascii_map, 100);
   auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_maxspeed");
-  auto result = gurka::route(map, "A", "F", "auto");
+  auto result = gurka::route(map, "A", "G", "auto");
 
   ASSERT_EQ(result.trip().routes(0).legs_size(), 1);
   auto leg = result.trip().routes(0).legs(0);
@@ -27,4 +30,5 @@ TEST(Standalone, Maxspeed) {
   EXPECT_EQ(leg.node(2).edge().speed_limit(), 0);   // CD
   EXPECT_EQ(leg.node(3).edge().speed_limit(), 0);   // DE
   EXPECT_EQ(leg.node(4).edge().speed_limit(), 255); // EF
+  EXPECT_EQ(leg.node(5).edge().speed_limit(), 40);  // FG
 }
