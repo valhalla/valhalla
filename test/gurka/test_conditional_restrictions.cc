2ca05d4d9751b3e101e208f59fca4a72e6a8c58d
kdiluca <kristen.diluca@mapbox.com>
2020-03-24 11:18:55 -0400

    lint

--- test/gurka/test_conditional_restrictions.cc
+++ test/gurka/test_conditional_restrictions.cc
@@ -15,23 +15,25 @@ protected:
          |  \
          D--C)";
 
-    const gurka::ways ways =
-        {{"AD", {{"highway", "service"}, {"motorcar", "no"}, {"motor_vehicle", "no"}}},
-         {"AB", {{"highway", "service"},
-             {"motorcar:conditional","yes @ (Mar 00:00-07:00);Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"},
-             {"motor_vehicle:conditional","yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"}
-             }},
-         {"BC", {{"highway", "service"},
-             {"motorcar:conditional","yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"},
-             {"motor_vehicle:conditional","yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"}
-             }},
-         {"CD", {{"highway", "service"},
-             {"motorcar:conditional","yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"},
-             {"motor_vehicle:conditional","yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"}
-             }}};
+    const gurka::ways ways = {
+        {"AD", {{"highway", "service"}, {"motorcar", "no"}, {"motor_vehicle", "no"}}},
+        {"AB",
+         {{"highway", "service"},
+          {"motorcar:conditional",
+           "yes @ (Mar 00:00-07:00);Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"},
+          {"motor_vehicle:conditional",
+           "yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"}}},
+        {"BC",
+         {{"highway", "service"},
+          {"motorcar:conditional",
+           "yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"},
+          {"motor_vehicle:conditional",
+           "yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"}}},
+        {"CD",
+         {{"highway", "service"}, {"motorcar:conditional", "yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"}, {"motor_vehicle:conditional", "yes @ (Mar 00:00-07:00;Mar 17:30-24:00;Apr 00:00-07:00;Apr 19:00-24:00;Aug 00:00-07:00;Aug 19:00-24:00;Sep 00:00-07:00;Sep 18:00-24:00;Oct 00:00-07:00;Oct 17:00-24:00;Jan-Feb 00:00-07:30;Jan-Feb 17:00-24:00;May-Jul 00:00-07:00;May-Jul 20:00-24:00)"}}}};
 
-
-    const gurka::relations relations = {{{{gurka::node_member, "A", "from"}, {gurka::node_member, "D", "to"}}}};
+    const gurka::relations relations = {
+        {{{gurka::node_member, "A", "from"}, {gurka::node_member, "D", "to"}}}};
 
     const auto layout = gurka::detail::map_to_coordinates(ascii_map, 100);
     map = gurka::buildtiles(layout, ways, {}, relations, "test/data/conditional_restrictions");
@@ -52,18 +54,16 @@ TEST_F(ConditionalRestrictions, NoRestrictionAuto) {
 }
 
 TEST_F(ConditionalRestrictions, RestrictionAuto) {
-// this tests that the expected exception is thrown
-    EXPECT_THROW({
-        try
-        {
+  // this tests that the expected exception is thrown
+  EXPECT_THROW(
+      {
+        try {
           auto result = gurka::route(map, "A", "D", "auto", "2020-04-02T12:00");
+        } catch (const std::exception& e) {
+          // and this tests that it has the correct message
+          EXPECT_STREQ("No path could be found for input", e.what());
+          throw;
         }
-        catch( const std::exception& e )
-        {
-            // and this tests that it has the correct message
-            EXPECT_STREQ( "No path could be found for input", e.what() );
-            throw;
-        }
-    }, std::exception );
+      },
+      std::exception);
 }
-
