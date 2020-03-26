6b9e6777c2a857c1764882a57b335c72a7f701a6
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2020-03-26 13:16:52 -0400

    Cleanup tests

--- test/gurka/test_search_filter.cc
+++ test/gurka/test_search_filter.cc
@@ -20,84 +20,75 @@ TEST(Standalone, HeadingFilter) {
 
   const double gridsize = 100;
   const auto layout = gurka::detail::map_to_coordinates(ascii_map, gridsize);
-  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/search_filter_1");
+  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/search_filter_heading");
 
-  rapidjson::Document doc;
-  doc.SetObject();
-  auto& allocator = doc.GetAllocator();
-  rapidjson::Value locations(rapidjson::kArrayType);
-
-  // first location
-  rapidjson::Value l0(rapidjson::kObjectType);
-  l0.AddMember("lon", map.nodes.at("1").lng(), allocator);
-  l0.AddMember("lat", map.nodes.at("1").lat(), allocator);
-  l0.AddMember("heading", "180", allocator);
-  l0.AddMember("heading_tolerance", "45", allocator);
-  locations.PushBack(l0, allocator);
-
-  // second location
-  rapidjson::Value l1(rapidjson::kObjectType);
-  l1.AddMember("lon", map.nodes.at("2").lng(), allocator);
-  l1.AddMember("lat", map.nodes.at("2").lat(), allocator);
-  locations.PushBack(l1, allocator);
-
-  doc.AddMember("locations", locations, allocator);
-  doc.AddMember("costing", "auto", allocator);
-
-  rapidjson::StringBuffer sb;
-  rapidjson::Writer<rapidjson::StringBuffer> writer(sb);
-  doc.Accept(writer);
-
-  auto result = gurka::route(map, sb.GetString());
+  const std::string& request =
+      (boost::format(
+           R"({"locations":[{"lat":%s,"lon":%s,"heading":180,"heading_tolerance":45},{"lat":%s,"lon":%s}],"costing":"auto"})") %
+       std::to_string(map.nodes.at("1").lat()) % std::to_string(map.nodes.at("1").lng()) %
+       std::to_string(map.nodes.at("2").lat()) % std::to_string(map.nodes.at("2").lng()))
+          .str();
+  auto result = gurka::route(map, request);
 
   // should take the long way around starting southbound
   gurka::assert::osrm::expect_route(result, {"AB", "AD", "CD", "BC"});
 }
 
-TEST(Standalone, RoadClassFilter) {
+/*************************************************************/
+class RoadClassFilter : public ::testing::Test {
+protected:
+  static gurka::map map;
 
-  const std::string ascii_map = R"(
-    B--2--C
-    |     |
-    |     |
-    |     |
-    A 1   D
-         )";
+  static void SetUpTestSuite() {
+    constexpr double gridsize = 100;
 
-  const gurka::ways ways = {{"AB", {{"highway", "motorway"}}},
-                            {"BC", {{"highway", "primary"}}},
-                            {"CD", {{"highway", "primary"}}}};
+    const std::string ascii_map = R"(
+      B--2--C
+      |     |
+      |     |
+      |     |
+      A 1   D
+          )";
 
-  const double gridsize = 100;
-  const auto layout = gurka::detail::map_to_coordinates(ascii_map, gridsize);
-  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/search_filter_2");
+    const auto layout = gurka::detail::map_to_coordinates(ascii_map, gridsize);
+    const gurka::ways ways = {{"AB", {{"highway", "motorway"}}},
+                              {"BC", {{"highway", "primary"}}},
+                              {"CD", {{"highway", "primary"}}}};
+    map = gurka::buildtiles(layout, ways, {}, {}, "test/data/search_filter_roadclass");
+  }
+};
 
+gurka::map RoadClassFilter::map = {};
+
+TEST_F(RoadClassFilter, Basic) {
   // Should snap origin to AB as it's closest
-  const std::string& request_1 =
+  const std::string& request =
       (boost::format(R"({"locations":[{"lat":%s,"lon":%s},{"lat":%s,"lon":%s}],"costing":"auto"})") %
        std::to_string(map.nodes.at("1").lat()) % std::to_string(map.nodes.at("1").lng()) %
        std::to_string(map.nodes.at("2").lat()) % std::to_string(map.nodes.at("2").lng()))
           .str();
-  auto result_1 = gurka::route(map, request_1);
-  gurka::assert::osrm::expect_route(result_1, {"AB", "BC"});
-
+  auto result = gurka::route(map, request);
+  gurka::assert::osrm::expect_route(result, {"AB", "BC"});
+}
+TEST_F(RoadClassFilter, MaxRoadClass) {
   // Should snap origin to CD as the search_filter disallows motorways
-  const std::string& request_2 =
+  const std::string& request =
       (boost::format(
            R"({"locations":[{"lat":%s,"lon":%s,"search_filter":{"max_road_class":"primary"}},{"lat":%s,"lon":%s}],"costing":"auto"})") %
        std::to_string(map.nodes.at("1").lat()) % std::to_string(map.nodes.at("1").lng()) %
        std::to_string(map.nodes.at("2").lat()) % std::to_string(map.nodes.at("2").lng()))
           .str();
-  auto result_2 = gurka::route(map, request_2);
-  gurka::assert::osrm::expect_route(result_2, {"CD", "BC"});
-
+  auto result = gurka::route(map, request);
+  gurka::assert::osrm::expect_route(result, {"CD", "BC"});
+}
+TEST_F(RoadClassFilter, MinRoadClass) {
   // Should snap destination to AB as the search_filter disallows primary
-  const std::string& request_3 =
+  const std::string& request =
       (boost::format(
            R"({"locations":[{"lat":%s,"lon":%s},{"lat":%s,"lon":%s,"search_filter":{"min_road_class":"motorway"}}],"costing":"auto"})") %
        std::to_string(map.nodes.at("1").lat()) % std::to_string(map.nodes.at("1").lng()) %
        std::to_string(map.nodes.at("2").lat()) % std::to_string(map.nodes.at("2").lng()))
           .str();
-  auto result_3 = gurka::route(map, request_3);
-  gurka::assert::osrm::expect_route(result_3, {"AB"});
+  auto result = gurka::route(map, request);
+  gurka::assert::osrm::expect_route(result, {"AB"});
 }
