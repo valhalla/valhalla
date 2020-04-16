61c79bf280344fd6fdd7b1c47911eeed672b5a5d
Kevin Kreiser <kevinkreiser@gmail.com>
2020-04-16 12:50:46 -0400

    fix comment

--- src/thor/bidirectional_astar.cc
+++ src/thor/bidirectional_astar.cc
@@ -933,13 +933,15 @@ std::vector<std::vector<PathInfo>> BidirectionalAStar::FormPath(GraphReader& gra
   // Reverse the list
   std::reverse(path.begin(), path.end());
 
-  // Special case code if the last edge of the forward path is
-  // the destination edge - update the elapsed time
+  // Special case code if the last edge of the forward path is the destination edge
+  // which means we need to worry about partial distance on the edge
   if (edgelabels_reverse_[idx2].predecessor() == kInvalidLabel) {
-    // destination is on a different edge than origin because of that we know that the forward path
-    // did not care about partial distance along the edge when it found the edge so what we need to do
-    // is look at the second to last edge and use the reverse path (who does care about partial
-    // distance on the destination edge) to recompute the elapsed time and cost
+    // the destination is on a different edge than origin but the forward path found it. because of
+    // that we know that the forward path did not care about partial distance along the edge. so the
+    // edge that it added was the full length (time and cost). so what we need to do is look at the
+    // second to last edge and use the reverse path (who does care about partial distance on the
+    // destination edge) to recompute the elapsed time and cost. dont forget the transition cost from
+    // the forward path
     if (path.size() > 1) {
       path.back().elapsed_time = path[path.size() - 2].elapsed_time +
                                  edgelabels_reverse_[idx2].cost().secs +
