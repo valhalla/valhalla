226e5e3aaef95afe696108f7e0df41182bc949f0
dnesbitt61 <dnesbitt61@gmail.com>
2020-04-29 08:54:27 -0400

    Add gurka test for Mtb access

---
+++ test/gurka/test_mtb_access.cc
@@ -0,0 +1,39 @@
+#include "gurka.h"
+#include <gtest/gtest.h>
+
+#if !defined(VALHALLA_SOURCE_DIR)
+#define VALHALLA_SOURCE_DIR
+#endif
+
+using namespace valhalla;
+
+// Check that MTB tags override SAC scale and allow bicycle access
+class MtbAccess : public ::testing::Test {
+protected:
+  static gurka::map map;
+
+  static void SetUpTestSuite() {
+std::cout << "SetUp MtbAccess test" << std::endl;
+    constexpr double gridsize = 100;
+
+    // A--B
+    const std::string ascii_map = R"(A----B----C)";
+    const gurka::ways ways =
+        {{"AB", {{"highway", "cycleway"}}},
+         {"BC", {{"highway", "cycleway"}}}};
+//        {{"AB", {{"highway", "cycleway"}, {"sac_scale", "mountain_hiking"}, {"mtb:scale:uphill", "2"}}},
+//         {"BC", {{"highway", "cycleway"}, {"sac_scale", "mountain_hiking"}, {"mtb:scale:uphill", "2"}}}};
+
+    const auto layout = gurka::detail::map_to_coordinates(ascii_map, 100);
+    map = gurka::buildtiles(layout, ways, {}, {}, "test/data/mtb_access");
+  }
+};
+
+gurka::map MtbAccess::map = {};
+
+/*************************************************************/
+
+TEST_F(MtbAccess, CheckMtbAccess) {
+  auto result = gurka::route(map, "A", "C", "bicycle");
+  gurka::assert::osrm::expect_route(result, {"AB", "BC"});
+}
