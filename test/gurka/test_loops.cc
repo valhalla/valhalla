b9bf37c27fcae493b1c625625ec1407879bb1778
Kevin Kreiser <kevinkreiser@gmail.com>
2020-05-19 21:27:22 -0400

    more horrible flat loop tests

--- test/gurka/test_loops.cc
+++ test/gurka/test_loops.cc
@@ -3,15 +3,7 @@
 
 using namespace valhalla;
 
-TEST(Standalone, FlatLoop) {
-  // we create a way that doubles back on itself
-  const std::string ascii_map = R"(A---B)";
-  const gurka::ways ways = {
-      {"ABA", {{"highway", "motorway"}}},
-  };
-  const auto layout = gurka::detail::map_to_coordinates(ascii_map, 10);
-  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_opp_edge");
-
+void check_opposing(const gurka::map& map) {
   // then we test the invariant that any edge should be the opposing edge of its opposing edge
   // edge == opposing(opposing(edge))
   valhalla::baldr::GraphReader reader(map.config.get_child("mjolnir"));
@@ -23,4 +15,74 @@ TEST(Standalone, FlatLoop) {
       EXPECT_EQ(edge, opposing_opposing);
     }
   }
+}
+
+TEST(loops, flat_loop) {
+  // we create a way that doubles back on itself
+  const std::string ascii_map = R"(A-----B)";
+  const gurka::ways ways = {
+      {"ABA", {{"highway", "motorway"}}},
+  };
+  const auto layout = gurka::detail::map_to_coordinates(ascii_map, 10);
+  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_flat_loop");
+  check_opposing(map);
+}
+
+TEST(loops, long_loop) {
+  // we create a way that doubles back on itself
+  const std::string ascii_map = R"(A--B--C--D--E)";
+  const gurka::ways ways = {
+      {"ABCDEDCBA", {{"highway", "motorway"}}},
+  };
+  const auto layout = gurka::detail::map_to_coordinates(ascii_map, 10);
+  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_flat_loop");
+  check_opposing(map);
+}
+
+TEST(loops, lollipop) {
+  // we create a way that doubles back on itself
+  const std::string ascii_map = R"(
+              C----D
+             /      \
+      A-----B        |
+             \      /
+              F----E)";
+  const gurka::ways ways = {
+      {"ABCDEFBA", {{"highway", "motorway"}}},
+  };
+  const auto layout = gurka::detail::map_to_coordinates(ascii_map, 10);
+  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_lollipop");
+  check_opposing(map);
+}
+
+TEST(loops, split_hair) {
+  // we create a way that doubles back on itself
+  const std::string ascii_map = R"(
+  F--E
+      \
+       C-----D
+      /
+  A--B)";
+  const gurka::ways ways = {
+      {"ABCDCEF", {{"highway", "motorway"}}},
+  };
+  const auto layout = gurka::detail::map_to_coordinates(ascii_map, 10);
+  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_split_hair");
+  check_opposing(map);
+}
+
+TEST(loops, split_lolli) {
+  // we create a way that doubles back on itself
+  const std::string ascii_map = R"(
+  K--J         I----H
+      \       /      \
+       C--D--E        |
+      /       \      /
+  A--B         F----G)";
+  const gurka::ways ways = {
+      {"ABCDEFGHIEDCJK", {{"highway", "motorway"}}},
+  };
+  const auto layout = gurka::detail::map_to_coordinates(ascii_map, 10);
+  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_split_lolli");
+  check_opposing(map);
 }
