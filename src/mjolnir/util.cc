9d2c2f57e5377034985f4828663f2cc804dcbd0d
Greg Knisely <greg.knisely@mapbox.com>
2020-05-27 10:45:03 -0400

    updated.

--- src/mjolnir/util.cc
+++ src/mjolnir/util.cc
@@ -255,8 +255,7 @@ bool build_tile_set(const boost::property_tree::ptree& config,
     // Read the OSM protocol buffer file. Callbacks for nodes
     // are defined within the PBFParser class
     PBFGraphParser::ParseNodes(config.get_child("mjolnir"), input_files, ways_bin, way_nodes_bin,
-                               intersections_bin, shapes_bin, bss_nodes_bin,
-                               tile_dir, osm_data);
+                               intersections_bin, shapes_bin, bss_nodes_bin, tile_dir, osm_data);
 
     // Free all protobuf memory - cannot use the protobuffer lib after this!
     if (release_osmpbf_memory) {
