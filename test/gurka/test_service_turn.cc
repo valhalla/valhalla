b30920491244134fc40936f6d2417ce22bd77fe4
dnesbitt61 <dnesbitt61@gmail.com>
2020-06-11 14:41:21 -0400

    Initial gurka test for using service roads to cut a corner. Fails until bicycle costing is updated

--- test/gurka/test_service_turn.cc
+++ test/gurka/test_service_turn.cc
@@ -10,18 +10,20 @@ TEST(Standalone, avoid_service) {
   // corners near intersections
   const std::string ascii_map = R"(
     A----B----C
-          \   |
-              D)";
+         |    |
+         D----E
+              |
+              F)";
 
-  const gurka::ways ways = {{"ABC", {{"highway", "secondary"}}}, {"BD", {{"highway", "service"}}}, 
-                            {"CD", {{"highway", "tertiary"}}}};
-
+  const gurka::ways ways = {{"ABC", {{"highway", "secondary"}}}, 
+                            {"CEF", {{"highway", "tertiary"}}},
+                            {"BDE", {{"highway", "service"}}}}; 
 
   const auto layout = gurka::detail::map_to_coordinates(ascii_map, 100);
   auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_avoid_service");
 
-  auto result = gurka::route(map, "A", "D", "bicycle");
+  auto result = gurka::route(map, "A", "F", "bicycle");
 
-  gurka::assert::osrm::expect_route(result, {"ABC", "CD"});
+  gurka::assert::osrm::expect_route(result, {"ABC", "CEF"});
 //  gurka::assert::raw::expect_path_length(result, 1.0, .001);
 }
