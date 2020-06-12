46c3629c78ed90b6a22db1c6ec1826d98fe67e5f
Kevin Kreiser <kevinkreiser@gmail.com>
2020-06-12 18:43:57 -0400

    test more parts of the wayid space

--- test/gurka/test_64bit_wayid.cc
+++ test/gurka/test_64bit_wayid.cc
@@ -3,21 +3,25 @@
 
 using namespace valhalla;
 
-TEST(UseDirectionOnWays, CheckNamesAndRefs) {
+TEST(WaysId, way_ids) {
   // build a very simple graph
-  const std::string ascii_map = R"(A----B----C)";
-  const gurka::ways ways = {
-      {"AB",
-       {
-           {"highway", "trunk"},
-           {"osm_id", std::to_string(static_cast<uint64_t>(-1))},
-       }},
-      {"BC",
-       {
-           {"highway", "trunk"},
-           {"osm_id", std::to_string(static_cast<uint64_t>(67132))},
-       }},
-  };
+  const std::string ascii_map = R"(ABCDFGHIJKLMNOPQRSTUVWXYZ)";
+  gurka::ways ways;
+  int node = 0;
+  std::multiset<uint64_t> osm_way_ids;
+  for (const auto& highway : std::vector<std::string>{"motorway", "secondary", "residential"}) {
+    for (uint64_t i = 0; i < 8; ++i) {
+      // compute way name and id
+      std::string name = ascii_map.substr(node++, 2);
+      uint64_t osm_id = (static_cast<uint64_t>(1) << (i * 8)) + highway[0];
+      // we'll see the ids twice, one for each direction of the edge
+      osm_way_ids.emplace(osm_id);
+      osm_way_ids.emplace(osm_id);
+      // add the way
+      ways[name] = {{"highway", highway}, {"osm_id", std::to_string(osm_id)}};
+    }
+  }
+  EXPECT_EQ(osm_way_ids.size(), 48);
   const auto layout = gurka::detail::map_to_coordinates(ascii_map, 100, {.1, .1});
   auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_64bit_wayid",
                                {{"mjolnir.timezone", "/path/to/timezone.sqlite"}});
@@ -29,14 +33,13 @@ TEST(UseDirectionOnWays, CheckNamesAndRefs) {
   for (const auto& tile_id : reader.GetTileSet()) {
     const auto* tile = reader.GetGraphTile(tile_id);
     for (auto edge = tile_id; edge.id() < tile->header()->directededgecount(); ++edge) {
+      // we should find every way id in the tile set
       auto info = tile->edgeinfo(tile->directededge(edge)->edgeinfo_offset());
-      auto names = info.GetNames();
-      if (names.at(0) == "AB")
-        EXPECT_EQ(info.wayid(), static_cast<uint64_t>(-1));
-      else if (names.at(0) == "BC")
-        EXPECT_EQ(info.wayid(), static_cast<uint64_t>(67132));
-      else
-        FAIL() << "Got an edge with an unexpected name";
+      auto found = osm_way_ids.find(info.wayid());
+      EXPECT_FALSE(found == osm_way_ids.cend());
+      osm_way_ids.erase(found);
     }
   }
+  // and because we found them all there should be none left
+  EXPECT_EQ(osm_way_ids.size(), 0);
 }
