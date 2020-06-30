23a6d7e24ac2a3e84cf2e1b535e55535855b7b1a
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-06-30 13:19:15 -0400

    Remove "leaked" dependence on robin-hood-hashing
    Modify thor::CostMatrix to make the target map an opaque type. This
    allows us to entirely encapsulate use of RHH in the implementation,
    avoiding what otherwise appears to be the only place where use of RHH
    leaked into the public interface. In particular, this means that
    consumers no longer need to be able to include the RHH headers. (Which
    is very fortunate, as we weren't installing them!)

--- src/thor/costmatrix.cc
+++ src/thor/costmatrix.cc
@@ -6,6 +6,8 @@
 #include "thor/costmatrix.h"
 #include "worker.h"
 
+#include <robin_hood.h>
+
 using namespace valhalla::baldr;
 using namespace valhalla::sif;
 
@@ -29,10 +31,15 @@ bool equals(const valhalla::LatLng& a, const valhalla::LatLng& b) {
 namespace valhalla {
 namespace thor {
 
+class CostMatrix::TargetMap : public robin_hood::unordered_map<uint64_t, std::vector<uint32_t>> {};
+
 // Constructor with cost threshold.
 CostMatrix::CostMatrix()
     : mode_(TravelMode::kDrive), access_mode_(kAutoAccess), source_count_(0), remaining_sources_(0),
-      target_count_(0), remaining_targets_(0), current_cost_threshold_(0) {
+      target_count_(0), remaining_targets_(0), current_cost_threshold_(0), targets_{new TargetMap} {
+}
+
+CostMatrix::~CostMatrix() {
 }
 
 float CostMatrix::GetCostThreshold(const float max_matrix_distance) {
@@ -59,7 +66,7 @@ float CostMatrix::GetCostThreshold(const float max_matrix_distance) {
 // construction.
 void CostMatrix::Clear() {
   // Clear the target edge markings
-  targets_.clear();
+  targets_->clear();
 
   // Clear all source adjacency lists, edge labels, and edge status
   for (auto& adj : source_adjacency_) {
@@ -395,8 +402,8 @@ void CostMatrix::CheckForwardConnections(const uint32_t source,
   // Get the opposing edge. Get a list of target locations whose reverse
   // search has reached this edge.
   GraphId oppedge = pred.opp_edgeid();
-  auto targets = targets_.find(oppedge);
-  if (targets == targets_.end()) {
+  auto targets = targets_->find(oppedge);
+  if (targets == targets_->end()) {
     return;
   }
 
@@ -615,7 +622,7 @@ void CostMatrix::BackwardSearch(const uint32_t index, GraphReader& graphreader) 
       adj->add(idx);
 
       // Add to the list of targets that have reached this edge
-      targets_[edgeid].push_back(index);
+      (*targets_)[edgeid].push_back(index);
     }
 
     // Handle transitions - expand from the end node of the transition
@@ -819,7 +826,7 @@ void CostMatrix::SetTargets(baldr::GraphReader& graphreader,
       target_adjacency_[index]->add(idx);
       target_edgestatus_[index].Set(opp_edge_id, EdgeSet::kUnreachedOrReset, idx,
                                     graphreader.GetGraphTile(opp_edge_id));
-      targets_[opp_edge_id].push_back(index);
+      (*targets_)[opp_edge_id].push_back(index);
     }
     index++;
   }
