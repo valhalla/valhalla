7b09629dcabb310a2c9e1b547acad0740cd13b32
Anders Bennehag <anders@bennehag.com>
2020-07-28 10:09:49 -0400

    fix(traffic-tile-version): Locks down traffic tile format
    and formalises process of changing format. Approach is same as the
    regular data tiles, i.e. forward compatible by only using spare bits.
    
    There's a 'break-the-glass' solution for when the format can move no
    more, and a new TRAFFIC_TILE_VERSION must be created
    
    Fixes https://github.com/valhalla/valhalla/issues/2500

--- valhalla/baldr/traffictile.h
+++ valhalla/baldr/traffictile.h
@@ -17,7 +17,6 @@
 #else
 #include <stdint.h>
 #endif
-#include "baldr/traffic_tile_version.h"
 
 #ifndef C_ONLY_INTERFACE
 namespace valhalla {
@@ -27,6 +26,14 @@ using std::uint16_t;
 using std::uint32_t;
 using std::uint64_t;
 #endif
+
+// The version of traffic tile format
+// This is not intended to allow for easy migration between in-compatible data-formats
+// Changes are still expected to be applied in a backwards compatible way by using
+// the spare bits available.
+// This is more of a break-the-glass escape hatch for when the current format has
+// reached its limits
+const uint8_t TRAFFIC_TILE_VERSION = 3;
 
 // This value _of bitfield_ (not in kph) signals that the live speed is not known (max value of 7 bit
 // number)
