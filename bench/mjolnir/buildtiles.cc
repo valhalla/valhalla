911209203768eabceddee142631ac3482df88540
Pavel Merkis <pavel.merkis@mapbox.com>
2020-07-28 11:07:19 +0300

    Benchmark single stages

--- bench/mjolnir/buildtiles.cc
+++ bench/mjolnir/buildtiles.cc
@@ -1,54 +1,43 @@
 #include <benchmark/benchmark.h>
 
 #include "baldr/rapidjson_utils.h"
-#include "filesystem.h"
 #include "mjolnir/util.h"
 
 using namespace valhalla::mjolnir;
 
-namespace {
-
-void BM_SomeFunction(benchmark::State& state, const std::string& pbf_path) {
+int main(int argc, char** argv) {
   valhalla::midgard::logging::Configure({{"type", ""}});
 
-  std::string const kConfigFilePath = VALHALLA_SOURCE_DIR "bench/mjolnir/config.json";
-  boost::property_tree::ptree config;
-  rapidjson::read_json(kConfigFilePath, config);
-  config.get_child("mjolnir").erase("tile_extract");
-  config.get_child("mjolnir").erase("tile_url");
+  const auto build_tiles = [](benchmark::State& state, const std::string& pbf_path,
+                              BuildStage stage) {
+    const std::string kConfigFilePath = VALHALLA_SOURCE_DIR "bench/mjolnir/config.json";
+    boost::property_tree::ptree config;
+    rapidjson::read_json(kConfigFilePath, config);
+    config.get_child("mjolnir").erase("tile_extract");
+    config.get_child("mjolnir").erase("tile_url");
 
-  BuildStage const start_stage_str = string_to_buildstage("initialize");
-  BuildStage const end_stage_str = string_to_buildstage("parseways");
+    for (auto _ : state) {
+      state.PauseTiming();
+      build_tile_set(config, {pbf_path}, BuildStage::kInitialize,
+                     static_cast<BuildStage>(static_cast<uint8_t>(stage) - 1));
+      state.ResumeTiming();
+      benchmark::DoNotOptimize(build_tile_set(config, {pbf_path}, stage, stage));
+    }
+  };
 
-  for (auto _ : state) {
-    build_tile_set(config, {pbf_path}, start_stage_str, end_stage_str);
-  }
-};
-
-struct FileWithSize {
-  filesystem::path path;
-  uintmax_t size;
-
-  bool operator<(const FileWithSize& file) const {
-    return size < file.size;
-  }
-};
-
-std::vector<FileWithSize> CollectInputProtobufs(const std::string& path) {
-  std::vector<FileWithSize> files;
-  for (filesystem::recursive_directory_iterator i(path), end; i != end; ++i)
-    files.push_back({i->path(), i->file_size()});
-  std::sort(files.begin(), files.end());
-  return files;
-}
-
-} // namespace
-
-int main(int argc, char** argv) {
-  auto protobufs = CollectInputProtobufs(VALHALLA_SOURCE_DIR "bench/mjolnir/pbf");
-  for (const FileWithSize& file : protobufs) {
-    benchmark::RegisterBenchmark(file.path.filename().c_str(), BM_SomeFunction, file.path.string())
-        ->Unit(benchmark::kMillisecond);
+  const std::string kProtobufs[] = {
+      "andorra.osm.pbf", "london.osm.pbf", "monaco.osm.pbf",    "moscow.osm.pbf",
+      "munchen.osm.pbf", "porto.osm.pbf",  "stockholm.osm.pbf", "sydney.osm.pbf",
+  };
+  const BuildStage kStages[] = {BuildStage::kParseWays, BuildStage::kParseRelations,
+                                BuildStage::kParseNodes, BuildStage::kBuild, BuildStage::kEnhance};
+  for (const BuildStage stage : kStages) {
+    for (const std::string& filename : kProtobufs) {
+      std::string bench_name = filename.substr(0, filename.size() - 8) + "/" + to_string(stage);
+      benchmark::RegisterBenchmark(bench_name.c_str(), build_tiles,
+                                   VALHALLA_SOURCE_DIR "bench/mjolnir/pbf/" + filename, stage)
+          ->Unit(benchmark::kMillisecond);
+    }
   }
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
