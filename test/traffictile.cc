7f5ec9af5d57963d2a8862ac8cc279ead5c2f2a3
Anders Bennehag <anders.bennehag@mapbox.com>
2020-07-29 12:38:24 -0400

    fix(traffic-tile-version): Locks down traffic tile format (#2503)
    * fix(traffic-tile-version): Locks down traffic tile format
    
    and formalises process of changing format. Approach is same as the
    regular data tiles, i.e. forward compatible by only using spare bits.
    
    There's a 'break-the-glass' solution for when the format can move no
    more, and a new TRAFFIC_TILE_VERSION must be created
    
    Fixes https://github.com/valhalla/valhalla/issues/2500

--- test/traffictile.cc
+++ test/traffictile.cc
@@ -34,8 +34,8 @@ TEST(Traffic, TileConstruction) {
   EXPECT_EQ(speed.get_speed(0), 98);
   EXPECT_EQ(speed.get_speed(1), UNKNOWN_TRAFFIC_SPEED_RAW << 1);
 
-  // Verify the version (Expect this value to change when traffictile.h is updated)
-  EXPECT_EQ(0xce2f, TRAFFIC_TILE_VERSION);
+  // Verify the version
+  EXPECT_EQ(3, TRAFFIC_TILE_VERSION);
   // Test with an invalid version
   testdata.header.traffic_tile_version = 78;
   auto const volatile& invalid_speed = tile.trafficspeed(2);
