d1809244fc30f397593378ef9ed87cdc867f6cd4
kdiluca <kristen.diluca@mapbox.com>
2020-08-13 10:12:37 -0400

    adding toll gantry to node type

--- src/mjolnir/pbfgraphparser.cc
+++ src/mjolnir/pbfgraphparser.cc
@@ -237,6 +237,14 @@ public:
           }
           n.set_type(NodeType::kBorderControl);
         }
+      } else if (tag.first == "toll_gantry") {
+        if (tag.second == "true") {
+          if (!intersection) {
+            intersection = true;
+            ++osmdata_.edge_count;
+          }
+          n.set_type(NodeType::kTollGantry);
+        }
       } else if (tag.first == "access_mask") {
         n.set_access(std::stoi(tag.second));
       } else if (has_junction_name) {
