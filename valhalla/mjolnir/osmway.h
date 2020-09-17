d4cfa4e0fcadcde7c1bc610e7601526097c3e7de
Greg Knisely <greg.knisely@mapbox.com>
2020-09-16 20:12:25 -0400

    added GetTaggedNames

--- valhalla/mjolnir/osmway.h
+++ valhalla/mjolnir/osmway.h
@@ -1522,6 +1522,8 @@ struct OSMWay {
    */
   std::vector<std::string>
   GetNames(const std::string& ref, const UniqueNames& name_offset_map, uint16_t& types) const;
+  std::vector<std::string>
+  GetTaggedNames(const UniqueNames& name_offset_map) const;
 
   // OSM way Id
   uint64_t osmwayid_;
