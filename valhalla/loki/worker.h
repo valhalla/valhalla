b9a7df628124d6353b0d2c3ab82d31589fe66bab
kdiluca <kristen.diluca@mapbox.com>
2020-09-22 11:40:56 -0400

    Revert "Revert "add back whitelisting and unit tests for it (#2545)""
    This reverts commit 57bbfa17646d402ac963334856d6bd5d466950ff.

--- valhalla/loki/worker.h
+++ valhalla/loki/worker.h
@@ -67,6 +67,7 @@ protected:
   sif::cost_ptr_t costing;
   std::shared_ptr<baldr::GraphReader> reader;
   std::shared_ptr<baldr::connectivity_map_t> connectivity_map;
+  std::unordered_set<Options::Action> actions;
   std::string action_str;
   std::unordered_map<std::string, size_t> max_locations;
   std::unordered_map<std::string, float> max_distance;
