98fb82f2037db9eac165da2503ed432ef5456089
Anders Bennehag <anders.bennehag@mapbox.com>
2020-09-23 10:38:02 -0400

    nit(GraphTile::FileSuffix): Allows specifying suffix (#2611)

--- test/astar.cc
+++ test/astar.cc
@@ -222,11 +222,11 @@ void make_tile() {
   }
 
   GraphTile tile(test_dir, tile_id);
-  ASSERT_EQ(tile.FileSuffix(tile_id, false), std::string("2/000/519/120.gph"))
+  ASSERT_EQ(tile.FileSuffix(tile_id), std::string("2/000/519/120.gph"))
       << "Tile ID didn't match the expected filename";
 
   ASSERT_PRED1(filesystem::exists,
-               test_dir + filesystem::path::preferred_separator + tile.FileSuffix(tile_id, false))
+               test_dir + filesystem::path::preferred_separator + tile.FileSuffix(tile_id))
       << "Expected tile file didn't show up on disk - are the fixtures in the right location?";
 }
 
