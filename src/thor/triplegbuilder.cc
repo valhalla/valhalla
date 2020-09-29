27da0b198dccb863bd1fc993e9ae9dff0950d6c4
Anders Bennehag <anders@bennehag.com>
2020-09-29 13:47:59 -0400

    nit don't do needless work

--- src/thor/triplegbuilder.cc
+++ src/thor/triplegbuilder.cc
@@ -102,15 +102,12 @@ void UpdateIncident(const std::shared_ptr<valhalla::incidents::IncidentsTile>& i
                     TripLeg& leg,
                     const valhalla::incidents::IncidentLocation* incident_location,
                     uint32_t index) {
-  auto found =
-      std::find_if(leg.mutable_incidents()->begin(), leg.mutable_incidents()->end(),
-                   [&incidents_tile, incident_location](const TripLeg::ValhallaIncident& candidate) {
-                     const valhalla::incidents::IncidentMetadata& meta =
-                         valhalla::baldr::grabMetadataFromEdgeRelation(incidents_tile,
-                                                                       *incident_location);
-                     bool is_match = meta.id() == candidate.metadata().id();
-                     return is_match;
-                   });
+  const uint64_t current_incident_id =
+      valhalla::baldr::grabMetadataFromEdgeRelation(incidents_tile, *incident_location).id();
+  auto found = std::find_if(leg.mutable_incidents()->begin(), leg.mutable_incidents()->end(),
+                            [current_incident_id](const TripLeg::ValhallaIncident& candidate) {
+                              return current_incident_id == candidate.metadata().id();
+                            });
   // Are we continuing an incident (this could be a hash look up)
   if (found != leg.mutable_incidents()->end()) {
     found->set_end_shape_index(index);
