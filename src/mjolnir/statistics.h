4ea3ee6a4db12cf1371ea15f73144b97039c21ac
John Famiglietti <johnf@wahoofitness.com>
2020-10-13 22:25:51 -0400

    Formatter changes

--- src/mjolnir/statistics.h
+++ src/mjolnir/statistics.h
@@ -10,8 +10,8 @@
 
 #include <sqlite3.h>
 
-#include "valhalla/baldr/graphconstants.h"
 #include "midgard/aabb2.h"
+#include "valhalla/baldr/graphconstants.h"
 #include <boost/property_tree/ptree.hpp>
 
 using namespace valhalla::baldr;
