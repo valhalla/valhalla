c8ec772162de6451979e55fc062f3709a777bd9f
Kevin Kreiser <kevinkreiser@gmail.com>
2020-10-15 17:09:00 -0400

    faster exp approximation

--- src/sif/osrm_cost.h
+++ src/sif/osrm_cost.h
@@ -1,15 +1,9 @@
-#ifndef VALHALLA_SIF_OSRM_COST_H_
-#define VALHALLA_SIF_OSRM_COST_H_
+#pragma once
 
 #include "baldr/directededge.h"
 #include "baldr/graphconstants.h"
 #include "baldr/nodeinfo.h"
-
-using namespace valhalla::midgard;
-using namespace valhalla::baldr;
-
-namespace valhalla {
-namespace sif {
+#include <fastexp.h>
 
 namespace {
 
@@ -19,56 +13,46 @@ constexpr double kTurnBias = 1.075;
 constexpr double kTurnBiasInv = 1.0 / kTurnBias;
 constexpr double kTrafficLightPenalty = 2;
 
-inline uint32_t CalculateTurnDegree(const baldr::DirectedEdge* edge,
-                                    const baldr::NodeInfo* node,
+inline uint32_t CalculateTurnDegree(const valhalla::baldr::DirectedEdge* edge,
+                                    const valhalla::baldr::NodeInfo* node,
                                     const uint32_t idx_pred_opp) {
   auto in_heading = node->heading(idx_pred_opp);
   in_heading = ((in_heading + 180) % 360);
   auto out_heading = node->heading(edge->localedgeidx());
-  uint32_t turn_degree = GetTurnDegree(in_heading, out_heading);
+  uint32_t turn_degree = valhalla::midgard::GetTurnDegree(in_heading, out_heading);
   return turn_degree;
 }
 
-} // namespace
-
-float OSRMTurnCost(const baldr::DirectedEdge* edge,
-                   const baldr::NodeInfo* node,
+// This is a port of:
+// https://github.com/Project-OSRM/osrm-backend/blob/f5ebe8bc3b51831b7b19e73d4879ebbad0161a19/profiles/car.lua#L455
+inline float OSRMTurnCost(const valhalla::baldr::DirectedEdge* edge,
+                   const valhalla::baldr::NodeInfo* node,
                    const uint32_t idx_pred_opp) {
 
   double turn_duration = node->traffic_signal() ? kTrafficLightPenalty : 0;
   uint32_t turn_degree = CalculateTurnDegree(edge, node, idx_pred_opp);
 
   // const auto is_uturn = guidance::getTurnDirection(turn_angle) ==
-  // guidance::DirectionModifier::UTurn; BUT for OSRM utorn is 0 angle only, not some range
-  const bool is_u_turn = Turn::GetType(turn_degree) == Turn::Type::kReverse;
+  // guidance::DirectionModifier::UTurn; BUT for OSRM uturn is 0 angle only, not a range
+  const bool is_u_turn =
+      valhalla::baldr::Turn::GetType(turn_degree) == valhalla::baldr::Turn::Type::kReverse;
   const uint32_t number_of_roads = node->local_edge_count();
 
-  // if turn.number_of_roads > 2 or turn.source_mode ~= turn.target_mode or turn.is_u_turn then
   if (number_of_roads > 2 || is_u_turn) {
     int32_t osrm_angle = turn_degree > 180 ? static_cast<int32_t>(turn_degree) - 360 : turn_degree;
     double turn_bias = !node->drive_on_right() ? kTurnBiasInv : kTurnBias;
     double turn_bias_inv = !node->drive_on_right() ? kTurnBias : kTurnBiasInv;
 
-    // if turn.angle >= 0 then
-    //  turn.duration = turn.duration + turn_penalty / (1 + math.exp( -((13 / turn_bias) *
-    //  turn.angle/180 - 6.5*turn_bias)))
-    // else
-    //  turn.duration = turn.duration + turn_penalty / (1 + math.exp( -((13 * turn_bias) *
-    //  -turn.angle/180 - 6.5/turn_bias)))
-    // end
     if (osrm_angle >= 0)
-      turn_duration +=
-          kTurnPenalty / (1 + std::exp(-((13 * turn_bias_inv) * osrm_angle / 180 - 6.5 * turn_bias)));
+      turn_duration += kTurnPenalty /
+                       (1 + fasterexp(-((13 * turn_bias_inv) * osrm_angle / 180 - 6.5 * turn_bias)));
     else
       turn_duration += kTurnPenalty /
-                       (1 + std::exp(-((13 * turn_bias) * -osrm_angle / 180 - 6.5 * turn_bias_inv)));
+                       (1 + fasterexp(-((13 * turn_bias) * -osrm_angle / 180 - 6.5 * turn_bias_inv)));
 
     turn_duration += is_u_turn ? kUTurnPenalty : 0;
   }
   return turn_duration;
 }
 
-} // namespace sif
-} // namespace valhalla
-
-#endif // VALHALLA_SIF_OSRM_COST_H_
+} // namespace
