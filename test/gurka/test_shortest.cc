2cdeec3b83605bc2a82c7a64ba7ea792abebf74f
nilsnolde <nils.nolde@gmail.com>
2020-10-16 13:49:31 +0200

    fix tests for ped, bike, scooter

--- test/gurka/test_shortest.cc
+++ test/gurka/test_shortest.cc
@@ -12,53 +12,52 @@ protected:
     constexpr double gridsize = 2000;
 
     const std::string ascii_map = R"(
-          G----H
-          |    | 
-       A--B----C--D
-          |    |
-          E----F
+          H-----I
+          |     | 
+       A--B--C--D--E
+          |     |
+          F-----G
     )";
 
     const gurka::ways ways = {
         // segments expected for shortest routes for all profiles
-        {"AB", {{"highway", "primary"}, {"maxspeed", "20"}}},
-        {"BC", {{"highway", "primary"}, {"maxspeed", "20"}}},
-        {"CD", {{"highway", "primary"}, {"maxspeed", "20"}}},
+        {"AB", {{"highway", "tertiary"}, {"sac_scale", "hiking"}, {"service", "alley"}, {"maxspeed", "20"}, {"surface", "dirt"}}},
+        {"BC", {{"highway", "tertiary"}, {"sac_scale", "hiking"}, {"service", "alley"}, {"maxspeed", "20"}, {"surface", "dirt"}}},
+        {"CD", {{"highway", "tertiary"}, {"sac_scale", "hiking"}, {"service", "alley"}, {"maxspeed", "20"}, {"surface", "dirt"}}},
+        {"DE", {{"highway", "tertiary"}, {"sac_scale", "hiking"}, {"service", "alley"}, {"maxspeed", "20"}, {"surface", "dirt"}}},
         // segments expected for fastest routes for motor_vehicles
-        {"BE", {{"highway", "primary"}, {"maxspeed", "100"}}},
-        {"EF", {{"highway", "primary"}, {"maxspeed", "100"}}},
-        {"FC", {{"highway", "primary"}, {"maxspeed", "100"}}},
+        {"BF", {{"highway", "tertiary"}, {"maxspeed", "100"}}},
+        {"FG", {{"highway", "tertiary"}, {"maxspeed", "100"}}},
+        {"GD", {{"highway", "tertiary"}, {"maxspeed", "100"}}},
         // segments expected for fastest routes for ped/bikes
-        {"BG", {{"highway", "primary"}, {"motor_vehicle", "no"}}},
-        {"GH", {{"highway", "primary"}, {"motor_vehicle", "no"}}},
-        {"HC", {{"highway", "primary"}, {"motor_vehicle", "no"}}},
+        {"BH", {{"highway", "path"}, {"foot", "yes"}, {"sidewalk", "right"}, {"surface", "paved"}}},
+        {"HI", {{"highway", "path"}, {"foot", "yes"}, {"sidewalk", "right"}, {"surface", "paved"}}},
+        {"ID", {{"highway", "path"}, {"foot", "yes"}, {"sidewalk", "right"}, {"surface", "paved"}}},
     };
 
     const auto layout = gurka::detail::map_to_coordinates(ascii_map, gridsize);
     shortest_map = gurka::buildtiles(layout, ways, {}, {}, "test/data/shortest");
   }
 
-  inline void getSummary(const valhalla::Api& route, double& distance, double& duration) {
-    auto summary = route.directions().routes(0).legs(0).summary();
-    distance = summary.length();
-    duration = summary.time();
+  inline float getSummary(const valhalla::Api& route) {
+    return route.directions().routes(0).legs(0).summary().length();
   }
 
   void doTests(const std::string& profile,
                const std::vector<std::string>& fastest_path,
                const std::unordered_map<std::string, std::string>& shortest_options) {
-    double fastest_l, fastest_t, shortest_l, shortest_t;
 
-    valhalla::Api fastest = gurka::route(shortest_map, "A", "D", profile);
-    getSummary(fastest, fastest_l, fastest_t);
+    valhalla::Api fastest = gurka::route(shortest_map, "A", "E", profile);
+    float fastest_l = getSummary(fastest);
 
-    valhalla::Api shortest = gurka::route(shortest_map, "A", "D", profile, shortest_options);
-    getSummary(shortest, shortest_l, shortest_t);
+    valhalla::Api shortest = gurka::route(shortest_map, "A", "E", profile, shortest_options);
+    float shortest_l = getSummary(shortest);
+
+    std::cout << "Lenghts: " << fastest_l << ", " << shortest_l << EOF;
 
     gurka::assert::raw::expect_path(fastest, fastest_path);
-    gurka::assert::raw::expect_path(shortest, {"AB", "BC", "CD"});
+    gurka::assert::raw::expect_path(shortest, {"AB", "BC", "CD", "DE"});
     ASSERT_GT(fastest_l, shortest_l);
-    ASSERT_LT(fastest_t, shortest_t);
   }
 };
 
@@ -66,38 +65,36 @@ gurka::map ShortestTest::shortest_map = {};
 
 TEST_F(ShortestTest, AutoShortest) {
   std::string profile = "auto";
-  doTests(profile, {"AB", "BE", "EF", "FC", "CD"},
+  doTests(profile, {"AB", "BF", "FG", "GD", "DE"},
           {{"/costing_options/" + profile + "/shortest", "1"}});
 }
 
 TEST_F(ShortestTest, TruckShortest) {
   std::string profile = "truck";
-  doTests(profile, {"AB", "BE", "EF", "FC", "CD"},
+  doTests(profile, {"AB", "BF", "FG", "GD", "DE"},
           {{"/costing_options/" + profile + "/shortest", "1"}});
 }
 
 TEST_F(ShortestTest, MotorbikeShortest) {
   std::string profile = "motorcycle";
-  doTests(profile, {"AB", "BE", "EF", "FC", "CD"},
+  doTests(profile, {"AB", "BF", "FG", "GD", "DE"},
           {{"/costing_options/" + profile + "/shortest", "1"}});
-}
-
-/**
- * Scooter/bike/ped still failing, no good edge setup idea how to make
- * "fastest" take the longer route and "shortest" the shorter one
- *
-TEST_F(ShortestTest, ScooterShortest) {
-  std::string profile = "motor_scooter";
-  doTests(profile, {"AB", "BE", "EF", "FC", "CD"},
-          {{"/costing_options/" + profile + "/shortest", "1"},
-           {"/costing_options/" + profile + "/top_speed", "120"},
-           {"/costing_options/" + profile + "/use_primary", "1.0"}});
 }
 
 TEST_F(ShortestTest, BikeShortest) {
   std::string profile = "bicycle";
-  doTests(profile, {"AB", "BG", "GH", "HC", "CD"},
-          {{"/costing_options/" + profile + "/shortest", "1"},
-           {"/costing_options/" + profile + "/use_roads", "1.0"}});
+  doTests(profile, {"AB", "BH", "HI", "ID", "DE"},
+          {{"/costing_options/" + profile + "/shortest", "1"}});
 }
-*/
+
+TEST_F(ShortestTest, PedestrianShortest) {
+  std::string profile = "pedestrian";
+  doTests(profile, {"AB", "BH", "HI", "ID", "DE"},
+          {{"/costing_options/" + profile + "/shortest", "1"}});
+}
+
+TEST_F(ShortestTest, ScooterShortest) {
+  std::string profile = "motor_scooter";
+  doTests(profile, {"AB", "BF", "FG", "GD", "DE"},
+          {{"/costing_options/" + profile + "/shortest", "1"}});
+}
