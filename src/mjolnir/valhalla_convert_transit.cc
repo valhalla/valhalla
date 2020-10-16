f995bc992eb33c82e73ae458027b172d72e261f3
Svetlana Bayarovich <svetlana.bayarovich@mapbox.com>
2020-10-16 17:08:09 +0300

    Fix cases of removed road class parameter in nodeinfo

--- src/mjolnir/valhalla_convert_transit.cc
+++ src/mjolnir/valhalla_convert_transit.cc
@@ -562,8 +562,7 @@ void AddToGraph(GraphTileBuilder& tilebuilder_transit,
 
       // Set the station lat,lon using the tile base LL
       PointLL base_ll = tilebuilder_transit.header_builder().base_ll();
-      NodeInfo station_node(base_ll, station_ll, RoadClass::kServiceOther, n_access,
-                            NodeType::kTransitStation, false);
+      NodeInfo station_node(base_ll, station_ll, n_access, NodeType::kTransitStation, false);
       station_node.set_stop_index(station_pbf_id.id());
 
       const std::string& tz = station.has_timezone() ? station.timezone() : "";
@@ -629,8 +628,7 @@ void AddToGraph(GraphTileBuilder& tilebuilder_transit,
 
         // Set the egress lat,lon using the tile base LL
         PointLL base_ll = tilebuilder_transit.header_builder().base_ll();
-        NodeInfo egress_node(base_ll, egress_ll, RoadClass::kServiceOther, n_access,
-                             NodeType::kTransitEgress, false);
+        NodeInfo egress_node(base_ll, egress_ll, n_access, NodeType::kTransitEgress, false);
         egress_node.set_stop_index(index);
         egress_node.set_timezone(timezone);
         egress_node.set_edge_index(tilebuilder_transit.directededges().size());
@@ -809,8 +807,7 @@ void AddToGraph(GraphTileBuilder& tilebuilder_transit,
 
     // Set the platform lat,lon using the tile base LL
     PointLL base_ll = tilebuilder_transit.header_builder().base_ll();
-    NodeInfo platform_node(base_ll, platform_ll, RoadClass::kServiceOther, n_access,
-                           NodeType::kMultiUseTransitPlatform, false);
+    NodeInfo platform_node(base_ll, platform_ll, n_access, NodeType::kMultiUseTransitPlatform, false);
     platform_node.set_mode_change(true);
     platform_node.set_stop_index(platform_index);
     platform_node.set_timezone(timezone);
