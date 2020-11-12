0b75024633d7f752be3c02f0f641f76e46f24e8d
Henadzi Klimuk <genadz.klimuk@mapbox.com>
2020-11-12 19:08:22 +0300

    Fix double precision for height action

--- src/loki/transit_available_action.cc
+++ src/loki/transit_available_action.cc
@@ -29,10 +29,10 @@ std::string loki_worker_t::transit_available(Api& request) {
       // Get a list of tiles required within the radius of the projected point
       const auto& ll = location.latlng_;
       DistanceApproximator<PointLL> approximator(ll);
-      float latdeg = (location.radius_ / kMetersPerDegreeLat);
-      float lngdeg = (location.radius_ / DistanceApproximator<PointLL>::MetersPerLngDegree(ll.lat()));
-      AABB2<PointLL> bbox(Point2(ll.lng() - lngdeg, ll.lat() - latdeg),
-                          Point2(ll.lng() + lngdeg, ll.lat() + latdeg));
+      double latdeg = double(location.radius_) / kMetersPerDegreeLat;
+      double lngdeg = double(location.radius_) / DistanceApproximator<PointLL>::MetersPerLngDegree(ll.lat());
+      AABB2<PointLL> bbox(Point2d(ll.lng() - lngdeg, ll.lat() - latdeg),
+                          Point2d(ll.lng() + lngdeg, ll.lat() + latdeg));
       std::vector<int32_t> tilelist = tiles.TileList(bbox);
       for (auto id : tilelist) {
         // transit is level hierarchy level 3
