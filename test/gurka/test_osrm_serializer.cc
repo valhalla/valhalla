21ebd039ef79404c104d0ec38bcbb256dfc1154b
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2020-11-24 17:41:14 -0500

    Fix polyline simplification float/double comparison

---
+++ test/gurka/test_osrm_serializer.cc
@@ -0,0 +1,34 @@
+#include "gurka.h"
+#include <gtest/gtest.h>
+
+using namespace valhalla;
+
+TEST(Standalone, OsrmSerializerShape) {
+  const std::string ascii_map = R"(
+    B---C---D
+    |
+    A
+  )";
+
+  const gurka::ways ways = {
+      {"AB", {{"highway", "primary"}}},
+      {"BC", {{"highway", "primary"}}},
+      {"CD", {{"highway", "primary"}}},
+  };
+
+  const auto layout = gurka::detail::map_to_coordinates(ascii_map, 10, {40.7351162, -73.985719});
+  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/osrm_serializer_shape");
+
+  // Test that full shape is returned by default
+  auto result = gurka::route(map, {"A", "D"}, "auto", {{"/shape_format", "geojson"}});
+  auto json = gurka::convert_to_json(result, Options::Format::Options_Format_osrm);
+  auto route_geometry = json["routes"][0]["geometry"]["coordinates"].GetArray();
+  EXPECT_EQ(route_geometry.Size(), 4);
+
+  // Test that shape is simplified (A-B-C should simplify out B)
+  result =
+      gurka::route(map, {"A", "D"}, "auto", {{"/generalize", "0"}, {"/shape_format", "geojson"}});
+  json = gurka::convert_to_json(result, Options::Format::Options_Format_osrm);
+  route_geometry = json["routes"][0]["geometry"]["coordinates"].GetArray();
+  EXPECT_EQ(route_geometry.Size(), 3);
+}
