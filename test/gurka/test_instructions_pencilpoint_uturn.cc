88a7b7bb1fe8bdf88ff514c54b76e236216b703c
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2020-12-15 11:23:07 -0500

    Fix gurka test

--- test/gurka/test_instructions_pencilpoint_uturn.cc
+++ test/gurka/test_instructions_pencilpoint_uturn.cc
@@ -26,7 +26,7 @@ protected:
         {{"ABE", {{"highway", "primary"}, {"name", "Silverbrook Road"}, {"oneway", "-1"}}},
          {"CDE", {{"highway", "primary"}, {"name", "Silverbrook Road"}, {"oneway", "yes"}}},
          {"EF", {{"highway", "primary"}, {"name", "Silverbrook Road"}, {"oneway", "yes"}}},
-         {"GEH", {{"highway", "primary"}, {"name", "Old Barrington Boulevard"}}}};
+         {"GEH", {{"highway", "secondary"}, {"name", "Old Barrington Boulevard"}}}};
 
     const auto layout =
         gurka::detail::map_to_coordinates(ascii_map, gridsize_metres, {5.1079374, 52.0887174});
