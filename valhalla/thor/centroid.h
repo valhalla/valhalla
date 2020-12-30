4e0e8d4664610bec62774c1930f2a86b138c5b5f
Kevin Kreiser <kevinkreiser@gmail.com>
2020-12-30 16:47:58 -0500

    lint

--- valhalla/thor/centroid.h
+++ valhalla/thor/centroid.h
@@ -121,7 +121,7 @@ protected:
    * about edges who have been settled which means we can completely ignore this
    */
   virtual void ExpandingNode(baldr::GraphReader&,
-                             graph_tile_ptr tile,
+                             graph_tile_ptr,
                              const baldr::NodeInfo*,
                              const sif::EdgeLabel&,
                              const sif::EdgeLabel*) override {
