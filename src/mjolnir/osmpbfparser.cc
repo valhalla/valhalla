ec5f0846b99b54812f3672e3ac62956427cd5cab
nilsnolde <nils.nolde@gmail.com>
2021-01-28 12:38:09 +0100

    guard for netinet headers with ninja

--- src/mjolnir/osmpbfparser.cc
+++ src/mjolnir/osmpbfparser.cc
@@ -28,7 +28,7 @@
 // This is largely based off of: https://github.com/CanalTP/libosmpbfreader
 // there have been some minor changes for our own purposes but its largely the same
 #include <cstdint>
-#ifdef _MSC_VER
+#if defined(_WIN32) && !defined(__MINGW32__)
 #include <winsock2.h> // ntohl
 #else
 #include <netinet/in.h>
