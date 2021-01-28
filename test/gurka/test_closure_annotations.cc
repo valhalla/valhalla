be0fafdbdc5faeb06574ec9c1e515c64fd0fb85d
Mandeep Sandhu <mandeep.sandhu@mapbox.com>
2021-01-27 23:57:30 -0800

    Add tests for closure annotations

--- test/gurka/test_closure_annotations.cc
+++ test/gurka/test_closure_annotations.cc
@@ -9,29 +9,53 @@ using LiveTrafficCustomize = test::LiveTrafficCustomize;
 
 namespace {
 
-inline void
-SetSubsegmentLiveSpeed(baldr::TrafficSpeed* live_speed, uint64_t speed, uint8_t subsegment) {
+inline void SetLiveSpeedFrom(baldr::TrafficSpeed* live_speed, uint64_t speed, uint8_t subsegment) {
   live_speed->breakpoint1 = subsegment;
-  live_speed->overall_speed = speed >> 1;
+  live_speed->breakpoint2 = 255;
+  live_speed->speed2 = speed >> 1;
+  live_speed->speed3 = UNKNOWN_TRAFFIC_SPEED_RAW;
+}
+
+inline void SetLiveSpeedUpto(baldr::TrafficSpeed* live_speed, uint64_t speed, uint8_t subsegment) {
+  live_speed->breakpoint1 = subsegment;
   live_speed->speed1 = speed >> 1;
 }
 
 inline void SetLiveSpeed(baldr::TrafficSpeed* live_speed, uint64_t speed) {
-  SetSubsegmentLiveSpeed(live_speed, speed, 255);
+  live_speed->breakpoint1 = 255;
+  live_speed->overall_speed = speed >> 1;
+  live_speed->speed1 = speed >> 1;
 }
 
-void close_partial_dir_edge(baldr::GraphReader& reader,
-                            baldr::TrafficTile& tile,
-                            uint32_t index,
-                            double percent_along,
-                            baldr::TrafficSpeed* current,
-                            const std::string& edge_name,
-                            const std::string& start_node,
-                            const gurka::map& map) {
+void close_partial_dir_edge_from(baldr::GraphReader& reader,
+                                 baldr::TrafficTile& tile,
+                                 uint32_t index,
+                                 double percent_along,
+                                 baldr::TrafficSpeed* current,
+                                 const std::string& edge_name,
+                                 const std::string& end_node,
+                                 const gurka::map& map) {
   baldr::GraphId tile_id(tile.header->tile_id);
-  auto edge = std::get<0>(gurka::findEdge(reader, map.nodes, edge_name, start_node));
+  auto edge = std::get<0>(gurka::findEdge(reader, map.nodes, edge_name, end_node));
   if (edge.Tile_Base() == tile_id && edge.id() == index) {
-    SetSubsegmentLiveSpeed(current, 0, static_cast<uint8_t>(255 * percent_along));
+    uint8_t subsegment = static_cast<uint8_t>(255 * percent_along);
+    SetLiveSpeedFrom(current, 0, subsegment);
+  }
+}
+
+void close_partial_dir_edge_upto(baldr::GraphReader& reader,
+                                 baldr::TrafficTile& tile,
+                                 uint32_t index,
+                                 double percent_along,
+                                 baldr::TrafficSpeed* current,
+                                 const std::string& edge_name,
+                                 const std::string& end_node,
+                                 const gurka::map& map) {
+  baldr::GraphId tile_id(tile.header->tile_id);
+  auto edge = std::get<0>(gurka::findEdge(reader, map.nodes, edge_name, end_node));
+  if (edge.Tile_Base() == tile_id && edge.id() == index) {
+    uint8_t subsegment = static_cast<uint8_t>(255 * percent_along);
+    SetLiveSpeedUpto(current, 0, subsegment);
   }
 }
 
@@ -40,9 +64,13 @@ void close_dir_edge(baldr::GraphReader& reader,
                     uint32_t index,
                     baldr::TrafficSpeed* current,
                     const std::string& edge_name,
-                    const std::string& start_node,
+                    const std::string& end_node,
                     const gurka::map& map) {
-  close_partial_dir_edge(reader, tile, index, 1., current, edge_name, start_node, map);
+  baldr::GraphId tile_id(tile.header->tile_id);
+  auto edge = std::get<0>(gurka::findEdge(reader, map.nodes, edge_name, end_node));
+  if (edge.Tile_Base() == tile_id && edge.id() == index) {
+    SetLiveSpeed(current, 0);
+  }
 }
 
 void close_bidir_edge(baldr::GraphReader& reader,
@@ -71,15 +99,15 @@ protected:
   static void SetUpTestSuite() {
     const std::string ascii_map = R"(
 
-    A-1----2-B
-             |
-             3
-             |
-             C
-             |
-             4
-             |
-             D-5----6-E
+     A-1----2-B
+              |
+              3
+              |
+              C
+              |
+              4
+              |
+              D-5---E
     )";
 
     const std::string speed_str = std::to_string(default_speed);
@@ -108,30 +136,28 @@ protected:
   virtual void SetUp() {
     set_default_speed_on_all_edges();
 
-    // Partially(50%) close 12
+    // Partially close AB from 50%
     test::customize_live_traffic_data(closure_map.config,
                                       [](baldr::GraphReader& reader, baldr::TrafficTile& tile,
                                          uint32_t index, baldr::TrafficSpeed* current) -> void {
-                                        // close_partial_dir_edge(reader, tile, index, 0.5, current,
-                                        // "AB", "B", closure_map);
-                                        close_partial_dir_edge(reader, tile, index, 0.5, current,
-                                                               "AB", "B", closure_map);
+                                        close_partial_dir_edge_from(reader, tile, index, 0.5, current,
+                                                                    "AB", "B", closure_map);
                                       });
 
-#if 0
     // Fully close BC
     test::customize_live_traffic_data(closure_map.config,
                                       [](baldr::GraphReader& reader, baldr::TrafficTile& tile,
                                          uint32_t index, baldr::TrafficSpeed* current) -> void {
-      close_bidir_edge(reader, tile, index, current, "BC", closure_map);
+                                        close_bidir_edge(reader, tile, index, current, "BC",
+                                                         closure_map);
                                       });
-    // Partially(50%) close DE
+    // Partially close DE upto 50%
     test::customize_live_traffic_data(closure_map.config,
                                       [](baldr::GraphReader& reader, baldr::TrafficTile& tile,
                                          uint32_t index, baldr::TrafficSpeed* current) -> void {
-      close_partial_dir_edge(reader, tile, index, 0.5, current, "DE", "D", closure_map);
+                                        close_partial_dir_edge_upto(reader, tile, index, 0.5, current,
+                                                                    "DE", "E", closure_map);
                                       });
-#endif
   }
 
   virtual void TearDown() {
@@ -185,6 +211,37 @@ const std::string req_with_closure_annotations = R"({
 }
 )";
 
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+#include <iostream>
+
+void print_json(const rapidjson::Document& doc) {
+  rapidjson::StringBuffer buffer;
+  rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+  doc.Accept(writer);
+  std::cout << buffer.GetString() << std::endl;
+}
+
+void expect_closures(const rapidjson::Document& response,
+                     const std::vector<std::pair<int, int>>& expected_closures,
+                     int expected_coord_count) {
+  auto route = response["routes"][0].GetObject();
+  auto leg = route["legs"][0].GetObject();
+  size_t num_coordinates = route["geometry"]["coordinates"].Size();
+  size_t num_closures = leg["closures"].Size();
+
+  ASSERT_EQ(num_coordinates, expected_coord_count);
+  ASSERT_EQ(num_closures, expected_closures.size());
+  int idx = 0;
+  for (const auto& start_end_pair : expected_closures) {
+    EXPECT_EQ(leg["closures"][idx]["geometry_index_start"].GetInt(), start_end_pair.first);
+    EXPECT_EQ(leg["closures"][idx]["geometry_index_end"].GetInt(), start_end_pair.second);
+    idx++;
+  }
+  auto last_closure = leg["closures"][num_closures - 1].GetObject();
+  ASSERT_LE(last_closure["geometry_index_end"].GetInt(), num_coordinates);
+}
+
 TEST_F(ClosureAnnotations, EndOnClosure) {
   const std::string& req =
       (boost::format(req_with_closure_annotations) % std::to_string(closure_map.nodes.at("1").lat()) %
@@ -195,5 +252,76 @@ TEST_F(ClosureAnnotations, EndOnClosure) {
   auto result = gurka::do_action(Options::route, closure_map, req, reader);
   gurka::assert::raw::expect_path(result, {"AB"});
 
-  // rapidjson::Document response = gurka::convert_to_json(result, valhalla::Options_Format_osrm);
+  rapidjson::Document response = gurka::convert_to_json(result, valhalla::Options_Format_osrm);
+  expect_closures(response, {{1, 2}}, 3);
+}
+
+TEST_F(ClosureAnnotations, EndWithConsecutiveClosures) {
+  const std::string& req =
+      (boost::format(req_with_closure_annotations) % std::to_string(closure_map.nodes.at("1").lat()) %
+       std::to_string(closure_map.nodes.at("1").lng()) %
+       std::to_string(closure_map.nodes.at("3").lat()) %
+       std::to_string(closure_map.nodes.at("3").lng()))
+          .str();
+  auto result = gurka::do_action(Options::route, closure_map, req, reader);
+  gurka::assert::raw::expect_path(result, {"AB", "BC"});
+
+  rapidjson::Document response = gurka::convert_to_json(result, valhalla::Options_Format_osrm);
+  expect_closures(response, {{1, 3}}, 4);
+}
+
+TEST_F(ClosureAnnotations, IntermediateClosure) {
+  const std::string& req =
+      (boost::format(req_with_closure_annotations) % std::to_string(closure_map.nodes.at("1").lat()) %
+       std::to_string(closure_map.nodes.at("1").lng()) %
+       std::to_string(closure_map.nodes.at("4").lat()) %
+       std::to_string(closure_map.nodes.at("4").lng()))
+          .str();
+  auto result = gurka::do_action(Options::route, closure_map, req, reader);
+  gurka::assert::raw::expect_path(result, {"AB", "BC", "CD"});
+
+  rapidjson::Document response = gurka::convert_to_json(result, valhalla::Options_Format_osrm);
+  expect_closures(response, {{1, 3}}, 5);
+}
+
+TEST_F(ClosureAnnotations, BeginAtClosure) {
+  const std::string& req =
+      (boost::format(req_with_closure_annotations) % std::to_string(closure_map.nodes.at("2").lat()) %
+       std::to_string(closure_map.nodes.at("2").lng()) %
+       std::to_string(closure_map.nodes.at("4").lat()) %
+       std::to_string(closure_map.nodes.at("4").lng()))
+          .str();
+  auto result = gurka::do_action(Options::route, closure_map, req, reader);
+  gurka::assert::raw::expect_path(result, {"AB", "BC", "CD"});
+
+  rapidjson::Document response = gurka::convert_to_json(result, valhalla::Options_Format_osrm);
+  expect_closures(response, {{0, 2}}, 4);
+}
+
+TEST_F(ClosureAnnotations, AllWithinClosure) {
+  const std::string& req =
+      (boost::format(req_with_closure_annotations) % std::to_string(closure_map.nodes.at("2").lat()) %
+       std::to_string(closure_map.nodes.at("2").lng()) %
+       std::to_string(closure_map.nodes.at("3").lat()) %
+       std::to_string(closure_map.nodes.at("3").lng()))
+          .str();
+  auto result = gurka::do_action(Options::route, closure_map, req, reader);
+  gurka::assert::raw::expect_path(result, {"AB", "BC"});
+
+  rapidjson::Document response = gurka::convert_to_json(result, valhalla::Options_Format_osrm);
+  expect_closures(response, {{0, 2}}, 3);
+}
+
+TEST_F(ClosureAnnotations, DiscontinuousClosures) {
+  const std::string& req =
+      (boost::format(req_with_closure_annotations) % std::to_string(closure_map.nodes.at("A").lat()) %
+       std::to_string(closure_map.nodes.at("A").lng()) %
+       std::to_string(closure_map.nodes.at("E").lat()) %
+       std::to_string(closure_map.nodes.at("E").lng()))
+          .str();
+  auto result = gurka::do_action(Options::route, closure_map, req, reader);
+  gurka::assert::raw::expect_path(result, {"AB", "BC", "CD", "DE"});
+
+  rapidjson::Document response = gurka::convert_to_json(result, valhalla::Options_Format_osrm);
+  expect_closures(response, {{1, 3}, {4, 5}}, 7);
 }
