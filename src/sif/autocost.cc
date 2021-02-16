5d20d93dacf2aef15fdab154aa938d29268c371a
Kevin Kreiser <kevinkreiser@gmail.com>
2021-02-16 12:51:10 -0500

    revert highway factor typo

--- src/sif/autocost.cc
+++ src/sif/autocost.cc
@@ -104,7 +104,7 @@ constexpr ranged_default_t<float> kUseDistanceRange{0, kDefaultUseDistance, 1.0f
 constexpr ranged_default_t<float> kUseLivingStreetsRange{0.f, kDefaultUseLivingStreets, 1.0f};
 
 constexpr float kHighwayFactor[] = {
-    10.0f, // Motorway
+    1.0f, // Motorway
     0.5f,  // Trunk
     0.0f,  // Primary
     0.0f,  // Secondary
