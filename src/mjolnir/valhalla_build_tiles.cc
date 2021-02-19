2fcfe89ea433546236b7dda093c4d06c55e44473
Pavel Merkis <pavel.merkis@mapbox.com>
2021-02-19 22:47:02 +0300

    main fixes

--- src/mjolnir/valhalla_build_tiles.cc
+++ src/mjolnir/valhalla_build_tiles.cc
@@ -11,18 +11,19 @@ using namespace valhalla::midgard;
 
 
 int main(int argc, char** argv) {
-  std::cout << sizeof(OSMWayNode) << std::endl;
-  return 0;
-//  sequence<OSMWayNode> seq("seq_origin.bin", true);
-//  for (size_t i = 0; i < 44739242 * 5; ++i) {
-//    OSMWayNode n;
-//    for (size_t j = 0; j < sizeof(n); ++j) {
-//      *(reinterpret_cast<char*>(&n) + j) = rand();
-//    }
-//    seq.push_back(n);
-//  }
-  sequence<OSMWayNode> seq("seq.bin");
-  seq.sort([](const OSMWayNode& lhs, const OSMWayNode& rhs) {
-    return lhs.node.osmid_ < rhs.node.osmid_;
-  });
+  if (argc > 1) {
+    sequence<OSMWayNode> seq("seq_origin.bin", true);
+    for (size_t i = 0; i < 44739242; ++i) {
+      OSMWayNode n;
+      for (size_t j = 0; j < sizeof(n); ++j) {
+        *(reinterpret_cast<char*>(&n) + j) = rand();
+      }
+      seq.push_back(n);
+    }
+  } else {
+    sequence<OSMWayNode> seq("seq.bin");
+    seq.sort([](const OSMWayNode& lhs, const OSMWayNode& rhs) {
+      return lhs.node.osmid_ < rhs.node.osmid_;
+    });
+  }
 }
