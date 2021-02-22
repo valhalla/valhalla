c28b45fdf20cfdb45beef9c20b347ad03e584c81
Svetlana Bayarovich <svetlana.bayarovich@mapbox.com>
2021-02-22 15:27:35 +0300

    Service roads avoidance for motorcycle. Improve test cases

--- test/gurka/test_service_turn.cc
+++ test/gurka/test_service_turn.cc
@@ -34,31 +34,65 @@ void validate_path(const valhalla::Api& result, const std::vector<std::string>& 
   gurka::assert::raw::expect_path(result, expected_names);
 }
 
-const std::vector<std::string>& costing = {"auto", "hov", "taxi", "bus", "truck"};
+const std::vector<std::string>& costing = {"auto", "hov", "taxi", "bus", "truck", "motorcycle"};
 
-TEST(ServiceStandalone, test_avoid_service_default) {
-  const std::string ascii_map = R"(
+class ServiceRoadsTest : public ::testing::Test {
+protected:
+  static gurka::map service_streets_map;
+
+  static void SetUpTestSuite() {
+    const std::string ascii_map = R"(
     A-1----B--------E-----2-F
            |        |       |
            |        |       |
-           |        |       3
-           |        |       |
-           C--------D       G
+           |        |       G
+           |        |
+           |        |
+           C--------D
                      \
                       I
     )";
 
-  const gurka::ways ways = {
-      {"AB", {{"highway", "secondary"}}}, {"BE", {{"highway", "service"}}},
-      {"EF", {{"highway", "secondary"}}}, {"FG", {{"highway", "secondary"}}},
-      {"BC", {{"highway", "secondary"}}}, {"CD", {{"highway", "secondary"}}},
-      {"DE", {{"highway", "secondary"}}}, {"DI", {{"highway", "residential"}}},
-  };
+    const gurka::ways ways = {
+        {"AB", {{"highway", "secondary"}}}, {"BE", {{"highway", "service"}}},
+        {"EF", {{"highway", "secondary"}}}, {"FG", {{"highway", "secondary"}}},
+        {"BC", {{"highway", "secondary"}}}, {"CD", {{"highway", "secondary"}}},
+        {"DE", {{"highway", "secondary"}}}, {"DI", {{"highway", "residential"}}},
+    };
 
-  const auto layout = gurka::detail::map_to_coordinates(ascii_map, 100);
-  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_avois_service_default");
+    const auto layout = gurka::detail::map_to_coordinates(ascii_map, 100);
+    service_streets_map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_service_streets");
+  }
+};
 
+gurka::map ServiceRoadsTest::service_streets_map = {};
+
+TEST_F(ServiceRoadsTest, test_default_value) {
   for (const auto& c : costing)
-    validate_path(gurka::do_action(valhalla::Options::route, map, {"1", "2"}, c),
+    validate_path(gurka::do_action(valhalla::Options::route, service_streets_map, {"1", "2"}, c),
+                  {"AB", "BC", "CD", "DE", "EF"});
+}
+
+TEST_F(ServiceRoadsTest, test_use_service_roads) {
+  for (const auto& c : costing)
+    if (c == "truck")
+      // truck is also impacted by 'low_class_penalty' option which is non-zero by default
+      validate_path(gurka::do_action(valhalla::Options::route, service_streets_map, {"1", "2"}, c,
+                                     {{"/costing_options/" + c + "/service_penalty", "0"},
+                                      {"/costing_options/" + c + "/service_factor", "0.1"},
+                                      {"/costing_options/" + c + "/low_class_penalty", "0"}}),
+                    {"AB", "BE", "EF"});
+    else
+      validate_path(gurka::do_action(valhalla::Options::route, service_streets_map, {"1", "2"}, c,
+                                     {{"/costing_options/" + c + "/service_penalty", "0"},
+                                      {"/costing_options/" + c + "/service_factor", "0.1"}}),
+                    {"AB", "BE", "EF"});
+}
+
+TEST_F(ServiceRoadsTest, test_avoid_service_roads) {
+  for (const auto& c : costing)
+    validate_path(gurka::do_action(valhalla::Options::route, service_streets_map, {"1", "2"}, c,
+                                   {{"/costing_options/" + c + "/service_penalty", "300"},
+                                    {"/costing_options/" + c + "/service_factor", "8"}}),
                   {"AB", "BC", "CD", "DE", "EF"});
 }
