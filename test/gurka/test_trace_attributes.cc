1ed13175d6ff9751f45f18580926d6350f49e1fb
dnesbitt61 <dnesbitt61@gmail.com>
2021-02-23 12:20:07 -0500

    Working version. Needed to pass in a json string to do_action and then convert the result into RapidJSON.

--- test/gurka/test_trace_attributes.cc
+++ test/gurka/test_trace_attributes.cc
@@ -17,27 +17,28 @@ TEST(Standalone, SacScaleAttributes) {
               D
          )";
 
-  const gurka::ways ways = {{"AB", {{"highway", "footway"}, {"sac_scale", "hiking"}}},
-                            {"BC", {{"highway", "footway"}, {"sac_scale", "alpine_hiking"}}},
-                            {"CD", {{"highway", "footway"}}}};
+  const gurka::ways ways = {{"AB", {{"highway", "track"}, {"sac_scale", "hiking"}}},
+                            {"BC", {{"highway", "track"}, {"sac_scale", "alpine_hiking"}}},
+                            {"CD", {{"highway", "track"}}}};
 
   const double gridsize = 10;
   const auto layout = gurka::detail::map_to_coordinates(ascii_map, gridsize);
   auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/sac_scale_attributes");
 
-  auto result = gurka::do_action(valhalla::Options::trace_attributes, map, {"1", "2", "3", "4", "5"},
-                                 "pedestrian", {}, {}, nullptr, "via");
+  std::string trace_json;
+  auto api = gurka::do_action(valhalla::Options::trace_attributes, map, {"1", "2", "3", "4", "5"},
+                              "auto", {}, {}, &trace_json, "via");
 
-  auto d = gurka::convert_to_json(result, valhalla::Options_Format_json);
-  auto edges = d["edges"].GetArray();
+  rapidjson::Document result;
+  result.Parse(trace_json.c_str());
 
+  auto edges = result["edges"].GetArray();
   ASSERT_EQ(edges.Size(), 3);
 
-  /*
-    EXPECT_TRUE(edges[0].HasMember("sac_scale"));
-    EXPECT_STREQ(edges[0]["sac_scale"].GetString(), "hiking");
-    EXPECT_TRUE(edges[1].HasMember("sac_scale"));
-    EXPECT_STREQ(edges[1]["sac_scale"].GetString(), "alpine_hiking");
-    EXPECT_FALSE(edges[2].HasMember("sac_scale"));
-  */
+  EXPECT_TRUE(edges[0].HasMember("sac_scale"));
+  EXPECT_EQ(edges[0]["sac_scale"].GetInt(), 1);
+  EXPECT_TRUE(edges[1].HasMember("sac_scale"));
+  EXPECT_EQ(edges[1]["sac_scale"].GetInt(), 4);
+  EXPECT_TRUE(edges[2].HasMember("sac_scale"));
+  EXPECT_EQ(edges[2]["sac_scale"].GetInt(), 0);
 }
