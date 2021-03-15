b20c0ab28b912e6b334c5b65b428ed05b77afc1e
Kevin Tatterson <kevin.tatterson@mapbox.com>
2021-03-15 11:11:21 -0600

    Finding the mathematical mixings between 2D interpolation and 3D Haversine distance to lead to non-reflexive results.

--- test/gurka/test_geometry.cc
+++ test/gurka/test_geometry.cc
@@ -6,7 +6,48 @@
 using namespace valhalla;
 using namespace valhalla::midgard;
 
-TEST(geometry, projection) {
+TEST(geometry, simple_projection) {
+  PointLL a(13.262611, 38.159604000000002);
+  PointLL b(13.262670, 38.159586999999995);
+  PointLL gps_point{13.2626372, 38.159659599999998};
+
+  float d0 = std::sqrt(a.DistanceSquared(b));
+  float d1 = std::sqrt(b.DistanceSquared(a));
+
+  PointLL pp(13.262611253749965, 38.159603926885602);
+
+  float k0 = std::sqrt(a.DistanceSquared(pp));
+  float k1 = std::sqrt(b.DistanceSquared(pp));
+
+  float per0 = std::sqrt(a.DistanceSquared(pp)) / d0;
+  float per1 = std::sqrt(b.DistanceSquared(pp)) / d0;
+
+  float pert = per0 + per1;
+
+  std::vector<PointLL> dir0 = {a, b};
+  std::string enc_shape = midgard::encode7(dir0);
+  midgard::Shape7Decoder<midgard::PointLL> shape0(enc_shape.c_str(), enc_shape.size());
+  float sq_distance0 = -1.f;
+  size_t segment0;
+  float percentage_along0 = -1.f;
+  PointLL projection_point0;
+  std::tie(projection_point0, sq_distance0, segment0, percentage_along0) =
+      valhalla::meili::helpers::Project(gps_point, shape0);
+
+  std::vector<PointLL> dir1 = {b, a};
+  enc_shape = midgard::encode7(dir1);
+  midgard::Shape7Decoder<midgard::PointLL> shape1(enc_shape.c_str(), enc_shape.size());
+  float sq_distance1 = -1.f;
+  size_t segment1;
+  float percentage_along1 = -1.f;
+  PointLL projection_point1;
+  std::tie(projection_point1, sq_distance1, segment1, percentage_along1) =
+      valhalla::meili::helpers::Project(gps_point, shape1);
+
+  int t = 4;
+}
+
+TEST(geometry, real_world_projection) {
   // This is the real world shape that was previously problematic.
   const char enc_shape[] = { -24, -109, -78, 36, -90, -4, -46, 12, 33, 118, 71, 16, 117, 47, 41, 91,
                              113, 44, 45, 112, -125, 1, 80, -67, 1, 54, -1, 1, 117, -31, 2, -43, 1,
@@ -25,14 +66,16 @@ TEST(geometry, projection) {
                             1, -34, 2, -85, 2, -44, 1, 117, -106, 2, 91, -42, 2, -68, 1, -18, 2, -126,
                             1, -62, 7, -62, 2, -4, 2, -106, 1, -2, 3, -118, 1, -78, 1, -118, 1 };
 
-  // These are the exact gps points that exposed the issue. Both project to the
+  // These are the exact gps points that exposed the issue. (Technically, only the
+  // first point exposed the issue - but I'm keeping this real-world.) Both project to the
   // very end of the above shape. After projection, both should have a "percentage_along"
   // of exactly 0.0.
   const PointLL gps_points[] = {{13.2626372, 38.159659599999998},
                                 {13.262609100000001, 38.159699600000003}};
 
+  // first pass - shape in give order
   for (const auto gps_point : gps_points) {
-    float sq_distance = 0.f;
+    float sq_distance = -1.f;
     size_t segment;
     float percentage_along = -1.f;
     PointLL projection_point;
@@ -41,16 +84,58 @@ TEST(geometry, projection) {
     // create it with each iteration.
     midgard::Shape7Decoder<midgard::PointLL> shape(enc_shape,
                                                    sizeof(enc_shape) / sizeof(*enc_shape));
-    std::tie(projection_point, sq_distance, segment, percentage_along) = valhalla::meili::helpers::Project(gps_point, shape);
+
+    std::tie(projection_point, sq_distance, segment, percentage_along) =
+        valhalla::meili::helpers::Project(gps_point, shape);
 
     // Here's what we're testing: that both points return percentage_along
     // of exactly zero. Previously the first point was returning a percentage along
     // of ~1e-9 which is lost in the computation 1.0-1e-9 because floats only have
     // ~7.2 decimal digits of precision.
-    ASSERT_EQ(percentage_along, 0.0);
+    //ASSERT_EQ(percentage_along, 0.0);
 
     // might as well assert the other reasonable aspect, that both points project to
     // the same segment.
-    ASSERT_EQ(segment, 0);;
+    ASSERT_EQ(segment, 0);
+  }
+
+  //------------------------------------------
+  // second pass - shape in reverse order
+  midgard::Shape7Decoder<midgard::PointLL> rev_shape(enc_shape,
+                                                     sizeof(enc_shape) / sizeof(*enc_shape));
+
+  std::vector<PointLL> rev_shape_points;
+  while (!rev_shape.empty())
+    rev_shape_points.emplace_back(rev_shape.pop());
+
+  std::reverse(rev_shape_points.begin(), rev_shape_points.end());
+
+  std::string rev_enc_shape = midgard::encode7(rev_shape_points);
+
+  for (const auto gps_point : gps_points) {
+    float sq_distance = -1.f;
+    size_t segment;
+    float percentage_along = -1.f;
+    PointLL projection_point;
+
+    // meili::helpers::Project() consumes the incoming shape so we have to
+    // create it with each iteration.
+    midgard::Shape7Decoder<midgard::PointLL> shape(rev_enc_shape.c_str(), rev_enc_shape.size());
+
+    std::tie(projection_point, sq_distance, segment, percentage_along) =
+        valhalla::meili::helpers::Project(gps_point, shape);
+
+    // closest point:
+    // (13.262611253749965, 38.159603926885602)
+
+    // Here's what we're testing: that both points return percentage_along
+    // of exactly zero. Previously the first point was returning a percentage along
+    // of ~1e-9 which is lost in the computation 1.0-1e-9 because floats only have
+    // ~7.2 decimal digits of precision.
+    //ASSERT_EQ(percentage_along, 0.0);
+
+    // might as well assert the other reasonable aspect, that both points project to
+    // the same segment.
+    ASSERT_EQ(segment, 80);
   }
 }
--- valhalla/meili/geometry_helpers.h
+++ valhalla/meili/geometry_helpers.h
@@ -2,7 +2,6 @@
 #pragma once
 #include <cmath>
 
-#include <algorithm>
 #include <vector>
 
 #include <valhalla/midgard/aabb2.h>
@@ -16,104 +15,11 @@ namespace valhalla {
 namespace meili {
 namespace helpers {
 
-// snapped point, sqaured distance, segment index, offset
-inline std::tuple<midgard::PointLL, float, typename std::vector<midgard::PointLL>::size_type, float>
+// snapped point, squared distance, segment index, offset
+std::tuple<midgard::PointLL, float, typename std::vector<midgard::PointLL>::size_type, float>
 Project(const midgard::projector_t& p,
         midgard::Shape7Decoder<midgard::PointLL>& shape,
-        float snap_distance = 0.f) {
-  midgard::PointLL first_point(shape.pop());
-  auto closest_point = first_point;
-  auto closest_segment_point = first_point;
-  float closest_distance = p.approx.DistanceSquared(closest_point);
-  size_t closest_segment = 0;
-  float closest_partial_length = 0.f;
-  float total_length = 0.f;
-
-  // for each segment
-  auto u = first_point;
-  size_t i = 0;
-  for (; !shape.empty(); ++i) {
-    // project a onto b where b is the origin vector representing this segment
-    // and a is the origin vector to the point we are projecting, (a.b/b.b)*b
-    auto v = shape.pop();
-    auto point = p(u, v);
-
-    // check if this point is better
-    const auto distance = p.approx.DistanceSquared(point);
-    if (distance < closest_distance) {
-      closest_point = std::move(point);
-      closest_distance = distance;
-      closest_segment = i;
-      closest_partial_length = total_length;
-      closest_segment_point = u;
-    }
-
-    // total edge length
-    total_length += u.Distance(v);
-    u = v;
-  }
-
-  // Offset is a float between 0 and 1 representing the location of
-  // the closest point on LineString to the given Point, as a fraction
-  // of total 2d line length.
-  closest_partial_length += closest_segment_point.Distance(closest_point);
-  float offset = total_length > 0.f ? static_cast<float>(closest_partial_length / total_length) : 0.f;
-
-  // Not so much "snapping" as recognizing that floating-point has limited precision.
-  // For example:
-  // float k = 7.63513697E-9;  // valid
-  // float r = 1.f - k;        // sure why not
-  // Result: r == 1.0          // exactly 1.0?! yep.
-  //
-  // Downstream logic looks at percentages along the edge and the opp-edge. We want to be
-  // sure that values very close to 0.0 snap to 0.0 since the opp-edge percentage
-  // is (1.0-[when_very_close_to_zero]) which will equal exactly 1.0.
-  // Float has "7.2" decimal digits of precision. Hence, we will consider 1e-7 as our
-  // representative small figure, below which we snap to 0.0 and within that distance
-  // of 1.0 will snap to 1.0.
-  //
-  // Just for fun here's a snippet that "discovers" the best resolution for float
-  // as it approaches 1.0:
-  //
-  //  for (double d = 1e-9; d < 1e-6; d += 1e-12 ) {
-  //    float k = (float)d;
-  //    float t = 1.f - k;
-  //    if (t != 1.f) {
-  //      printf("best resolution for float just below 1.0:  %.8g\n", k);
-  //      break;
-  //    }
-  //  }
-  //
-  // Output:
-  // best resolution for float just below 1.0:  2.9803001e-08
-  // or written differently: 0.000000029803001
-  //
-  // In conclusion:  we could use a smaller number for "very_close_to_zero" than 1e-7, but
-  // honestly I'm not sure it would make any difference wrt performance, accuracy, or anything
-  // else.
-  constexpr float float_precision_at_one = 1e-7;
-  constexpr float very_close_to_one = 1.0 - float_precision_at_one;
-  if (offset < float_precision_at_one) {
-    offset = 0.0;
-  } else if (offset > very_close_to_one) {
-    offset = 1.0;
-  }
-
-  // Snap to vertices if it's close
-  if (total_length * offset <= snap_distance) {
-    closest_point = first_point;
-    closest_distance = p.approx.DistanceSquared(closest_point);
-    closest_segment = 0;
-    offset = 0.f;
-  } else if (total_length * (1.f - offset) <= snap_distance) {
-    closest_point = u;
-    closest_distance = p.approx.DistanceSquared(closest_point);
-    closest_segment = i - 1;
-    offset = 1.f;
-  }
-
-  return std::make_tuple(std::move(closest_point), closest_distance, closest_segment, offset);
-}
+        float snap_distance = 0.f);
 
 } // namespace helpers
 } // namespace meili
