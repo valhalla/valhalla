ed6d6dd161150de514ba5e3e0d9e6ad7f9c96469
Mandeep Sandhu <mandeep.sandhu@mapbox.com>
2021-03-17 21:51:41 -0700

    Pass thor config object to path algo constructor

--- src/thor/worker.cc
+++ src/thor/worker.cc
@@ -71,9 +71,10 @@ namespace thor {
 
 thor_worker_t::thor_worker_t(const boost::property_tree::ptree& config,
                              const std::shared_ptr<baldr::GraphReader>& graph_reader)
-    : mode(valhalla::sif::TravelMode::kPedestrian), bidir_astar(config), bss_astar(config),
-      multi_modal_astar(config), timedep_forward(config), timedep_reverse(config),
-      isochrone_gen(config), matcher_factory(config, graph_reader),
+    : mode(valhalla::sif::TravelMode::kPedestrian), bidir_astar(config.get_child("thor")),
+      bss_astar(config.get_child("thor")), multi_modal_astar(config.get_child("thor")),
+      timedep_forward(config.get_child("thor")), timedep_reverse(config.get_child("thor")),
+      isochrone_gen(config.get_child("thor")), matcher_factory(config, graph_reader),
       reader(graph_reader), controller{} {
   // If we weren't provided with a graph reader make our own
   if (!reader)
