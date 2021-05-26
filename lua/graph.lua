f79da5d58e8f5ba37940f192cdd93301b4105efb
Greg Knisely <greg.knisely@mapbox.com>
2021-05-26 17:33:44 -0400

    access tag wins over bus or taxi tag

--- lua/graph.lua
+++ lua/graph.lua
@@ -896,14 +896,12 @@ function filter_tags_generic(kv)
       end
     end
 
-    if kv["taxi_tag"] == nil or kv["bus_tag"] == nil then
-      if kv["access"] == "psv" then
-        kv["taxi_forward"] = "true"
-        kv["taxi_tag"] = "true"
+    if kv["access"] == "psv" then
+      kv["taxi_forward"] = "true"
+      kv["taxi_tag"] = "true"
 
-	kv["bus_forward"] = "true"
-        kv["bus_tag"] = "true"
-      end
+      kv["bus_forward"] = "true"
+      kv["bus_tag"] = "true"
     end
 
     if kv["motorroad"] == "yes" then
@@ -983,14 +981,12 @@ function filter_tags_generic(kv)
         end
       end
 
-      if kv["taxi_tag"] == nil or kv["bus_tag"] == nil then
-        if kv["access"] == "psv" then
-          kv["taxi_forward"] = "true"
-          kv["taxi_tag"] = "true"
+      if kv["access"] == "psv" then
+        kv["taxi_forward"] = "true"
+        kv["taxi_tag"] = "true"
 
-          kv["bus_forward"] = "true"
-          kv["bus_tag"] = "true"
-        end
+        kv["bus_forward"] = "true"
+        kv["bus_tag"] = "true"
       end
 
       if kv["motorroad"] == "yes" then
@@ -1706,7 +1702,17 @@ function nodes_proc (kv, nokeys)
   if auto_tag == nil then
     auto_tag = motor_vehicle_tag
   end
-  local bus_tag = bus_node[kv["bus"]]
+  local bus_tag
+  local taxi_tag
+
+  if (kv["access"] == "psv") then
+    bus_tag = 64
+    taxi_tag = 32
+  else
+    bus_tag = bus_node[kv["bus"]]
+    taxi_tag = taxi_node[kv["taxi"]]
+  end
+
   if bus_tag == nil then
     bus_tag = psv_bus_node[kv["psv"]]
   end
@@ -1725,7 +1731,6 @@ function nodes_proc (kv, nokeys)
     hov_tag = 128
   end
 
-  local taxi_tag = taxi_node[kv["taxi"]]
   if taxi_tag == nil then
     taxi_tag = psv_taxi_node[kv["psv"]]
   end
--- test/gurka/test_access_psv.cc
+++ test/gurka/test_access_psv.cc
@@ -14,40 +14,89 @@ const std::vector<std::string>& costing = {"auto",          "hov",        "taxi"
                                            "bus",           "truck",      "bicycle",
                                            "motor_scooter", "motorcycle", "pedestrian"};
 
-TEST(Standalone, AccessPsv) {
+TEST(Standalone, AccessPsvWay) {
   constexpr double gridsize_metres = 10;
 
   const std::string ascii_map = R"(
                
-        A---B---C---D
-                |   |
-                E---F---G
+        A---B---C---D---E
+                |       |
+                F-------G
+                |
+                H
     )";
 
   const gurka::ways ways = {
       {"AB", {{"highway", "primary"}}},
       {"BC", {{"highway", "primary"}}},
       {"CD", {{"highway", "primary"}}},
-      {"CE",
+      {"DE", {{"highway", "primary"}}},
+      {"EG", {{"highway", "primary"}}},
+      {"FG", {{"highway", "primary"}}},
+      {"CF",
        {
            {"highway", "primary"},
-           {"bus", "no"}, // access tag wins over bus tag
-           {"access", "psv"},
+           {"access", "psv"}, // access key wins over bus or taxi tag
+           {"bike", "no"},
+           {"bus", "no"},
        }},
-      {"EF", {{"highway", "primary"}}},
-      {"FG", {{"highway", "primary"}}},
-      {"DF", {{"highway", "primary"}}},
+      {"FH", {{"highway", "primary"}}},
+
   };
 
   const auto layout =
       gurka::detail::map_to_coordinates(ascii_map, gridsize_metres, {5.1079374, 52.0887174});
-  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_access_psv", build_config);
+  auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/gurka_access_psv_way", build_config);
   for (auto& c : costing) {
-    auto result = gurka::do_action(valhalla::Options::route, map, {"A", "G"}, c);
+    auto result = gurka::do_action(valhalla::Options::route, map, {"A", "H"}, c);
 
     if (c == "bus" || c == "taxi")
-      gurka::assert::raw::expect_path(result, {"AB", "BC", "CE", "EF", "FG"});
+      gurka::assert::raw::expect_path(result, {"AB", "BC", "CF", "FH"});
     else
-      gurka::assert::raw::expect_path(result, {"AB", "BC", "CD", "DF", "FG"});
+      gurka::assert::raw::expect_path(result, {"AB", "BC", "CD", "DE", "EG", "FG", "FH"});
   }
+}
+
+TEST(Standalone, AccessPsvNode) {
+  constexpr double gridsize_metres = 10;
+
+  const std::string ascii_map = R"(
+               
+        A---B---C---D---E
+                |       |
+                F       |
+                |       |
+                H-------G
+    )";
+
+  const gurka::ways ways = {
+      {"AB", {{"highway", "primary"}}},
+      {"BC", {{"highway", "primary"}}},
+      {"CD", {{"highway", "primary"}}},
+      {"DE", {{"highway", "primary"}}},
+      {"EG", {{"highway", "primary"}}},
+      {"HG", {{"highway", "primary"}}},
+      {"CF", {{"highway", "primary"}}},
+      {"FH", {{"highway", "primary"}}},
+
+  };
+
+  const gurka::nodes nodes = {{"F",
+                               {
+                                   {"access", "psv"}, // access tag wins over bus or taxi tag
+                                   {"taxi", "no"},
+                                   {"bus", "no"},
+                               }}};
+
+  const auto layout =
+        gurka::detail::map_to_coordinates(ascii_map, gridsize_metres, {5.1079374, 52.0887174});
+    auto map = gurka::buildtiles(layout, ways, nodes, {}, "test/data/gurka_access_psv_way", build_config);
+    for (auto& c : costing) {
+      auto result = gurka::do_action(valhalla::Options::route, map, {"A", "H"}, c);
+
+      if (c == "bus" || c == "taxi")
+        gurka::assert::raw::expect_path(result, {"AB", "BC", "CF", "FH"});
+      else
+        gurka::assert::raw::expect_path(result, {"AB", "BC", "CD", "DE", "EG", "HG"});
+    }
 }
