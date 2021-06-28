34e1343f4175cc27572f7e96a329128b7f65ade7
Greg Knisely <greg.knisely@mapbox.com>
2021-06-28 09:30:08 -0400

    added types

--- src/mjolnir/graphenhancer.cc
+++ src/mjolnir/graphenhancer.cc
@@ -1527,7 +1527,8 @@ void enhance(const boost::property_tree::ptree& pt,
         }
 
         // Update the named flag
-        auto names = tilebuilder->edgeinfo(&directededge).GetNamesAndTypes();
+        std::vector<uint8_t> types;
+        auto names = tilebuilder->edgeinfo(&directededge).GetNamesAndTypes(types, false);
         directededge.set_named(names.size() > 0);
 
         // Speed assignment
@@ -1538,8 +1539,9 @@ void enhance(const boost::property_tree::ptree& pt,
         uint32_t ntrans = nodeinfo.local_edge_count();
         for (uint32_t k = 0; k < ntrans; k++) {
           DirectedEdge& fromedge = tilebuilder->directededge(nodeinfo.edge_index() + k);
+          std::vector<uint8_t> types;
           if (ConsistentNames(country_code, names,
-                              tilebuilder->edgeinfo(&fromedge).GetNamesAndTypes())) {
+                              tilebuilder->edgeinfo(&fromedge).GetNamesAndTypes(types, false))) {
             directededge.set_name_consistency(k, true);
           }
         }
