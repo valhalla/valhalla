d7ed14dd7b3ae3b3dace3c57e4a34fdb65eefde1
Mateusz Łoskot <mateusz@loskot.net>
2021-08-08 18:20:01 +0200

    Fix setting of expansion_within_factor

--- src/sif/dynamiccost.cc
+++ src/sif/dynamiccost.cc
@@ -263,7 +263,7 @@ void DynamicCost::RelaxHierarchyLimits(const float factor, const float expansion
 void DynamicCost::RelaxHierarchyLimits(const bool using_bidirectional) {
   // since bidirectional A* does about half the expansion we can do half the relaxation here
   const float relax_factor = using_bidirectional ? 8.f : 16.f;
-  const float expansion_within_factor = using_bidirectional ? 4.0f : 2.0f;
+  const float expansion_within_factor = using_bidirectional ? 2.0f : 4.0f;
   RelaxHierarchyLimits(relax_factor, expansion_within_factor);
 }
 
