bb7bdbb76060a79be593e650540756fe3d4f0cc2
Hainan Chen <hainan.ch.chen@daimler.com>
2021-08-24 14:07:03 +0200

    add function to update fake traffic for performance test

--- test/customize_traffic.cc
+++ test/customize_traffic.cc
@@ -39,34 +39,81 @@ void customize_traffic(const boost::property_tree::ptree& config,
   test::customize_live_traffic_data(config, generate_traffic);
 }
 
+// TODO: only support one edge id each time. To be improve performance, List shall be supported
+void show_traffic_fake(const boost::property_tree::ptree& config) {
+  // loop over all tiles in the tileset
+  valhalla::baldr::GraphReader reader(config.get_child("mjolnir"));
+  auto tile_dir = config.get<std::string>("mjolnir.tile_dir");
+  for (const auto& tile_id : reader.GetTileSet()) {
+    auto tile = reader.GetGraphTile(baldr::GraphId(tile_id));
+    LOG_INFO("tile id = " + std::to_string(tile_id));
+    GraphId edgeid = tile_id;
+    for (size_t j = 0; j < tile->header()->directededgecount(); ++j, ++edgeid) {
+      auto edge = tile->directededge(edgeid);
+
+      if (tile->GetSpeed(edge, 255, 1) !=
+          tile->GetSpeed(tile->directededge(j), kConstrainedFlowMask, 1))
+        LOG_INFO(std::to_string(j) + ": traffic speed: " +
+                 std::to_string(tile->GetSpeed(edge, 255, 1)) + "; graph speed: " +
+                 std::to_string(tile->GetSpeed(tile->directededge(j), kConstrainedFlowMask, 1)));
+    }
+  }
+}
+
+// create fake traffic for (100/step) % of edges
+void customize_traffic_fake(const boost::property_tree::ptree& config, const int step) {
+  std::function<void(baldr::GraphReader&, baldr::TrafficTile&, int, baldr::TrafficSpeed*)>
+      generate_traffic = [&step](baldr::GraphReader& reader, baldr::TrafficTile& tile, int index,
+                                 baldr::TrafficSpeed* current) -> void {
+    baldr::GraphId tile_id(tile.header->tile_id);
+    auto graph_tile = reader.GetGraphTile(baldr::GraphId(tile_id));
+    auto edge_id = baldr::GraphId(tile_id.tileid(), tile_id.level(), index);
+    auto edge = graph_tile->directededge(edge_id);
+
+    // reduce the speed to 50% of graph tile speed
+    if (index % step == 0) {
+      auto speed = static_cast<int>(graph_tile->GetSpeed(edge, 255, 1) * 0.5);
+      current->breakpoint1 = 255;
+      current->overall_encoded_speed = speed >> 1;
+      current->encoded_speed1 = speed >> 1;
+    }
+  };
+  test::customize_live_traffic_data(config, generate_traffic);
+  LOG_INFO("customize_live_traffic_data with fake data");
+}
+
 } // namespace
 
 int main(int argc, char** argv) {
 
   boost::property_tree::ptree pt;
-  rapidjson::read_json("/home/router/valhalla/data/bw/valhalla_bw.json", pt);
+  rapidjson::read_json("/home/hainan/hainche/02_gen20xi3/valhalla/data/bw/valhalla_bw.json", pt);
 
-  // Build the default traffic data with invalid values
+  // // Build the default traffic data with invalid values
   // test::build_live_traffic_data(pt);
 
-  // Boebingen, Lessingstraße: way id: 13865735: 0,621568686498,1,621770013090
-  auto tgt_edge_id = baldr::GraphId{621568686498};
-  customize_traffic(pt, tgt_edge_id, 30); // original: 30
+  // // update the fake traffic for 5% of edges
+  // customize_traffic_fake(pt, 20);
 
-  tgt_edge_id = baldr::GraphId{621770013090};
-  customize_traffic(pt, tgt_edge_id, 30); // original: 30
+  show_traffic_fake(pt);
+  //   // Boebingen, Lessingstraße: way id: 13865735: 0,621568686498,1,621770013090
+  //   auto tgt_edge_id = baldr::GraphId{621568686498};
+  //   customize_traffic(pt, tgt_edge_id, 30); // original: 30
 
-  // STR > Sifi A81: 309175327,1,3802388259096
-  tgt_edge_id = baldr::GraphId{3802388259096};
-  customize_traffic(pt, tgt_edge_id, 0); // original: 100
+  //   tgt_edge_id = baldr::GraphId{621770013090};
+  //   customize_traffic(pt, tgt_edge_id, 30); // original: 30
 
-  // auto tgt_edge_id = baldr::GraphId{3802388259096};
+  //   // STR > Sifi A81: 309175327,1,3802388259096
+  //   tgt_edge_id = baldr::GraphId{3802388259096};
+  //   customize_traffic(pt, tgt_edge_id, 0); // original: 100
 
-  // Read the information
-  GraphReader reader(pt.get_child("mjolnir"));
-  auto tile = reader.GetGraphTile(baldr::GraphId(tgt_edge_id));
-  auto edge = tile->directededge(tgt_edge_id);
+  //   // auto tgt_edge_id = baldr::GraphId{3802388259096};
 
-  LOG_INFO("speed: " + std::to_string(tile->GetSpeed(edge, 255, 1)));
-  LOG_INFO("wayid: " + std::to_string(tile->edgeinfo(edge).wayid()));
+  //   // Read the information
+  //   GraphReader reader(pt.get_child("mjolnir"));
+  //   auto tile = reader.GetGraphTile(baldr::GraphId(tgt_edge_id));
+  //   auto edge = tile->directededge(tgt_edge_id);
+
+  //   LOG_INFO("speed: " + std::to_string(tile->GetSpeed(edge, 255, 1)));
+  //   LOG_INFO("wayid: " + std::to_string(tile->edgeinfo(edge).wayid()));
 }
