52b414c8ce1eb9de81928a88148094ac06cc3d2d
Kristen <kristen.diluca@mapbox.com>
2021-09-28 17:30:03 -0400

    removed duplicate generate routes script and modified original to output multipoint routes if needed

--- run_route_scripts/gen_requests.py
+++ run_route_scripts/gen_requests.py
@@ -8,7 +8,7 @@ import random
 #./gen_requests.py 40.2690,-76.8713,10000,10 39.9561,-76.7313,10000,10 40.0507,-76.3083,10000,10 40.3319,-75.9402,10000,10 40.5910,-75.4788,10000,10 40.0108,-75.2316,10000,10 > a.txt
 
 # example for multipoint routes
-#./gen_requests.py 40.2690,-76.8713,1000,2,through  35.9825,-83.9246,1000,2,through 39.775,-105.007,1000,2,through 37.7653,-122.4316,1000,2,through 52.590,13.381,1000,2,through > multi_point_through_auto_routes.txt
+#./gen_requests.py 40.2690,-76.8713,1000,2,through 35.9825,-83.9246,1000,2,through 39.775,-105.007,1000,2,through 37.7653,-122.4316,1000,2,through 52.590,13.381,1000,2,through > multi_point_through_auto_routes.txt
 
 # TODO: use argparse to parse other request options and validate quadruplets
 
@@ -43,24 +43,35 @@ def get_random_intermediate_locs(yc, xc, radius, count, intermediate):
         intermediate_locs.append({'lon':round(x, 6),'lat':round(y, 6),'type':intermediate});
     return intermediate_locs
 
-
 # do each input quadruplet
 locs = []
 intermediate_locs = []
 for arg in sys.argv[1:]:
-    y, x, radius, count, intermediate = arg.split(',')
-    locs.extend(get_random_locs(float(y), float(x), float(radius), int(count)))
-    intermediate_locs.extend(get_random_intermediate_locs(float(y), float(x), float(radius), int(count), str(intermediate)))
+    if (len(arg.split(',')) > 4):
+        y, x, radius, count, intermediate = arg.split(',')
+        locs.extend(get_random_locs(float(y), float(x), float(radius), int(count)))
+        intermediate_locs.extend(get_random_intermediate_locs(float(y), float(x),   float(radius), int(count), str(intermediate)))
+    else:
+        y, x, radius, count = arg.split(',')
+        locs.extend(get_random_locs(float(y), float(x), float(radius), int(count)))
 
 # make requests
-for start in locs:
-  for intermediates in intermediate_locs:
-     for end in locs:
-        if start == end:
-            continue
-        req = {'locations':[start, intermediates, end], 'costing': 'auto', 'shape_format': 'geojson', 'format':'osrm'}
+if (intermediate_locs):
+    for start in locs:
+        for intermediates in intermediate_locs:
+            for end in locs:
+                if start == end:
+                    continue
+            req = {'locations':[start, intermediates, end], 'costing': 'auto', 'shape_format': 'polyline6', 'format':'osrm'}
+            #req = {'locations':[start, intermediates[start], end], 'costing': 'bicycle'}
+            #req = {'locations':[start, intermediates[start], end], 'costing': 'pedestrian'}
+            print(json.dumps(req, separators=(',', ':')))
+else:
+    for start in locs:
+        for end in locs:
+            if start == end:
+                continue
+            req = {'locations':[start, end], 'costing': 'auto', 'shape_format': 'polyline6', 'format':'osrm'}
+            #req = {'locations':[start, end], 'costing': 'bicycle'}
+            #req = {'locations':[start, end], 'costing': 'pedestrian'}
         print(json.dumps(req, separators=(',', ':')))
-        #req = {'locations':[start, via, end], 'costing': 'bicycle'}
-        #print(json.dumps(req, separators=(',', ':')))
-        #req = {'locations':[start, via, end], 'costing': 'pedestrian'}
-        #print(json.dumps(req, separators=(',', ':')))
