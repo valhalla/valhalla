f9e28344bf69f7f18812a60c9bd69f47a8ed7718
Muluh MG Godson <godsonmuluh@gmail.com>
2022-04-21 00:30:38 +0100

    change variable name from i to costing for looping costing methods

--- test/gurka/test_warnings.cc
+++ test/gurka/test_warnings.cc
@@ -27,9 +27,9 @@ gurka::map Warning::map = {};
 /*************************************************************/
 // test for route endpoint
 TEST_F(Warning, routes) {
-  for (auto i : deprecated_costing_methods) {
+  for (auto& costing : deprecated_costing_methods) {
     valhalla::Api result =
-        gurka::do_action(valhalla::Options::route, map, {"A", "C"}, i, {{"/best_paths", "2"}});
+        gurka::do_action(valhalla::Options::route, map, {"A", "C"}, costing, {{"/best_paths", "2"}});
     ASSERT_TRUE(result.info().warnings_size() != 0);
     EXPECT_EQ(result.info().warnings_size(), 2);
   }
@@ -38,12 +38,12 @@ TEST_F(Warning, routes) {
 // test for matrix endpoint
 TEST_F(Warning, matrix) {
   Api request;
-  for (auto i : deprecated_costing_methods) {
+  for (auto& costing : deprecated_costing_methods) {
     std::string request_str = R"({"sources":[{"lat":)" + std::to_string(map.nodes["A"].lat()) +
                               R"(,"lon":)" + std::to_string(map.nodes["A"].lng()) +
                               R"(}],"targets":[{"lat":)" + std::to_string(map.nodes["C"].lat()) +
                               R"(,"lon":)" + std::to_string(map.nodes["C"].lng()) +
-                              R"(}],"costing":")" + i + R"("})";
+                              R"(}],"costing":")" + costing + R"("})";
     ParseApi(request_str, Options::sources_to_targets, request);
     ASSERT_FALSE(request.info().warnings_size() == 0);
     EXPECT_EQ(request.info().warnings_size(), 1);
@@ -52,8 +52,8 @@ TEST_F(Warning, matrix) {
 
 // test for locate endpoint
 TEST_F(Warning, locate) {
-  for (auto i : deprecated_costing_methods) {
-    valhalla::Api result = gurka::do_action(valhalla::Options::locate, map, {"B"}, i);
+  for (auto& costing : deprecated_costing_methods) {
+    valhalla::Api result = gurka::do_action(valhalla::Options::locate, map, {"B"}, costing);
     ASSERT_TRUE(result.info().warnings_size() != 0);
     EXPECT_EQ(result.info().warnings_size(), 1);
   }
@@ -61,9 +61,9 @@ TEST_F(Warning, locate) {
 
 // test for isochrone endpoint
 TEST_F(Warning, isochrone) {
-  for (auto i : deprecated_costing_methods) {
+  for (auto& costing : deprecated_costing_methods) {
     valhalla::Api result =
-        gurka::do_action(valhalla::Options::isochrone, map, {"B"}, i,
+        gurka::do_action(valhalla::Options::isochrone, map, {"B"}, costing,
                          {{"/contours/0/time", "10"}, {"/denoise", "0"}, {"/generalize", "0"}});
     ASSERT_FALSE(result.info().warnings_size() == 0);
     EXPECT_EQ(result.info().warnings_size(), 1);
@@ -88,11 +88,11 @@ TEST_F(Warning, height) {
 
 // test for map_matching endpoint
 TEST_F(Warning, map_matching) {
-  for (auto i : deprecated_costing_methods) {
+  for (auto& costing : deprecated_costing_methods) {
     valhalla::Api trace_route_result =
-        gurka::do_action(valhalla::Options::trace_route, map, {"A", "B", "C"}, i);
+        gurka::do_action(valhalla::Options::trace_route, map, {"A", "B", "C"}, costing);
     valhalla::Api trace_attributes_result =
-        gurka::do_action(valhalla::Options::trace_attributes, map, {"A", "B", "C"}, i);
+        gurka::do_action(valhalla::Options::trace_attributes, map, {"A", "B", "C"}, costing);
     ASSERT_FALSE(trace_route_result.info().warnings_size() == 0);
     EXPECT_EQ(trace_route_result.info().warnings_size(), 1);
     ASSERT_TRUE(trace_attributes_result.info().warnings_size() != 0);
