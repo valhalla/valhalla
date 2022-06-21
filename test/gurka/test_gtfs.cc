23e0a40b1db4a627147c4a20a32d967067a5d9f0
chris_jpark <chrisj.park@mail.utoronto.ca>
2022-06-21 21:47:17 +0900

    make sure reading is proper

--- test/gurka/test_gtfs.cc
+++ test/gurka/test_gtfs.cc
@@ -7,14 +7,19 @@
 using namespace gtfs;
 using namespace valhalla;
 
+#define tripOneID "bar"
+#define stopOneID "foo"
+#define stopTwoID "foo2"
+#define shapeOneID "square"
+#define serviceOneID "bon"
 // test to read gtfs data from sample
-TEST(GtfsExample, ReadGtfs) {
-  Feed feed("../test/data/gtfs_sample");
-  EXPECT_EQ(feed.read_agencies().code, ResultCode::OK);
-  const auto& agencies = feed.get_agencies();
-  EXPECT_EQ(agencies.size(), 1);
-  EXPECT_EQ(agencies[0].agency_id, "DTA");
-}
+// TEST(GtfsExample, ReadGtfs) {
+//   Feed feed("../test/data/gtfs_sample");
+//   EXPECT_EQ(feed.read_agencies().code, ResultCode::OK);
+//   const auto& agencies = feed.get_agencies();
+//   EXPECT_EQ(agencies.size(), 1);
+//   EXPECT_EQ(agencies[0].agency_id, "DTA");
+// }
 
 // test to write gtfs files
 TEST(GtfsExample, WriteGtfs) {
@@ -31,7 +36,7 @@ TEST(GtfsExample, WriteGtfs) {
 
   // write stops.txt
   struct gtfs::Stop nemo {
-    .stop_id = "foo", .coordinates_present = true, .stop_lat = 0.1, .stop_lon = 0.1,
+    .stop_id = stopOneID, .coordinates_present = true, .stop_lat = 0.1, .stop_lon = 0.1,
     .parent_station = "0", .location_type = gtfs::StopLocationType::StopOrPlatform,
     .stop_name = gtfs::Text("POINT NEMO"), .stop_timezone = "America/Toronto",
     .wheelchair_boarding = "1",
@@ -39,7 +44,7 @@ TEST(GtfsExample, WriteGtfs) {
   feed.add_stop(nemo);
 
   struct gtfs::Stop secondStop {
-    .stop_id = "foo2", .coordinates_present = true, .stop_lat = 0.2, .stop_lon = 0.2,
+    .stop_id = stopTwoID, .coordinates_present = true, .stop_lat = 0.2, .stop_lon = 0.2,
     .parent_station = "1", .location_type = gtfs::StopLocationType::StopOrPlatform,
     .stop_name = gtfs::Text("SECOND STOP"), .stop_timezone = "America/Toronto",
     .wheelchair_boarding = "1",
@@ -58,8 +63,8 @@ TEST(GtfsExample, WriteGtfs) {
 
   // write trips.txt
   struct gtfs::Trip tripOne {
-    .route_id = "baz", .service_id = "bon", .trip_id = "bar", .trip_headsign = "haha",
-    .block_id = "block", .shape_id = "square", .wheelchair_accessible = gtfs::TripAccess::Yes,
+    .route_id = "baz", .service_id = serviceOneID, .trip_id = tripOneID, .trip_headsign = "haha",
+    .block_id = "block", .shape_id = shapeOneID, .wheelchair_accessible = gtfs::TripAccess::Yes,
     .bikes_allowed = gtfs::TripAccess::No,
   };
   feed.add_trip(tripOne);
@@ -67,8 +72,8 @@ TEST(GtfsExample, WriteGtfs) {
 
   // write stop_times.txt
   struct StopTime stopTimeOne {
-    .trip_id = "bar", .arrival_time = Time("6:00:00"), .departure_time = Time("6:00:00"),
-    .stop_id = "foo", .stop_sequence = 0, .stop_headsign = "head", .shape_dist_traveled = 0.0,
+    .trip_id = tripOneID, .arrival_time = Time("6:00:00"), .departure_time = Time("6:00:00"),
+    .stop_id = stopOneID, .stop_sequence = 0, .stop_headsign = "head", .shape_dist_traveled = 0.0,
     .timepoint = gtfs::StopTimePoint::Exact,
   };
   feed.add_stop_time(stopTimeOne);
@@ -76,7 +81,7 @@ TEST(GtfsExample, WriteGtfs) {
 
   // write calendar.txt
   struct CalendarItem calendarOne {
-    .service_id = "bon", // connected to trip
+    .service_id = serviceOneID, // connected to trip
         .monday = CalendarAvailability::Available, .tuesday = CalendarAvailability::Available,
     .wednesday = CalendarAvailability::Available, .thursday = CalendarAvailability::Available,
     .friday = CalendarAvailability::Available, .saturday = CalendarAvailability::Available,
@@ -88,50 +93,49 @@ TEST(GtfsExample, WriteGtfs) {
 
   // write calendar_dates.txt
   struct CalendarDate dateOne {
-    .service_id = "bon", // connected to trip
+    .service_id = serviceOneID, // connected to trip
         .date = Date(2022, 2, 2), .exception_type = gtfs::CalendarDateException::Added,
   };
   feed.add_calendar_date(dateOne);
   feed.write_calendar_dates("../test/data/gtfs_test/");
 
   // write shapes.txt
-  struct ShapePoint shapePointOne {
-    .shape_id = "square", .shape_pt_lat = 0.2, .shape_pt_lon = 0.2, .shape_pt_sequence = 1
-  };
-  struct ShapePoint shapePointTwo {
-    .shape_id = "square", .shape_pt_lat = 0.21, .shape_pt_lon = 0.21, .shape_pt_sequence = 2
-  };
-  struct ShapePoint shapePointThree {
-    .shape_id = "square", .shape_pt_lat = 0.22, .shape_pt_lon = 0.22, .shape_pt_sequence = 3
-  };
-  struct ShapePoint shapePointFour {
-    .shape_id = "square", .shape_pt_lat = 0.23, .shape_pt_lon = 0.23, .shape_pt_sequence = 4
-  };
-  feed.add_shape(shapePointOne);
-  feed.add_shape(shapePointTwo);
-  feed.add_shape(shapePointThree);
-  feed.add_shape(shapePointFour);
+  for (size_t i = 0; i < 4; i++) {
+    struct ShapePoint shapePointTest {
+      .shape_id = shapeOneID, .shape_pt_lat = 0.2, .shape_pt_lon = 0.2, .shape_pt_sequence = (i + 1),
+    };
+    feed.add_shape(shapePointTest);
+  }
+
   feed.write_shapes("../test/data/gtfs_test/");
 
   // write frequencies.txt
   struct Frequency freqOne {
-    .trip_id = "bar", .start_time = Time(0, 0, 0), .end_time = Time(2, 0, 0), .headway_secs = 1800,
-    .exact_times = gtfs::FrequencyTripService::FrequencyBased,
+    .trip_id = tripOneID, .start_time = Time(0, 0, 0), .end_time = Time(2, 0, 0),
+    .headway_secs = 1800, .exact_times = gtfs::FrequencyTripService::FrequencyBased,
   };
   feed.add_frequency(freqOne);
   feed.write_frequencies("../test/data/gtfs_test/");
 
+  Feed feed_reader("../test/data/gtfs_test/");
   // make sure files are actually written
-  const auto& trips = feed.get_trips();
-  EXPECT_EQ(trips[0].trip_id, "bar");
+  EXPECT_EQ(feed_reader.read_trips().code, ResultCode::OK);
+  const auto& trips = feed_reader.get_trips();
+  EXPECT_EQ(trips.size(), 1);
+  EXPECT_EQ(trips[0].trip_id, tripOneID);
 
-  const auto& stops = feed.get_stops();
+  EXPECT_EQ(feed_reader.read_stops().code, ResultCode::OK);
+  const auto& stops = feed_reader.get_stops();
   EXPECT_EQ(stops.size(), 2);
-  EXPECT_EQ(stops[1].stop_id, "foo2");
+  EXPECT_EQ(stops[1].stop_id, stopTwoID);
 
-  const auto& shapes = feed.get_shapes();
-  EXPECT_EQ(shapes[0].shape_id, "square");
+  EXPECT_EQ(feed_reader.read_shapes().code, ResultCode::OK);
+  const auto& shapes = feed_reader.get_shapes();
+  EXPECT_EQ(shapes.size(), 4);
+  EXPECT_EQ(shapes[0].shape_id, shapeOneID);
 
-  const auto& calendarGTFS = feed.get_calendar();
-  EXPECT_EQ(calendarGTFS[0].service_id, "bon");
+  EXPECT_EQ(feed_reader.read_calendar().code, ResultCode::OK);
+  const auto& calendarGTFS = feed_reader.get_calendar();
+  EXPECT_EQ(calendarGTFS.size(), 1);
+  EXPECT_EQ(calendarGTFS[0].service_id, serviceOneID);
 }
