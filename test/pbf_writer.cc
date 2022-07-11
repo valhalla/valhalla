987c8fa17da99358773e6783a85c20f0a1dd30bf
Hainan Chen <hainan.ch.chen@daimler.com>
2022-07-11 13:30:09 +0000

    rework for considering shapepoints in pbf_writer

--- test/pbf_writer.cc
+++ test/pbf_writer.cc
@@ -37,9 +37,22 @@ const std::string LINK_FRC_FILE = "link_frc";
 const std::string LINK_LENGTH_FILE = "link_length";
 const std::string LINK_SPEED_FILE = "link_speed";
 const std::string LINK_ONEWAY_FILE = "link_oneway";
+const std::string LINK_SHAPEPOINTS_LAT_FILE = "link_shapepoints_lat";
+const std::string LINK_SHAPEPOINTS_LON_FILE = "link_shapepoints_lon";
+const std::string LINK_SHAPEPOINTS_INDEX_FILE = "link_shapepoints_index";
+
+constexpr auto RESTRICTION_SIMPLE_FILE = "restriction_simple";
+
+constexpr auto ndsUnitsPerWgs84Deg = (std::numeric_limits<uint32_t>::max() + 1.0) / 360.0;
 
 namespace valhalla {
 namespace detail {
+
+struct MyRestriction {
+  uint64_t from_edge_id;
+  uint64_t via_node_id;
+  uint64_t to_edge_id;
+};
 
 long GetFileSize(std::string filename) {
   struct stat stat_buf;
@@ -63,121 +76,142 @@ inline void build_pbf(const std::string inPath,
   const size_t initial_buffer_size = 10000;
   osmium::memory::Buffer buffer{initial_buffer_size, osmium::memory::Buffer::auto_grow::yes};
 
-  LOG_INFO("reading files for nodes");
-  std::vector<uint64_t> node_ids =
-      valhalla::detail::read_vector<uint64_t>(inPath + NODE_SOURCE_IDS_FILE);
-  std::vector<int32_t> node_lats = valhalla::detail::read_vector<int32_t>(inPath + NODE_POS_LAT_FILE);
-  std::vector<int32_t> node_lons = valhalla::detail::read_vector<int32_t>(inPath + NODE_POS_LON_FILE);
-  std::vector<uint32_t> node_artificials =
-      valhalla::detail::read_vector<uint32_t>(inPath + NODE_IS_ARTIFICIAL_FILE);
+  {
+    LOG_INFO("reading files for nodes");
+    std::vector<uint64_t> node_ids =
+        valhalla::detail::read_vector<uint64_t>(inPath + NODE_SOURCE_IDS_FILE);
+    std::vector<int32_t> node_lats =
+        valhalla::detail::read_vector<int32_t>(inPath + NODE_POS_LAT_FILE);
+    std::vector<int32_t> node_lons =
+        valhalla::detail::read_vector<int32_t>(inPath + NODE_POS_LON_FILE);
+    std::vector<uint32_t> node_artificials =
+        valhalla::detail::read_vector<uint32_t>(inPath + NODE_IS_ARTIFICIAL_FILE);
 
-  LOG_INFO("processing " + std::to_string(node_ids.size()) + " nodes");
-  for (auto i = 0; i < node_ids.size(); ++i) {
-    std::vector<std::pair<std::string, std::string>> tags;
-    if (node_artificials.at(i) == 1)
-      tags.push_back({"artificial", "true"});
-    else
-      tags.push_back({"artificial", "false"});
-    osmium::builder::add_node(buffer, osmium::builder::attr::_id(node_ids.at(i)),
-                              osmium::builder::attr::_version(1),
-                              osmium::builder::attr::_timestamp(std::time(nullptr)),
-                              osmium::builder::attr::_location(
-                                  osmium::Location{static_cast<float>(node_lons.at(i)) / 100000,
-                                                   static_cast<float>(node_lats.at(i)) / 100000}),
-                              osmium::builder::attr::_tags(tags));
-  }
+    LOG_INFO("processing " + std::to_string(node_ids.size()) + " nodes");
+    for (auto i = 0; i < node_ids.size(); ++i) {
+      std::vector<std::pair<std::string, std::string>> tags;
+      if (node_artificials.at(i) == 1)
+        tags.push_back({"artificial", "true"});
+      else
+        tags.push_back({"artificial", "false"});
 
-  node_ids.clear();
-  node_ids.shrink_to_fit();
-  node_lats.clear();
-  node_lats.shrink_to_fit();
-  node_lons.clear();
-  node_lons.shrink_to_fit();
-  node_artificials.clear();
-  node_artificials.shrink_to_fit();
-
-  LOG_INFO("reading files for links");
-  std::vector<uint64_t> link_ids =
-      valhalla::detail::read_vector<uint64_t>(inPath + LINK_SOURCE_IDS_FILE);
-  std::vector<uint64_t> link_froms = valhalla::detail::read_vector<uint64_t>(inPath + LINK_FROM_FILE);
-  std::vector<uint64_t> link_tos = valhalla::detail::read_vector<uint64_t>(inPath + LINK_TO_FILE);
-  std::vector<uint32_t> link_frcs = valhalla::detail::read_vector<uint32_t>(inPath + LINK_FRC_FILE);
-  std::vector<uint32_t> linkd_speeds =
-      valhalla::detail::read_vector<uint32_t>(inPath + LINK_SPEED_FILE);
-  std::vector<uint32_t> link_oneways =
-      valhalla::detail::read_vector<uint32_t>(inPath + LINK_ONEWAY_FILE);
-
-  const std::string ROAD_CLASS[] = {"motorway", "trunk", "secondary", "tertiary", "residential"};
-  // const std::string ROAD_CLASS[] = {"motorway", "trunk",        "secondary",
-  //                                   "tertiary", "unclassified", "residential"};
-
-  LOG_INFO("processing " + std::to_string(link_ids.size()) + " nodes");
-  for (auto i = 0; i < link_ids.size(); ++i) {
-    std::vector<uint64_t> nodeids;
-    nodeids.emplace_back(link_froms.at(i));
-    nodeids.emplace_back(link_tos.at(i));
-
-    std::vector<std::pair<std::string, std::string>> tags;
-
-    tags.push_back({"highway", ROAD_CLASS[link_frcs.at(i) - 1]});
-    tags.push_back({"maxspeed", std::to_string(linkd_speeds.at(i))});
-    if (link_oneways.at(i) == 2) {
-      tags.push_back({"oneway", "yes"});
-    } else if (link_oneways.at(i) == 1) {
-      tags.push_back({"oneway", "-1"});
-    } else if (link_oneways.at(i) == 3) {
-      tags.push_back({"oneway", "yes"});
-      tags.push_back({"oneway", "-1"});
+      // TODO: int to float for lat and lon
+      osmium::builder::add_node(buffer, osmium::builder::attr::_id(node_ids.at(i)),
+                                osmium::builder::attr::_version(1),
+                                osmium::builder::attr::_timestamp(std::time(nullptr)),
+                                osmium::builder::attr::_location(
+                                    osmium::Location{static_cast<float>(node_lons.at(i)) /
+                                                         ndsUnitsPerWgs84Deg,
+                                                     static_cast<float>(node_lats.at(i)) /
+                                                         ndsUnitsPerWgs84Deg}),
+                                osmium::builder::attr::_tags(tags));
     }
-
-    osmium::builder::add_way(buffer, osmium::builder::attr::_id(link_ids.at(i)),
-                             osmium::builder::attr::_version(1), osmium::builder::attr::_cid(1001),
-                             osmium::builder::attr::_timestamp(std::time(nullptr)),
-                             osmium::builder::attr::_nodes(nodeids),
-                             osmium::builder::attr::_tags(tags));
   }
 
-  link_ids.clear();
-  link_ids.shrink_to_fit();
-  link_froms.clear();
-  link_froms.shrink_to_fit();
-  link_tos.clear();
-  link_tos.shrink_to_fit();
-  link_frcs.clear();
-  link_frcs.shrink_to_fit();
-  linkd_speeds.clear();
-  linkd_speeds.shrink_to_fit();
-  link_oneways.clear();
-  link_oneways.shrink_to_fit();
-  /*
+  // {
+  //   std::vector<int32_t> link_shapepoints_lat =
+  //       valhalla::detail::read_vector<int32_t>(inPath + LINK_SHAPEPOINTS_LAT_FILE);
+  //   std::vector<int32_t> link_shapepoints_lon =
+  //       valhalla::detail::read_vector<int32_t>(inPath + LINK_SHAPEPOINTS_LON_FILE);
 
+  //   LOG_INFO("processing " + std::to_string(link_shapepoints_lon.size()) + " shapepoints");
+  //   for (uint64_t i = 0; i < link_shapepoints_lon.size(); ++i) {
+  //     std::vector<std::pair<std::string, std::string>> tags;
+  //     tags.push_back({"artificial", "false"});
+
+  //     // TODO: int to float for lat and lon
+  //     osmium::builder::add_node(buffer, osmium::builder::attr::_id(i),
+  //                               osmium::builder::attr::_version(1),
+  //                               osmium::builder::attr::_timestamp(std::time(nullptr)),
+  //                               osmium::builder::attr::_location(
+  //                                   osmium::Location{static_cast<float>(link_shapepoints_lon.at(i))
+  //                                   /
+  //                                                        ndsUnitsPerWgs84Deg,
+  //                                                    static_cast<float>(link_shapepoints_lat.at(i))
+  //                                                    /
+  //                                                        ndsUnitsPerWgs84Deg}),
+  //                               osmium::builder::attr::_tags(tags));
+  //   }
+  // }
+
+  {
+    LOG_INFO("reading files for links");
+    std::vector<uint64_t> link_ids =
+        valhalla::detail::read_vector<uint64_t>(inPath + LINK_SOURCE_IDS_FILE);
+    std::vector<uint64_t> link_froms =
+        valhalla::detail::read_vector<uint64_t>(inPath + LINK_FROM_FILE);
+    std::vector<uint64_t> link_tos = valhalla::detail::read_vector<uint64_t>(inPath + LINK_TO_FILE);
+    std::vector<uint32_t> link_frcs = valhalla::detail::read_vector<uint32_t>(inPath + LINK_FRC_FILE);
+    std::vector<uint32_t> linkd_speeds =
+        valhalla::detail::read_vector<uint32_t>(inPath + LINK_SPEED_FILE);
+    std::vector<uint32_t> link_oneways =
+        valhalla::detail::read_vector<uint32_t>(inPath + LINK_ONEWAY_FILE);
+
+    std::vector<uint64_t> link_shapepoints_index =
+        valhalla::detail::read_vector<uint64_t>(inPath + LINK_SHAPEPOINTS_INDEX_FILE);
+
+    const std::string ROAD_CLASS[] = {"motorway", "trunk", "secondary", "tertiary", "residential"};
+    // const std::string ROAD_CLASS[] = {"motorway", "trunk",        "secondary",
+    //                                   "tertiary", "unclassified", "residential"};
+    uint64_t pos = 0;
+
+    LOG_INFO("processing " + std::to_string(link_ids.size()) + " links");
+    for (auto i = 0; i < link_ids.size(); ++i) {
+      std::vector<uint64_t> nodeids;
+      nodeids.emplace_back(link_froms.at(i)); // from node
+
+      // // add shapepoints
+      // for (auto j = pos; j < link_shapepoints_index[i]; ++j) {
+      //   nodeids.emplace_back(j);
+      // }
+      // pos = link_shapepoints_index[i];
+
+      nodeids.emplace_back(link_tos.at(i)); // to node
+
+      std::vector<std::pair<std::string, std::string>> tags;
+
+      tags.push_back({"highway", ROAD_CLASS[link_frcs.at(i) - 1]});
+      tags.push_back({"maxspeed", std::to_string(linkd_speeds.at(i))});
+      if (link_oneways.at(i) == 2) {
+        tags.push_back({"oneway", "yes"});
+      } else if (link_oneways.at(i) == 1) {
+        tags.push_back({"oneway", "-1"});
+      } else if (link_oneways.at(i) == 3) {
+        tags.push_back({"oneway", "yes"});
+        tags.push_back({"oneway", "-1"});
+      }
+
+      osmium::builder::add_way(buffer, osmium::builder::attr::_id(link_ids.at(i)),
+                               osmium::builder::attr::_version(1), osmium::builder::attr::_cid(1001),
+                               osmium::builder::attr::_timestamp(std::time(nullptr)),
+                               osmium::builder::attr::_nodes(nodeids),
+                               osmium::builder::attr::_tags(tags));
+    }
+  }
+
+  {
+
+    std::vector<MyRestriction> relations =
+        valhalla::detail::read_vector<MyRestriction>(inPath + RESTRICTION_SIMPLE_FILE);
+    int osm_id = 0;
     for (const auto& relation : relations) {
 
       std::vector<osmium::builder::attr::member_type> members;
-      for (const auto& member : relation.members) {
-        if (member.type == node_member) {
-          members.push_back({osmium::item_type::node,
-    static_cast<int64_t>(node_osm_id_map[member.ref]), member.role.c_str()}); } else { if
-    (way_osm_id_map.count(member.ref) == 0) { throw std::runtime_error("Relation member refers to an
-    undefined way " + member.ref);
-          }
-          members.push_back({osmium::item_type::way, static_cast<int64_t>(way_osm_id_map[member.ref]),
-                             member.role.c_str()});
-        }
-      }
+      members.push_back({osmium::item_type::way, relation.from_edge_id, "from"});
+      members.push_back({osmium::item_type::node, relation.via_node_id, "via"});
+      members.push_back({osmium::item_type::way, relation.to_edge_id, "to"});
 
       std::vector<std::pair<std::string, std::string>> tags;
-      for (const auto& tag : relation.tags) {
-        tags.push_back({tag.first, tag.second});
-      }
-
+      tags.push_back({"type", "restriction"});
+      tags.push_back({"restriction", "no_straight_on"});
       osmium::builder::add_relation(buffer, osmium::builder::attr::_id(osm_id++),
                                     osmium::builder::attr::_version(1),
                                     osmium::builder::attr::_timestamp(std::time(nullptr)),
                                     osmium::builder::attr::_members(members),
                                     osmium::builder::attr::_tags(tags));
     }
-*/
+  }
+
   // Create header and set generator.
   osmium::io::Header header;
   header.set("generator", "valhalla-test-creator");
