6c4f707b1fbbd3b8bcd530b679cd1d4ac94f2cdb
nilsnolde <nilsnolde@proton.me>
2022-10-13 23:54:19 +0200

    add matrix to gurka

---
+++ test/gurka/test_matrix.cc
@@ -0,0 +1,46 @@
+#include "gurka.h"
+#include <boost/format.hpp>
+#include <gtest/gtest.h>
+
+using namespace valhalla;
+
+TEST(MatrixTest, MatrixSimple) {
+  constexpr double gridsize = 100;
+
+  // the grid below is 50 x 100 meters at 36 km/h, i.e. 10 m/s
+  const std::string ascii_map = R"(
+      A----B
+      |    |
+      |    |
+      C----D
+    )";
+
+  const gurka::ways ways = {
+      {"AB", {{"highway", "residential"}, {"maxspeed", "50"}}},
+      {"AC", {{"highway", "residential"}, {"maxspeed", "50"}}},
+      {"BD", {{"highway", "residential"}, {"maxspeed", "50"}}},
+      {"CD", {{"highway", "residential"}, {"maxspeed", "50"}}},
+  };
+
+  const auto layout = gurka::detail::map_to_coordinates(ascii_map, gridsize);
+  const auto map = gurka::buildtiles(layout, ways, {}, {}, "test/data/shortest");
+
+  const std::vector<double> exp_dists = {0.3, 0.8, 0.8, 0.3};
+  const std::vector<uint32_t> exp_times = {21, 58, 58, 21};
+  std::string res;
+  const auto result = gurka::do_action(Options::sources_to_targets, map, {"A", "B"}, {"C", "D"},
+                                       "auto", {}, {}, &res);
+
+  // get the MultiLineString feature
+  rapidjson::Document res_doc;
+  res_doc.Parse(res.c_str());
+
+  size_t i = 0;
+  for (const auto& origin_row : res_doc["sources_to_targets"].GetArray()) {
+    for (const auto& v : origin_row.GetArray()) {
+      EXPECT_EQ(v.GetObject()["distance"].GetDouble(), exp_dists[i]);
+      EXPECT_EQ(v.GetObject()["time"].GetUint(), exp_times[i]);
+      i++;
+    }
+  }
+}
