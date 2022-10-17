ea4f62c095d8f38054d14971e8efa8a43b075144
nilsnolde <nilsnolde@proton.me>
2022-10-17 22:51:29 +0200

    change to uint8_t

--- src/sif/truckcost.cc
+++ src/sif/truckcost.cc
@@ -52,7 +52,7 @@ constexpr float kDefaultTruckAxleLoad = 9.07f; // Metric Tons (20,000 lbs)
 constexpr float kDefaultTruckHeight = 4.11f;   // Meters (13 feet 6 inches)
 constexpr float kDefaultTruckWidth = 2.6f;     // Meters (102.36 inches)
 constexpr float kDefaultTruckLength = 21.64f;  // Meters (71 feet)
-constexpr uint32_t kDefaultAxleCount = 5;      // 5 axles for above truck config
+constexpr uint8_t kDefaultAxleCount = 5;       // 5 axles for above truck config
 
 // Turn costs based on side of street driving
 constexpr float kRightSideTurnCosts[] = {kTCStraight,       kTCSlight,  kTCFavorable,
@@ -73,7 +73,7 @@ constexpr ranged_default_t<float> kTruckHeightRange{0, kDefaultTruckHeight, 10.0
 constexpr ranged_default_t<float> kTruckWidthRange{0, kDefaultTruckWidth, 10.0f};
 constexpr ranged_default_t<float> kTruckLengthRange{0, kDefaultTruckLength, 50.0f};
 constexpr ranged_default_t<float> kUseTollsRange{0, kDefaultUseTolls, 1.0f};
-constexpr ranged_default_t<uint32_t> kAxleCountRange{2, kDefaultAxleCount, 20};
+constexpr ranged_default_t<uint8_t> kAxleCountRange{2, kDefaultAxleCount, 20};
 
 BaseCostingOptionsConfig GetBaseCostOptsConfig() {
   BaseCostingOptionsConfig cfg{};
@@ -270,13 +270,13 @@ public:
   float low_class_penalty_;  // Penalty (seconds) to go to residential or service road
 
   // Vehicle attributes (used for special restrictions and costing)
-  bool hazmat_;         // Carrying hazardous materials
-  float weight_;        // Vehicle weight in metric tons
-  float axle_load_;     // Axle load weight in metric tons
-  float height_;        // Vehicle height in meters
-  float width_;         // Vehicle width in meters
-  float length_;        // Vehicle length in meters
-  uint32_t axle_count_; // Vehicle axle count
+  bool hazmat_;        // Carrying hazardous materials
+  float weight_;       // Vehicle weight in metric tons
+  float axle_load_;    // Axle load weight in metric tons
+  float height_;       // Vehicle height in meters
+  float width_;        // Vehicle width in meters
+  float length_;       // Vehicle length in meters
+  uint8_t axle_count_; // Vehicle axle count
 
   // Density factor used in edge transition costing
   std::vector<float> trans_density_factor_;
@@ -353,7 +353,7 @@ bool TruckCost::ModeSpecificAllowed(const baldr::AccessRestriction& restriction)
       }
       break;
     case AccessType::kMaxAxles:
-      if (axle_count_ > static_cast<uint32_t>(restriction.value())) {
+      if (axle_count_ > static_cast<uint8_t>(restriction.value())) {
         return false;
       }
       break;
@@ -657,7 +657,7 @@ void ParseTruckCostOptions(const rapidjson::Document& doc,
   JSON_PBF_RANGED_DEFAULT(co, kTruckWidthRange, json, "/width", width);
   JSON_PBF_RANGED_DEFAULT(co, kTruckLengthRange, json, "/length", length);
   JSON_PBF_RANGED_DEFAULT(co, kUseTollsRange, json, "/use_tolls", use_tolls);
-  co->set_fixed_speed(
+  co->set_axle_count(
       kAxleCountRange(rapidjson::get<uint32_t>(json, "/axle_count", co->axle_count())));
 }
 
