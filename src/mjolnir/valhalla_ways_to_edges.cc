04137130f608d1255ad3944866a376490563b22f
nilsnolde <nils.nolde@gmail.com>
2022-12-21 12:49:48 +0100

    log when finished

--- src/mjolnir/valhalla_ways_to_edges.cc
+++ src/mjolnir/valhalla_ways_to_edges.cc
@@ -18,6 +18,7 @@
 #include "baldr/graphtile.h"
 #include "baldr/tilehierarchy.h"
 #include "filesystem.h"
+#include "midgard/logging.h"
 
 using namespace valhalla::baldr;
 using namespace valhalla::midgard;
@@ -131,6 +132,8 @@ int main(int argc, char** argv) {
     ways_file << std::endl;
   }
   ways_file.close();
+
+  LOG_INFO("Finished with " + std::to_string(ways_edges.size()) + " ways.");
 
   return EXIT_SUCCESS;
 }
