db6710b1527daa273cd3e7e6c99534f88291b86c
totratt <tobias.tratt@mercedes-benz.com>
2023-01-05 11:32:46 +0100

    always display version

--- pipeline/release/scripts/01-version.sh
+++ pipeline/release/scripts/01-version.sh
@@ -4,4 +4,6 @@ if [ -z "$APP_VERSION" ]
 then
   echo "Current build version: latest"
   echo "##vso[task.setvariable variable=app.version;]latest"
+else
+  echo "Current build version: ${APP_VERSION}"
 fi
