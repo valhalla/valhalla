f638a629e95555a3e93dc22d9470a45c054fcdae
totratt <tobias.tratt@mercedes-benz.com>
2023-01-17 15:04:05 +0100

    replace SONAR_CERT_PASS with SONAR_CERT_PASSPHRASE

--- pipeline/build/scripts/20-scan.sh
+++ pipeline/build/scripts/20-scan.sh
@@ -9,7 +9,7 @@ then
     -e SONAR_HOST_URL=${SONAR_URL} \
     -e SONAR_LOGIN=${SONAR_TOKEN} \
     -e SONAR_SCANNER_OPTS="-Djavax.net.ssl.keyStore=${SONAR_CERT_PATH} -Djavax.net.ssl.keyStoreType=PKCS12 -Djavax.net.ssl.keyStorePassword=${SONAR_CERT_PASSPHRASE}" \
-    -v "${BUILD_SOURCEDIRECTORY}:/src" \
+    -v "${BUILD_SOURCESDIRECTORY}:/src" \
     -v "${SONAR_CERT_PATH}:${SONAR_CERT_PATH}" \
     sonarsource/sonar-scanner-cli \
     -Dsonar.projectKey=WAYVALHAL \
