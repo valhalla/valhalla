a736802059140c57bb4a5e68d420620af96436b9
totratt <tobias.tratt@mercedes-benz.com>
2023-01-17 14:36:37 +0100

    replace SONAR_CERT_PASS with SONAR_CERT_PASSPHRASE

--- pipeline/build/scripts/20-scan.sh
+++ pipeline/build/scripts/20-scan.sh
@@ -3,12 +3,12 @@
 shopt -s nocasematch
 if [[ "${SONAR_ENABLED}" == true ]]
 then
-  keytool -list -keystore ${SONAR_CERT_PATH} -storepass ${SONAR_CERT_PASS}
+  keytool -list -keystore ${SONAR_CERT_PATH} -storepass ${SONAR_CERT_PASSPHRASE}
   docker run \
     --rm \
     -e SONAR_HOST_URL=${SONAR_URL} \
     -e SONAR_LOGIN=${SONAR_TOKEN} \
-    -e SONAR_SCANNER_OPTS="-Djavax.net.ssl.keyStore=${SONAR_CERT_PATH} -Djavax.net.ssl.keyStoreType=PKCS12 -Djavax.net.ssl.keyStorePassword=${SONAR_CERT_PASS}" \
+    -e SONAR_SCANNER_OPTS="-Djavax.net.ssl.keyStore=${SONAR_CERT_PATH} -Djavax.net.ssl.keyStoreType=PKCS12 -Djavax.net.ssl.keyStorePassword=${SONAR_CERT_PASSPHRASE}" \
     -v "${BUILD_SOURCEDIRECTORY}:/src" \
     -v "${SONAR_CERT_PATH}:${SONAR_CERT_PATH}" \
     sonarsource/sonar-scanner-cli \
