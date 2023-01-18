6d0f44274ec621a22b5f11a3b0d6241e85b238ae
totratt <tobias.tratt@mercedes-benz.com>
2023-01-18 15:28:46 +0100

    version

--- pipeline/release/scripts/01-version.sh
+++ pipeline/release/scripts/01-version.sh
@@ -3,6 +3,7 @@
 if [ -z "$APP_VERSION" ]
 then
   echo '##vso[task.setvariable variable=app.version]latest'
+  sleep 5
   echo "Current build version empty, setting to latest."
 else
   echo "Current build version already set to: ${APP_VERSION}"
