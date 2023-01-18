3bd74b4124e5c32e38168fcfa692a5c3ad04750a
totratt <tobias.tratt@mercedes-benz.com>
2023-01-18 09:42:26 +0100

    SonarQube Settings

--- pipeline/build/scripts/20-scan.sh
+++ pipeline/build/scripts/20-scan.sh
@@ -9,14 +9,15 @@ then
     -e SONAR_HOST_URL=${SONAR_URL} \
     -e SONAR_LOGIN=${SONAR_TOKEN} \
     -e SONAR_SCANNER_OPTS="-Djavax.net.ssl.keyStore=${SONAR_CERT_PATH} -Djavax.net.ssl.keyStoreType=PKCS12 -Djavax.net.ssl.keyStorePassword=${SONAR_CERT_PASSPHRASE}" \
-    -v "${BUILD_SOURCESDIRECTORY}:/src" \
+    -v "${BUILD_SOURCESDIRECTORY}:/project" \
     -v "${SONAR_CERT_PATH}:${SONAR_CERT_PATH}" \
     sonarsource/sonar-scanner-cli \
+    -Dsonar.projectBaseDir=/project \
     -Dsonar.projectKey=WAYVALHAL \
     -Dsonar.projectName=${APP_NAME} \
     -Dsonar.projectVersion=${APP_VERSION} \
-    -Dsonar.branchName=${BUILD_SOURCEBRANCHNAME} \
-    -Dsonar.cfamily.compile-commands=compile_commands.json
+    -Dsonar.branch.name=${BUILD_SOURCEBRANCHNAME} \
+    -Dsonar.sources=. \
 fi
 
 if [[ "${BLACKDUCK_ENABLED}" == true ]]
