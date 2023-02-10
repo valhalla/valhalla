a43527bb024b08cc1987ba2c58a1d6b814adbdf8
patrick.qian <patrick.qian@kisio.com>
2023-02-10 18:12:39 +0100

    Add OSM node id into BSS info
    reapply pr2 and pr3

--- src/mjolnir/pbfgraphparser.cc
+++ src/mjolnir/pbfgraphparser.cc
@@ -2004,6 +2004,7 @@ struct graph_parser {
     n.set_latlng(node.location().lon(), node.location().lat());
     n.set_type(NodeType::kBikeShare);
     valhalla::BikeShareStationInfo bss_info;
+    bss_info.set_osm_node_id(osmid);
 
     for (auto& key_value : tags) {
       if (key_value.first == "name") {
@@ -4210,7 +4211,7 @@ struct graph_parser {
                   return; // should not make it here; has to be bad data.
                 }
               } // else
-            }   // if (condition.empty())
+            } // if (condition.empty())
 
             std::vector<std::string> conditions = GetTagTokens(condition, ';');
 
@@ -4237,7 +4238,7 @@ struct graph_parser {
             }
             return;
           } // if (isConditional)
-        }   // end turning into complex restriction
+        } // end turning into complex restriction
 
         restriction.set_modes(modes);
 
