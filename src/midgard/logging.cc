ea03f35a28d58ac0f9a575f779be49e3432c8c56
Luke Seelenbinder <me@lukeseelenbinder.com>
2023-02-20 14:20:40 +0100

    Add small comment.

--- src/midgard/logging.cc
+++ src/midgard/logging.cc
@@ -41,6 +41,8 @@ std::string TimeStamp() {
   std::string buffer("year/mo/dy hr:mn:sc.xxxxxx0");
   snprintf(&buffer.front(), buffer.length(), "%04d/%02d/%02d %02d:%02d:%09.6f", gmt.tm_year + 1900,
            gmt.tm_mon + 1, gmt.tm_mday, gmt.tm_hour, gmt.tm_min, fractional_seconds.count());
+
+  // Remove trailing null terminator added by snprintf.
   buffer.pop_back();
   return buffer;
 }
