7d05f165597411810f63fbdd2506325a563c048a
vesperlou <Vesper_Lou@outlook.com>
2023-03-20 17:09:02 +0100

    Update to a parameterized test

--- test/gurka/test_disable_hierarchy.cc
+++ test/gurka/test_disable_hierarchy.cc
@@ -1,4 +1,5 @@
 #include "gurka.h"
+#include "test.h"
 #include "proto/options.pb.h"
 #include <boost/format.hpp>
 #include <gtest/gtest.h>
@@ -20,64 +21,35 @@ void set_disable_hierarchy_pruning(Options& options, Costing::Type type) {
   co->set_disable_hierarchy_pruning(true);
 }
 
-// Test the hierarchy limits are actually disabled when disable_hierarchy_pruning = true
-void doTest(Costing::Type costing_type) {
-  // Set costing options
-  Options options;
-  set_disable_hierarchy_pruning(options, costing_type);
-  create_costing_options(options, costing_type);
-  auto travel_mode = valhalla::sif::TravelMode::kDrive;
-  const auto mode_costing = valhalla::sif::CostFactory().CreateModeCosting(options, travel_mode);
+class HierarchyTest : public ::testing::TestWithParam<Costing::Type> {
+protected:
+  // Test the hierarchy limits are actually disabled when disable_hierarchy_pruning = true
+  void doTest(Costing::Type costing_type) {
+    // Set costing options
+    Options options;
+    set_disable_hierarchy_pruning(options, costing_type);
+    create_costing_options(options, costing_type);
+    valhalla::sif::TravelMode travel_mode;
+    const auto mode_costing = valhalla::sif::CostFactory().CreateModeCosting(options, travel_mode);
 
-  // Check hierarchy limits
-  auto& hierarchy_limits =
-      mode_costing[int(travel_mode)]->GetHierarchyLimits(); // access mutable limits
-  for (auto& hierarchy : hierarchy_limits) {
-    EXPECT_EQ(hierarchy.max_up_transitions, kUnlimitedTransitions);
+    // Check hierarchy limits
+    auto& hierarchy_limits =
+        mode_costing[int(travel_mode)]->GetHierarchyLimits();
+    for (auto& hierarchy : hierarchy_limits) {
+      EXPECT_EQ(hierarchy.max_up_transitions, kUnlimitedTransitions);
+    }
   }
+};
+
+TEST_P(HierarchyTest, TestDisableHierarchy) {
+  doTest(GetParam());
 }
 
-TEST(DisableHierarchyPruningTest, Auto) {
-  Costing::Type type = Costing::auto_;
-  doTest(type);
-}
-
-TEST(DisableHierarchyPruningTest, Bike) {
-  Costing::Type type = Costing::bicycle;
-  doTest(type);
-}
-
-TEST(DisableHierarchyPruningTest, Bus) {
-  Costing::Type type = Costing::bus;
-  doTest(type);
-}
-
-TEST(DisableHierarchyPruningTest, MotorScooter) {
-  Costing::Type type = Costing::motor_scooter;
-  doTest(type);
-}
-
-TEST(DisableHierarchyPruningTest, Pedestrian) {
-  Costing::Type type = Costing::pedestrian;
-  doTest(type);
-}
-
-TEST(DisableHierarchyPruningTest, Truck) {
-  Costing::Type type = Costing::truck;
-  doTest(type);
-}
-
-TEST(DisableHierarchyPruningTest, Motorcycle) {
-  Costing::Type type = Costing::motorcycle;
-  doTest(type);
-}
-
-TEST(DisableHierarchyPruningTest, Taxi) {
-  Costing::Type type = Costing::taxi;
-  doTest(type);
-}
-
-TEST(DisableHierarchyPruningTest, None) {
-  Costing::Type type = Costing::none_;
-  doTest(type);
-}
+INSTANTIATE_TEST_SUITE_P(disable_hierarchy_pruning,
+                         HierarchyTest,
+                         ::testing::Values(Costing::auto_,
+                                           Costing::bus,
+                                           Costing::motor_scooter,
+                                           Costing::truck,
+                                           Costing::motorcycle,
+                                           Costing::taxi));
