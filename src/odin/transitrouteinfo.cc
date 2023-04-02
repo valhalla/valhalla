56716c46ff22653e6e88f83ae83078099fc44057
cvvergara <vicky@georepublic.de>
2023-04-02 11:17:10 -0600

    [transitrouteinfo] adusting code to inlined Get_String

--- src/odin/transitrouteinfo.cc
+++ src/odin/transitrouteinfo.cc
@@ -7,10 +7,13 @@ namespace valhalla {
 namespace odin {
 #ifdef LOGGING_LEVEL_TRACE
 std::string TransitRouteInfo::ToParameterString() const {
-  return "{ " +
-         Get_String(", ", onestop_id, block_id, trip_id, short_name, long_name, headsign, color,
-                    text_color, description, operator_onestop_id, operator_name, operator_url) +
-         " }";
+  std::string str;
+  str.reserve(256);
+  str += "{ ";
+  Get_String(str, ", ", onestop_id, block_id, trip_id, short_name, long_name, headsign, color,
+             text_color, description, operator_onestop_id, operator_name, operator_url);
+  str += " }";
+  return str;
 }
 #endif
 
