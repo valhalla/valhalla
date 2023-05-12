a437da40ea0404be25e230e1d1c5437bdb072c07
cvvergara <vicky@georepublic.de>
2023-05-12 12:37:56 -0600

    [test/sample.cc] Using std::filesystem

--- test/sample.cc
+++ test/sample.cc
@@ -244,9 +244,9 @@ TEST(Sample, store) {
   // empty file
   EXPECT_FALSE(s.store("/N00/N00E009.hgt", {}));
 
-  filesystem::remove("test/data/sample/N00/N00E009.hgt");
-  filesystem::remove("test/data/sample/N00/N00E005.hgt");
-  filesystem::remove("test/data/sample/N00/N00E005.hgt.gz");
+  std::filesystem::remove("test/data/sample/N00/N00E009.hgt");
+  std::filesystem::remove("test/data/sample/N00/N00E005.hgt");
+  std::filesystem::remove("test/data/sample/N00/N00E005.hgt.gz");
 }
 
 } // namespace
