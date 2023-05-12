a18de703ccf6c776e137389f390e1d55b7fc12b9
cvvergara <vicky@georepublic.de>
2023-05-12 12:37:56 -0600

    [src/mjolnir/valhalla_build_statistics.cc] Using std::filesystem

--- src/mjolnir/valhalla_build_statistics.cc
+++ src/mjolnir/valhalla_build_statistics.cc
@@ -25,17 +25,17 @@
 #include "baldr/graphreader.h"
 #include "baldr/nodeinfo.h"
 #include "baldr/tilehierarchy.h"
-#include "filesystem.h"
 #include "midgard/aabb2.h"
 #include "midgard/distanceapproximator.h"
 #include "midgard/logging.h"
 #include "midgard/pointll.h"
+#include <filesystem>
 
 using namespace valhalla::midgard;
 using namespace valhalla::baldr;
 using namespace valhalla::mjolnir;
 
-filesystem::path config_file_path;
+std::filesystem::path config_file_path;
 
 namespace {
 
@@ -597,8 +597,8 @@ bool ParseArguments(int argc, char* argv[]) {
     }
 
     if (result.count("config") &&
-        filesystem::is_regular_file(config_file_path =
-                                        filesystem::path(result["config"].as<std::string>()))) {
+        std::filesystem::is_regular_file(
+            config_file_path = std::filesystem::path(result["config"].as<std::string>()))) {
       return true;
     } else {
       std::cerr << "Configuration file is required\n\n" << options.help() << "\n\n";
