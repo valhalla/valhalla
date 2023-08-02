69f88fab1113dbef90cf5b813146aa1b8af939b0
Junzhen Lou <vesperlou@gmail.com>
2023-08-02 08:48:24 +0200

    add extra white space in docstrings

--- valhalla/mjolnir/landmark_builder.h
+++ valhalla/mjolnir/landmark_builder.h
@@ -97,6 +97,7 @@ public:
    * Create a new LandmarkDatabase object that connects to the SQLite landmark database
    * with the given `db_name`. The `read_only` parameter determines whether the
    * database connection is read-only or read-write.
+   *
    * @param db_name The file path of the SQLite database to connect to.
    * @param read_only Set to true to open the database in read-only mode, false for read-write.
    */
@@ -106,6 +107,7 @@ public:
    * Insert a new landmark into the database.
    * This function inserts a new landmark into the database with the given `name`, `type`,
    * `lng`, and `lat`. The landmark's primary key will be automatically assigned by the database.
+   *
    * @param name The name of the landmark.
    * @param type The type of the landmark (LandmarkType enum).
    * @param lng The longitude of the landmark.
@@ -119,7 +121,8 @@ public:
   /**
    * Retrieve a vector of landmarks from the database within the specified bounding box.
    * Bounding box is a rectangle area defined by min latitude, max latitude, min longitude, and max
-   * longitude.s
+   * longitude.
+   *
    * @param minLat The minimum latitude of the bounding box.
    * @param minLong The minimum longitude of the bounding box.
    * @param maxLat The maximum latitude of the bounding box.
@@ -136,6 +139,7 @@ public:
    * NOTE: The return size may be less than the size of the input if some of the provided
    * primary keys do not exist in the database. In such cases, the function will skip the missing IDs
    * without throwing an exception.
+   *
    * @param pkeys A vector of int64_t representing the primary keys of the landmarks to retrieve.
    * @return A vector of Landmark objects matching the given primary keys.
    */
@@ -151,6 +155,7 @@ protected:
  * This function reads landmark data from PBF files specified in the `input_files` vector.
  * It then parses the data to extract landmark nodes and stores the landmarks in the database.
  * The database is automatically built based on the configuration settings provided in `pt`.
+ *
  * @param pt The configuration settings for the landmark building process.
  * @param input_files A vector of file paths to the PBF input files.
  * @return True if landmarks were successfully built and inserted into the database, false otherwise.
