276eb1ceabe90af76f57baa5ef8bb5fe0e631b52
Greg Knisely <greg@hammerhead.io>
2023-08-10 20:14:04 -0400

    kLanguage removed.  moved kNone to 5 to avoid versioning of the tiles

--- valhalla/baldr/graphconstants.h
+++ valhalla/baldr/graphconstants.h
@@ -377,11 +377,10 @@ inline std::string to_string(Use u) {
 
 enum class TaggedValue : uint8_t { // must start at 1 due to nulls
   kLayer = 1,
-  kPronunciation = 2,
+  kLinguistic = 2,
   kBssInfo = 3,
   kLevel = 4,
   kLevelRef = 5,
-  kLanguage = 6,
   // we used to have bug when we encoded 1 and 2 as their ASCII codes, but not actual 1 and 2 values
   // see https://github.com/valhalla/valhalla/issues/3262
   kTunnel = static_cast<uint8_t>('1'),
@@ -390,11 +389,12 @@ enum class TaggedValue : uint8_t { // must start at 1 due to nulls
 };
 
 enum class PronunciationAlphabet : uint8_t {
-  kNone = 0,
+  // kNone = 0 has been deprecated as this introduced a bug while processing the linguistic records.
   kIpa = 1,
   kKatakana = 2,
   kJeita = 3,
-  kNtSampa = 4
+  kNtSampa = 4,
+  kNone = 5
 };
 // must start at 1 due to nulls
 // must start at 1 due to nulls
