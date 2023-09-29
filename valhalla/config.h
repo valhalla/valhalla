dbde116108f9db9fd1497ce58ec7672037b4af06
Janusz Spatz <janusz.spatz@mercedes-benz.com>
2023-09-29 11:32:42 +0000

    Add error handling and new exception to intercept

--- valhalla/config.h
+++ valhalla/config.h
@@ -40,7 +40,14 @@
 
 namespace valhalla {
 const boost::property_tree::ptree& config(const std::string&);
-}
+
+class ConfigUninitializedException : std::exception {
+public:
+  const char* what() const throw() {
+    return "Config singleton was not initialized before usage";
+  }
+};
+} // namespace valhalla
 
 namespace {
 struct config_singleton_t {
@@ -50,14 +57,23 @@ protected:
   config_singleton_t() = delete;
   config_singleton_t(const std::string& config_file_or_inline) {
     if (config_file_or_inline.empty()) {
-      throw std::runtime_error("no config provided");
+      throw valhalla::ConfigUninitializedException();
     }
 
-    if (std::filesystem::is_regular_file(config_file_or_inline)) {
-      rapidjson::read_json(config_file_or_inline, config_);
-    } else {
-      auto inline_config = std::stringstream(config_file_or_inline);
-      rapidjson::read_json(inline_config, config_);
+    try {
+      if (std::filesystem::is_regular_file(config_file_or_inline)) {
+        rapidjson::read_json(config_file_or_inline, config_);
+      } else {
+        auto inline_config = std::stringstream(config_file_or_inline);
+        rapidjson::read_json(inline_config, config_);
+      }
+    } catch (const std::filesystem::filesystem_error& e) {
+      if (e.code() == std::errc::filename_too_long) {
+        auto inline_config = std::stringstream(config_file_or_inline);
+        rapidjson::read_json(inline_config, config_);
+      } else {
+        throw e;
+      }
     }
   }
 
