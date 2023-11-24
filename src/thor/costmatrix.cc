764c376c02ca4fae5a16fe30dc86c1b907641cf7
cvvergara <vicky@erosion.dev>
2023-11-24 14:27:02 -0600

    [src/thor/costmatrix.cc] Fixing -Wreorder-ctor warning

--- src/thor/costmatrix.cc
+++ src/thor/costmatrix.cc
@@ -48,8 +48,8 @@ class CostMatrix::TargetMap : public robin_hood::unordered_map<uint64_t, std::ve
 CostMatrix::CostMatrix(const boost::property_tree::ptree& config)
     : max_reserved_labels_count_(config.get<uint32_t>("max_reserved_labels_count_bidir_dijkstras",
                                                       kInitialEdgeLabelCountBidirDijkstra)),
-      targets_{new TargetMap},
-      clear_reserved_memory_(config.get<bool>("clear_reserved_memory", false)) {
+      clear_reserved_memory_(config.get<bool>("clear_reserved_memory", false)), targets_{
+                                                                                    new TargetMap} {
   // Note, most things are being initialized in Initialize() or before
 }
 
