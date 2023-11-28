9495be1dcdc39dd5d1f5175bb6a8b8479d0bbf61
Nils <nilsnolde@proton.me>
2023-11-28 18:04:24 +0100

    cleanup ferry connection test after dave's recent fixes (#4416)

--- test/gurka/test_ferry_connections.cc
+++ test/gurka/test_ferry_connections.cc
@@ -230,10 +230,8 @@ TEST(Standalone, ReclassifyFerryUntagDestOnly) {
   // see https://github.com/valhalla/valhalla/issues/3941#issuecomment-1407713742
   // CD is destonly -> should untag BC, CD and take the faster path
   // EF is not destonly -> shouldn't untag FG and take the detour
-
-  // the X is bcs of https://github.com/valhalla/valhalla/issues/4164
   const std::string ascii_map = R"(
-    A--B--C--D------E--F-X-G--H--M
+    A--B--C--D------E--F---G--H--M
        |  |            |   |
        I--J            K---L
   )";
@@ -261,9 +259,7 @@ TEST(Standalone, ReclassifyFerryUntagDestOnly) {
         {"motorcycle", "yes"},
         {"route", "ferry"}}},
       {"EF", not_destonly},
-      {"FX", destonly}, // should stay destonly and low-class bcs connecting edge isn't and this will
-                        // be penalized
-      {"XG", destonly}, // should stay destonly and low-class bcs connecting edge isn't and this will
+      {"FG", destonly}, // should stay destonly and low-class bcs connecting edge isn't and this will
                         // be penalized
       {"FKLG", not_destonly},
       {"GH", not_destonly},
@@ -280,14 +276,9 @@ TEST(Standalone, ReclassifyFerryUntagDestOnly) {
   EXPECT_TRUE(std::get<1>(tagged)->classification() == valhalla::baldr::RoadClass::kPrimary);
 
   // see if FX & XG are still tagged and low class
-  const std::vector<std::pair<std::string, std::string>>& edges = {{"FX", "X"}, {"XG", "G"}};
-  for (const auto& edge : edges) {
-    std::string way, end_node;
-    std::tie(way, end_node) = edge;
-    auto untagged = gurka::findEdge(reader, layout, way, end_node);
-    EXPECT_TRUE(std::get<1>(untagged)->destonly()) << "Edge " + way + " should be destonly";
-    EXPECT_FALSE(std::get<1>(untagged)->classification() == valhalla::baldr::RoadClass::kPrimary);
-  }
+  auto untagged = gurka::findEdge(reader, layout, "FG", "G");
+  EXPECT_TRUE(std::get<1>(untagged)->destonly()) << "Edge FG should be destonly";
+  EXPECT_FALSE(std::get<1>(untagged)->classification() == valhalla::baldr::RoadClass::kPrimary);
 
   // see if FKLG is upclassed
   auto upclassed = gurka::findEdge(reader, layout, "FKLG", "G");
