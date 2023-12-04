42afb06762b92cecb79a01f3acc3998093eee90f
danthe42 <danthe666@gmail.com>
2023-12-04 16:14:28 +0100

    linking error possible fix

--- src/config.cc
+++ src/config.cc
@@ -1,5 +1,44 @@
 #include <mutex>
 #include "config.h"
+#include <filesystem>
+#include "baldr/rapidjson_utils.h"
+
+namespace {
+struct config_singleton_t {
+protected:
+  boost::property_tree::ptree config_;
+
+  config_singleton_t() = delete;
+
+  config_singleton_t(const std::string& config_file_or_inline) {
+    if (config_file_or_inline.empty()) {
+      throw valhalla::ConfigUninitializedException();
+    }
+
+    try {
+      if (std::filesystem::is_regular_file(config_file_or_inline)) {
+        rapidjson::read_json(config_file_or_inline, config_);
+      } else {
+        auto inline_config = std::stringstream(config_file_or_inline);
+        rapidjson::read_json(inline_config, config_);
+      }
+    } catch (const std::filesystem::filesystem_error& e) {
+      if (e.code() == std::errc::filename_too_long) {
+        auto inline_config = std::stringstream(config_file_or_inline);
+        rapidjson::read_json(inline_config, config_);
+      } else {
+        throw e;
+      }
+    }
+  }
+
+public:
+  config_singleton_t(config_singleton_t const&) = delete;
+  void operator=(const config_singleton_t&) = delete;
+  friend const boost::property_tree::ptree&
+  valhalla::config(const std::string& config_file_or_inline);
+};
+} // namespace
 
 namespace valhalla {
 
--- valhalla/config.h
+++ valhalla/config.h
@@ -1,11 +1,6 @@
 #pragma once
-#include <filesystem>
-#include <stdexcept>
 #include <string>
-
 #include <boost/property_tree/ptree.hpp>
-
-#include "baldr/rapidjson_utils.h"
 #include "valhalla.h"
 
 #define VALHALLA_STRINGIZE_NX(A) #A
@@ -39,7 +34,7 @@
 #define PACKAGE_VERSION VALHALLA_VERSION
 
 namespace valhalla {
-const boost::property_tree::ptree& config(const std::string&);
+const boost::property_tree::ptree& config(const std::string& config_file_or_inline = "");
 
 class ConfigUninitializedException : std::exception {
 public:
@@ -49,41 +44,3 @@ public:
 };
 } // namespace valhalla
 
-namespace {
-struct config_singleton_t {
-protected:
-  boost::property_tree::ptree config_;
-
-  config_singleton_t() = delete;
-  config_singleton_t(const std::string& config_file_or_inline) {
-    if (config_file_or_inline.empty()) {
-      throw valhalla::ConfigUninitializedException();
-    }
-
-    try {
-      if (std::filesystem::is_regular_file(config_file_or_inline)) {
-        rapidjson::read_json(config_file_or_inline, config_);
-      } else {
-        auto inline_config = std::stringstream(config_file_or_inline);
-        rapidjson::read_json(inline_config, config_);
-      }
-    } catch (const std::filesystem::filesystem_error& e) {
-      if (e.code() == std::errc::filename_too_long) {
-        auto inline_config = std::stringstream(config_file_or_inline);
-        rapidjson::read_json(inline_config, config_);
-      } else {
-        throw e;
-      }
-    }
-  }
-
-public:
-  config_singleton_t(config_singleton_t const&) = delete;
-  void operator=(const config_singleton_t&) = delete;
-  friend const boost::property_tree::ptree&
-  valhalla::config(const std::string& config_file_or_inline);
-};
-} // namespace
-namespace valhalla {
-  const boost::property_tree::ptree& config(const std::string& config_file_or_inline = "");
-} // namespace valhalla
