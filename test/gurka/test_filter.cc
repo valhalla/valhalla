5eadf37c0266448a9cf050edea21fe8420a75ebe
Nils <nilsnolde@proton.me>
2023-12-07 17:26:22 +0100

    more mainstream test setup (#4438)
    * more mainstream test setup
    
    * Update test_filter.cc
    
    ---------
    
    Co-authored-by: Greg Knisely <greg@hammerhead.io>

--- test/gurka/test_filter.cc
+++ test/gurka/test_filter.cc
@@ -4,19 +4,15 @@
 #include "gurka.h"
 #include "test/test.h"
 
-#if !defined(VALHALLA_SOURCE_DIR)
-#define VALHALLA_SOURCE_DIR
-#endif
-
 using namespace valhalla;
 using namespace valhalla::baldr;
 using namespace valhalla::gurka;
 
-valhalla::gurka::map the_map = {};
-
 class FilterData : public ::testing::Test {
 protected:
-  valhalla::gurka::map BuildPBF(const std::string& workdir) {
+  static gurka::map the_map;
+
+  static void SetUpTestSuite() {
     constexpr double gridsize_metres = 100;
 
     const std::string ascii_map = R"(
@@ -43,49 +39,20 @@ protected:
         {"BD", {{"highway", "footway"}}},
     };
 
-    if (!filesystem::exists(workdir)) {
-      bool created = filesystem::create_directories(workdir);
-      EXPECT_TRUE(created);
-    }
-
-    constexpr double gridsize = 100;
-
     const auto layout =
         gurka::detail::map_to_coordinates(ascii_map, gridsize_metres, {-82.68811, 40.22535});
 
-    auto pbf_filename = workdir + "/map.pbf";
-    detail::build_pbf(layout, ways, {}, {}, pbf_filename);
-
-    valhalla::gurka::map result;
-    result.nodes = layout;
-    return result;
+    the_map =
+        gurka::buildtiles(layout, ways, {}, {}, VALHALLA_BUILD_DIR "test/data/gurka_filter",
+                          {{"mjolnir.admin", {VALHALLA_SOURCE_DIR "test/data/language_admin.sqlite"}},
+                           {"mjolnir.include_pedestrian", "false"}});
   }
 };
 
+gurka::map FilterData::the_map = {};
+
 ///////////////////////////////////////////////////////////////////////////////
 TEST_F(FilterData, CheckStreetNamesAfterFilter) {
-
-  const std::string workdir = "test/data/gurka_filter";
-
-  if (!filesystem::exists(workdir)) {
-    bool created = filesystem::create_directories(workdir);
-    EXPECT_TRUE(created);
-  }
-
-  the_map = BuildPBF(workdir);
-
-  const std::string sqlite = {VALHALLA_SOURCE_DIR "test/data/language_admin.sqlite"};
-  the_map.config =
-      test::make_config(workdir,
-                        {{"mjolnir.admin", {VALHALLA_SOURCE_DIR "test/data/language_admin.sqlite"}},
-                         {"mjolnir.tile_dir", workdir + "/tiles"},
-                         {"mjolnir.include_pedestrian", "false"}});
-
-  std::vector<std::string> input_files = {workdir + "/map.pbf"};
-
-  build_tile_set(the_map.config, input_files, mjolnir::BuildStage::kInitialize,
-                 mjolnir::BuildStage::kCleanup, false);
-
   GraphReader graph_reader(the_map.config.get_child("mjolnir"));
 
   GraphId ABC_edge_id;
