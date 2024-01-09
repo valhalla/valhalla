825323a24fdb7981ea746b53b77ac134fcf1aeac
adrika.mukherjee <adrika.mukherjee@daimler.com>
2024-01-09 13:00:13 +0000

    modified dynamic cost

--- src/sif/dynamiccost.cc
+++ src/sif/dynamiccost.cc
@@ -138,6 +138,9 @@ BaseCostingOptionsConfig::BaseCostingOptionsConfig()
                                                                                   1.f},
       use_living_streets_{0.f, kDefaultUseLivingStreets, 1.f}, use_lit_{0.f, kDefaultUseLit, 1.f},
       closure_factor_{kClosureFactorRange}, exclude_unpaved_(false),
+      exclude_highways_(false),
+      exclude_tunnels_(false),
+      exclude_tolls_(false),
       exclude_cash_only_tolls_(false), include_hot_{false}, include_hov2_{false}, include_hov3_{
                                                                                       false} {
 }
@@ -453,6 +456,10 @@ void ParseBaseCostOptions(const rapidjson::Value& json,
 
   JSON_PBF_DEFAULT(co, cfg.exclude_cash_only_tolls_, json, "/exclude_cash_only_tolls",
                    exclude_cash_only_tolls);
+
+  JSON_PBF_DEFAULT(co, cfg.exclude_highways_, json, "/exclude_highways", exclude_highways);
+  JSON_PBF_DEFAULT(co, cfg.exclude_tunnels_, json, "/exclude_tunnels", exclude_tunnels);
+  JSON_PBF_DEFAULT(co, cfg.exclude_tolls_, json, "/exclude_tolls", exclude_tolls);
 
   // service_penalty
   JSON_PBF_RANGED_DEFAULT(co, cfg.service_penalty_, json, "/service_penalty", service_penalty);
