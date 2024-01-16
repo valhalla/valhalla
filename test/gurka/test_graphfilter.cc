0e14d9e578f7c14e57903e4e3d67ad51390c63d1
Greg Knisely <greg@hammerhead.io>
2024-01-16 12:55:33 -0500

    added test

---
+++ test/gurka/test_graphfilter.cc
@@ -0,0 +1,134 @@
+#include "baldr/graphreader.h"
+#include "mjolnir/util.h"
+
+#include "gurka.h"
+#include "test/test.h"
+
+using namespace valhalla;
+using namespace valhalla::baldr;
+using namespace valhalla::gurka;
+
+const std::string work_dir = {VALHALLA_BUILD_DIR "test/data/gurka_graphfilter"};
+
+TEST(Standalone, SimpleFilter) {
+  constexpr double gridsize_metres = 10;
+
+  const std::string ascii_map = R"(
+
+                     M              P         R              W    Y
+                     |              |         |              |    |
+                     |              |         |              |    |
+		A----B----C----D----E----F----G----H----I----J----K----L
+                          |    |         |         |    |         |
+                          |    |         Q----S----T    |         |
+                          N    O              |         V         X
+                                              |
+                                              U 
+  )";
+
+  const gurka::ways ways = {
+      {"ABCDEFGHIJKL", {{"highway", "residential"}, {"osm_id", "100"}, {"name", "East Fort Avenue"}}},
+      {"BM", {{"highway", "residential"}, {"osm_id", "101"}, {"name", "Hull Street"}}},
+      {"CN",
+       {{"highway", "residential"},
+        {"osm_id", "102"},
+        {"name", "Latrobe Park Terrace"},
+        {"oneway", "yes"}}},
+      {"DO", {{"highway", "footway"}, {"osm_id", "103"}, {"foot", "yes"}}},
+      {"EP",
+       {{"highway", "residential"},
+        {"osm_id", "104"},
+        {"name", "Cooksie Street"},
+        {"oneway", "yes"}}},
+      {"FQ", {{"highway", "footway"}, {"osm_id", "105"}, {"foot", "yes"}}},
+      {"QST", {{"highway", "footway"}, {"osm_id", "106"}, {"foot", "yes"}}},
+      {"SU", {{"highway", "footway"}, {"osm_id", "107"}, {"foot", "yes"}}},
+      {"GR", {{"highway", "residential"}, {"osm_id", "108"}, {"name", "Towson Street"}}},
+      {"HT", {{"highway", "footway"}, {"osm_id", "109"}, {"foot", "yes"}}},
+      {"IV", {{"highway", "footway"}, {"osm_id", "110"}, {"foot", "yes"}}},
+      {"JW", {{"highway", "residential"}, {"osm_id", "111"}, {"name", "Richardson Street"}}},
+      {"YKX", {{"highway", "residential"}, {"osm_id", "112"}, {"name", "Andre Street"}}},
+  };
+
+  auto layout = gurka::detail::map_to_coordinates(ascii_map, gridsize_metres, {-76.59223, 39.26825});
+  auto map =
+      gurka::buildtiles(layout, ways, {}, {}, work_dir,
+                        {{"mjolnir.admin", {VALHALLA_SOURCE_DIR "test/data/language_admin.sqlite"}},
+                         {"mjolnir.include_pedestrian", "false"}});
+
+  GraphReader graph_reader(map.config.get_child("mjolnir"));
+
+  GraphId DO_edge_id;
+  const DirectedEdge* DO_edge = nullptr;
+  GraphId OD_edge_id;
+  const DirectedEdge* OD_edge = nullptr;
+  std::tie(DO_edge_id, DO_edge, OD_edge_id, OD_edge) =
+      findEdge(graph_reader, map.nodes, "DO", "O", baldr::GraphId{});
+  EXPECT_EQ(DO_edge, nullptr);
+  EXPECT_EQ(OD_edge, nullptr);
+}
+
+TEST(Standalone, AccessFilter) {
+
+  constexpr double gridsize_metres = 10;
+
+  const std::string ascii_map = R"(
+
+		J----K----L----M----------------------N
+                |    |    |    |                      |
+                |    H    I    |                      |
+                |    |    |    |                      |
+		A----B----C----D----E------------F----G
+                |              |     \          /     |
+                |              |      \        /      |
+                O              P       Q------R       S                  
+  )";
+
+  const gurka::ways ways = {
+      {"JKLMN",
+       {{"highway", "secondary"},
+        {"osm_id", "100"},
+        {"bicycle", "no"},
+        {"foot", "no"},
+        {"name", "Vondellaan"},
+        {"oneway", "yes"}}},
+      {"ABCDEFG", {{"highway", "cycleway"}, {"osm_id", "101"}, {"oneway", "yes"}}},
+      {"JAO",
+       {{"highway", "residential"},
+        {"osm_id", "102"},
+        {"bicycle", "no"},
+        {"foot", "no"},
+        {"name", "Croesestraat"},
+        {"oneway", "yes"},
+        {"oneway:bicycle", "no"}}},
+      {"BHK", {{"highway", "cycleway"}, {"osm_id", "103"}, {"oneway", "yes"}}},
+      {"CIL", {{"highway", "footway"}, {"osm_id", "104"}}},
+      {"MDP", {{"highway", "service"}, {"osm_id", "105"}, {"access", "private"}}},
+      {"EQRF",
+       {{"highway", "service"}, {"osm_id", "106"}, {"access", "delivery"}, {"oneway", "yes"}}},
+      {"NG", {{"highway", "unclassified"}, {"osm_id", "107"}, {"name", "Oosterkade"}}},
+      {"GS", {{"highway", "cycleway"}, {"osm_id", "108"}}},
+  };
+
+  const gurka::nodes nodes = {
+      {"H", {{"highway", "traffic_signals"}}},
+      {"I", {{"highway", "traffic_signals"}, {"traffic_signals:direction", "backward"}}},
+  };
+
+  auto layout = gurka::detail::map_to_coordinates(ascii_map, gridsize_metres, {5.11668, 52.07826});
+  auto map =
+      gurka::buildtiles(layout, ways, nodes, {}, work_dir,
+                        {{"mjolnir.admin", {VALHALLA_SOURCE_DIR "test/data/language_admin.sqlite"}},
+                         {"mjolnir.include_pedestrian", "false"}});
+
+  GraphReader graph_reader(map.config.get_child("mjolnir"));
+
+  GraphId LC_edge_id;
+  const DirectedEdge* LC_edge = nullptr;
+  GraphId CL_edge_id;
+  const DirectedEdge* CL_edge = nullptr;
+  std::tie(LC_edge_id, LC_edge, CL_edge_id, CL_edge) =
+      findEdge(graph_reader, map.nodes, "LC", "C", baldr::GraphId{});
+  EXPECT_EQ(LC_edge, nullptr);
+  EXPECT_EQ(CL_edge, nullptr);
+}
