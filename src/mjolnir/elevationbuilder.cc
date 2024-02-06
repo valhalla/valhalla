c3af5974b534b02e53a214d85d2e89f2a71bc2df
David Nesbitt <dnesbitt61@gmail.com>
2024-02-06 15:09:54 -0500

    Revert back to debug log statement for heights when elevation error occurs.

--- src/mjolnir/elevationbuilder.cc
+++ src/mjolnir/elevationbuilder.cc
@@ -100,7 +100,7 @@ std::vector<int8_t> encode_btf_elevation(const std::unique_ptr<valhalla::skadi::
     for (size_t i = 1; i < heights.size(); i++) {
       auto d = std::abs(heights[i] - heights[i - 1]);
       diff = d < diff ? diff : d;
-      LOG_INFO("  " + std::to_string(heights[i]));
+      LOG_DEBUG("  " + std::to_string(heights[i]));
     }
     LOG_WARN("BTF edge elevation wayid = " + std::to_string(wayid) + " exceeds difference with " +
              std::to_string(diff) + " meters.");
