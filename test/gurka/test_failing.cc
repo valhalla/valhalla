00e58496ba5335b50e3d2911183fddcdaf81cf9e
nilsnolde <nilsnolde@proton.me>
2024-03-15 19:33:49 +0100

    clearer now

--- test/gurka/test_failing.cc
+++ test/gurka/test_failing.cc
@@ -7,10 +7,10 @@ using namespace valhalla;
 
 TEST(StandAlone, CostMatrixTrivialRoutes) {
   const std::string ascii_map = R"(
-    A-2---1-B
+    A-1---2-B
   )";
   const gurka::ways ways = {
-      {"AB", {{"highway", "residential"}, {"maxspeed:forward", "100"}, {"maxspeed:backward", "10"}}},
+      {"AB", {{"highway", "residential"}, {"maxspeed:forward", "10"}, {"maxspeed:backward", "100"}}},
   };
   const auto layout = gurka::detail::map_to_coordinates(ascii_map, 100);
   auto map =
@@ -19,7 +19,7 @@ TEST(StandAlone, CostMatrixTrivialRoutes) {
   {
     auto matrix = gurka::do_action(valhalla::Options::sources_to_targets, map, {"1"}, {"2"}, "auto");
     EXPECT_EQ(matrix.matrix().distances(0), 400);
-    // this fails because it's getting the wrong directed edge:
+    // this fails because it's getting the wrong directed edge with 100 kmh:
     // https://github.com/valhalla/valhalla/issues/4433
     EXPECT_NEAR(matrix.matrix().times(0), 144.f, 0.1);
   }
