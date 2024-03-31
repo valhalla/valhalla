6c874a8880cbde1d71859aea8e8c1e4d83532f0b
cvvergara <vicky@erosion.dev>
2024-03-31 09:37:16 -0600

    (test/utrecht.cc) remove defined but not used function

--- test/utrecht.cc
+++ test/utrecht.cc
@@ -35,13 +35,6 @@ const auto node_predicate = [](const OSMWayNode& a, const OSMWayNode& b) {
   return a.node.osmid_ < b.node.osmid_;
 };
 
-OSMNode GetNode(uint64_t node_id, sequence<OSMWayNode>& way_nodes) {
-  auto found = way_nodes.find({node_id}, node_predicate);
-  if (found == way_nodes.end())
-    throw std::runtime_error("Couldn't find node: " + std::to_string(node_id));
-  return (*found).node;
-}
-
 auto way_predicate = [](const OSMWay& a, const OSMWay& b) { return a.osmwayid_ < b.osmwayid_; };
 
 OSMWay GetWay(uint32_t way_id, sequence<OSMWay>& ways) {
