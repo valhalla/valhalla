62b1a34b5b5f0d4d2fff02115a51bbede3e45583
cvvergara <vicky@erosion.dev>
2024-04-07 20:09:43 -0600

    (test_gtfs.cc) fix W set but unused

--- test/gurka/test_gtfs.cc
+++ test/gurka/test_gtfs.cc
@@ -13,6 +13,12 @@
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
 
+/*
+   The following file has warning:
+   third_party/just_gtfs/include/just_gtfs/just_gtfs.h
+   Therefore this file has warnings
+   TODO third_party header shuold be treated a system headers??
+*/
 using point_type = boost::geometry::model::d2::point_xy<double>;
 using polygon_type = boost::geometry::model::polygon<point_type>;
 using boost::geometry::within;
@@ -575,7 +581,7 @@ TEST(GtfsExample, MakeProto) {
   std::vector<float> last_dest_dist_traveled;
 
   // get the full shape length
-  float shape_length = 0;
+  [[maybe_unused]] float shape_length = 0;
 
   auto layout = create_layout();
   const std::vector<PointLL> transit_lls{layout["1"], layout["a"], layout["b"],
