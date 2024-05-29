1b0305fd7b7e8e716a2eeaf60fb13f8ef3a48a0d
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2024-05-29 09:15:10 +0200

    remove whitespace

--- src/baldr/directededge.cc
+++ src/baldr/directededge.cc
@@ -352,7 +352,6 @@ void DirectedEdge::set_reverseaccess(const uint32_t modes) {
 
 // Sets the average speed in KPH.
 void DirectedEdge::set_speed(const uint32_t speed) {
-
   if (speed > kMaxAssumedSpeed) {
     LOG_WARN("Exceeding maximum.  Average speed: " + std::to_string(speed));
     speed_ = kMaxAssumedSpeed;
