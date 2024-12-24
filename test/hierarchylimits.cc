b149942bd3971cf7ba79b0e9b324493834158535
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2024-12-24 14:30:10 +0100

    format again

--- test/hierarchylimits.cc
+++ test/hierarchylimits.cc
@@ -39,10 +39,10 @@ struct HierarchyLimitsTestParams {
                             bool use_pbf,
                             std::unordered_map<std::string, std::string> overrides = {})
       : request(req), cfg(make_test_config(std::move(overrides))), hierarchy_limits_config_path(""),
-        expected_hierarchy_limits(hl), pbf(use_pbf) {};
+        expected_hierarchy_limits(hl), pbf(use_pbf){};
   HierarchyLimitsTestParams(std::unordered_map<std::string, std::string> overrides,
                             std::string& config_path)
-      : cfg(make_test_config(std::move(overrides))), hierarchy_limits_config_path(config_path) {};
+      : cfg(make_test_config(std::move(overrides))), hierarchy_limits_config_path(config_path){};
 
   std::string request;
   boost::property_tree::ptree cfg;
