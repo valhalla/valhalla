5ba4cb1c462e903141a0fabb72c2a2e8885fb7e0
Eike Send <mail@eike.se>
2025-01-03 18:30:23 +0100

    Simplify logic of #Allowed and #IsAccessible in DynamicCost

--- valhalla/sif/dynamiccost.h
+++ valhalla/sif/dynamiccost.h
@@ -346,8 +346,7 @@ public:
         ((disallow_mask & kDisallowSimpleRestriction) && edge->restrictions()) ||
         ((disallow_mask & kDisallowShortcut) && edge->is_shortcut());
     return accessible && !assumed_restricted &&
-           (edge->use() != baldr::Use::kConstruction ||
-            (ignore_construction_ && edge->use() == baldr::Use::kConstruction));
+           (edge->use() != baldr::Use::kConstruction || ignore_construction_);
   }
 
   /**
@@ -361,11 +360,11 @@ public:
     // you have forward access for the mode you care about
     // you dont care about what mode has access so long as its forward
     // you dont care about the direction the mode has access to
+    // AND (it is not a construction site OR you don't care about construction sites)
     return (((edge->forwardaccess() & access_mask_) ||
              (ignore_access_ && (edge->forwardaccess() & baldr::kAllAccess)) ||
              (ignore_oneways_ && (edge->reverseaccess() & access_mask_))) &&
-            (edge->use() != baldr::Use::kConstruction)) ||
-           (ignore_construction_ && edge->use() == baldr::Use::kConstruction);
+           (edge->use() != baldr::Use::kConstruction || ignore_construction_));
   }
 
   inline virtual bool ModeSpecificAllowed(const baldr::AccessRestriction&) const {
