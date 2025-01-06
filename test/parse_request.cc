4bdb59b822ab9c1f8642b3a028778b4042d9f4d0
Frank Elsinga <frank@elsinga.de>
2025-01-06 16:53:14 +0100

    tested a hypothesis how parsing works

--- test/parse_request.cc
+++ test/parse_request.cc
@@ -1793,12 +1793,13 @@ TEST(ParseRequest, test_transport_type) {
   costing = Costing::bicycle;
   transport_type_key = "bicycle_type";
   for (const auto& transport_type_value : {"road", "cross", "hybrid", "mountain"}) {
+    /// parsing does not change the caseness of the parameters, this has to be handled later
     test_transport_type_parsing(costing, transport_type_key, transport_type_value,
                                 transport_type_value);
     std::string uppercased_transport_type_value{transport_type_value};
     uppercased_transport_type_value[0] = std::toupper(uppercased_transport_type_value[0]);
     test_transport_type_parsing(costing, transport_type_key, uppercased_transport_type_value,
-                                transport_type_value);
+                                uppercased_transport_type_value);
   }
 }
 
