48cca04005354aa26e4699acdec2be9708798a9d
Kevin Kreiser <kevinkreiser@gmail.com>
2025-01-21 11:57:35 -0500

    Update test_route_with_narrative_languages.cc

--- test/gurka/test_route_with_narrative_languages.cc
+++ test/gurka/test_route_with_narrative_languages.cc
@@ -61,6 +61,7 @@ const std::vector<std::pair<std::string, std::string>> RouteWithNarrativeLanguag
      {"el", "Στρίψτε δεξιά στη Main Street."},
      {"el-GR", "Στρίψτε δεξιά στη Main Street."},
      {"en-AU", "Turn right onto Main Street."},
+     {"au", "Turn right onto Main Street."},
      {"en-GB", "Turn right onto Main Street."},
      {"en", "Turn right onto Main Street."},
      {"en-US", "Turn right onto Main Street."},
