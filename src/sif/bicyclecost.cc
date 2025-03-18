470e9f0651c65d1659603cb5e3fdc350a13fb3c5
Frank Elsinga <frank@elsinga.de>
2025-03-18 14:06:26 +0100

    lowercased the `bicycle_type`
    Co-authored-by: Kevin Kreiser <kevinkreiser@gmail.com>

--- src/sif/bicyclecost.cc
+++ src/sif/bicyclecost.cc
@@ -446,7 +446,9 @@ BicycleCost::BicycleCost(const Costing& costing)
   get_base_costs(costing);
 
   // Get the bicycle type - enter as string and convert to enum
-  const std::string& bicycle_type = costing_options.transport_type();
+  std::string bicycle_type = costing_options.transport_type();
+  std::transform(bicycle_type.begin(), bicycle_type.end(), bicycle_type.begin(),
+    [](unsigned char c){ return std::tolower(c); });
   if (bicycle_type == "cross" || bicycle_type == "Cross") {
     type_ = BicycleType::kCross;
   } else if (bicycle_type == "road" || bicycle_type == "Road") {
