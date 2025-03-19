0ef992daf40515fdb7d25e358f87cfabad7a595a
mohammad <mohamedwork216@gmail.com>
2025-03-19 22:42:47 +0200

    fix: now __func__ inside the scoped timer is translated into function name instead of operator()

--- src/mjolnir/scoped_timer.h
+++ src/mjolnir/scoped_timer.h
@@ -12,7 +12,7 @@ namespace mjolnir {
 #define SCOPED_TIMER()                                                                               \
   auto _scoped_timer_start_##__COUNTER__ = std::chrono::high_resolution_clock::now();                \
   auto _scoped_timer_finally_##__COUNTER__ =                                                         \
-      valhalla::midgard::make_finally([_scoped_timer_start_##__COUNTER__]() {                        \
+      valhalla::midgard::make_finally([_scoped_timer_start_##__COUNTER__, func_name = __func__]() {  \
         auto _scoped_timer_end = std::chrono::high_resolution_clock::now();                          \
         auto _scoped_timer_duration = std::chrono::duration_cast<std::chrono::seconds>(              \
                                           _scoped_timer_end - _scoped_timer_start_##__COUNTER__)     \
@@ -22,7 +22,7 @@ namespace mjolnir {
         size_t len = valhalla_dir_str.length();                                                      \
         std::string relative_file_path = file_path.substr(len + 1);                                  \
         valhalla::midgard::logging::Log(std::string(relative_file_path) +                            \
-                                            "::" + std::string(__func__) + " took " +                \
+                                            "::" + std::string(func_name) + " took " +               \
                                             std::to_string(_scoped_timer_duration) + "s",            \
                                         " [TIMING] ");                                               \
       })
