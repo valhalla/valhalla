b663213bad1283a830fcbe8b4ab926426beb9447
Lajos Kovács <lajos.kovacs@nng.com>
2025-04-24 14:11:49 +0200

    Fix tests

--- src/thor/route_matcher.cc
+++ src/thor/route_matcher.cc
@@ -55,23 +55,24 @@ float length_comparison(const float length, const bool exact_match) {
 }
 
 // check if the intermediate shape points are also on the edge
-bool MatchShape(const graph_tile_ptr& tile,
-                const DirectedEdge* de,
-                const google::protobuf::RepeatedPtrField<valhalla::Location>& shape,
-                uint32_t from,
-                uint32_t to) {
+bool check_shape(const graph_tile_ptr& tile,
+                 const DirectedEdge* de,
+                 const google::protobuf::RepeatedPtrField<valhalla::Location>& shape,
+                 uint32_t from,
+                 uint32_t to) {
   if (to - from == 1 && de->length() == 0) {
     return true;
   }
   const auto& edgeinfo = tile->edgeinfo(de);
   const auto& edge_shape = edgeinfo.shape();
-  if (edge_shape.size() != to - from + 1) {
+  int32_t i = edge_shape.size() - (to - from);
+  if (i < 1 || from > 0 && i != 1) {
     return false;
   }
   bool forward = de->forward();
-  for (uint32_t i = 1, j = from + 1; j < to; i++, j++) {
-    if (!to_ll(shape.Get(j).ll())
-             .ApproximatelyEqual(forward ? edge_shape[i] : edge_shape[edge_shape.size() - 1 - i])) {
+  for (uint32_t j = from + 1; j < to; i++, j++) {
+    const uint32_t shape_idx = forward ? i : edge_shape.size() - 1 - i;
+    if (!to_ll(shape.Get(j).ll()).ApproximatelyEqual(edge_shape[shape_idx])) {
       return false;
     }
   }
@@ -199,7 +200,7 @@ bool expand_from_node(const mode_costing_t& mode_costing,
       // Found a match if shape equals directed edge LL within tolerance
       if (to_ll(shape.Get(index).ll()).ApproximatelyEqual(de_end_ll) &&
           de->length() < length_comparison(length, true) &&
-          MatchShape(tile, de, shape, correlated_index, index)) {
+          check_shape(tile, de, shape, correlated_index, index)) {
 
         // Figure out what time it is right now, the first iteration is a no-op
         auto offset_time_info = nodeinfo
@@ -385,7 +386,7 @@ bool RouteMatcher::FormPath(const sif::mode_costing_t& mode_costing,
     midgard::PointLL de_end_ll = end_node_tile->get_node_ll(de->endnode());
 
     // Initialize indexes and shape
-    size_t index = 0;
+    size_t index = 1;
     float length = 0.0f;
     float de_remaining_length = de->length() * (1 - edge.percent_along());
     float de_length = length_comparison(de_remaining_length, true);
@@ -405,7 +406,8 @@ bool RouteMatcher::FormPath(const sif::mode_costing_t& mode_costing,
 
       // Check if shape is within tolerance at the end node
       if (to_ll(options.shape(index).ll()).ApproximatelyEqual(de_end_ll) &&
-          de_remaining_length < length_comparison(length, true)) {
+          de_remaining_length < length_comparison(length, true) &&
+          check_shape(begin_edge_tile, de, options.shape(), 0, index)) {
 
         // Figure out what time it is right now, the first iteration is a no-op
         auto offset_time_info = nodeinfo
