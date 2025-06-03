c89d17b38276a47754cb937eb36e1a31e09e6f44
mohammad <mohamedwork216@gmail.com>
2025-06-03 20:00:44 +0300

    refator TileManifest::ToString() to use rapidjson instead to get rid of graphid::json()

--- src/baldr/graphid.cc
+++ src/baldr/graphid.cc
@@ -4,20 +4,6 @@
 namespace valhalla {
 namespace baldr {
 
-// The json representation of the Id
-// used in valhalla/mjolnir/util.h
-json::Value GraphId::json() const {
-  if (Is_Valid()) {
-    return json::map({
-        {"level", static_cast<uint64_t>(level())},
-        {"tile_id", static_cast<uint64_t>(tileid())},
-        {"id", static_cast<uint64_t>(id())},
-        {"value", value},
-    });
-  }
-  return static_cast<std::nullptr_t>(nullptr);
-}
-
 // the json representation of the Id
 void GraphId::json(rapidjson::writer_wrapper_t& writer) const {
   if (Is_Valid()) {
