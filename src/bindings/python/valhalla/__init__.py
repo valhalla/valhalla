4e494c4db55878088d4d767be489fbcd14c3083c
Nils <nilsnolde@proton.me>
2025-06-10 23:48:08 +0200

    fix: some failures after merging python packaging PR (#5304)

--- src/bindings/python/valhalla/__init__.py
+++ src/bindings/python/valhalla/__init__.py
@@ -6,6 +6,11 @@ except ModuleNotFoundError:
     from _valhalla import *
 from .actor import Actor
 from .config import get_config, get_help
-from .__version__ import __version__
+
+# if run from CMake, Docker or tests
+try:
+    from .__version__ import __version__
+except ModuleNotFoundError:
+    __version__ = "undefined"
 
 PYVALHALLA_DIR = Path(__file__).parent.resolve()
