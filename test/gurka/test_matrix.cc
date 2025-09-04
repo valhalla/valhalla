98925ab2e2df90dc008086c2bd89c2aedf78b9e2
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2025-09-04 16:56:09 +0200

    CostMatrix: correctly penalize candidates based on distance to projected point (#5376)

--- test/gurka/test_matrix.cc
+++ test/gurka/test_matrix.cc
@@ -1139,3 +1139,33 @@ TEST_P(TestConnectionCheck, MultipleTrivialRoutes) {
 }
 
 INSTANTIATE_TEST_SUITE_P(connection_check, TestConnectionCheck, ::testing::Values("1", "0"));
+
+TEST(StandAlone, TrivialKeepExpanding) {
+  // target candidates includes AB but should be penalized
+  // so that path 1B, BC, Cx has less cost than the trivial one
+  const std::string ascii_map = R"(
+    A---1-----------B
+                    |
+                    |
+                2   |
+    D-----------x---C
+  )";
+  // clang-format off
+  const gurka::ways ways = {
+      {"AB", {{"highway", "residential"}}}, 
+      {"BC", {{"highway", "residential"}}}, 
+      {"CD", {{"highway", "residential"}}}, 
+  };
+  // clang-format on
+
+  const auto layout = gurka::detail::map_to_coordinates(ascii_map, 25);
+
+  auto map = gurka::buildtiles(layout, ways, {}, {},
+                               VALHALLA_BUILD_DIR "test/data/costmatrix_keep_expanding",
+                               {{"thor.costmatrix.check_reverse_connection", "1"}});
+
+  auto result = gurka::do_action(valhalla::Options::sources_to_targets, map, {"1"}, {"2"}, "auto",
+                                 {{"/targets/0/radius", "80"}}, nullptr);
+
+  EXPECT_EQ(result.matrix().distances(0), 500);
+}
