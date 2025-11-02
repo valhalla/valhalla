5e4952130873db68a6a5d5197ae80529d3876d75
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2025-11-02 09:33:50 +0100

    fix warning

--- valhalla/baldr/rapidjson_utils.h
+++ valhalla/baldr/rapidjson_utils.h
@@ -57,14 +57,14 @@ get_optional(V&& v, const char* source) {
   // if we dont have this key bail
   auto* ptr = rapidjson::Pointer{source}.Get(std::forward<V>(v));
   if (!ptr) {
-    return boost::none;
+    return boost::optional<T>{};
   }
   // if its the exact right type give it back
   if (ptr->template Is<T>()) {
     return ptr->template Get<T>();
   }
   // give up
-  return boost::none;
+  return boost::optional<T>{};
 }
 
 // if you do want an arithmetic type dont try lexical casting as a last resort
@@ -74,7 +74,7 @@ get_optional(V&& v, const char* source) {
   // if we dont have this key bail
   auto* ptr = rapidjson::Pointer{source}.Get(std::forward<V>(v));
   if (!ptr) {
-    return boost::none;
+    return boost::optional<T>{};
   }
   // if its the exact right type give it back
   if (ptr->template Is<T>()) {
@@ -107,7 +107,7 @@ get_optional(V&& v, const char* source) {
     return static_cast<T>(ptr->GetDouble());
   }
   // give up
-  return boost::none;
+  return boost::optional<T>{};
 }
 
 template <typename T, typename V> inline T get(V&& v, const char* source, const T& t) {
