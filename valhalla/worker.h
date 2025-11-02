9a5bce70e7ab44be9330c1cd51f19a24a51226bc
Nils <nilsnolde@proton.me>
2025-11-02 17:39:04 +0100

    fix: add tiff mime type (#5665)

--- valhalla/worker.h
+++ valhalla/worker.h
@@ -88,6 +88,7 @@ const content_type JSON_MIME{"Content-type", "application/json;charset=utf-8"};
 const content_type JS_MIME{"Content-type", "application/javascript;charset=utf-8"};
 const content_type PBF_MIME{"Content-type", "application/x-protobuf"};
 const content_type GPX_MIME{"Content-type", "application/gpx+xml;charset=utf-8"};
+const content_type TIFF_MIME("Content-type", "image/tiff");
 } // namespace worker
 
 prime_server::worker_t::result_t to_response(const std::string& data,
