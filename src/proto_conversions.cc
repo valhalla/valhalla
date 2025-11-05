046bc65bc277767e234b10d8149c73da280fa58b
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2025-11-05 08:52:18 +0100

    proto rename

--- src/proto_conversions.cc
+++ src/proto_conversions.cc
@@ -451,8 +451,8 @@ const std::string& Expansion_EdgeStatus_Enum_Name(const Expansion_EdgeStatus sta
 bool Options_ReverseTimeTracking_Enum_Parse(const std::string& strategy,
                                             Options::ReverseTimeTracking* f) {
   static const std::unordered_map<std::string, Options::ReverseTimeTracking> strategies{
-      {"disabled", Options_ReverseTimeTracking_disabled},
-      {"heuristic", Options_ReverseTimeTracking_heuristic},
+      {"disabled", Options::rtt_disabled},
+      {"heuristic", Options::rtt_heuristic},
   };
   auto i = strategies.find(strategy);
   if (i == strategies.cend())
