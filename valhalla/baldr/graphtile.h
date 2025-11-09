35dd33b4d40a39a3f2fcbd622ce10ce3f4beabb5
Stefan Kizim <stsiapan.kizim@gmail.com>
2025-11-09 14:01:21 +0100

    feat: Make possible to use `-DCMAKE_UNITY_BUILD=ON` (#5691)

--- valhalla/baldr/graphtile.h
+++ valhalla/baldr/graphtile.h
@@ -581,6 +581,33 @@ public:
   const TransitRoute* GetTransitRoute(const uint32_t idx) const;
 
   /**
+   * Get an operator Id from a map of operator strings or add it to the map.
+   * @param routeid The route ID to look up
+   * @param operators Map of operator names to IDs (will be modified if new operator found)
+   * @return The operator ID, or 0 if there is no operator for the route.
+   */
+  uint32_t GetTransitOperatorId(uint32_t routeid,
+                                std::unordered_map<std::string, uint32_t>& operators) const {
+    const TransitRoute* transit_route = GetTransitRoute(routeid);
+
+    // Test if the transit operator changed
+    if (transit_route && transit_route->op_by_onestop_id_offset()) {
+      // Get the operator name and look up in the operators map
+      std::string operator_name = GetName(transit_route->op_by_onestop_id_offset());
+      auto operator_itr = operators.find(operator_name);
+      if (operator_itr == operators.end()) {
+        // Operator not found - add to the map
+        const uint32_t id = operators.size() + 1;
+        operators[operator_name] = id;
+        return id;
+      } else {
+        return operator_itr->second;
+      }
+    }
+    return 0;
+  }
+
+  /**
    * Get the transit schedule given its schedule index.
    * @param   idx     Schedule index within the tile.
    * @return  Returns a pointer to the transit schedule information. Returns
