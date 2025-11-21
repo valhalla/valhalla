a806dc1e3d38aa456f1a1cb7eae3583b346bc7ba
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2025-11-21 09:12:22 +0100

    parse concurrency arg

--- src/valhalla_benchmark_loki.cc
+++ src/valhalla_benchmark_loki.cc
@@ -215,7 +215,7 @@ int main(int argc, char** argv) {
     options.parse_positional({"input_files"});
     options.positional_help("LOCATIONS.TXT");
     auto result = options.parse(argc, argv);
-    if (!parse_common_args(program, options, result, &config, "loki.logging"))
+    if (!parse_common_args(program, options, result, &config, "loki.logging", true))
       return EXIT_SUCCESS;
 
     if (!result.count("input_files")) {
