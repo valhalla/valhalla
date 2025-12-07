228a812ff51df3c2850ef7d798715377900e3ddf
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2025-12-07 19:13:03 +0100

    format

--- src/thor/costmatrix.cc
+++ src/thor/costmatrix.cc
@@ -120,9 +120,9 @@ CostMatrix::CostMatrix(const boost::property_tree::ptree& config)
       max_iterations_(
           std::max(config.get<uint32_t>("costmatrix.max_iterations", kDefaultMaxIterations),
                    static_cast<uint32_t>(1))),
-      access_mode_(kAutoAccess), mode_(travel_mode_t::kDrive), locs_count_{0, 0},
-      locs_remaining_{0, 0}, current_pathdist_threshold_(0), targets_{new ReachedMap},
-      sources_{new ReachedMap} {
+      access_mode_(kAutoAccess),
+      mode_(travel_mode_t::kDrive), locs_count_{0, 0}, locs_remaining_{0, 0},
+      current_pathdist_threshold_(0), targets_{new ReachedMap}, sources_{new ReachedMap} {
 }
 
 CostMatrix::~CostMatrix() {
