30c4d2d441ff4a7a7f56ef62378b115dd252392a
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2025-12-19 22:27:27 +0100

    more tests

--- test/gurka/test_multimodal_drive.cc
+++ test/gurka/test_multimodal_drive.cc
@@ -1,5 +1,6 @@
 #include "gurka.h"
 #include "test.h"
+#include "tyr/actor.h"
 #include "valhalla/baldr/graphconstants.h"
 
 #include <gtest/gtest.h>
@@ -32,6 +33,32 @@ TEST(Standalone, ParseParking) {
   EXPECT_EQ(nodeinfo->access(), 2047)
       << "Expected vehicular and pedestrian access , got " << nodeinfo->access();
 
-  // auto result = gurka::do_action(valhalla::Options::route, map, {"A", "D"}, "multimodal_drive",
-  // {});
+  auto result = gurka::do_action(valhalla::Options::route, map, {"A", "D"}, "multimodal_drive", {});
+
+  EXPECT_EQ(result.directions().routes(0).legs(0).maneuver_size(), 3);
+  EXPECT_EQ(result.directions().routes(0).legs(0).maneuver(1).type(),
+            DirectionsLeg_Maneuver::kParkVehicle);
+
+  // travel mode changes along the route
+  EXPECT_EQ(result.directions().routes(0).legs(0).maneuver(0).travel_mode(), TravelMode::kDrive);
+  EXPECT_EQ(result.directions().routes(0).legs(0).maneuver(2).travel_mode(), TravelMode::kPedestrian);
+}
+
+TEST(Standalone, Utrecht) {
+  const auto utrecht_conf = test::make_config(VALHALLA_BUILD_DIR "test/data/utrecht_tiles");
+  auto reader = test::make_clean_graphreader(utrecht_conf.get_child("mjolnir"));
+  tyr::actor_t actor(utrecht_conf);
+
+  // Request the same tile without shortcuts (default)
+  std::string request =
+      R"({"locations": [{"lat": 52.105031, "lon": 5.077844}, {"lat": 52.0942903, "lon": 5.1300778}], "costing": "multimodal_drive", "format": "pbf"})";
+  auto r = actor.route(request);
+  valhalla::Api response;
+
+  response.ParseFromString(r);
+  ASSERT_EQ(response.directions().routes_size(), 1);
+  EXPECT_EQ(response.directions().routes(0).legs(0).maneuver(0).travel_mode(), TravelMode::kDrive);
+  EXPECT_EQ(response.directions().routes(0).legs(0).maneuver(20).travel_mode(),
+            TravelMode::kPedestrian);
+  actor.cleanup();
 }
