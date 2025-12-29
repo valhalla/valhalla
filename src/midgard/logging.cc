48366ab7bd8f5d6e64e814cb652f933355e80d6d
Eric Lemesre <ericlemesre@gmail.com>
2025-12-29 14:38:48 +0100

    Fix truncate error

--- src/midgard/logging.cc
+++ src/midgard/logging.cc
@@ -38,16 +38,16 @@ std::string TimeStamp() {
   std::chrono::duration<double> fractional_seconds =
       (tp - std::chrono::system_clock::from_time_t(tt)) + std::chrono::seconds(gmt.tm_sec);
   // format the string
-  std::string buffer("year/mo/dy hr:mn:sc.xxxxxx0");
+  char buffer[64];
   [[maybe_unused]] int ret =
-      snprintf(&buffer.front(), buffer.length(), "%04d/%02d/%02d %02d:%02d:%09.6f",
+      snprintf(buffer, sizeof(buffer), "%04d/%02d/%02d %02d:%02d:%09.6f",
                gmt.tm_year + 1900, gmt.tm_mon + 1, gmt.tm_mday, gmt.tm_hour, gmt.tm_min,
                fractional_seconds.count());
-  assert(ret == static_cast<int>(buffer.length()) - 1);
+  //assert(ret == static_cast<int>(buffer.length()) - 1);
 
   // Remove trailing null terminator added by snprintf.
-  buffer.pop_back();
-  return buffer;
+  //buffer.pop_back();
+  return std::string(buffer);
 }
 
 // the Log levels we support
