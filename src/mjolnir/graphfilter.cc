a00193194518a93b263c4fa210f44c8034f63719
kinkard <stsiapan.kizim@gmail.com>
2026-04-23 23:42:08 +0200

    fix: formatting

--- src/mjolnir/graphfilter.cc
+++ src/mjolnir/graphfilter.cc
@@ -125,8 +125,7 @@ bool ExpandFromNodeInner(GraphReader& reader,
 
     const NodeInfo* en_info = tile->node(de->endnode().id());
     // check the direction, if we looped back, or are we done
-    if ((de->endnode() != prev_node) && (de->forward() == forward) &&
-        (de->endnode() != from_node)) {
+    if ((de->endnode() != prev_node) && (de->forward() == forward) && (de->endnode() != from_node)) {
       if (edge_info.wayid() == way_id &&
           (en_info->mode_change() || (node_info->mode_change() && !en_info->mode_change()))) {
 
