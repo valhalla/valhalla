0f37805a97aa02abc52692941ae88e5131d41b0e
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2026-07-29 15:43:18 +0200

    unused code

--- src/tyr/matrix_serializer.cc
+++ src/tyr/matrix_serializer.cc
@@ -41,20 +41,6 @@ void serialize_distance(const valhalla::Matrix& matrix,
   }
 }
 
-void serialize_cost(const valhalla::Matrix& matrix,
-                    rapidjson::writer_wrapper_t& writer,
-                    size_t start_td,
-                    const size_t td_count) {
-  for (size_t i = start_td; i < start_td + td_count; ++i) {
-    // check to make sure a route was found; if not, return null for cost in matrix result
-    if (matrix.costs()[i] != kMaxCost) {
-      writer(static_cast<double>(matrix.costs()[i]));
-    } else {
-      writer(nullptr);
-    }
-  }
-}
-
 void serialize_shape(const valhalla::Matrix& matrix,
                      rapidjson::writer_wrapper_t& writer,
                      const size_t start_td,
