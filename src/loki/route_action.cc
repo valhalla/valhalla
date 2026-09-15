a70a3cef74b43a5fe32bc37f061635cebd30352b
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2026-09-15 15:05:22 +0200

    changelog

--- src/loki/route_action.cc
+++ src/loki/route_action.cc
@@ -117,10 +117,10 @@ void loki_worker_t::route(Api& request) {
     // maybe squeeze in the first and last locations of each user specified feature for cost factor
     // lines as we'll need those for edge walking
     for (const auto& line : options.cost_factor_lines()) {
-      google::protobuf::RepeatedPtrField<Location> first_and_last;
       if (line.shape_size() == 0) {
         throw valhalla_exception_t{173, "feature coordinates are empty"};
       }
+      google::protobuf::RepeatedPtrField<Location> first_and_last;
       first_and_last.Add()->CopyFrom(*line.shape().begin());
       first_and_last.Add()->CopyFrom(*line.shape().rbegin());
       Api dummy;
