8cd5729b54d8bd291cc49032feedd23ccdb738dd
nilsnolde <nilsnolde@proton.me>
2026-09-17 14:49:12 +0200

    need to include prime_server headers globally, not only where it's linked

--- src/bindings/python/valhalla/_valhalla.pyi
+++ src/bindings/python/valhalla/_valhalla.pyi
@@ -10,7 +10,7 @@ import numpy
 from numpy.typing import NDArray
 
 
-VALHALLA_PRINT_VERSION: str = '3.8.3'
+VALHALLA_PRINT_VERSION: str = '3.8.3-aabbccdd'
 
 class ValhallaError(RuntimeError):
     """
